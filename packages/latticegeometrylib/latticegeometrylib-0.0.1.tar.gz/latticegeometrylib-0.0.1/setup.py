1b388c28539c4db2e50faff2b9e943873d027dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:55 +0000

    Add package latticegeometrylib/0.0.1/latticegeometrylib-0.0.1.tar.gz

---
+++ packages/latticegeometrylib/latticegeometrylib-0.0.1.tar.gz/latticegeometrylib-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Package for generating periodic truss based lattices'
+LONG_DESCRIPTION = DESCRIPTION
+
+# Setting up
+setup(
+    # the name must match the folder name 'latticegeometrylib'
+    name="latticegeometrylib",
+    author="Dennis Schulz",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'cadquery'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
