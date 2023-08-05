3cec126a06cf448e58b35e6d316e8eeda7e375ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:33 +0000

    Add package sampo/0.1.1.188/sampo-0.1.1.188.tar.gz

---
+++ packages/sampo/sampo-0.1.1.188.tar.gz/sampo-0.1.1.188/sampo/scheduler/utils/peaks.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+from sampo.schemas.schedule import Schedule
+
+
+def get_total_resource_usage(schedule: Schedule) -> dict[str, list[int]]:
+    usage = defaultdict(lambda: [0 for _ in range(schedule.execution_time.value + 1)])
+
+    for work in schedule.works:
+        for worker in work.workers:
+            for day in range(work.start_time.value, work.finish_time.value + 1):
+                usage[worker.name][day] += worker.count
+    return usage
+
+
+def get_peak_resource_usage(schedule: Schedule) -> dict[str, int]:
+    return {res: max(res_usage) for res, res_usage in get_total_resource_usage(schedule).items()}
+
+
+def get_absolute_peak_resource_usage(schedule: Schedule) -> int:
+    return sum(get_peak_resource_usage(schedule).values())
