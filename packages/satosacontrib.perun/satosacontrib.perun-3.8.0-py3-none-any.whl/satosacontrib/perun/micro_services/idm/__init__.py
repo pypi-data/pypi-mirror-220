02135ba3c1af72e169494f5ffd1211fe810da7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:55 +0000

    Add package satosacontrib.perun/3.8.0/satosacontrib.perun-3.8.0-py3-none-any.whl

---
+++ packages/satosacontrib.perun/satosacontrib.perun-3.8.0-py3-none-any.whl/satosacontrib/perun/micro_services/idm/__init__.py
@@ -0,0 +1,17 @@
+from .additional_identifiers import AdditionalIdentifiers
+from .attributes import Attributes
+from .ensure_member import EnsureMember
+from .entitlement import Entitlement
+from .perun_user import PerunUser
+from .sp_authorization import SpAuthorization
+from .update_user_ext_source import UpdateUserExtSource
+
+__all__ = [
+    "PerunUser",
+    "EnsureMember",
+    "Entitlement",
+    "Attributes",
+    "SpAuthorization",
+    "UpdateUserExtSource",
+    "AdditionalIdentifiers",
+]
