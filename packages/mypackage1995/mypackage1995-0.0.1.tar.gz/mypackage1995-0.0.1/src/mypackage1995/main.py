3be2e2102b5702972f8647a31815dee6c0f01211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:11 +0000

    Add package mypackage1995/0.0.1/mypackage1995-0.0.1.tar.gz

---
+++ packages/mypackage1995/mypackage1995-0.0.1.tar.gz/mypackage1995-0.0.1/src/mypackage1995/main.py
@@ -0,0 +1,9 @@
+class main:
+    def __init__(self):
+        pass
+    def addition(self):
+        return number+ 2*number
+    def subtraction(self):
+        return number-2*number
+    def division(self):
+        return number/2*number
