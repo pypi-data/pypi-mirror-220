da533183d13b83b499a8266681427e9600343b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package wunderkafka/0.10.4/wunderkafka-0.10.4.tar.gz

---
+++ packages/wunderkafka/wunderkafka-0.10.4.tar.gz/wunderkafka-0.10.4/tests/unit/test_derive.py
@@ -0,0 +1,299 @@
+import sys
+import json
+import time
+from typing import Optional
+from datetime import datetime
+from dataclasses import dataclass
+
+import pytest
+from pydantic import Field, BaseModel, BaseSettings
+
+from wunderkafka.compat.types import AvroModel
+from wunderkafka.serdes.avromodel import derive
+
+
+@dataclass
+class SomeData(AvroModel):
+    field1: int
+    field2: str
+
+
+@dataclass
+class SomeDefaultData(AvroModel):
+    field1: Optional[int] = None
+    field2: Optional[str] = None
+
+
+class Metrics(BaseModel):
+    line_speed: Optional[int]
+    defect_detected: Optional[bool] = False
+    model_on: Optional[bool] = False
+    squad_number: int = 0
+
+
+class Metric(BaseSettings):
+    line_speed: Optional[int]
+    defect_detected: Optional[bool] = False
+    model_on: Optional[bool] = False
+    squad_number: int = 0
+
+
+class Event(BaseModel):
+    id: Optional[int]
+    ts: Optional[int] = None
+
+    class Meta:
+        namespace = "any.data"
+
+
+class Mixed(BaseModel):
+    text: str
+    value: float
+    integer: int = 0
+    string: str = 'str'
+
+
+# Pydantic allows following of non-default arguments, but dataclasses are not.
+class ImageData(BaseModel):
+    name: str = 'str'
+    image: bytes
+    camera: str
+    ts: float = Field(default_factory=time.time)
+
+
+class OtherImageData(BaseModel):
+    image: bytes
+    camera: str
+    ts: float = Field(default_factory=time.time)
+    name: str = 'str'
+
+
+class TsWithMeta(BaseModel):
+    ts: datetime = Field(default_factory=datetime.now)
+
+    class Meta:
+        namespace = 'com.namespace.my'
+        name = 'MsgKey'
+
+
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires >= Python3.7")
+def test_dataclass() -> None:
+    schema = derive(SomeData, topic='test_data_1')
+    assert json.loads(schema) == {
+        'type': 'record',
+        'name': 'test_data_1_value',
+        'fields': [
+            {
+                'name': 'field1',
+                'type': 'long',
+            },
+            {
+                'name': 'field2',
+                'type': 'string',
+            },
+        ],
+    }
+
+
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires >= Python3.7")
+def test_dataclass_defaults() -> None:
+    schema = derive(SomeDefaultData, topic='test_data_2')
+
+    assert json.loads(schema) == {
+        'type': 'record',
+        'name': 'test_data_2_value',
+        'fields': [
+            {
+                'name': 'field1',
+                'type': ['null', 'long'],
+                'default': None,
+            },
+            {
+                'name': 'field2',
+                'type': ['null', 'string'],
+                'default': None,
+            },
+        ],
+    }
+
+
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires >= Python3.7")
+def test_pydantic_with_defaults() -> None:
+    schema = derive(Metrics, topic='some_topic')
+
+    assert json.loads(schema) == {
+      "type": "record",
+      "name": "some_topic_value",
+      "fields": [
+        {
+          "name": "line_speed",
+          "type": [
+            "long",
+            "null"
+          ]
+        },
+        {
+          "name": "defect_detected",
+          "type": [
+            "boolean",
+            "null"
+          ],
+          "default": False
+        },
+        {
+          "name": "model_on",
+          "type": [
+            "boolean",
+            "null"
+          ],
+          "default": False
+        },
+        {
+          "name": "squad_number",
+          "type": "long",
+          "default": 0,
+        }
+      ]
+    }
+
+
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires >= Python3.7")
+def test_pydantic_defaults() -> None:
+    schema = derive(Mixed, topic='topic')
+
+    assert json.loads(schema) == {
+        'type': 'record',
+        'name': 'topic_value',
+        'fields': [
+            {
+                'type': 'string',
+                'name': 'text',
+            },
+            {
+                'type': 'double',
+                'name': 'value',
+            },
+            {
+                'type': 'long',
+                'name': 'integer',
+                'default': 0,
+            },
+            {
+                'type': 'string',
+                'name': 'string',
+                'default': 'str'
+            },
+        ],
+    }
+
+
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires >= Python3.7")
+def test_pydantic_defaults_none() -> None:
+    schema = derive(Event, topic='topic')
+
+    assert json.loads(schema) == {
+        'type': 'record',
+        'name': 'topic_value',
+        'namespace': 'any.data',
+        'fields': [
+            {
+                'type': ['long', 'null'],
+                'name': 'id',
+            },
+            {
+                'type': ['null', 'long'],
+                'name': 'ts',
+                'default': None,
+            },
+        ],
+    }
+
+
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires >= Python3.7")
+def test_pydantic_mixed_defaults() -> None:
+    s1 = derive(ImageData, topic='topic')
+    s2 = derive(OtherImageData, topic='topic')
+    assert json.loads(s1) == {
+        'type': 'record',
+        'name': 'topic_value',
+        'fields': [
+            {
+                'name': 'name',
+                'type': 'string',
+                'default': 'str'
+            },
+            {
+                'name': 'image',
+                'type': 'bytes',
+            },
+            {
+                'name': 'camera',
+                'type': 'string',
+            },
+            {
+                'name': 'ts',
+                'type': 'double',
+            },
+        ],
+    }
+    assert json.loads(s1) != json.loads(s2)
+
+
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires >= Python3.7")
+def test_pydantic_with_meta() -> None:
+    schema = derive(TsWithMeta, topic='topic')
+
+    assert json.loads(schema) == {
+        'type': 'record',
+        'namespace': 'com.namespace.my',
+        'name': 'MsgKey',
+        'fields': [
+            {
+                'name': 'ts',
+                'type': {
+                    'logicalType': 'timestamp-millis',
+                    'type': 'long',
+                }
+            }
+        ],
+    }
+
+
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires >= Python3.7")
+def test_pydantic_base_settings_with_defaults() -> None:
+    schema = derive(Metric, topic='some_topic')
+
+    assert json.loads(schema) == {
+      "type": "record",
+      "name": "some_topic_value",
+      "fields": [
+        {
+          "name": "line_speed",
+          "type": [
+            "long",
+            "null"
+          ]
+        },
+        {
+          "name": "defect_detected",
+          "type": [
+            "boolean",
+            "null"
+          ],
+          "default": False
+        },
+        {
+          "name": "model_on",
+          "type": [
+            "boolean",
+            "null"
+          ],
+          "default": False
+        },
+        {
+          "name": "squad_number",
+          "type": "long",
+          "default": 0,
+        }
+      ]
+    }
---
+++ packages/wunderkafka/wunderkafka-0.10.4.tar.gz/wunderkafka-0.10.4/wunderkafka/serdes/avromodel.py
@@ -0,0 +1,58 @@
+import json
+from typing import Any, Dict, Type
+from dataclasses import is_dataclass
+
+from pydantic import BaseModel
+from pydantic.fields import ModelField, UndefinedType
+
+from wunderkafka.compat.types import AvroModel
+
+
+def derive(model: Type[object], topic: str, *, is_key: bool = False) -> str:
+    if is_dataclass(model):
+        # https://github.com/python/mypy/issues/14941
+        model_schema = model.avro_schema_to_python()                                                      # type: ignore
+    else:
+        # non-dataclasses objects may allow mixing defaults and non-default fields order,
+        # so to still reuse dataclasses_avroschema, we extract fields and reorder them to satisfy dataclasses
+        # restrictions, than reorder them back.
+        # All this hacks will work only for flat schemas: we avoid describing objects via nested types for HDFS's sake.
+        attributes = _extract_attributes(model)
+        ordering = list(attributes['__annotations__'])
+        crafted_model = _construct_model(attributes, model)
+        model_schema = crafted_model.avro_schema_to_python()
+        fields_map = {field_data['name']: field_data for field_data in model_schema['fields']}
+        reordered_fields = [fields_map[attr] for attr in ordering]
+        model_schema['fields'] = reordered_fields
+    model_schema.pop('doc', None)
+    suffix = 'key' if is_key else 'value'
+    if hasattr(model, 'Meta') and hasattr(model.Meta, 'name'):
+        model_schema['name'] = model.Meta.name
+    else:
+        model_schema['name'] = '{0}_{1}'.format(topic, suffix)
+    return json.dumps(model_schema)
+
+
+def _construct_model(attrs: Dict[str, Any], type_: Type[object]) -> AvroModel:
+    if issubclass(type_, BaseModel):
+        for field in vars(type_).get('__fields__', {}).values():
+            if isinstance(field, ModelField) and not isinstance(field.field_info.default, UndefinedType):
+                attrs[field.name] = field.default
+                tp = attrs['__annotations__'].pop(field.name)
+                attrs['__annotations__'].update({field.name: tp})
+    # https://docs.python.org/3/library/functions.html?highlight=type#type
+    return type(type_.__name__, (AvroModel,), attrs)                                                      # type: ignore
+
+
+def _extract_attributes(type_: Type[object]) -> Dict[str, Any]:
+    fields = vars(type_).get('__annotations__', {})
+    for base in type_.mro():
+        fields = {**vars(base).get('__annotations__', {}), **fields}
+    for field in ['__slots__', 'klass', 'metadata', 'schema_def', '__config__']:
+        fields.pop(field, None)
+    attributes = {}
+    attributes.update({'__annotations__': fields})
+    meta = vars(type_).get('Meta', None)
+    if meta is not None:
+        attributes.update({'Meta': meta})
+    return attributes
