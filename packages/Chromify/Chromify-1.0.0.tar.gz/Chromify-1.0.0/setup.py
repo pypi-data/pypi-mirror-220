3ec215f5048baefb29e5314b610de44de5930dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:29 +0000

    Add package Chromify/1.0.0/Chromify-1.0.0.tar.gz

---
+++ packages/Chromify/Chromify-1.0.0.tar.gz/Chromify-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Chromify',
+    version='1.0.0',
+    author='Plaraje',
+    author_email='mat.demoya@gmail.com',
+    description='Colorful Terminal Prints',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
