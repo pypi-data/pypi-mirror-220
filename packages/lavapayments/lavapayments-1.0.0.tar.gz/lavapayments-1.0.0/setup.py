17de3439cc420bba13e84269943b9d522c8d6ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:21 +0000

    Add package lavapayments/1.0.0/lavapayments-1.0.0.tar.gz

---
+++ packages/lavapayments/lavapayments-1.0.0.tar.gz/lavapayments-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="lavapayments",
+    version="1.0.0",
+    description="Библиотека для работы с Бизнес API Lava.ru",
+    packages=["lavapayments"],
+    author_email="dimabykov189@gmail.com",
+    zip_safe=False
+)
