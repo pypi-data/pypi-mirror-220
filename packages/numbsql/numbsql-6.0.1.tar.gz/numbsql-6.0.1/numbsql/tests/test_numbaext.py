43aff01613288119c31cc9b02fdbcfd91b7bbb8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:28 +0000

    Add package numbsql/6.0.1/numbsql-6.0.1.tar.gz

---
+++ packages/numbsql/numbsql-6.0.1.tar.gz/numbsql-6.0.1/numbsql/tests/test_numbaext.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import pytest
+from numba import TypingError, boolean, int64, njit
+
+from numbsql.numbaext import is_not_null_pointer, sizeof, unsafe_cast
+
+
+def test_sizeof_invalid() -> None:
+    with pytest.raises(TypingError):
+
+        @njit(int64(int64))  # type: ignore[misc]
+        def bad_sizeof(x: int) -> int:  # pragma: no cover
+            return sizeof(x)
+
+
+@pytest.mark.xfail(  # type: ignore[misc]
+    reason="Numba converts c_void_p from ctypes into an integer"
+)
+def test_is_null_invalid() -> None:
+    with pytest.raises(TypingError):
+
+        @njit(boolean(int64))  # type: ignore[misc]
+        def bad_is_null_pointer(x: int) -> bool:  # pragma: no cover
+            return is_not_null_pointer(x)
+
+
+def test_unsafe_case_invalid() -> None:
+    with pytest.raises(TypingError):
+
+        @njit(int64(int64))  # type: ignore[misc]
+        def bad_unsafe_cast(x: int) -> int:  # pragma: no cover
+            return unsafe_cast(x, int64)
