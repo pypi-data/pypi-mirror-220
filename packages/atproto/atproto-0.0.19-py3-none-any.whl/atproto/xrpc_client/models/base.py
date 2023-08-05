3c134a0f987f2c4cc947c60003294b9e15a1c4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:13 +0000

    Add package atproto/0.0.19/atproto-0.0.19-py3-none-any.whl

---
+++ packages/atproto/atproto-0.0.19-py3-none-any.whl/atproto/xrpc_client/models/base.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+from atproto.exceptions import ModelFieldNotFoundError
+
+
+@dataclass
+class ModelBase:
+    def __getitem__(self, item: str):
+        if hasattr(self, item):
+            return getattr(self, item)
+
+        raise ModelFieldNotFoundError(f"Can't find field '{item}' in the object of type {type(self)}.")
+
+
+@dataclass
+class ParamsModelBase(ModelBase):
+    pass
+
+
+@dataclass
+class DataModelBase(ModelBase):
+    pass
+
+
+@dataclass
+class ResponseModelBase(ModelBase):
+    pass
+
+
+@dataclass
+class RecordModelBase(ModelBase):
+    pass
