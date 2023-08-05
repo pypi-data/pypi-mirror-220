f201299eba90be400a76c9c9db65be63d41294f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:45 +0000

    Add package retake/0.1.9/retake-0.1.9.tar.gz

---
+++ packages/retake/retake-0.1.9.tar.gz/retake-0.1.9/core/extract/postgres.py
@@ -0,0 +1,77 @@
+import psycopg2
+import select
+import json
+import threading
+import queue
+
+from psycopg2.extras import LogicalReplicationConnection
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from typing import List, Generator, Dict, Any, Optional, cast
+
+from core.extract.base import Extractor, ExtractorResult
+
+
+class ConnectionError(Exception):
+    pass
+
+
+class PostgresExtractor(Extractor):
+    def __init__(self, dsn: str) -> None:
+        self.dsn = dsn
+        self._connect(dsn)
+
+    def _connect(self, dsn: str) -> None:
+        try:
+            self.connection = psycopg2.connect(
+                self.dsn, connection_factory=LogicalReplicationConnection
+            )
+        except psycopg2.ProgrammingError:
+            raise ConnectionError("Unable to connect to database")
+        except psycopg2.OperationalError:
+            raise ConnectionError("Unable to connect to database")
+
+        self.cursor = self.connection.cursor()
+
+    def teardown(self) -> None:
+        self.cursor.close()  # type: ignore
+        self.connection.close()
+
+    def count(self, relation: str) -> int:
+        self.cursor.execute(f"SELECT COUNT(*) FROM {relation}")
+        row = self.cursor.fetchone()
+        if row:
+            return cast(int, row[0])
+        else:
+            return 0
+
+    def extract_all(
+        self, relation: str, columns: List[str], primary_key: str, chunk_size: int
+    ) -> Generator[ExtractorResult, None, None]:
+        offset = 0
+        columns_str = ", ".join(columns)
+
+        while True:
+            self.cursor.execute(
+                f"""
+                SELECT {columns_str}, {primary_key}
+                FROM {relation}
+                ORDER BY {primary_key}
+                LIMIT %s
+                OFFSET %s
+            """,
+                (chunk_size, offset),
+            )
+
+            rows = self.cursor.fetchall()
+
+            if not rows:
+                break
+
+            # Extract primary keys from rows
+            primary_keys = [row[-1] for row in rows]
+
+            # Remove primary keys from rows
+            rows = [row[:-1] for row in rows]
+
+            yield {"rows": rows, "primary_keys": primary_keys}
+            offset += chunk_size
---
+++ packages/retake/retake-0.1.9.tar.gz/retake-0.1.9/core/sdk/source.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class PostgresSource(BaseModel):
+    dsn: str
+
+    @property
+    def config(self) -> dict[str, str]:
+        fields = {}
+        pairs = self.dsn.split(" ")
+        for pair in pairs:
+            key, value = pair.split("=")
+            fields[key] = value
+        return fields
+
+
+class Source:
+    @classmethod
+    def Postgres(
+        cls,
+        host: str,
+        database: str,
+        user: str,
+        password: str,
+        port: int,
+    ) -> PostgresSource:
+        dsn = (
+            f"dbname={database} user={user} password={password} host={host} port={port}"
+        )
+        return PostgresSource(dsn=dsn)
