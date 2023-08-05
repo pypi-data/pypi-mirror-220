1aa035bdef47e12afb5e10e9f6c0ba10a440e64e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:33 +0000

    Add package EnStp-de/1.0.0/EnStp_de-1.0.0.tar.gz

---
+++ packages/EnStp-de/EnStp_de-1.0.0.tar.gz/EnStp_de-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='EnStp_de',
+      version='1.0.0',
+      description='german stopwords for energy policy for content analysis based on LDA topic modeling',
+      author='Meixi Zhang',
+      packages=['EnStp_de'],
+      zip_safe=False)
