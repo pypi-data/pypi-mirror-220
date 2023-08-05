1e281b4251612fdc34b19da724136ae57402e7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:41 +0000

    Add package drug-list-ner/0.0.11/drug_list_ner-0.0.11-py3-none-any.whl

---
+++ packages/drug-list-ner/drug_list_ner-0.0.11-py3-none-any.whl/drug_list_ner/example.py
@@ -0,0 +1,47 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+import os
+
+from pathlib import Path 
+from huggingface_hub import hf_hub_download
+
+os.environ["TRANSFORMERS_OFFLINE"] = "1"
+tokenizer_path = Path("./tokenizer2")
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, model_max_length=10000000)
+model_path = "./model2"
+
+
+pytorch_model_url = "https://huggingface.co/ronoys/testDeploy/blob/main/pytorch_model.bin"
+
+
+def drug_search(sentence):
+    if not os.path.exists(os.path.join(model_path, "pytorch_model.bin")):
+        hf_hub_download(repo_id="ronoys/testDeploy", filename="pytorch_model.bin", local_dir = "./model2")
+
+    model = AutoModelForTokenClassification.from_pretrained("model2")
+
+    effect_ner_model = pipeline(task="ner", model=model, tokenizer=tokenizer)
+    val = effect_ner_model(sentence)
+
+    result = []
+    curr = ""
+
+    for x in val:
+        if x["entity"] == "LABEL_1":
+            curr = x["word"]
+        elif x["entity"] == "LABEL_2":
+            curr += x["word"][2:]
+        else:
+            result.append(curr)
+        res = [*set(result)]
+
+    while("" in res):
+        res.remove("")
+    return (res)
+
+
+'''share/
+HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 \
+python3 example.py --pytorch_model.bin t5-small
+
+'''
+
