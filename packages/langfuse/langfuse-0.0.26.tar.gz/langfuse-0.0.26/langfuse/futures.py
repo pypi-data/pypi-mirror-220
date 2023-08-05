571841b43a56375f772fe0ba29c40f9d814ceee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:24 +0000

    Add package langfuse/0.0.26/langfuse-0.0.26.tar.gz

---
+++ packages/langfuse/langfuse-0.0.26.tar.gz/langfuse-0.0.26/langfuse/futures.py
@@ -0,0 +1,58 @@
+import concurrent.futures
+import asyncio
+
+class FuturesStore:
+    def __init__(self):
+        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+        self.futures = {}
+
+    def append(self, id:  str, func, *args, future_id=None, **kwargs):
+
+
+        if future_id is None:
+            # If no future_id is provided, store the function and arguments for later execution
+            self.futures[id] = (func, args, kwargs)
+        else:
+            # If a future_id is provided, create a dependent future and store it for later execution
+            dependent_future = (func, args, kwargs, future_id)
+            self.futures[id] = dependent_future
+
+        return id
+
+    def flush(self):
+        async def run_task(future_details):
+            if len(future_details) == 3:  # If there are no dependencies
+                func, args, kwargs = future_details
+                return await func(*args, **kwargs)
+            else:  # If there is a dependency
+                func, args, kwargs, dependent_id = future_details
+                if dependent_id in resolved_dependencies:  # If dependency is resolved
+                    dependent_result = results[dependent_id]  # get the result from the parent future
+                    return await func(dependent_result, *args, **kwargs)
+
+        loop = asyncio.get_running_loop()
+        results = {}
+        resolved_dependencies = set()
+        waiting_for_resolution = set(self.futures.keys())
+        final_result = {'status': 'success'}
+
+        try:
+            while waiting_for_resolution:
+                for future_id in list(waiting_for_resolution):  # Iterate over a copy of the set
+                    future_details = self.futures[future_id]
+                    task = loop.create_task(run_task(future_details))
+                    results[future_id] = loop.run_until_complete(task)
+                    resolved_dependencies.add(future_id)
+                    waiting_for_resolution.remove(future_id)
+
+        except Exception as e:
+            print(e)
+            final_result['status'] = 'failed'
+            final_result['error'] = str(e)
+        
+        self.futures.clear()
+
+        return final_result
+
+
+
