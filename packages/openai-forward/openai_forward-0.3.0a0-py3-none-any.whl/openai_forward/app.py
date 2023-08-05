990f43881d46d5521de613301495ae61758ecc37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:40 +0000

    Add package openai-forward/0.3.0a0/openai_forward-0.3.0a0-py3-none-any.whl

---
+++ packages/openai-forward/openai_forward-0.3.0a0-py3-none-any.whl/openai_forward/app.py
@@ -0,0 +1,15 @@
+from sparrow.api import create_app
+
+from .openai import Openai
+from .routers.openai_v1 import router as router_v1
+
+app = create_app(title="openai_forward", version="1.0")
+openai = Openai()
+
+app.add_route(
+    openai.ROUTE_PREFIX + "/{api_path:path}",
+    openai.reverse_proxy,
+    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH", "TRACE"],
+)
+
+app.include_router(router_v1)
