0483f0b34838803de96f2bb52b07a9925a2025d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:39 +0000

    Add package gentrace-py/0.13.0/gentrace_py-0.13.0-py3-none-any.whl

---
+++ packages/gentrace-py/gentrace_py-0.13.0-py3-none-any.whl/gentrace/models/__init__.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from gentrace.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from gentrace.model.feedback_request import FeedbackRequest
+from gentrace.model.feedback_response import FeedbackResponse
+from gentrace.model.pipeline_run_request import PipelineRunRequest
+from gentrace.model.pipeline_run_response import PipelineRunResponse
+from gentrace.model.test_case import TestCase
+from gentrace.model.test_run import TestRun
+from gentrace.model.test_set import TestSet
