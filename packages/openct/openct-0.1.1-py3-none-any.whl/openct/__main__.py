9e071cc92b7f2267ab717db95e7f7f90fbcb7dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:06 +0000

    Add package openct/0.1.1/openct-0.1.1-py3-none-any.whl

---
+++ packages/openct/openct-0.1.1-py3-none-any.whl/openct/__main__.py
@@ -0,0 +1,33 @@
+"""Main entry point for the OpenCT application."""
+
+import logging
+
+from tqdm import tqdm
+
+from openct.setup import setup_config, Config
+from openct.datastore import Datastore, get_datastore
+from openct.devices import RouterOSDevice
+
+
+config: Config = setup_config()
+
+Dstore = get_datastore(config.settings.datastore_type)
+datastore: Datastore = Dstore()
+devices = datastore.get_data()
+
+with tqdm(total=100) as pbar:
+    for host in devices:
+        logging.info("Backing up device %s", host)
+        pbar.update(100 / len(devices))
+
+        device = RouterOSDevice(
+            host,
+            config.identity.username,
+            config.settings.connection_timeout,
+            config.identity.key_file,
+        )
+
+        if device.is_available():
+            device.fetch_backup()
+        else:
+            logging.error("Could not connect to device")
