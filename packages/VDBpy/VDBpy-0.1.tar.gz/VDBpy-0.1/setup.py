1ceb7fff1a9e707e23486169af78d9328f9aeef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:20 +0000

    Add package VDBpy/0.1/VDBpy-0.1.tar.gz

---
+++ packages/VDBpy/VDBpy-0.1.tar.gz/VDBpy-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='VDBpy',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple vector database allows difference search methods (consine similarity and euclidean distance ect.)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Justin He',
+    author_email='justin.he814@gmail.com',
+    url='https://github.com/Ateee329/VDBpy',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'math',
+        'collections',
+    ],
+    python_requires='>=3.6',
+)
