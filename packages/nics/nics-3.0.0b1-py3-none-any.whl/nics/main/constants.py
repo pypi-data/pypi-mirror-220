b69f94ec54b077b262843a48264a507e507173bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:37 +0000

    Add package nics/3.0.0b1/nics-3.0.0b1-py3-none-any.whl

---
+++ packages/nics/nics-3.0.0b1-py3-none-any.whl/nics/main/constants.py
@@ -0,0 +1,32 @@
+import os
+import pkg_resources
+
+
+try:
+    __version__ = pkg_resources.get_distribution('nics').version
+except pkg_resources.DistributionNotFound:  # This exception occurred during development (before the software installed via pip)
+    __version__ = 'dev'
+
+
+ROOT_DIR_PTH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+DIST_DIR_PTH = os.path.join(ROOT_DIR_PTH, 'nics')
+MAIN_DIR_PTH = os.path.join(ROOT_DIR_PTH, 'nics', 'main')
+NICS_COMPILER_DIR_PTH = os.path.join(ROOT_DIR_PTH, 'nics-compiler')
+
+TEMPLATE_DOCS_DIR_PTH = os.path.join(ROOT_DIR_PTH, 'nics', 'main', 'template')
+TEMPLATE_WEB_DIR_PTH  = os.path.join(ROOT_DIR_PTH, 'nics-compiler', 'template')
+
+SETTINGS_KEYS = [
+    'author',
+    'color_hue',
+    'lowercase_the_url',
+    'show_credit',
+    
+    '_email',
+    '_gh_username',
+    '_gh_repo',
+    '_main_branch_name',
+    
+    '_nics_version',
+]
