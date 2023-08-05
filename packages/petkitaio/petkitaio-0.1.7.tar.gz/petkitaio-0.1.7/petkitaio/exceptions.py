21b4beaca9b97d6dfe66f73941ad5439b82be08b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:48 +0000

    Add package petkitaio/0.1.7/petkitaio-0.1.7.tar.gz

---
+++ packages/petkitaio/petkitaio-0.1.7.tar.gz/petkitaio-0.1.7/petkitaio/exceptions.py
@@ -0,0 +1,40 @@
+"""Exceptions for PetKit."""
+
+from typing import Any
+
+
+class PetKitError(Exception):
+    """Error from PetKit api."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
+
+
+class AccountTypeError(Exception):
+    """User has set account region to more than one region."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
+
+class AuthError(Exception):
+    """Authentication issue from PetKit api."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
+
+class BluetoothError(Exception):
+    """Bluetooth issue from PetKit api."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
+
+class ServerError(Exception):
+    """PetKit server error."""
+
+    def __init__(self, *args: Any) -> None:
+        """Initialize the exception."""
+        Exception.__init__(self, *args)
