0ab0bce5c47a863c3635d03c6ac4fdcf5cc7b160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:42 +0000

    Add package venture-ai/0.9.8/venture_ai-0.9.8-py3-none-any.whl

---
+++ packages/venture-ai/venture_ai-0.9.8-py3-none-any.whl/venture/objects/token_counts.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import Dict
+
+@dataclass
+class TokenCounts:
+    retrieval_function_name_to_count:Dict[str,int]
+    retrieval_caller_system_count:int
+    retrieval_verdict_system_count:int
+
+    explore_all_prompt_token_count:int
+
+    cosmos_function_name_all_prompt_count:int
+    cosmos_system_summary_count:int
+    cosmos_system_ownership_all_prompt_count:int
