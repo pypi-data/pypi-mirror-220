895ea46e93d0bce4d5bb5c5079c9a91417e22981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:51 +0000

    Add package osism/0.20230718.0/osism-0.20230718.0-py3-none-any.whl

---
+++ packages/osism/osism-0.20230718.0-py3-none-any.whl/osism/settings.py
@@ -0,0 +1,26 @@
+import os
+
+
+# Read secret from file
+def read_secret(secret_name):
+    try:
+        f = open("/run/secrets/" + secret_name, "r", encoding="utf-8")
+    except EnvironmentError:
+        return ""
+    else:
+        with f:
+            return f.readline().strip()
+
+OPENSEARCH_ADDRESS = os.getenv("OPENSEARCH_ADDRESS")
+OPENSEARCH_PORT = os.getenv("OPENSEARCH_PORT")
+
+NETBOX_URL = os.getenv("NETBOX_API")
+NETBOX_TOKEN = os.getenv("NETBOX_TOKEN", read_secret("NETBOX_TOKEN"))
+IGNORE_SSL_ERRORS = os.getenv("IGNORE_SSL_ERRORS", "True") == "True"
+
+BASE_PATH = os.getenv("BASE_PATH", "/devicetype-library/device-types/")
+VENDORS = os.getenv("VENDORS", "").split()
+
+# 43200 seconds = 12 hours
+GATHER_FACTS_SCHEDULE = 43200.0
+INVENTORY_RECONCILER_SCHEDULE = 600.0
