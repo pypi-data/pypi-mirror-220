33e7d871a8b43ba9c3e429c45ad3caf0b436b23a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:05 +0000

    Add package fateful/0.3.0/fateful-0.3.0.tar.gz

---
+++ packages/fateful/fateful-0.3.0.tar.gz/fateful-0.3.0/fateful/http.py
@@ -0,0 +1,124 @@
+import logging
+from enum import Enum
+from functools import partial
+from json import JSONDecodeError
+from typing import Any, Type
+
+from aiohttp import ClientError, ClientSession
+
+from fateful.json import JsArray, JsObject, try_parse
+from fateful.monad.async_result import async_try
+from fateful.monad.result import Err, Ok
+
+
+class HttpMethods(str, Enum):
+    """ """
+
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    DELETE = "DELETE"
+    PATCH = "PATCH"
+    OPTIONS = "OPTIONS"
+
+
+class ContentType:
+    """ """
+
+    value = "Content-Type"
+    APPLICATION_JSON = "application/json"
+
+
+class NetworkError(Exception):
+    """ """
+
+    ...
+
+
+class HttpException(NetworkError):
+    """ """
+
+    def __init__(self, code: int, detail: str):
+        self.code = code
+        self.detail = detail
+
+
+class NetworkException(NetworkError):
+    """ """
+
+    ...
+
+
+async def request(
+    method: HttpMethods,
+    url,
+    /,
+    *,
+    session: ClientSession,
+    response_class: Type[Any] | None = None,
+    **kwargs: Any,
+) -> Ok[str | JsArray | JsObject | Any] | Err[
+    NetworkError | ClientError | JSONDecodeError | Exception
+]:
+    """
+    Generic method for making a request
+    """
+    try:
+        async with session.request(method.value, url, **kwargs) as resp:
+            try:
+                content_type = resp.headers.getall(ContentType.value)
+                is_json = ContentType.APPLICATION_JSON in content_type
+                resp_as_text = await resp.text()
+                if resp.status >= 400:
+                    return Err(HttpException(resp.status, resp_as_text))
+                if is_json:
+                    return try_parse(resp_as_text, response_class=response_class)
+                return Ok(resp_as_text)
+            except ClientError as e:  # pragma: no cover
+                logging.error(e)
+                return Err(e)
+    except ClientError as e:
+        logging.exception(e)
+        return Err(e)
+
+
+get = partial(request, HttpMethods.GET)
+
+
+def try_get(url, **kwargs):
+    return async_try(get)(url, **kwargs)
+
+
+post = partial(request, HttpMethods.POST)
+
+
+def try_post(url, **kwargs):  # pragma: no cover
+    return async_try(post)(url, **kwargs)
+
+
+put = partial(request, HttpMethods.PUT)
+
+
+def try_put(url, **kwargs):  # pragma: no cover
+    return async_try(put)(url, **kwargs)
+
+
+delete = partial(request, HttpMethods.DELETE)
+
+
+def try_delete(url, **kwargs):  # pragma: no cover
+    return async_try(delete)(url, **kwargs)
+
+
+patch = partial(request, HttpMethods.PATCH)
+
+
+def try_patch(url, **kwargs):  # pragma: no cover
+    return async_try(patch)(url, **kwargs)
+
+
+options = partial(request, HttpMethods.OPTIONS)
+
+
+def try_options(url, **kwargs):  # pragma: no cover
+    return async_try(options)(url, **kwargs)
---
+++ packages/fateful/fateful-0.3.0.tar.gz/fateful-0.3.0/tests/test_json.py
@@ -0,0 +1,69 @@
+import typing
+import unittest
+from dataclasses import dataclass
+from json import JSONDecodeError
+
+from fateful.json import JsArray, JsObject, js, parse, try_parse
+
+
+@dataclass
+class Toto:
+    a: str
+
+
+class Test(unittest.TestCase):
+    def test_john_object(self):
+        i = js({"z-index": 1000})
+
+        self.assertEqual(i["z-index"].or_(0), 1000)
+        i["toto"] = [1, 2, 3]
+        i.toto = [4, 5, 6]
+        i.zozo.or_(12)
+
+        str(i)
+        self.assertEqual(i["zindex"].or_(0), 0)
+        json_obj = js(a=12, k=13, c={1: 3})
+        self.assertEqual(json_obj.a.get(), 12)
+        # self.assertEqual(i.toto.map(lambda x: x.to_list()).or_none(), [4, 5, 6])
+
+    def test_stringify(self):
+        i = js({"z-index": 1000})
+        self.assertEqual(i.stringify(sort_keys=True), """{"z-index": 1000}""")
+
+    def test_parse_obj(self):
+        value = """[
+        {"a": "toto"},
+        {"a": "titi"}
+      ]
+      """
+
+        parse(value)
+
+        value = parse(value, response_class=Toto)
+        print(value)
+
+        value = """{"a": "toto"}"""
+        parse(value, response_class=Toto)
+        parse(value)
+
+    def test_fail_parse(self):
+        value = """[
+        {"a": "toto"},
+        {"a": "titi"}
+
+      """
+        with self.assertRaises(JSONDecodeError):
+            try_parse(value).get()
+        with self.assertRaises(JSONDecodeError):
+            try_parse(value, response_class=Toto).get()
+
+    def test_parse_array(self):
+        value = """[1, 2, 3, {"a": "12"}]"""
+        result: JsArray = typing.cast(JsArray, try_parse(value).get())
+        self.assertEqual(result[0], 1)
+        self.assertEqual(result[2], 3)
+        with self.assertRaises(IndexError):
+            result[10]
+
+        self.assertIsInstance(result[3], JsObject)
+        self.assertEqual(result[3].map_to(Toto), Toto(a="12"))
