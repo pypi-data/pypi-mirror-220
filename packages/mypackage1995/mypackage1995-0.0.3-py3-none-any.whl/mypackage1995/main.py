a2f26d0fe87f7d92046e20d481ba34778f96d9b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:02 +0000

    Add package mypackage1995/0.0.3/mypackage1995-0.0.3-py3-none-any.whl

---
+++ packages/mypackage1995/mypackage1995-0.0.3-py3-none-any.whl/mypackage1995/main.py
@@ -0,0 +1,9 @@
+class main:
+    def __init__(self):
+        pass
+    def addition(self, number):
+        return number+ 2*number
+    def subtraction(self, number):
+        return number-2*number
+    def division(self, number):
+        return number/2*number
