f359afe82d6a7b1f7102da32668a7e670ec0a801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:33 +0000

    Add package schemachange-hellofresh/3.5.3/schemachange_hellofresh-3.5.3.tar.gz

---
+++ packages/schemachange-hellofresh/schemachange_hellofresh-3.5.3.tar.gz/schemachange_hellofresh-3.5.3/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="schemachange_hellofresh",
+    version="3.5.3",
+    author="US OPS Analytics",
+    author_email="sca-automation@hellofresh.com",
+    long_description="Fork of schemachange for urgent PyYaml bump",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    include_package_data=True
+)
