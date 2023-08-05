9cfdf3b027049236ecb43b8c3b24b6e80c0229cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:24 +0000

    Add package py115/0.0.3/py115-0.0.3.tar.gz

---
+++ packages/py115/py115-0.0.3.tar.gz/py115-0.0.3/py115/__init__.py
@@ -0,0 +1,44 @@
+__author__ = 'deadblue'
+
+"""
+Python SDK for 115 cloud storage service.
+
+Example:
+
+import py115
+import py115.types
+
+cloud = py115.connect(py115.types.Credential(
+    uid='', cid='', seid=''
+))
+storage = cloud.storage()
+
+for file in storage.list():
+    print(f'File: {file.name}')
+
+"""
+
+VERSION = '0.0.3'
+
+import py115.cloud
+import py115.types
+
+def connect(
+        credential: py115.types.Credential = None,
+        protocol_kwargs: dict=  None
+) -> py115.cloud.Cloud:
+    """Connect to 115 cloud.
+
+    Args:
+        credential (py115.types.Credential): 
+            Credential data to identity user.
+        protocol_kwargs (dict): 
+            Keyword arguments for underlying protocol client.
+
+    Return:
+        py115.cloud.Cloud: Cloud instance.
+    """
+    return py115.cloud.Cloud(
+        credential=credential, 
+        protocol_kwargs=protocol_kwargs
+    )
