9e6caa36f48529785f3f4a90e2858a00db076f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:24 +0000

    Add package air2phin/0.0.22a0/air2phin-0.0.22a0.tar.gz

---
+++ packages/air2phin/air2phin-0.0.22a0.tar.gz/air2phin-0.0.22a0/src/air2phin/utils/string.py
@@ -0,0 +1,23 @@
+from air2phin.constants import Keyword, Number, Token
+
+
+def convert_schedule(val: str) -> str:
+    """Convert airflow schedule string to dolphinscheduler's.
+
+    Will convert including:
+    * crontab schedule string from ``5 4 * * *`` to ```0 5 4 * * ? *``
+    * shortcut schedule string like ``@daily`` to ``0 0 0 * * ? *``.
+    """
+    if (
+        len(val) == Number.SCHEDULE_TOTAL_NUM
+        and val.count(Token.SPACE) == Number.SCHEDULE_SPACE_NUM
+    ):
+        val_list = val.split(Token.SPACE)
+        val_list.insert(0, Token.ZERO)
+        val_list.insert(-1, Token.QUESTION)
+        return Token.SPACE.join(val_list)
+    return Keyword.DEFAULT_SCHEDULE
+
+
+def escape_marco_bracket(key: str) -> str:
+    return key.replace("(", "\\(").replace(")", "\\)")
