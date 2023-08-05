804951a5c79a6bd2400726f768d4ec9432674168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:41 +0000

    Add package regret/2023.7.6/regret-2023.7.6.tar.gz

---
+++ packages/regret/regret-2023.7.6.tar.gz/regret-2023.7.6/noxfile.py
@@ -0,0 +1,147 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+import os
+
+import nox
+
+ROOT = Path(__file__).parent
+PYPROJECT = ROOT / "pyproject.toml"
+DOCS = ROOT / "docs"
+PACKAGE = ROOT / "regret"
+CONTRIBUTING = ROOT / "CONTRIBUTING.rst"
+
+
+nox.options.sessions = []
+
+
+def session(default=True, **kwargs):
+    def _session(fn):
+        if default:
+            nox.options.sessions.append(kwargs.get("name", fn.__name__))
+        return nox.session(**kwargs)(fn)
+
+    return _session
+
+
+@session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "pypy3"])
+def tests(session):
+    session.install(ROOT, "virtue", "-r", ROOT / "test-requirements.txt")
+
+    if session.posargs and session.posargs[0] == "coverage":
+        if len(session.posargs) > 1 and session.posargs[1] == "github":
+            github = os.environ["GITHUB_STEP_SUMMARY"]
+        else:
+            github = None
+
+        session.install("coverage[toml]")
+        session.run("coverage", "run", "-m", "virtue", PACKAGE)
+        if github is None:
+            session.run("coverage", "report")
+        else:
+            with open(github, "a") as summary:
+                summary.write("### Coverage\n\n")
+                summary.flush()  # without a flush, output seems out of order.
+                session.run(
+                    "coverage",
+                    "report",
+                    "--format=markdown",
+                    stdout=summary,
+                )
+    else:
+        session.run("python", "-m", "virtue", *session.posargs, "regret")
+
+
+@session()
+def pytest_tests(session):
+    session.install(ROOT, "pytest", "-r", ROOT / "test-requirements.txt")
+    session.run("pytest", *session.posargs, PACKAGE)
+
+
+@session()
+def audit(session):
+    session.install("pip-audit", ROOT)
+    session.run("python", "-m", "pip_audit")
+
+
+@session(tags=["build"])
+def build(session):
+    session.install("build", "docutils", "twine")
+    with TemporaryDirectory() as tmpdir:
+        session.run("python", "-m", "build", ROOT, "--outdir", tmpdir)
+        session.run("twine", "check", "--strict", tmpdir + "/*")
+        session.run(
+            "python",
+            "-m",
+            "docutils",
+            "--strict",
+            CONTRIBUTING,
+            os.devnull,
+        )
+
+
+@session(tags=["style"])
+def style(session):
+    session.install("ruff")
+    session.run("ruff", "check", ROOT)
+
+
+@session()
+def typing(session):
+    session.install("pyright", ROOT)
+    session.run("pyright", PACKAGE)
+
+
+@session(tags=["docs"])
+@nox.parametrize(
+    "builder",
+    [
+        nox.param(name, id=name)
+        for name in [
+            "dirhtml",
+            "doctest",
+            "linkcheck",
+            "man",
+            "spelling",
+        ]
+    ],
+)
+def docs(session, builder):
+    session.install("-r", DOCS / "requirements.txt")
+    with TemporaryDirectory() as tmpdir_str:
+        tmpdir = Path(tmpdir_str)
+        argv = ["-n", "-T", "-W"]
+        if builder != "spelling":
+            argv += ["-q"]
+        session.run(
+            "python",
+            "-m",
+            "sphinx",
+            "-b",
+            builder,
+            DOCS,
+            tmpdir / builder,
+            *argv,
+        )
+
+
+@session(tags=["docs", "style"], name="docs(style)")
+def docs_style(session):
+    session.install(
+        "doc8",
+        "pygments",
+        "pygments-github-lexers",
+    )
+    session.run("python", "-m", "doc8", "--config", PYPROJECT, DOCS)
+
+
+@session(default=False)
+def requirements(session):
+    session.install("pip-tools")
+    for each in [DOCS / "requirements.in", ROOT / "test-requirements.in"]:
+        session.run(
+            "pip-compile",
+            "--resolver",
+            "backtracking",
+            "-U",
+            each.relative_to(ROOT),
+        )
