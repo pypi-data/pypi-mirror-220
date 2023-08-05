c74247ca078d22b1212a8454ec302500ce1160f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:29 +0000

    Add package calendar-cli/1.0.1/calendar_cli-1.0.1.tar.gz

---
+++ packages/calendar-cli/calendar_cli-1.0.1.tar.gz/calendar_cli-1.0.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+from setuptools import setup, find_packages
+
+from calendar_cli.metadata import metadata
+metadata_ = metadata.copy()
+
+for x in metadata:
+    if not x in ('author', 'version', 'license', 'maintainer', 'author_email',
+                 'status', 'name', 'description', 'url', 'description'):
+        metadata_.pop(x)
+
+setup(
+    packages=['calendar_cli',
+              ],
+    classifiers=[
+        #"Development Status :: ..."
+        "Environment :: Web Environment",
+        #"Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+    ],
+    scripts=['bin/calendar-cli.py', 'bin/calendar-cli'],
+    install_requires=[
+        'icalendar',
+        'caldav>=0.12-dev0',
+#        'isodate',
+        'pytz', ## pytz is supposed to be obsoleted, but see https://github.com/collective/icalendar/issues/333 
+        'tzlocal',
+        'Click',
+        'six'
+    ],
+   **metadata_
+)
