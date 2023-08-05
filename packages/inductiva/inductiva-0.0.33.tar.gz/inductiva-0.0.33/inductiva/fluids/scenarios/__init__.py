7d28dda3ca203b603b4ad954edf1133a96b8abdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:39 +0000

    Add package inductiva/0.0.33/inductiva-0.0.33.tar.gz

---
+++ packages/inductiva/inductiva-0.0.33.tar.gz/inductiva-0.0.33/inductiva/fluids/scenarios/__init__.py
@@ -0,0 +1,13 @@
+#pylint: disable=missing-module-docstring
+from .dam_break import DamBreak
+from .fluid_block import FluidBlock
+from .wind_tunnel import WindTunnel
+from .wind_tunnel import WindTunnelSimulationOutput
+from .fluid_tank import (
+    FluidTank,
+    CubicTankOutlet,
+    CylindricalTankOutlet,
+    RectangularTankInlet,
+    CircularTankInlet,
+)
+from .heat_sink import HeatSink
