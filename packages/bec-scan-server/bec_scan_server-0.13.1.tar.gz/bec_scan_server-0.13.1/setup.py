aa964b2c16eff9b03ddf299d726903d397e7adbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package bec-scan-server/0.13.1/bec_scan_server-0.13.1.tar.gz

---
+++ packages/bec-scan-server/bec_scan_server-0.13.1.tar.gz/bec_scan_server-0.13.1/setup.py
@@ -0,0 +1,30 @@
+import os
+import pathlib
+import subprocess
+
+from setuptools import setup
+
+current_path = pathlib.Path(__file__).parent.resolve()
+
+utils = f"{current_path}/../bec_lib/"
+
+
+def get_version():
+    """load the version from the version file"""
+    version_file = os.path.join(current_path, "../semantic_release", "__init__.py")
+    with open(version_file, "r", encoding="utf-8") as file:
+        res = file.readline()
+        version = res.split("=")[1]
+    return version.strip().strip('"')
+
+
+if __name__ == "__main__":
+    setup(
+        install_requires=["numpy", "msgpack", "pyyaml", "cytoolz", "rich"],
+        version=get_version(),
+        extras_require={"dev": ["pytest", "pytest-random-order", "coverage"]},
+        entry_points={"console_scripts": ["bec-scan-server = scan_server:main"]},
+    )
+    local_deps = [utils]
+    for dep in local_deps:
+        subprocess.run(f"pip install -e {dep}", shell=True, check=True)
