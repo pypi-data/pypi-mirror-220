eb9b7f24222f05e08c45d54f10bc9c73f2a57816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:26 +0000

    Add package bingo-bongo-cat/0.0.3/bingo-bongo-cat-0.0.3.tar.gz

---
+++ packages/bingo-bongo-cat/bingo-bongo-cat-0.0.3.tar.gz/bingo-bongo-cat-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Cat that prints messages.'
+LONG_DESCRIPTION = 'A package which makes troubleshooting code a little less painful by adding a cute cat to let you know where you went wrong.'
+
+# Setting up
+setup(
+    name="bingo-bongo-cat",
+    version=VERSION,
+    author="Kyle Levy",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'debugging', 'cat', 'funny'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
