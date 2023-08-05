3ab39ebbca7af3f82672928e90f34f8e68ac6948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:14 +0000

    Add package powerapi/2.1.0/powerapi-2.1.0-py3-none-any.whl

---
+++ packages/powerapi/powerapi-2.1.0-py3-none-any.whl/powerapi/cli/generator.py
@@ -0,0 +1,342 @@
+# Copyright (c) 2021, INRIA
+# Copyright (c) 2021, University of Lille
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import logging
+import os
+import sys
+from typing import Dict, Type
+
+from powerapi.actor import Actor
+from powerapi.database.influxdb2 import InfluxDB2
+from powerapi.exception import PowerAPIException, ModelNameAlreadyUsed, DatabaseNameDoesNotExist, ModelNameDoesNotExist, \
+    DatabaseNameAlreadyUsed
+from powerapi.filter import Filter
+from powerapi.report import HWPCReport, PowerReport, ControlReport, ProcfsReport, Report, FormulaReport
+from powerapi.database import MongoDB, CsvDB, InfluxDB, OpenTSDB, SocketDB, PrometheusDB, DirectPrometheusDB, \
+    VirtioFSDB, FileDB
+from powerapi.puller import PullerActor
+from powerapi.pusher import PusherActor
+
+from powerapi.report_modifier.libvirt_mapper import LibvirtMapper
+from powerapi.puller.simple.simple_puller_actor import SimplePullerActor
+from powerapi.pusher.simple.simple_pusher_actor import SimplePusherActor
+
+COMPONENT_TYPE_KEY = 'type'
+COMPONENT_MODEL_KEY = 'model'
+COMPONENT_DB_NAME_KEY = 'db'
+COMPONENT_DB_COLLECTION_KEY = 'collection'
+COMPONENT_DB_MANAGER_KEY = 'db_manager'
+COMPONENT_DB_MAX_BUFFER_SIZE = 'max_buffer_size'
+
+GENERAL_CONF_STREAM_MODE_KEY = 'stream'
+GENERAL_CONF_VERBOSE_KEY = 'verbose'
+
+
+class Generator:
+    """
+    Generate an actor class and actor start message from config dict.
+    The config dict has the following structure:
+    {
+        "arg1_key": value,
+        "arg2_key": value
+        ...
+        "component_group_name1":{
+            "arg1_cpn1_key": value,
+            "arg2_cpn1_key": value,
+            ...
+
+        }
+        component_group_name2:{
+            ...
+        }
+        ...
+    }
+    """
+
+    def __init__(self, component_group_name):
+        self.component_group_name = component_group_name
+
+    def generate(self, main_config: dict) -> Dict[str, Type[Actor]]:
+        """
+        Generate an actor class and actor start message from config dict
+        """
+        if self.component_group_name not in main_config:
+            raise PowerAPIException('Configuration error : no ' + self.component_group_name + ' specified')
+
+        actors = {}
+
+        for component_name, component_config in main_config[self.component_group_name].items():
+            component_type = ''
+            try:
+                component_type = component_config[COMPONENT_TYPE_KEY]
+                actors[component_name] = self._gen_actor(component_config, main_config, component_name)
+            except KeyError as exn:
+                msg = 'Configuration error : argument ' + exn.args[0]
+                msg += ' needed with output ' + component_type
+                print(msg, file=sys.stderr)
+                raise PowerAPIException(msg) from exn
+
+        return actors
+
+    def _gen_actor(self, component_config: dict, main_config: dict, component_name: str) -> Type[Actor]:
+        raise NotImplementedError()
+
+
+def gen_tag_list(db_config: dict):
+    """
+    Generate tag list from tag string
+    """
+    if 'tags' not in db_config:
+        return []
+    return db_config['tags'].split(',')
+
+
+class BaseGenerator(Generator):
+    """
+    Generate an Actor and Start message from config
+    """
+
+    def __init__(self, component_group_name: str):
+        Generator.__init__(self, component_group_name)
+        self.report_classes = {
+            'HWPCReport': HWPCReport,
+            'PowerReport': PowerReport
+        }
+
+    def _gen_actor(self, component_config: dict, main_config: dict, component_name: str):
+        model = self._get_report_class(component_config[COMPONENT_MODEL_KEY], component_config)
+        component_config[COMPONENT_MODEL_KEY] = model
+
+        actor = self._actor_factory(component_name, main_config, component_config)
+        return actor
+
+    def _get_report_class(self, model_name: str, component_config: dict):
+        if model_name not in self.report_classes:
+            raise PowerAPIException(f'Configuration error: model type {model_name} unknown')
+
+        return self.report_classes[component_config[COMPONENT_MODEL_KEY]]
+
+    def _actor_factory(self, actor_name: str, main_config: dict, component_config: dict):
+        raise NotImplementedError
+
+
+class DBActorGenerator(BaseGenerator):
+    """
+    ActorGenerator that initialise the start message with a database from config
+    """
+
+    def __init__(self, component_group_name: str):
+        BaseGenerator.__init__(self, component_group_name)
+        self.report_classes['FormulaReport'] = FormulaReport
+        self.report_classes['ControlReport'] = ControlReport
+        self.report_classes['ProcfsReport'] = ProcfsReport
+
+        self.db_factory = {
+            'mongodb': lambda db_config: MongoDB(report_type=db_config['model'], uri=db_config['uri'],
+                                                 db_name=db_config['db'], collection_name=db_config['collection']),
+            'socket': lambda db_config: SocketDB(db_config['model'], db_config['port']),
+            'csv': lambda db_config: CsvDB(report_type=db_config['model'], tags=gen_tag_list(db_config),
+                                           current_path=os.getcwd() if 'directory' not in db_config else db_config[
+                                               'directory'],
+                                           files=[] if 'files' not in db_config else db_config['files']),
+            'influxdb': lambda db_config: InfluxDB(report_type=db_config['model'], uri=db_config['uri'],
+                                                   port=db_config['port'], db_name=db_config['db'],
+                                                   tags=gen_tag_list(db_config)),
+            'influxdb2': lambda db_config: InfluxDB2(report_type=db_config['model'], url=db_config['uri'],
+                                                     org=db_config['org'],
+                                                     bucket_name=db_config['db'], token=db_config['token'],
+                                                     tags=gen_tag_list(db_config),
+                                                     port=None if 'port' not in db_config else db_config['port']),
+            'opentsdb': lambda db_config: OpenTSDB(report_type=db_config['model'], host=db_config['uri'],
+                                                   port=db_config['port'], metric_name=db_config['metric_name']),
+            'prom': lambda db_config: PrometheusDB(report_type=db_config['model'], port=db_config['port'],
+                                                   address=db_config['uri'], metric_name=db_config['metric_name'],
+                                                   metric_description=db_config['metric_description'],
+                                                   aggregation_periode=db_config['aggregation_period'],
+                                                   tags=gen_tag_list(db_config)),
+            'direct_prom': lambda db_config: DirectPrometheusDB(report_type=db_config['model'], port=db_config['port'],
+                                                                address=db_config['uri'],
+                                                                metric_name=db_config['metric_name'],
+                                                                metric_description=db_config['metric_description'],
+                                                                tags=gen_tag_list(db_config)),
+            'virtiofs': lambda db_config: VirtioFSDB(report_type=db_config['model'],
+                                                     vm_name_regexp=db_config['vm_name_regexp'],
+                                                     root_directory_name=db_config['root_directory_name'],
+                                                     vm_directory_name_prefix=db_config['vm_directory_name_prefix'],
+                                                     vm_directory_name_suffix=db_config['vm_directory_name_suffix']),
+            'filedb': lambda db_config: FileDB(report_type=db_config['model'], filename=db_config['filename'])
+        }
+
+    def remove_report_class(self, model_name: str):
+        """
+        remove a Model from generator
+        """
+        if model_name not in self.report_classes:
+            raise ModelNameDoesNotExist(model_name)
+        del self.report_classes[model_name]
+
+    def remove_db_factory(self, database_name: str):
+        """
+        remove a database from generator
+        """
+        if database_name not in self.db_factory:
+            raise DatabaseNameDoesNotExist(database_name)
+        del self.db_factory[database_name]
+
+    def add_report_class(self, model_name: str, report_class: Type[Report]):
+        """
+        add a report class to generator
+        """
+        if model_name in self.report_classes:
+            raise ModelNameAlreadyUsed(model_name)
+        self.report_classes[model_name] = report_class
+
+    def add_db_factory(self, db_name: str, db_factory_function):
+        """
+        add a database to generator
+        """
+        if db_name in self.db_factory:
+            raise DatabaseNameAlreadyUsed(db_name)
+        self.db_factory[db_name] = db_factory_function
+
+    def _generate_db(self, db_name: str, component_config: dict):
+        if db_name not in self.db_factory:
+            msg = 'Configuration error : database type ' + db_name + ' unknown'
+            print(msg, file=sys.stderr)
+            raise PowerAPIException(msg)
+        else:
+            return self.db_factory[db_name](component_config)
+
+    def _gen_actor(self, component_config: dict, main_config: dict, actor_name: str):
+        model = self._get_report_class(component_config[COMPONENT_MODEL_KEY], component_config)
+        component_config[COMPONENT_MODEL_KEY] = model
+        database_manager = self._generate_db(component_config[COMPONENT_TYPE_KEY], component_config)
+        component_config[COMPONENT_DB_MANAGER_KEY] = database_manager
+
+        actor = self._actor_factory(actor_name, main_config, component_config)
+        return actor
+
+
+class PullerGenerator(DBActorGenerator):
+    """
+    Generate Puller Actor class and Puller start message from config
+    """
+
+    def __init__(self, report_filter: Filter, report_modifier_list=None):
+        DBActorGenerator.__init__(self, 'input')
+        self.report_filter = report_filter
+
+        if report_modifier_list is None:
+            report_modifier_list = []
+        self.report_modifier_list = report_modifier_list
+
+    def _actor_factory(self, actor_name: str, main_config, component_config: dict):
+        return PullerActor(name=actor_name, database=component_config[COMPONENT_DB_MANAGER_KEY],
+                           report_filter=self.report_filter, stream_mode=main_config[GENERAL_CONF_STREAM_MODE_KEY],
+                           report_modifier_list=self.report_modifier_list,
+                           report_model=component_config[COMPONENT_MODEL_KEY],
+                           level_logger=logging.DEBUG if main_config[GENERAL_CONF_VERBOSE_KEY] else logging.INFO)
+
+
+COMPONENT_NUMBER_OF_REPORTS_TO_SEND_KEY = 'number_of_reports_to_send'
+
+
+class SimplePullerGenerator(BaseGenerator):
+    """
+    Generate Simple Puller Actor class and Simple Puller start message from config
+    """
+
+    def __init__(self, report_filter, report_modifier_list=None):
+        BaseGenerator.__init__(self, 'input')
+        self.report_filter = report_filter
+
+        if report_modifier_list is None:
+            report_modifier_list = []
+        self.report_modifier_list = report_modifier_list
+
+    def _actor_factory(self, actor_name: str, main_config: dict, component_config: dict):
+        return SimplePullerActor(name=actor_name, report_filter=self.report_filter,
+                                 number_of_reports_to_send=component_config[COMPONENT_NUMBER_OF_REPORTS_TO_SEND_KEY],
+                                 report_type_to_send=component_config[COMPONENT_MODEL_KEY])
+
+
+COMPONENT_NUMBER_OF_REPORTS_TO_STORE_KEY = 'number_of_reports_to_store'
+
+
+class PusherGenerator(DBActorGenerator):
+    """
+    Generate Pusher actor and Pusher start message from config
+    """
+
+    def __init__(self):
+        DBActorGenerator.__init__(self, 'output')
+
+    def _actor_factory(self, actor_name: str, main_config: dict, component_config: dict):
+        if 'max_buffer_size' in component_config.keys():
+            return PusherActor(name=actor_name, report_model=component_config[COMPONENT_MODEL_KEY],
+                               database=component_config[COMPONENT_DB_MANAGER_KEY],
+                               max_size=component_config[COMPONENT_DB_MAX_BUFFER_SIZE])
+
+        return PusherActor(name=actor_name, report_model=component_config[COMPONENT_MODEL_KEY],
+                           database=component_config[COMPONENT_DB_MANAGER_KEY],
+                           level_logger=logging.DEBUG if main_config[GENERAL_CONF_VERBOSE_KEY] else logging.INFO)
+
+
+class SimplePusherGenerator(BaseGenerator):
+    """
+    Generate Simple Pusher actor and Simple Pusher start message from config
+    """
+
+    def __init__(self):
+        BaseGenerator.__init__(self, 'output')
+
+    def _actor_factory(self, actor_name: str, main_config: dict, component_config: dict):
+        return SimplePusherActor(name=actor_name,
+                                 number_of_reports_to_store=component_config['number_of_reports_to_store'])
+
+
+class ReportModifierGenerator:
+    """
+    Generate Report modifier list from config
+    """
+
+    def __init__(self):
+        self.factory = {'libvirt_mapper': lambda config: LibvirtMapper(config['uri'], config['domain_regexp'])}
+
+    def generate(self, config: dict):
+        """
+        Generate Report modifier list from config
+        """
+        report_modifier_list = []
+        if 'report_modifier' not in config:
+            return []
+        for report_modifier_name in config['report_modifier'].keys():
+            report_modifier = self.factory[report_modifier_name](config['report_modifier'][report_modifier_name])
+            report_modifier_list.append(report_modifier)
+        return report_modifier_list
---
+++ packages/powerapi/powerapi-2.1.0-py3-none-any.whl/powerapi/database/prometheus_db.py
@@ -0,0 +1,180 @@
+# Copyright (c) 2021, INRIA
+# Copyright (c) 2021, University of Lille
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import logging
+from typing import List, Type
+try:
+    from prometheus_client import start_http_server, Gauge
+except ImportError:
+    logging.getLogger().info("prometheus-client is not installed.")
+
+from powerapi.report import Report
+from powerapi.utils import StatBuffer
+from .base_db import BaseDB
+
+
+class BasePrometheusDB(BaseDB):
+    """
+    Base class to expose data to prometheus instance
+    """
+    def __init__(self, report_type: Type[Report], port: int, address: str, metric_name: str,
+                 metric_description: str, tags: List[str]):
+        BaseDB.__init__(self, report_type)
+        self.address = address
+        self.port = port
+        self.metric_name = metric_name
+        self.metric_description = metric_description
+        self.tags = tags
+
+    def _init_metrics(self):
+        raise NotImplementedError()
+
+    def connect(self):
+        """
+        Start a HTTP server exposing metrics
+        """
+        self._init_metrics()
+        start_http_server(self.port)
+
+
+class PrometheusDB(BasePrometheusDB):
+    """
+    Database that expose received data as metric in order to be scrapped by a prometheus instance
+    Could only be used with a pusher actor
+    """
+
+    def __init__(self, report_type: Type[Report], port: int, address: str, metric_name: str,
+                 metric_description: str, aggregation_periode: int, tags: List[str]):
+        """
+        :param address:             address that expose the metric
+        :param port:
+        :param metric_name:
+        :param metric_description:  short sentence that describe the metric
+        :param aggregation_periode: number of second for the value must be aggregated before compute statistics on them
+        :param tags: metadata used to tag metric
+        """
+        BasePrometheusDB.__init__(self, report_type, port, address, metric_name, metric_description, tags)
+        self.aggregation_periode = aggregation_periode
+        self.final_tags = ['sensor', 'target'] + tags
+
+        self.mean_metric = None
+        self.std_metric = None
+        self.min_metric = None
+        self.max_metric = None
+
+        self.exposed_measure = {}
+        self.measure_for_current_period = {}
+        self.current_period_end = 0
+
+        self.buffer = StatBuffer(aggregation_periode)
+
+    def __iter__(self):
+        raise NotImplementedError()
+
+    def _init_metrics(self):
+        self.mean_metric = Gauge(self.metric_name + '_mean', self.metric_description + '(MEAN)', self.final_tags)
+        self.std_metric = Gauge(self.metric_name + '_std', self.metric_description + '(STD)', self.final_tags)
+        self.min_metric = Gauge(self.metric_name + '_min', self.metric_description + '(MIN)', self.final_tags)
+        self.max_metric = Gauge(self.metric_name + '_max', self.metric_description + '(MAX)', self.final_tags)
+
+    def _expose_data(self, key):
+        aggregated_value = self.buffer.get_stats(key)
+        if aggregated_value is None:
+            return
+
+        kwargs = {label: aggregated_value['tags'][label] for label in self.final_tags}
+        try:
+            self.mean_metric.labels(**kwargs).set(aggregated_value['mean'])
+            self.std_metric.labels(**kwargs).set(aggregated_value['std'])
+            self.min_metric.labels(**kwargs).set(aggregated_value['min'])
+            self.max_metric.labels(**kwargs).set(aggregated_value['max'])
+        except TypeError:
+            self.mean_metric.labels(kwargs).set(aggregated_value['mean'])
+            self.std_metric.labels(kwargs).set(aggregated_value['std'])
+            self.min_metric.labels(kwargs).set(aggregated_value['min'])
+            self.max_metric.labels(kwargs).set(aggregated_value['max'])
+
+    def _report_to_measure_and_key(self, report):
+        value = self.report_type.to_prometheus(report, self.tags)
+        key = ''.join([str(value['tags'][tag]) for tag in self.final_tags])
+        return key, value
+
+    def _update_exposed_measure(self):
+        updated_exposed_measure = {}
+
+        for key in self.exposed_measure:
+            if key not in self.measure_for_current_period:
+                args = self.exposed_measure[key]
+                self.mean_metric.remove(*args)
+                self.std_metric.remove(*args)
+                self.min_metric.remove(*args)
+                self.max_metric.remove(*args)
+            else:
+                updated_exposed_measure[key] = self.exposed_measure[key]
+        self.exposed_measure = updated_exposed_measure
+
+    def _append_measure_from_old_period_to_buffer_and_expose_data(self):
+        for old_key, old_measure_list in self.measure_for_current_period.items():
+            for old_measure in old_measure_list:
+                self.buffer.append(old_measure, old_key)
+            self._expose_data(old_key)
+
+    def _reinit_persiod(self, new_measure_time):
+        self.current_period_end = new_measure_time + self.aggregation_periode
+        self.measure_for_current_period = {}
+
+    def save(self, report: Report):
+        """
+        Override from BaseDB
+
+        :param report: Report to save
+        """
+        key, measure = self._report_to_measure_and_key(report)
+        if measure['time'] > self.current_period_end:
+            self._append_measure_from_old_period_to_buffer_and_expose_data()
+            self._update_exposed_measure()
+            self._reinit_persiod(measure['time'])
+
+        if key not in self.exposed_measure:
+            args = [measure['tags'][label] for label in self.final_tags]
+            self.exposed_measure[key] = args
+
+        if key not in self.measure_for_current_period:
+            self.measure_for_current_period[key] = []
+
+        self.measure_for_current_period[key].append(measure)
+
+    def save_many(self, reports: List[Report]):
+        """
+        Save a batch of data
+
+        :param reports: Batch of data.
+        """
+        for report in reports:
+            self.save(report)
