a029168752ff3a6e675f0264d2ec35e6b37b6f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:39 +0000

    Add package jupysql/0.8.0/jupysql-0.8.0-py3-none-any.whl

---
+++ packages/jupysql/jupysql-0.8.0-py3-none-any.whl/sql/__init__.py
@@ -0,0 +1,12 @@
+from .magic import RenderMagic, SqlMagic, load_ipython_extension
+from .connection import PLOOMBER_DOCS_LINK_STR
+
+__version__ = "0.8.0"
+
+
+__all__ = [
+    "RenderMagic",
+    "SqlMagic",
+    "load_ipython_extension",
+    "PLOOMBER_DOCS_LINK_STR",
+]
