0523d2761551bca83322982eb25996e1437e1ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:39 +0000

    Add package sceptre/4.2.2/sceptre-4.2.2-py3-none-any.whl

---
+++ packages/sceptre/sceptre-4.2.2-py3-none-any.whl/sceptre/hooks/cmd.py
@@ -0,0 +1,28 @@
+import subprocess
+from sceptre.hooks import Hook
+from sceptre.exceptions import InvalidHookArgumentTypeError
+
+
+class Cmd(Hook):
+    """
+    Cmd implements a Sceptre hook which can run arbitrary commands.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Cmd, self).__init__(*args, **kwargs)
+
+    def run(self):
+        """
+        Runs the argument string in a subprocess.
+
+        :raises: sceptre.exceptions.InvalidTaskArgumentTypeException
+        :raises: subprocess.CalledProcessError
+        """
+        envs = self.stack.connection_manager.create_session_environment_variables()
+        try:
+            subprocess.check_call(self.argument, shell=True, env=envs)
+        except TypeError:
+            raise InvalidHookArgumentTypeError(
+                'The argument "{0}" is the wrong type - cmd hooks require '
+                "arguments of type string.".format(self.argument)
+            )
