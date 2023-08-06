bd409c0e21bf109cb0bdaee2466049e31112ea2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:48 +0000

    Add package volvopy/0.1.14/volvopy-0.1.14.tar.gz

---
+++ packages/volvopy/volvopy-0.1.14.tar.gz/volvopy-0.1.14/examples/demo1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+
+# Running this demo requires the `volvopy` package to be installed by `pip`!
+import volvopy as vp
+
+DEBUG = True
+HERE = os.path.realpath(__file__).split("/")
+MYID = HERE[-1]
+
+example_con = vp.Connected_Vehicle(debug=DEBUG)
+example_con.get_all()
+
+example_nrg = vp.Energy(debug=DEBUG)
+example_nrg.get_all()
+
+example_xtnd = vp.Extended_Vehicle(debug=DEBUG)
+example_xtnd.get_all()
+
+example_loc = vp.Location(debug=DEBUG)
+example_loc.get_all()
