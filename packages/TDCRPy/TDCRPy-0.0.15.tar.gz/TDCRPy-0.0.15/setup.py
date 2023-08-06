abbf504fb583410d30dc21cdc5fd2f95e6fa1acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:23 +0000

    Add package TDCRPy/0.0.15/TDCRPy-0.0.15.tar.gz

---
+++ packages/TDCRPy/TDCRPy-0.0.15.tar.gz/TDCRPy-0.0.15/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = "0.0.15"
+DESCRIPTION = "TDCR model"
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name = "TDCRPy",
+    version = VERSION,
+    author = "RomainCoulon (Romain Coulon)",
+    author_email = "<romain.coulon@bipm.org>",
+    description = DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RomainCoulon/TDCRPy",
+    packages = find_packages(),
+    install_requires = ["numpy"],
+    keywords = ["python","TDCR","Monte-Carlo","radionuclide","scintillation","counting"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+	"License :: OSI Approved :: MIT License",
+	"Natural Language :: English",
+	"Natural Language :: French",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+	"Topic :: Scientific/Engineering :: Physics",
+    ]
+)
