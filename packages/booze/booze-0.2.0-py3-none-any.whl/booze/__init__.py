a51cab6ad45b8fc40e75a77af494264ddacdff78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:05 +0000

    Add package booze/0.2.0/booze-0.2.0-py3-none-any.whl

---
+++ packages/booze/booze-0.2.0-py3-none-any.whl/booze/__init__.py
@@ -0,0 +1,32 @@
+from coercer.coercer import Coerce
+from coercer.base import Base
+from coercer.errors import ParsingError
+
+
+"""
+import { z } from "zod";
+
+// primitive values
+z.string();
+z.number();
+z.bigint();
+z.boolean();
+z.date();
+z.symbol();
+z.literal();
+
+// empty types
+z.undefined();
+z.null();
+z.void(); // accepts undefined
+
+// catch-all types
+// allows any value
+z.any();
+z.unknown();
+
+// never type
+// allows no values
+z.never();
+"""
+
