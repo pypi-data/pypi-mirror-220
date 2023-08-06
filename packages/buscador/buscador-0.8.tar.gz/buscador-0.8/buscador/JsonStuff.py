0d2122caf1d9540b9fe33d05193f344fef452589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:57 +0000

    Add package buscador/0.8/buscador-0.8.tar.gz

---
+++ packages/buscador/buscador-0.8.tar.gz/buscador-0.8/buscador/JsonStuff.py
@@ -0,0 +1,38 @@
+def find_keys_value(adict: dict, key: str):
+    """Returns value of a given key, even if nested
+
+    Args:
+        adict (dict): target dictionary
+        key (str): target dictionary key
+
+    Returns:
+        Value of target dictionary key
+    """
+    stack = [adict]
+    while stack:
+        d = stack.pop()
+        if key in d:
+            return d[key]
+        for v in d.values():
+            if isinstance(v, dict):
+                stack.append(v)
+            if isinstance(v, list):
+                stack += v
+
+
+def get_paths(my_dict: dict, path=None):
+    """
+    Get paths for nested keys in dictionaries.
+    Args:
+        my_dict (dict): Target dictionary.
+        path (list, optional): Defaults to None.
+    """
+    if path is None:
+        path = []
+    for k, v in my_dict.items():
+        newpath = path + [k]
+        if isinstance(v, dict):
+            for u in get_paths(v, newpath):
+                yield u
+        else:
+            yield newpath, v
---
+++ packages/buscador/buscador-0.8.tar.gz/buscador-0.8/buscador/__init__.py
@@ -0,0 +1,2 @@
+from buscador.JsonStuff import find_keys_value, get_paths
+from buscador.S3Stuff import write_json, read_json, read_jsonl, read_csv, write_csv
