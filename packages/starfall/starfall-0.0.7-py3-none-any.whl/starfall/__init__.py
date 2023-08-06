c123e782957c3b5782ace11214ef00da18fd3f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:11 +0000

    Add package starfall/0.0.7/starfall-0.0.7-py3-none-any.whl

---
+++ packages/starfall/starfall-0.0.7-py3-none-any.whl/starfall/__init__.py
@@ -0,0 +1,13 @@
+'''
+Author: Alex
+LastEditors: Alex yxfacw@163.com
+Date: 2023-07-18 17:54:46
+Description:  入口
+'''
+
+# from . import helper, executor
+
+from starfall.setting import init
+from starfall.main import start_serve
+
+__all__ = ([init, start_serve])
