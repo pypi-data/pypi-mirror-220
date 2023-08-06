c34e46d384d4935d695e4d12e6d7a65c76e76aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:01 +0000

    Add package macaquedb/0.1.3/macaquedb-0.1.3.tar.gz

---
+++ packages/macaquedb/macaquedb-0.1.3.tar.gz/macaquedb-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='macaquedb',
+    version='0.1.3',
+    description='SQLite Database interface for PRIME-DE data',
+    author='Sam Alldritt',
+    author_email='samuel.alldritt@childmind.org',
+    url='https://github.com/samalldritt/MacaqueDB',
+    packages=find_packages(),
+    install_requires=[
+        'sqlite>=3.40.1'
+    ]
+)
