fb40e841deb6aed1d3f0b95d4c0f4aea121f3dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:03 +0000

    Add package kiba-core/0.5.2.dev4/kiba_core-0.5.2.dev4-py3-none-any.whl

---
+++ packages/kiba-core/kiba_core-0.5.2.dev4-py3-none-any.whl/core/slack_client.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from core.requester import KibaResponse
+from core.requester import Requester
+from core.notification_client import NotificationClient
+
+class SlackClient(NotificationClient):
+
+    def __init__(self, webhookUrl: str, requester: Requester, defaultChannel: str, defaultSender: Optional[str] = 'kiba-server', defaultIconEmoji: Optional[str] = ':robot_face:') -> None:
+        self.webhookUrl = webhookUrl
+        self.requester = requester
+        self.defaultChannel = defaultChannel
+        self.defaultSender = defaultSender
+        self.defaultIconEmoji = defaultIconEmoji
+
+    async def post(self, messageText: str) -> KibaResponse:
+        response = await self.requester.post_json(url=self.webhookUrl, dataDict={
+            'text': messageText,
+            'username': self.defaultSender,
+            'channel': self.defaultChannel,
+            'icon_emoji': self.defaultIconEmoji,
+        })
+        return response
