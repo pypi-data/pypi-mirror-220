3dff971ba6efbd650294a43df28f9cfec58544b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:28 +0000

    Add package my-email-parser/1.0/my_email_parser-1.0.tar.gz

---
+++ packages/my-email-parser/my_email_parser-1.0.tar.gz/my_email_parser-1.0/settingup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_email_parser',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'win32com',
+        'openpyxl'
+    ],
+    author='Your Name',
+    description='A library for parsing Outlook emails and saving data to Excel.',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
