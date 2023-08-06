2f812903eaf04949af1d6c9d68e34530e0cb00e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:46 +0000

    Add package bec-scan-bundler/0.13.2/bec_scan_bundler-0.13.2.tar.gz

---
+++ packages/bec-scan-bundler/bec_scan_bundler-0.13.2.tar.gz/bec_scan_bundler-0.13.2/setup.py
@@ -0,0 +1,30 @@
+import os
+import pathlib
+import subprocess
+
+from setuptools import setup
+
+current_path = pathlib.Path(__file__).parent.resolve()
+
+utils = f"{current_path}/../bec_lib/"
+
+
+def get_version():
+    """load the version from the version file"""
+    version_file = os.path.join(current_path, "../semantic_release", "__init__.py")
+    with open(version_file, "r", encoding="utf-8") as file:
+        res = file.readline()
+        version = res.split("=")[1]
+    return version.strip().strip('"')
+
+
+if __name__ == "__main__":
+    setup(
+        install_requires=["numpy", "pyyaml", "msgpack"],
+        version=get_version(),
+        entry_points={"console_scripts": ["bec-scan-bundler = scan_bundler:main"]},
+        extras_require={"dev": ["pytest", "pytest-random-order", "coverage"]},
+    )
+    local_deps = [utils]
+    for dep in local_deps:
+        subprocess.run(f"pip install -e {dep}", shell=True, check=True)
