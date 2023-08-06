c4d9b773aa3bf636911f19978a5172ff025ab7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:33 +0000

    Add package radiens/2.0.1/radiens-2.0.1.tar.gz

---
+++ packages/radiens/radiens-2.0.1.tar.gz/radiens-2.0.1/radiens/__init__.py
@@ -0,0 +1,9 @@
+from .version import __version__
+# from .lib.channel_metadata import ChannelMetadata
+# from .lib.dataset_metadata import DatasetMetadata
+# from .lib.signals_snapshot import SignalsSnapshot
+from .curate_client import CurateClient
+from .videre_client import VidereClient
+from .allego_client import AllegoClient
+from .file_sys_client import FileSystemClient
+name = "radiens"
