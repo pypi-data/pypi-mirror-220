f7678313d86588b44a38c656d44a169a49b923ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:40 +0000

    Add package logger-local/0.0.22/logger-local-0.0.22.tar.gz

---
+++ packages/logger-local/logger-local-0.0.22.tar.gz/logger-local-0.0.22/logger_local_python_package/localLogger.py
@@ -0,0 +1,14 @@
+from logger_local_python_package.LoggerService import LoggerService
+import logzio
+from logzio.handler import LogzioHandler
+import os
+import logging
+
+logger_local = LoggerService()
+
+
+
+
+
+
+
