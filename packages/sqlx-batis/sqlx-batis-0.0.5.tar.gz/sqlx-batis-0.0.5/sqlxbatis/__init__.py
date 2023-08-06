718f98422241fba5c8131f601b8d697d9166da93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:23 +0000

    Add package sqlx-batis/0.0.5/sqlx-batis-0.0.5.tar.gz

---
+++ packages/sqlx-batis/sqlx-batis-0.0.5.tar.gz/sqlx-batis-0.0.5/sqlxbatis/__init__.py
@@ -0,0 +1,10 @@
+from sqlexec import (
+    connection,
+    transaction,
+    with_connection,
+    with_transaction,
+    get_connection,
+    Engin
+)
+
+from sqlexec.support import DBError
---
+++ packages/sqlx-batis/sqlx-batis-0.0.5.tar.gz/sqlx-batis-0.0.5/sqlxbatis/exec_support.py
@@ -0,0 +1,24 @@
+import sqlexec
+from . import DBError
+from .support import SqlAction
+from .sql_holder import do_get_sql
+from sqlexec.sql_support import get_select_key
+
+
+def do_save(sql_model, batch, param_names, *args, **kwargs):
+    assert SqlAction.INSERT.value == sql_model.action, 'Only insert sql can return primary key.'
+    sql, args = do_get_sql(sql_model, batch, param_names, *args, **kwargs)
+    select_key = sql_model.select_key
+    pk_seq = sql_model.pk_seq
+    return  do_save0(select_key, pk_seq, sql, *args)
+
+
+def do_save0(select_key, pk_seq, sql, *args):
+    if select_key:
+        return sqlexec.save_sql(select_key, sql, *args)
+
+    select_key = get_select_key(pk_seq, None, sql)
+    if select_key:
+        return sqlexec.save_sql(select_key, sql, *args)
+
+    raise DBError("Can not get select key.")
