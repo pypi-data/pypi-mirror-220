39cdf09f8a70b1a78686728e392ccd809130b600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:08 +0000

    Add package result/0.13.1/result-0.13.1-py3-none-any.whl

---
+++ packages/result/result-0.13.1-py3-none-any.whl/result/__init__.py
@@ -0,0 +1,12 @@
+from .result import Err, Ok, OkErr, Result, UnwrapError, as_async_result, as_result
+
+__all__ = [
+    "Err",
+    "Ok",
+    "OkErr",
+    "Result",
+    "UnwrapError",
+    "as_async_result",
+    "as_result",
+]
+__version__ = "0.13.1"
