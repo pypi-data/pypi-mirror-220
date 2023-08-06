27e0445e1b2762553c59ad06366ddf978035de85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:17 +0000

    Add package zimran-events/0.3.1/zimran_events-0.3.1-py3-none-any.whl

---
+++ packages/zimran-events/zimran_events-0.3.1-py3-none-any.whl/zimran/events/utils.py
@@ -0,0 +1,48 @@
+from aiormq.exceptions import AMQPChannelError, AMQPConnectionError
+
+
+try:
+    from loguru import logger
+except ImportError:
+    import logging
+
+    logger = logging.getLogger(__name__)
+
+from .exceptions import ChannelPropertiesTypeError, ExchangeTypeError
+from .schemas import ChannelPropertiesScheme, ExchangeScheme
+
+
+def validate_exchange(exchange: ExchangeScheme):
+    if not isinstance(exchange, ExchangeScheme):
+        raise ExchangeTypeError('ExchangeTypeError: <exchange> must be instance of <ExchangeScheme>')
+
+
+def validate_channel_properties(properties: ChannelPropertiesScheme):
+    if not isinstance(properties, ChannelPropertiesScheme):
+        raise ChannelPropertiesTypeError(
+            'ChannelPropertiesTypeError: <properties> must be instance of <ChannelPropertiesScheme>',
+        )
+
+
+def cleanup_and_normalize_queue_name(queue_name: str):
+    if '*' in queue_name:
+        queue_name = queue_name.replace('*', '')
+
+    if '#' in queue_name:
+        queue_name = queue_name.replace('#', '')
+
+    if '..' in queue_name:
+        queue_name = queue_name.replace('..', '.')
+
+    if queue_name.endswith('.'):
+        queue_name = queue_name[:-1]
+
+    return f'{queue_name}_q'
+
+
+def retry_policy(info):
+    if isinstance(info.exception, (AMQPConnectionError, AMQPChannelError)):
+        logger.warning(f'Retrying connection... | attempt amount: {info.fails}')
+        return info.fails > 3, (info.fails - 1) * 2
+
+    return True, 0
