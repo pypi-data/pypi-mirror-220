e2145a47d2a7ae663110512deb8d4715e9acbcce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:29 +0000

    Add package oda-wd-client/0.0.1/oda_wd_client-0.0.1.tar.gz

---
+++ packages/oda-wd-client/oda_wd_client-0.0.1.tar.gz/oda_wd_client-0.0.1/oda_wd_client/types/resource_management.py
@@ -0,0 +1,15 @@
+from oda_wd_client.service.resource_management.types import (
+    Supplier,
+    SupplierInvoice,
+    SupplierInvoiceLine,
+    TaxApplicability,
+    TaxOption,
+)
+
+__all__ = [
+    "TaxApplicability",
+    "TaxOption",
+    "Supplier",
+    "SupplierInvoice",
+    "SupplierInvoiceLine",
+]
