6cf62b3109f753d60d3820cb9d10eaef33acf7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:06 +0000

    Add package simsopt/0.19.0/simsopt-0.19.0-cp38-cp38-musllinux_1_1_x86_64.whl

---
+++ packages/simsopt/simsopt-0.19.0-cp38-cp38-musllinux_1_1_x86_64.whl/simsopt/geo/__init__.py
@@ -0,0 +1,36 @@
+from jax.config import config
+config.update("jax_enable_x64", True)
+from .config import *
+
+from .curve import *
+from .curvehelical import *
+from .curverzfourier import *
+from .curvexyzfourier import *
+from .curveperturbed import *
+from .curveobjectives import *
+
+from .finitebuild import *
+from .plotting import *
+
+from .boozersurface import *
+from .qfmsurface import *
+from .surface import *
+from .surfacegarabedian import *
+from .surfacehenneberg import *
+from .surfaceobjectives import *
+from .surfacerzfourier import *
+from .surfacexyzfourier import *
+from .surfacexyztensorfourier import *
+
+from .permanent_magnet_grid import *
+
+__all__ = (curve.__all__ + curvehelical.__all__ +
+           curverzfourier.__all__ + curvexyzfourier.__all__ +
+           curveperturbed.__all__ + curveobjectives.__all__ +
+           finitebuild.__all__ + plotting.__all__ +
+           boozersurface.__all__ + qfmsurface.__all__ +
+           surface.__all__ +
+           surfacegarabedian.__all__ + surfacehenneberg.__all__ +
+           surfacerzfourier.__all__ + surfacexyzfourier.__all__ +
+           surfacexyztensorfourier.__all__ + surfaceobjectives.__all__ +
+           permanent_magnet_grid.__all__)
