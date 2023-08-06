b943853f9fea6a1a837e06741115ea1140593240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:48 +0000

    Add package pwvalid/0.1/pwvalid-0.1.tar.gz

---
+++ packages/pwvalid/pwvalid-0.1.tar.gz/pwvalid-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='pwvalid',
+    version='0.1',
+    description='Validate Emails with ease',
+    long_description= "Validate Emails with ease in No time!",
+    author="Vinay Chaudhary",
+    packages=['emlpackage'],
+    install_requires=['requests'],
+)
