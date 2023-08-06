ab49831d7625919b0607713f707b9215d66a49e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:32 +0000

    Add package SherLocking/0.0.4/SherLocking-0.0.4.tar.gz

---
+++ packages/SherLocking/SherLocking-0.0.4.tar.gz/SherLocking-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Applies activation for any program.'
+LONG_DESCRIPTION = 'A package to apply activation required to run any program, like a license.'
+
+setup(
+    name="SherLocking",
+    version=VERSION,
+    author="Armando Chaparro",
+    author_email="<pylejandria@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        'ttkbootstrap'
+    ],
+    keywords=['python', 'locking', 'activation', 'trial', 'license'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
