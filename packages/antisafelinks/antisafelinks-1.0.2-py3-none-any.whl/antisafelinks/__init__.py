c08f1548b86d78cabb9cac0b3eda76ecdd27acf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:10 +0000

    Add package antisafelinks/1.0.2/antisafelinks-1.0.2-py3-none-any.whl

---
+++ packages/antisafelinks/antisafelinks-1.0.2-py3-none-any.whl/antisafelinks/__init__.py
@@ -0,0 +1,9 @@
+from .safelink_decoder import recover_link
+from .safelink_decoder import recover_links_in_text
+from .safelink_decoder import has_safelinks
+from .safelink_decoder import recover_email
+from .safelink_decoder import recover_email_from_file
+from .safelink_decoder import recover_maildir
+from .safelink_decoder import this_is_a_test
+from .safelink_decoder import main
+
