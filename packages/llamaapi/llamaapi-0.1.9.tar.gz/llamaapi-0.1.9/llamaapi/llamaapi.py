477a90d9b4a74e26659e3468a18a9ff81c303760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:41 +0000

    Add package llamaapi/0.1.9/llamaapi-0.1.9.tar.gz

---
+++ packages/llamaapi/llamaapi-0.1.9.tar.gz/llamaapi-0.1.9/llamaapi/llamaapi.py
@@ -0,0 +1,47 @@
+import aiohttp
+import asyncio
+import requests
+import nest_asyncio
+
+hostname = 'https://llama-api3.fly.dev'
+
+class LlamaAPI:
+    def __init__(self, api_token):
+        self.hostname = hostname
+        self.api_token = api_token
+        self.headers = {'Llama-API-Token': self.api_token}
+
+        # Apply nest_asyncio to enable nested usage of asyncio's event loop
+        nest_asyncio.apply()
+
+    async def _run_async(self, api_request_json):
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"{self.hostname}/api/chat", headers=self.headers, json=api_request_json) as resp:
+                return await resp.text()
+
+    def _run_stream(self, api_request_json):
+        # For simpler non-Jupyter execution, we use Python's built-in asyncio.run
+        return asyncio.run(self._run_async(api_request_json))
+    
+    def _run_stream_jupyter(self, api_request_json):
+        # For Jupyter Notebook or Google Colab, we use asyncio's event loop through nest_asyncio
+        loop = asyncio.get_event_loop()
+        return loop.run_until_complete(self._run_async(api_request_json))
+
+    def _run_sync(self, api_request_json):
+        response = requests.post(f"{self.hostname}/api/chat", headers=self.headers, json=api_request_json)
+        if response.status_code != 200:
+            raise Exception(f"POST {response.status_code}")
+        return response.json()
+
+    def run(self, api_request_json):
+        if api_request_json.get('stream', False):
+            return self._run_stream(api_request_json)
+        else:
+            return self._run_sync(api_request_json)
+        
+    def run_jupyter(self, api_request_json):
+        if api_request_json.get('stream', False):
+            return self._run_stream_jupyter(api_request_json)
+        else:
+            return self._run_sync(api_request_json)
