abdfdd105de4389ed6b1418a28deb4d21f745a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:33 +0000

    Add package up-nextflap/0.0.4/up_nextflap-0.0.4.tar.gz

---
+++ packages/up-nextflap/up_nextflap-0.0.4.tar.gz/up_nextflap-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+long_description=\
+"""============================================================
+    UP_NEXTFLAP
+ ============================================================
+"""
+
+setup(
+    name='up_nextflap',
+    version='0.0.4',
+    description='up_nextflap',
+    long_description=long_description,
+    long_description_content_type ="text/markdown",
+    author='Oscar Sapena',
+    author_email='ossaver@upv.es',
+    options = {
+        'build_apps': {
+            'platforms': ['win_amd64',],
+        }
+    },
+    data_files=[('.', ['libz3.dll', 'nextflap.pyd'])],
+    install_requires=[
+        'numpy', 'unified-planning'
+    ],
+)
