c3189854b84e345ef857c1fb10b7babb1c92d534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:08 +0000

    Add package deepcellpredictor/0.1/deepcellpredictor-0.1.tar.gz

---
+++ packages/deepcellpredictor/deepcellpredictor-0.1.tar.gz/deepcellpredictor-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.1'
+DESCRIPTION = "a transfer learning approach that explicitly models changes in transcriptional variance using a combination of variational autoencoders and normalizing flows"
+
+setup(
+    name='deepcellpredictor',
+    version=VERSION,
+    description='transfer learning approach',
+    long_description='DESCRIPTION',
+    packages=find_packages(),
+    install_requires=[
+        'numpy','pandas','matplotlib','torch','anndata','pytorch_lightning','typing','scanpy','scipy','scvi'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+  python_requires=">=3.6",
+)
+
