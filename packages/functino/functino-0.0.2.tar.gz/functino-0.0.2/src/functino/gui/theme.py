83289bff8dc0c9062231a68fe4591bcf735d49ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:13 +0000

    Add package functino/0.0.2/functino-0.0.2.tar.gz

---
+++ packages/functino/functino-0.0.2.tar.gz/functino-0.0.2/src/functino/gui/theme.py
@@ -0,0 +1,98 @@
+from importlib.abc import Traversable
+from xml.etree import ElementTree
+
+from PyQt6.QtGui import QColor, QPalette
+
+
+class Theme:
+    """
+    Holds theme data.
+
+    Internally, this class imports Notepad++ theme files and provides convenient access
+    to the relevant data.
+    """
+
+    def __init__(self, theme_path: Traversable) -> None:
+        self._root = ElementTree.fromstring(theme_path.read_text())
+
+    def get_global_background_color(self) -> str:
+        """
+        Get the globally-defined background color.
+        """
+        return self._get_global_override_node().attrib["bgColor"]
+
+    def get_global_text_color(self) -> str:
+        """
+        Get the globally-defined text color.
+        """
+        return self._get_global_override_node().attrib["fgColor"]
+
+    def get_lexer_color_map(self, lexer_name: str) -> dict[int, str]:
+        """
+        Get map of style IDs to colors for the given lexer.
+        """
+        lexer_color_map = {}
+        for word_style_node in self._get_lexer_node(lexer_name):
+            lexer_color_map[int(word_style_node.attrib["styleID"])] = (
+                "#" + word_style_node.attrib["fgColor"]
+            )
+        return lexer_color_map
+
+    def _get_global_override_node(self) -> ElementTree.Element:
+        """
+        Get xml node with global override theme data.
+        """
+        global_override_node = self._root.find(
+            "./GlobalStyles/WidgetStyle[@name='Global Override']"
+        )
+        if global_override_node is None:
+            raise RuntimeError("couldn't find global override node in current theme")
+        return global_override_node
+
+    def _get_lexer_node(self, lexer_name: str) -> ElementTree.Element:
+        """
+        Get xml node with styles for the given lexer.
+        """
+        lexer_node = self._root.find(f"./LexerStyles/LexerType[@name='{lexer_name}']")
+        if lexer_node is None:
+            raise RuntimeError(
+                f"couldn't find lexer node for '{lexer_name}' in current theme"
+            )
+        return lexer_node
+
+
+def get_themed_palette(theme: Theme, palette: QPalette) -> QPalette:
+    """
+    Create a palette based on the given one with the current theme applied.
+    """
+    themed_palette = QPalette(palette)
+    global_text_color = theme.get_global_text_color()
+    themed_palette.setColor(QPalette.ColorRole.Text, QColor(global_text_color))
+    themed_palette.setColor(QPalette.ColorRole.WindowText, QColor(global_text_color))
+    global_background_color = theme.get_global_background_color()
+    themed_palette.setColor(QPalette.ColorRole.Base, QColor(global_background_color))
+    themed_palette.setColor(QPalette.ColorRole.Window, QColor(global_background_color))
+    return themed_palette
+
+
+def get_uniform_palette(palette: QPalette) -> QPalette:
+    """
+    Create a palette based on the given one with more uniform colors.
+
+    The goal with this palette is to make widgets blend together better and to prevent
+    widgets from displaying different colors when the window is out of focus.
+    """
+    uniform_palette = QPalette(palette)
+    uniform_palette.setColor(
+        QPalette.ColorRole.Base, palette.color(QPalette.ColorRole.Base)
+    )
+    uniform_palette.setColor(
+        QPalette.ColorRole.Button, palette.color(QPalette.ColorRole.Button)
+    )
+    uniform_palette.setColor(
+        QPalette.ColorRole.ButtonText, palette.color(QPalette.ColorRole.ButtonText)
+    )
+    uniform_palette.setColor(
+        QPalette.ColorRole.Window, palette.color(QPalette.ColorRole.Base)
+    )
+    return uniform_palette
---
+++ packages/functino/functino-0.0.2.tar.gz/functino-0.0.2/src/functino/language.py
@@ -0,0 +1,143 @@
+from importlib.abc import Traversable
+from itertools import chain
+import os
+from pathlib import Path
+import platform
+import tomllib
+from typing import Any
+
+from functino.platform_path import get_user_language_profiles_path
+from functino.project_path import get_built_in_language_profiles_path
+
+
+class LanguageProfile:
+    """
+    Execution information for a programming language.
+
+    Each instance of this class corresponds to a config file that specifies how to
+    execute files for a particular language.
+    """
+
+    def __init__(self, profile_config_path: Path | Traversable) -> None:
+        profile_data: dict[str, Any] = {}
+        match profile_config_path:
+            case Path():
+                with open(profile_config_path, "rb") as f:
+                    profile_data = tomllib.load(f)
+            case Traversable():
+                profile_data = tomllib.loads(profile_config_path.read_text())
+        self._name: str = profile_data["name"]
+        self._language_id: str = profile_data["language_id"]
+        self._source_file_extension: str = profile_data["source_file_extension"]
+        self._compile: bool = profile_data["compile"]
+        self._command: tuple[str] = tuple(profile_data["command"]["default"])
+        this_system = platform.system()
+        if this_system in profile_data["command"]:
+            self._command = tuple(profile_data["command"][this_system])
+
+    @property
+    def name(self) -> str:
+        """
+        The display name of this profile.
+
+        Each profile must have a unique name attribute.
+        """
+        return self._name
+
+    @property
+    def language_id(self) -> str:
+        """
+        The ID of the language to use with this profile.
+
+        These IDs should correspond to the LexerType name attributes in the theme files.
+        """
+        return self._language_id
+
+    @property
+    def source_file_extension(self) -> str:
+        """
+        The file extension used for source files of this language.
+
+        These extensions should omit the precending period.
+        """
+        return self._source_file_extension
+
+    @property
+    def compile(self) -> bool:
+        """
+        Whether or not this profile requires separate compilation and execution stages.
+        """
+        return self._compile
+
+    @property
+    def command(self) -> tuple[str]:
+        """
+        The command template used to compile or execute a file for this profile.
+        """
+        return self._command
+
+    def generate_command(
+        self, source_file_path: str, executable_path: str | None = None
+    ) -> tuple[str]:
+        """
+        Generate command tuple from this profile's template.
+
+        If the current profile requires a compilation step, executable_path must be set
+        to a writeable path. Conversely, if the current profile does not require a
+        compilation step, executable_path must be None.
+        """
+        if self._compile and executable_path is None:
+            raise RuntimeError("executable path must be set for compile profiles")
+        if not self._compile and executable_path is not None:
+            raise RuntimeError(
+                "executable path must not be set for non-compile profiles"
+            )
+        command_args = []
+        source_file_path_template_found = False
+        executable_path_template_found = False
+        for arg in self._command:
+            if arg == r"{source_file_path}":
+                arg = arg.format(source_file_path=source_file_path)
+                source_file_path_template_found = True
+            elif self._compile and arg == r"{executable_path}":
+                arg = arg.format(executable_path=executable_path)
+                executable_path_template_found = True
+            command_args.append(arg)
+        if not source_file_path_template_found:
+            raise RuntimeError("command template did not contain a file path template")
+        if self._compile and not executable_path_template_found:
+            raise RuntimeError(
+                "command template did not contain an executable path template"
+            )
+        return tuple(command_args)
+
+
+def get_language_profiles() -> tuple[LanguageProfile]:
+    """
+    Return tuple of all language profiles.
+    """
+    language_profiles = list(map(LanguageProfile, get_language_profile_paths()))
+    language_profiles.sort(key=lambda profile: profile.name)
+    for i in range(len(language_profiles) - 1):
+        if language_profiles[i].name == language_profiles[i + 1].name:
+            raise RuntimeError(
+                (
+                    "language profile names must all be unique"
+                    f" (duplicate name: {language_profiles[i].name})"
+                )
+            )
+    return tuple(language_profiles)
+
+
+def get_language_profile_paths() -> tuple[Path | Traversable]:
+    """
+    Get paths of all language profiles.
+
+    Language profiles can be built-in and can also be found in the user-specific
+    profiles directory.
+    """
+    built_in_profiles = get_built_in_language_profiles_path().iterdir()
+    user_profiles_dir = get_user_language_profiles_path()
+    os.makedirs(user_profiles_dir, mode=0o755, exist_ok=True)
+    user_profiles = user_profiles_dir.glob("*.toml")
+    return tuple(chain(built_in_profiles, user_profiles))
