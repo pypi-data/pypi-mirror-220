437197783b0892b00387932081a62bf05b05c834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:24 +0000

    Add package nuclia/1.1.4/nuclia-1.1.4.tar.gz

---
+++ packages/nuclia/nuclia-1.1.4.tar.gz/nuclia-1.1.4/nuclia/sdk/nua.py
@@ -0,0 +1,14 @@
+from nuclia import BASE
+from nuclia.sdk.predict import NucliaPredict
+from nuclia.sdk.process import NucliaProcess
+from nuclia.sdk.train import NucliaTrain
+
+LIST_KBS = BASE + "/api/v1/account/{account}/kbs"
+ADD_KB = BASE + "/api/v1/account/{account}/kbs"
+
+
+class NucliaNUA:
+    def __init__(self):
+        self.predict = NucliaPredict()
+        self.process = NucliaProcess()
+        self.train = NucliaTrain()
