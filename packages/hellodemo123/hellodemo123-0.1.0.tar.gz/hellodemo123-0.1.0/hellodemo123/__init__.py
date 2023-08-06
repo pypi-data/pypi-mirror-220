a42f374ccf37d9cc9944337b90a2303c4b907183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:08 +0000

    Add package hellodemo123/0.1.0/hellodemo123-0.1.0.tar.gz

---
+++ packages/hellodemo123/hellodemo123-0.1.0.tar.gz/hellodemo123-0.1.0/hellodemo123/__init__.py
@@ -0,0 +1,8 @@
+# !/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""=====================================
+@author : kf.zhang
+@time   : 2023/7/21 9:53
+@contact: devinzhang1994@163.com
+@desc   : 
+====================================="""
