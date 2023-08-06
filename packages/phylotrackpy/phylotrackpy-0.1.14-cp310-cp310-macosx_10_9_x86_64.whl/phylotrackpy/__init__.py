1ad1c95175fb0de595625ff81928f7f6c810f26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:22 +0000

    Add package phylotrackpy/0.1.14/phylotrackpy-0.1.14-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/phylotrackpy/phylotrackpy-0.1.14-cp310-cp310-macosx_10_9_x86_64.whl/phylotrackpy/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for phylotrackpy."""
+
+__author__ = """Emily Dolson"""
+__email__ = 'emilydolson@gmail.com'
+__version__ = '0.1.14'
+
+import phylotrackpy.systematics
+
+__all__ = [
+    "systematics",
+]
