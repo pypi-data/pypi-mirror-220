7ea992952f5dfa0778309bb2d7a1c7344f025f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:39 +0000

    Add package FlyDB/0.0.3/FlyDB-0.0.3.tar.gz

---
+++ packages/FlyDB/FlyDB-0.0.3.tar.gz/FlyDB-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FlyDB',
+    version='0.0.3',
+    packages=find_packages(),
+    author='GodQI',
+    author_email='bsgodqi@163.com',
+    description='FlyDB python version of the sdk',
+    url='https://github.com/ByteStorage/FlyDB-SDK-Python',
+    install_requires=[
+        'grpcio','protobuf',
+    ],
+)
