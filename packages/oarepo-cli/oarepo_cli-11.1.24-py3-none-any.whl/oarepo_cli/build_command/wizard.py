2454ab3550137fc43fc2555855de1e326b84d81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:30 +0000

    Add package oarepo-cli/11.1.24/oarepo_cli-11.1.24-py3-none-any.whl

---
+++ packages/oarepo-cli/oarepo_cli-11.1.24-py3-none-any.whl/oarepo_cli/build_command/wizard.py
@@ -0,0 +1,19 @@
+from oarepo_cli.develop.steps.check_site import CheckSiteStep
+from oarepo_cli.develop.steps.check_ui import CheckUIStep
+from oarepo_cli.develop.steps.check_venv import CheckVirtualenvStep
+from oarepo_cli.site.site_support import SiteSupport
+from oarepo_cli.wizard import Wizard
+from oarepo_cli.wizard.docker import DockerRunner
+
+
+class BuildWizard(Wizard):
+    def __init__(
+        self, runner: DockerRunner, site_support: SiteSupport, production: bool
+    ):
+        super().__init__(
+            *runner.wrap_docker_steps(
+                CheckVirtualenvStep(clean=True),
+                CheckSiteStep(clean=True),
+                CheckUIStep(production=production),
+            ),
+        )
