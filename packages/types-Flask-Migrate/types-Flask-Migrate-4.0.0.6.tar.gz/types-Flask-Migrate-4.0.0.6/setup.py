4dd1f7a1f05e1ef4eb649128d7d157a000367ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:08 +0000

    Add package types-Flask-Migrate/4.0.0.6/types-Flask-Migrate-4.0.0.6.tar.gz

---
+++ packages/types-Flask-Migrate/types-Flask-Migrate-4.0.0.6.tar.gz/types-Flask-Migrate-4.0.0.6/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+name = "types-Flask-Migrate"
+description = "Typing stubs for Flask-Migrate"
+long_description = '''
+## Typing stubs for Flask-Migrate
+
+This is a PEP 561 type stub package for the `Flask-Migrate` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`Flask-Migrate`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/Flask-Migrate. All fixes for
+types and metadata should be contributed there.
+
+This stub package is marked as [partial](https://peps.python.org/pep-0561/#partial-stub-packages).
+If you find that annotations are missing, feel free to contribute and help complete them.
+
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `afe18e95a9592434e93b648de5194cfe54443f84` and was tested
+with mypy 1.4.1, pyright 1.1.318, and
+pytype 2023.6.16.
+'''.lstrip()
+
+setup(name=name,
+      version="4.0.0.6",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/Flask-Migrate.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=['Flask>=2.0.0', 'Flask-SQLAlchemy>=3.0.1'],
+      packages=['flask_migrate-stubs'],
+      package_data={'flask_migrate-stubs': ['__init__.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
