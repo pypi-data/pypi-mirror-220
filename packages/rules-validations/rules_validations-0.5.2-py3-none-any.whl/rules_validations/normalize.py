df185c142d2348a2058e319f2e955ba18c4881c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:01 +0000

    Add package rules-validations/0.5.2/rules_validations-0.5.2-py3-none-any.whl

---
+++ packages/rules-validations/rules_validations-0.5.2-py3-none-any.whl/rules_validations/normalize.py
@@ -0,0 +1,20 @@
+def normalize_value(value) -> str:
+    return value \
+        .lower() \
+        .replace('á', 'a').replace('ä', 'a') \
+        .replace('é', 'e').replace('ë', 'e') \
+        .replace('í', 'i').replace('ï', 'i') \
+        .replace('ó', 'o').replace('ö', 'o') \
+        .replace('ú', 'u').replace('ü', 'u') \
+        .replace('ñ', 'n') \
+        .replace('(', '_') \
+        .replace(')', '_') \
+        .replace('/', '_') \
+        .replace('\\', '_') \
+        .replace('+', 'p') \
+        .replace('.', '') \
+        .replace(',', '_') \
+        .replace(':', '') \
+        .replace(';', '') \
+        .replace('-', '_') \
+        .replace(' ', '_')
