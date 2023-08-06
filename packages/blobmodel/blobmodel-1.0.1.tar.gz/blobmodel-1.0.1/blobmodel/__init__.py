561cccde5d4b0e35f8a7569fc464d9c34d070195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:54 +0000

    Add package blobmodel/1.0.1/blobmodel-1.0.1.tar.gz

---
+++ packages/blobmodel/blobmodel-1.0.1.tar.gz/blobmodel-1.0.1/blobmodel/__init__.py
@@ -0,0 +1,6 @@
+from .model import Model
+from .blobs import Blob
+from .plotting import show_model
+from .stochasticality import BlobFactory, DefaultBlobFactory
+from .geometry import Geometry
+from .blob_shape import AbstractBlobShape, BlobShapeImpl
