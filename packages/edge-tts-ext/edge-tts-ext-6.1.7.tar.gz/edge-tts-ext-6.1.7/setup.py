956d70b6cafae26c6dc318659d7585d453d64b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:58 +0000

    Add package edge-tts-ext/6.1.7/edge-tts-ext-6.1.7.tar.gz

---
+++ packages/edge-tts-ext/edge-tts-ext-6.1.7.tar.gz/edge-tts-ext-6.1.7/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="edge-tts-ext",
+    install_requires=[
+        "aiohttp>=3.8.0",
+    ],
+)
