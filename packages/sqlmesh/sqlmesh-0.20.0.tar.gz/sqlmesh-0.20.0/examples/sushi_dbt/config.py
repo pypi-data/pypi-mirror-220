730991cbda5b5c82dc20612634067645726ff86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:14 +0000

    Add package sqlmesh/0.20.0/sqlmesh-0.20.0.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.20.0.tar.gz/sqlmesh-0.20.0/examples/sushi_dbt/config.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from sqlmesh.core.config import AirflowSchedulerConfig
+from sqlmesh.dbt.loader import sqlmesh_config
+
+config = sqlmesh_config(Path(__file__).parent, default_sql_dialect="duckdb")
+
+
+test_config = config
+
+
+airflow_config = sqlmesh_config(
+    Path(__file__).parent,
+    default_scheduler=AirflowSchedulerConfig(),
+    default_sql_dialect="duckdb",
+)
