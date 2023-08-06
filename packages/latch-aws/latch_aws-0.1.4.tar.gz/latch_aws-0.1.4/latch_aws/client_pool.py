dbc35b464010c95de38e9f0d58ef8adf51840790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:35 +0000

    Add package latch-aws/0.1.4/latch_aws-0.1.4.tar.gz

---
+++ packages/latch-aws/latch_aws-0.1.4.tar.gz/latch_aws-0.1.4/latch_aws/client_pool.py
@@ -0,0 +1,38 @@
+from asyncio.locks import BoundedSemaphore
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
+
+from types_aiobotocore_s3.client import S3Client
+from .aws import sess
+from opentelemetry.trace import get_tracer
+
+
+tracer = get_tracer(__name__)
+
+class AsyncS3ClientPool:
+    """simple pool for async s3 resources"""
+
+    async def open(self, n: int):
+        self.free_clients: list[S3Client] = []
+        for _ in range(n):
+            client = await sess.create_client("s3").__aenter__()
+            self.free_clients.append(client)
+        self.client_sema = BoundedSemaphore(n)
+
+    async def close(self):
+        for client in self.free_clients:
+            await client.__aexit__(None, None, None)
+        self.free_clients = []
+
+    @asynccontextmanager
+    async def s3_client(self) -> AsyncGenerator[S3Client, None]:
+        async with self.client_sema:
+            client = self.free_clients.pop()
+
+            try:
+                yield client
+            finally:
+                self.free_clients.append(client)
+
+
+s3_pool = AsyncS3ClientPool()
