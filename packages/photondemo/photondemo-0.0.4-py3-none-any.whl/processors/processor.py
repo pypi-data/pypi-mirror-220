4896f7a9892b7c1e6bb630d5123c517beecfcd49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:59 +0000

    Add package photondemo/0.0.4/photondemo-0.0.4-py3-none-any.whl

---
+++ packages/photondemo/photondemo-0.0.4-py3-none-any.whl/processors/processor.py
@@ -0,0 +1,34 @@
+
+import json
+
+
+class PhotonProcessor:
+
+    def __init__(self, provider: str = None, response: dict = None, *args, **kwargs) -> None:
+        self.provider = provider
+        self.args = args
+        self.kwargs = kwargs
+        self.response = response
+        self.providers = ["openai", "anthropic", "replicate"] 
+
+    def process_openai_response(self, *args, **kwargs) -> None:
+        result = self.response.choices[0].text
+        return {
+            "result": result[:200],
+            "provider": "openai",
+            "prompt": kwargs["prompt"],
+            "model": kwargs["model"],
+            "tokens": self.response.usage.total_tokens,
+            "response": json.dumps(self.response)
+        }
+
+    def process_provider(self, *args, **kwargs) -> None:
+        # Check if provider is valid
+        if self.provider not in self.providers:
+            raise ValueError(f"Provider must be one of {self.providers}")
+        
+        if self.provider == "openai":
+            return self.process_openai_response(*args, **kwargs)
+            # Do something
+
+        return None
