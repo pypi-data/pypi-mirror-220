73d1d3ae75efaca2ae452e101c5069a088917766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:58 +0000

    Add package discohooker/0.0.0b6/discohooker-0.0.0b6-py3-none-any.whl

---
+++ packages/discohooker/discohooker-0.0.0b6-py3-none-any.whl/discohooker/errors.py
@@ -0,0 +1,17 @@
+class Errors:
+
+    
+    class WebhookURLError(Exception):
+        f"""{Exception}"""
+
+
+    class WebhookNotFound(Exception):
+        f"""{Exception}"""
+
+
+    class URLError(Exception):
+        f"""{Exception}"""
+
+
+    class MessageNotFound(Exception):
+        f"""{Exception}"""
