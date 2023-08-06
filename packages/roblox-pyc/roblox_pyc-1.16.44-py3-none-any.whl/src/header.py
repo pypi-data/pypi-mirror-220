9110fa25958106c6f6a6c153a30b464ccc3cf979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:21 +0000

    Add package roblox-pyc/1.16.44/roblox_pyc-1.16.44-py3-none-any.whl

---
+++ packages/roblox-pyc/roblox_pyc-1.16.44-py3-none-any.whl/src/header.py
@@ -0,0 +1,35 @@
+headertemplate = """--// Compiled using roblox-pyc \\--
+		
+		
+------------------------------------ BUILT IN -------------------------------
+local py, import, builtin = unpack(require(game.ReplicatedStorage["roblox.pyc"])(script).py)
+
+{}
+-----------------------------------------------------------------------------
+"""
+lunarheadertemplate = """--// Compiled using roblox-pyc \\--
+		
+		
+------------------------------------ BUILT IN -------------------------------
+local import, builtin = unpack(require(game.ReplicatedStorage["roblox.pyc"])(script).lunar)
+
+{}
+-----------------------------------------------------------------------------
+"""
+
+
+def header(functions):
+    code = ""
+    for i in functions:
+        code += "local "+i+" = builtin."+i+"\n"
+    
+    #print(code)
+    return headertemplate.format(code)
+
+def lunarheader(functions):
+    code = ""
+    for i in functions:
+        code += "local "+i+" = builtin."+i+"\n"
+    
+    #print(code)
+    return lunarheadertemplate.format(code)
