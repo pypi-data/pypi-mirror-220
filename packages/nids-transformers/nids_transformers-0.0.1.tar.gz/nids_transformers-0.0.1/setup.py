b2ba1cc66fa8621e80e7dc408f1c1ca86d2dc8f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:46 +0000

    Add package nids-transformers/0.0.1/nids_transformers-0.0.1.tar.gz

---
+++ packages/nids-transformers/nids_transformers-0.0.1.tar.gz/nids_transformers-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='nids_transformers',
+    version='0.0.1',
+    description="Tag Generation and Text Generation for Network Packets using Transformers",
+    keywords="NIDS Transformers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    author="Pahalavan R D",
+    author_email='rdpahalavan24@gmail.com',
+    packages=['nids_transformers'],
+    python_requires='>=3.7.0',
+    url='https://github.com/rdpahalavan/BERTSimilarWords',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements
+)
