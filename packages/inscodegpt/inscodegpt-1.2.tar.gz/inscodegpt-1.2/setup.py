3bb2b8e875aaae61075c955fc8c620e6fe3f9e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:27 +0000

    Add package inscodegpt/1.2/inscodegpt-1.2.tar.gz

---
+++ packages/inscodegpt/inscodegpt-1.2.tar.gz/inscodegpt-1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inscodegpt', 
+    version='1.2',
+    author='Yanhui',
+    author_email='yanhui@csdn.net',
+    description='Inscode GPT API python package',
+    packages=find_packages(),
+    install_requires=['requests'], 
+)
