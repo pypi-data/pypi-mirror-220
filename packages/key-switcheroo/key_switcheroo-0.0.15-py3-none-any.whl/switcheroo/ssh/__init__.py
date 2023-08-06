09310b8f239b4a19138bf39d79310a5df8e79124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:53 +0000

    Add package key-switcheroo/0.0.15/key_switcheroo-0.0.15-py3-none-any.whl

---
+++ packages/key-switcheroo/key_switcheroo-0.0.15-py3-none-any.whl/switcheroo/ssh/__init__.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class AuthorizedKeysCmndProvider(ABC):
+    @property
+    @abstractmethod
+    def command(self) -> str:
+        "Provides an AuthorizedKeysCommand for the sshd to use"
+
+
+# pylint: disable=too-few-public-methods
+class MetricConstants:
+    NAME_SPACE = "Key Switcheroo"
+    COUNTER_METRIC_NAME = "Key Count"
+    TIMING_METRIC_NAME = "Time to Publish Keys"
