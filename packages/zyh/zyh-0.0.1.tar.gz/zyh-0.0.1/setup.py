062a24ec672b751cdb5aaae92691dea0bf5ce00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:40 +0000

    Add package zyh/0.0.1/zyh-0.0.1.tar.gz

---
+++ packages/zyh/zyh-0.0.1.tar.gz/zyh-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Easily show off amazing scripys by zyh'
+
+setup(
+    name="zyh",
+    version=VERSION,
+    author="zhangyuhan",
+    author_email="yhanzh0608@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open('README.md',encoding="UTF8").read(),
+    packages=find_packages(),
+    install_requires=['transformers', 'datasets', 'evaluate', 'peft', 'accelerate', 'gradio', 'optimum',
+                      'sentencepiece', 'scikit-learn', 'pandas', 'matplotlib', 'tensorboard', 'rouge', 'nlkt',
+                      'jupyterlab'],
+    keywords=['python', 'nlp', 'show off'],
+
+    entry_points={
+    'console_scripts': [
+        'zyh = zyh.main:main'
+    ]
+    },
+
+    license="MIT",
+    url="https://github.com/amanyara/zyh",
+    scripts=['zyh/setup_test.py'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
