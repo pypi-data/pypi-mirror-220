d108de13b6bd2c74212332653e270817b0b01e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:21 +0000

    Add package dataset.sh/0.0.1/dataset.sh-0.0.1.tar.gz

---
+++ packages/dataset.sh/dataset.sh-0.0.1.tar.gz/dataset.sh-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='dataset.sh',
+    version='0.0.1',
+    description='',
+    author='Hao Wu',
+    author_email='haowu@dataset.sh',
+    url='https://dataset.sh',
+    packages=['dataset_sh'],
+)
