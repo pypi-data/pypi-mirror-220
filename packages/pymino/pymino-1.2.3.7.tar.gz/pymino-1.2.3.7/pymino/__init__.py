868da65d02e4c23d61b85bff66287f7ce5d2689e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:56 +0000

    Add package pymino/1.2.3.7/pymino-1.2.3.7.tar.gz

---
+++ packages/pymino/pymino-1.2.3.7.tar.gz/pymino-1.2.3.7/pymino/__init__.py
@@ -0,0 +1,21 @@
+__title__ = 'pymino'
+__author__ = 'cynical'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2023 Cynical'
+__version__ = '1.2.3.7'
+__description__ = 'A Python wrapper for the aminoapps.com API'
+
+from .bot import Bot as Bot
+from .async_bot import AsyncBot as AsyncBot
+from .client import Client as Client
+from .async_client import AsyncClient as AsyncClient
+
+from requests import get
+from colorama import Fore, Style
+
+print("Join the pymino discord server: https://discord.gg/RuRzyya55Z")
+
+latestVersion = get("https://pypi.org/pypi/pymino/json").json()["info"]["version"]
+
+if __version__ != latestVersion:
+    print(f"{Fore.RED}WARNING:{Style.RESET_ALL} You are using an outdated version of pymino ({__version__}). The latest version is {latestVersion}.")
---
+++ packages/pymino/pymino-1.2.3.7.tar.gz/pymino-1.2.3.7/pymino/ext/utilities/generate.py
@@ -0,0 +1,37 @@
+from hmac import new
+from hashlib import sha1
+from base64 import b64encode
+from secrets import token_hex
+
+def device_id() -> str:
+    """
+    `generate_device_id` Generates a device ID based on a specific string.
+
+    `**Returns**`
+    - `str` - Returns a device ID as a string.
+    """
+    encoded_data = sha1(str(token_hex(20)).encode('utf-8')).hexdigest()
+
+    digest = new(
+        b"\xe70\x9e\xcc\tS\xc6\xfa`\x00['e\xf9\x9d\xbb\xc9e\xc8\xe9",
+        b"\x19" + bytes.fromhex(encoded_data),
+        sha1).hexdigest()
+
+    return f"19{encoded_data}{digest}".upper()
+
+def generate_signature(data: str) -> str:
+    """
+    `generate_signature` Generates a signature based on a specific string.
+    
+    `**Parameters**`
+    - `data` - Data to generate a signature from
+    `**Returns**`
+    - `str` - Returns a signature as a string.
+    """
+    signature = [ 0x19 ]
+    
+    signature.extend(new(
+        b'\xdf\xa5\xed\x19-\xdan\x88\xa1/\xe1!0\xdcb\x06\xb1%\x1eD',
+        str(data).encode("utf-8"), sha1).digest())
+
+    return b64encode(bytes(signature)).decode("utf-8")
