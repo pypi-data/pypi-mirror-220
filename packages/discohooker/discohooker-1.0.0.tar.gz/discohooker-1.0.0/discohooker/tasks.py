f5adabc1747b5ec3d2b62b0f41c5a48dcadb238d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:43 +0000

    Add package discohooker/1.0.0/discohooker-1.0.0.tar.gz

---
+++ packages/discohooker/discohooker-1.0.0.tar.gz/discohooker-1.0.0/discohooker/tasks.py
@@ -0,0 +1,36 @@
+from .errors import Errors
+from datetime import datetime
+import asyncio
+import pytz
+
+
+class Tasks:
+
+
+    def is_day(date: str, timezone: str=None):
+        def decorator(fun):
+            def wait():
+                running=True
+                while (running == True):
+                    if timezone == None:
+                        now=datetime.now()
+                    elif timezone not in pytz.all_timezones:
+                       raise Errors.TimeZoneNotFound("Cannnot find this timezone. Please make sure the time is correct!")
+                    else:
+                        now=datetime.now(pytz.timezone(timezone))
+                    if f"{now.date}-{now.month}-{now.year}" == date:
+                        running=False
+                    else:
+                        loop=asyncio.get_event_loop()
+                        loop.run_until_complete(asyncio.sleep(1))
+                loop=asyncio.get_event_loop()
+                return loop.run_until_complete(fun())
+            return wait()
+        return decorator()
+
+
+    def worker(fun):
+        def work():
+            loop=asyncio.get_event_loop()
+            return loop.run_until_complete(fun())
+        return work()
