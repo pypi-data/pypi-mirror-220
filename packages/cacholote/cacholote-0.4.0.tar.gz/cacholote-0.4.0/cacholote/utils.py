7fef123b893f8447809c2f615478e0825b1e36af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:05 +0000

    Add package cacholote/0.4.0/cacholote-0.4.0.tar.gz

---
+++ packages/cacholote/cacholote-0.4.0.tar.gz/cacholote-0.4.0/cacholote/utils.py
@@ -0,0 +1,133 @@
+"""Utilities."""
+
+# Copyright 2019, B-Open Solutions srl.
+# Copyright 2022, European Union.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.import hashlib
+
+import datetime
+import hashlib
+import io
+import time
+import warnings
+from types import TracebackType
+from typing import Any, Optional, Tuple, Type
+
+import fsspec
+
+from . import config
+
+
+def hexdigestify(text: str) -> str:
+    """Convert text to its hash made of hexadecimal digits."""
+    hash_req = hashlib.sha3_224(text.encode())
+    return hash_req.hexdigest()[:32]
+
+
+def get_cache_files_fs_dirname() -> Tuple[fsspec.AbstractFileSystem, str]:
+    """Return the ``fsspec`` filesystem and directory name where cache files are stored."""
+    fs, _, (path,) = fsspec.get_fs_token_paths(
+        config.get().cache_files_urlpath,
+        storage_options=config.get().cache_files_storage_options,
+    )
+    return (fs, path)
+
+
+def copy_buffered_file(
+    f_in: Any,
+    f_out: fsspec.spec.AbstractBufferedFile,
+    buffer_size: Optional[int] = None,
+) -> None:
+    """Copy file in chunks.
+
+    Parameters
+    ----------
+    f_in, f_out: fsspec.spec.AbstractBufferedFile
+        Source and destination buffered files.
+    buffer_size: int, optional, default=None
+        Maximum size for chunks in bytes.
+        None: use ``io.DEFAULT_BUFFER_SIZE``
+    """
+    if buffer_size is None:
+        buffer_size = io.DEFAULT_BUFFER_SIZE
+    while True:
+        data = f_in.read(buffer_size)
+        if not data:
+            break
+        f_out.write(data if isinstance(data, bytes) else data.encode())
+
+
+class _Locker:
+    def __init__(
+        self,
+        fs: fsspec.AbstractFileSystem,
+        urlpath: str,
+        lock_validity_period: Optional[float] = None,
+    ) -> None:
+        self.fs = fs
+        self.urlpath = urlpath
+        self.lockfile = urlpath + ".lock"
+        self.lock_validity_period = lock_validity_period
+
+    @property
+    def file_exists(self) -> bool:
+        return bool(self.fs.exists(self.urlpath))
+
+    def acquire(self) -> None:
+        self.fs.touch(self.lockfile)
+
+    def release(self) -> None:
+        if self.fs.exists(self.lockfile):
+            self.fs.rm(self.lockfile)
+
+    @property
+    def is_locked(self) -> bool:
+        if not self.fs.exists(self.lockfile):
+            return False
+
+        delta = datetime.datetime.now() - self.fs.modified(self.lockfile)
+        if self.lock_validity_period is None or delta < datetime.timedelta(
+            seconds=self.lock_validity_period
+        ):
+            return True
+
+        return False
+
+    def wait_until_released(self) -> None:
+        warned = False
+        while self.is_locked:
+            if not warned:
+                warnings.warn(
+                    f"can NOT proceed until file is released: {self.lockfile!r}.",
+                    UserWarning,
+                )
+                warned = True
+            time.sleep(1)
+
+    def __enter__(self) -> bool:
+        self.wait_until_released()
+        self.acquire()
+        return self.file_exists
+
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        self.release()
+
+
+def utcnow() -> datetime.datetime:
+    """See https://discuss.python.org/t/deprecating-utcnow-and-utcfromtimestamp/26221."""
+    return datetime.datetime.now(tz=datetime.timezone.utc)
---
+++ packages/cacholote/cacholote-0.4.0.tar.gz/cacholote-0.4.0/tests/test_50_io_encoder.py
@@ -0,0 +1,214 @@
+import hashlib
+import importlib
+import io
+import pathlib
+import threading
+from typing import Any, Dict, Tuple, Union
+
+import fsspec
+import pytest
+import pytest_httpserver
+import structlog
+
+from cacholote import cache, config, decode, encode, extra_encoders, utils
+
+
+@cache.cacheable
+def cached_open(*args: Any, **kwargs: Any) -> fsspec.spec.AbstractBufferedFile:
+    with fsspec.open(*args, **kwargs) as f:
+        return f
+
+
+@pytest.mark.parametrize("io_delete_original", [True, False])
+def test_dictify_io_object(tmpdir: pathlib.Path, io_delete_original: bool) -> None:
+    # Define readonly dir
+    readonly_dir = str(tmpdir / "readonly")
+    fsspec.filesystem("file").mkdir(readonly_dir)
+    config.set(
+        io_delete_original=io_delete_original, cache_files_urlpath_readonly=readonly_dir
+    )
+
+    # Create file
+    tmpfile = tmpdir / "test.txt"
+    fsspec.filesystem("file").pipe_file(tmpfile, b"test")
+    tmp_hash = f"{fsspec.filesystem('file').checksum(tmpfile):x}"
+
+    # Check dict and cached file
+    actual = extra_encoders.dictify_io_object(open(tmpfile, "rb"))
+    href = f"{readonly_dir}/{tmp_hash}.txt"
+    local_path = f"{tmpdir}/cache_files/{tmp_hash}.txt"
+    expected = {
+        "type": "python_call",
+        "callable": "cacholote.extra_encoders:decode_io_object",
+        "args": (
+            {
+                "type": "text/plain",
+                "href": href,
+                "file:checksum": f"{fsspec.filesystem('file').checksum(local_path):x}",
+                "file:size": 4,
+                "file:local_path": local_path,
+            },
+            {},
+        ),
+        "kwargs": {"mode": "rb"},
+    }
+    assert actual == expected
+    assert fsspec.filesystem("file").exists(tmpfile) is not io_delete_original
+
+    # Use href when local_path is missing or corrupted
+    fsspec.filesystem("file").mv(local_path, href)
+    assert decode.loads(encode.dumps(actual)).read() == b"test"
+
+
+@pytest.mark.parametrize("obj", [io.BytesIO(b"test"), io.StringIO("test")])
+def test_dictify_bytes_io_object(
+    tmpdir: pathlib.Path, obj: Union[io.BytesIO, io.StringIO]
+) -> None:
+    actual = extra_encoders.dictify_io_object(obj)["args"]
+    obj_hash = hashlib.md5(f"{hash(obj)}".encode()).hexdigest()
+    local_path = f"{tmpdir}/cache_files/{obj_hash}"
+    type = (
+        "text/plain"
+        if importlib.util.find_spec("magic")
+        else "application/octet-stream"
+    )
+    expected: Tuple[Dict[str, Any], ...] = (
+        {
+            "type": type,
+            "href": local_path,
+            "file:checksum": f"{fsspec.filesystem('file').checksum(local_path):x}",
+            "file:size": 4,
+            "file:local_path": local_path,
+        },
+        {},
+    )
+    assert actual == expected
+    assert open(local_path).read() == "test"
+
+
+@pytest.mark.parametrize("set_cache", ["file", "cads"], indirect=True)
+def test_copy_from_http_to_cache(
+    tmpdir: pathlib.Path,
+    httpserver: pytest_httpserver.HTTPServer,
+    set_cache: str,
+) -> None:
+    # cache-db to check
+    con = config.get().engine.raw_connection()
+    cur = con.cursor()
+
+    # http server
+    httpserver.expect_request("/test").respond_with_data(b"test")
+    url = httpserver.url_for("/test")
+    cached_basename = f"{fsspec.filesystem('http').checksum(url):x}"
+
+    # cache http file
+    fs, dirname = utils.get_cache_files_fs_dirname()
+    for expected_counter in (1, 2):
+        result = cached_open(url)
+
+        # Check hits
+        cur.execute("SELECT counter FROM cache_entries", ())
+        assert cur.fetchall() == [(expected_counter,)]
+
+        # Check result
+        assert result.read() == b"test"
+
+        # Check file in cache
+        assert result.path == f"{dirname}/{cached_basename}"
+        assert fs.exists(result.path)
+
+
+def test_io_corrupted_files(
+    tmpdir: pathlib.Path, httpserver: pytest_httpserver.HTTPServer
+) -> None:
+    # http server
+    httpserver.expect_request("/test").respond_with_data(b"test")
+    url = httpserver.url_for("/test")
+    cached_basename = f"{fsspec.filesystem('http').checksum(url):x}"
+
+    # cache file
+    fs, dirname = utils.get_cache_files_fs_dirname()
+    cached_open(url)
+
+    # Warn if file is corrupted
+    fs.touch(f"{dirname}/{cached_basename}")
+    touched_info = fs.info(f"{dirname}/{cached_basename}")
+    with pytest.warns(UserWarning, match="checksum mismatch"):
+        result = cached_open(url)
+    assert result.read() == b"test"
+    assert fs.info(f"{dirname}/{cached_basename}") != touched_info
+
+    # Warn if file is deleted
+    fs.rm(f"{dirname}/{cached_basename}")
+    with pytest.warns(UserWarning, match="No such file or directory"):
+        result = cached_open(url)
+    assert result.read() == b"test"
+    assert fs.exists(f"{dirname}/{cached_basename}")
+
+
+def test_io_locker_warning(tmpdir: pathlib.Path) -> None:
+    # Create tmpfile
+    tmpfile = tmpdir / "test.txt"
+    fsspec.filesystem("file").touch(tmpfile)
+
+    # Acquire lock
+    fs, dirname = utils.get_cache_files_fs_dirname()
+    file_hash = f"{fsspec.filesystem('file').checksum(tmpfile):x}"
+    lock = f"{dirname}/{file_hash}.txt.lock"
+    fs.touch(lock)
+
+    def release_lock(fs: fsspec.AbstractFileSystem, lock: str) -> None:
+        fs.rm(lock)
+
+    # Threading
+    t1 = threading.Timer(0, cached_open, args=(tmpfile,))
+    t2 = threading.Timer(0.1, release_lock, args=(fs, lock))
+    with pytest.warns(
+        UserWarning, match=f"can NOT proceed until file is released: {lock!r}."
+    ):
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+
+
+@pytest.mark.parametrize("set_cache", ["cads"], indirect=True)
+def test_content_type(tmpdir: pathlib.Path, set_cache: str) -> None:
+    tmpfile = str(tmpdir / "test.grib")
+    fsspec.filesystem("file").touch(tmpfile)
+    fs, _ = utils.get_cache_files_fs_dirname()
+    cached_grib = cached_open(tmpfile)
+    assert fs.info(cached_grib)["ContentType"] == "application/x-grib"
+
+
+@pytest.mark.parametrize("set_cache", ["cads"], indirect=True)
+def test_io_logging(capsys: pytest.CaptureFixture[str], tmpdir: pathlib.Path) -> None:
+    config.set(logger=structlog.get_logger(), io_delete_original=True)
+
+    # Cache file
+    tmpfile = tmpdir / "test.txt"
+    fsspec.filesystem("file").touch(tmpfile)
+    cached_file = cached_open(tmpfile)
+    captured = iter(capsys.readouterr().out.splitlines())
+
+    line = next(captured)
+    assert "start upload" in line
+    assert f"urlpath=s3://{cached_file.path}" in line
+    assert "size=0" in line
+
+    line = next(captured)
+    assert "end upload" in line
+    assert f"urlpath=s3://{cached_file.path}" in line
+    assert "upload_time=" in line
+    assert "size=0" in line
+
+    line = next(captured)
+    assert "start remove" in line
+    assert f"urlpath=file://{tmpfile}" in line
+    assert "size=0" in line
+
+    line = next(captured)
+    assert "end remove" in line
+    assert f"urlpath=file://{tmpfile}" in line
+    assert "remove_time=" in line
+    assert "size=0" in line
