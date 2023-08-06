2a41547348d3b589f81dd5975c3c8c83d4ff96e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:30 +0000

    Add package osintbuddy/0.0.4rc45.post1/osintbuddy-0.0.4rc45.post1-py3-none-any.whl

---
+++ packages/osintbuddy/osintbuddy-0.0.4rc45.post1-py3-none-any.whl/osintbuddy/__init__.py
@@ -0,0 +1,14 @@
+#   -------------------------------------------------------------
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""Python Package Template"""
+from __future__ import annotations
+from osintbuddy.plugins import (
+    OBRegistry as Registry,
+    OBPlugin as Plugin,
+    OBAuthorUse as PluginUse,
+    discover_plugins,
+    transform
+)
+
+__version__ = "0.0.4-rc45-post1"
