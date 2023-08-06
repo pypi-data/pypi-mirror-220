066f2deb5f9748f8bca0d5ddc71ee2f7fd7c8555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:19 +0000

    Add package connex/0.3.3/connex-0.3.3.tar.gz

---
+++ packages/connex/connex-0.3.3.tar.gz/connex-0.3.3/connex/__init__.py
@@ -0,0 +1,11 @@
+from . import nn
+from ._network import NeuralNetwork
+from ._plasticity import (
+    add_connections,
+    add_hidden_neurons,
+    add_input_neurons,
+    add_output_neurons,
+    remove_connections,
+    remove_neurons,
+    set_dropout_p,
+)
---
+++ packages/connex/connex-0.3.3.tar.gz/connex-0.3.3/connex/nn/__init__.py
@@ -0,0 +1,2 @@
+from ._dense_mlp import DenseMLP
+from ._mlp import MLP
