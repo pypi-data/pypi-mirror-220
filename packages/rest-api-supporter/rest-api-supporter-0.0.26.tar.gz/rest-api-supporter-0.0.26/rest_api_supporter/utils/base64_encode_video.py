92d38fc4d74d2d4540728c3ea842e1a05076f9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:01 +0000

    Add package rest-api-supporter/0.0.26/rest-api-supporter-0.0.26.tar.gz

---
+++ packages/rest-api-supporter/rest-api-supporter-0.0.26.tar.gz/rest-api-supporter-0.0.26/rest_api_supporter/utils/base64_encode_video.py
@@ -0,0 +1,13 @@
+import base64
+import numpy as np
+
+def base64_encode_video(video):
+    if isinstance(video, bytes):
+        base64_encoded = base64.b64encode(video)
+        base64_encoded = base64_encoded.decode("utf-8") 
+        return "data:video/mp4;base64,"+base64_encoded
+    elif isinstance(video, np.ndarray):
+        bytes_value = video.tobytes()
+        base64_encoded = base64.b64encode(bytes_value)
+        base64_encoded = base64_encoded.decode("utf-8") 
+        return "data:video/mp4;base64,"+base64_encoded
