3d67a8554581215fd836c302cfee3568eeeaa6c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:24 +0000

    Add package cornflakes/3.3.8/cornflakes-3.3.8-cp39-cp39-manylinux_2_35_x86_64.whl

---
+++ packages/cornflakes/cornflakes-3.3.8-cp39-cp39-manylinux_2_35_x86_64.whl/cornflakes/decorator/click/options/_bg_process.py
@@ -0,0 +1,45 @@
+from inspect import getfile
+import logging
+from os.path import abspath
+import subprocess  # noqa: S404
+import sys
+from typing import Callable
+
+from cornflakes.decorator.click.options._global import global_option
+
+
+@global_option(
+    ["-b", "--background-process"],
+    is_flag=True,
+    help="Run in Background without console logger.",
+)
+def bg_process_option(self: Callable[..., None], background_process: bool, *func_args, **func_kwargs):
+    """Default Option for running in background."""
+    if background_process:
+        stdout_file = f"{self.__name__}.log"
+        stderr_file = f"{self.__name__}_error.log"
+        logging.debug(
+            f"Method {self.__name__} is running in background. "
+            f"See logs at stdout: {stdout_file}, stderr: {stderr_file}."
+        )
+        stdout = open(stdout_file, "w")
+        stderr = open(stderr_file, "w")
+
+        command = (
+            f"import sys; exec(open("
+            f"{abspath(getfile(self))!r}).read());"
+            f"{self.__name__}(*{func_args},**{func_kwargs});"
+        )
+
+        logging.debug(f"Python Command: {command}")
+
+        subprocess.Popen(  # noqa: S603
+            [
+                sys.executable,
+                "-c",
+                command,
+            ],
+            stdout=stdout,
+            stderr=stderr,
+        )
+        sys.exit(0)
