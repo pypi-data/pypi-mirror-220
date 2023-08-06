81ebad7891e32b56c2476e5c72ee9279d9f7a880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:52 +0000

    Add package dash-solana-components/1.0.0/dash_solana_components-1.0.0.tar.gz

---
+++ packages/dash-solana-components/dash_solana_components-1.0.0.tar.gz/dash_solana_components-1.0.0/dash_solana_components/App.py
@@ -0,0 +1,36 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class App(Component):
+    """An App component.
+
+
+Keyword arguments:
+
+- id (string; optional):
+    Unique ID to identify this component in Dash callbacks.
+
+- network (a value equal to: 'devnet', 'mainnet', 'testnet'; required)"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'dash_solana_components'
+    _type = 'App'
+    @_explicitize_args
+    def __init__(self, network=Component.REQUIRED, id=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'network']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'network']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        for k in ['network']:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+
+        super(App, self).__init__(**args)
