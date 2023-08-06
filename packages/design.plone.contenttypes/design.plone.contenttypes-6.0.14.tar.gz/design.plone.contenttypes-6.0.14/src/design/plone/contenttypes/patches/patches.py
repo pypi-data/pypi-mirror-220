ab052053d7eca189495b0d00eff366e72c62cf19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:57 +0000

    Add package design.plone.contenttypes/6.0.14/design.plone.contenttypes-6.0.14.tar.gz

---
+++ packages/design.plone.contenttypes/design.plone.contenttypes-6.0.14.tar.gz/design.plone.contenttypes-6.0.14/src/design/plone/contenttypes/patches/patches.py
@@ -0,0 +1,10 @@
+from collective.taxonomy.vocabulary import Vocabulary
+
+
+def eea_api_taxonomy_taxonomy_call(self, context):
+    if not self.data:
+        return Vocabulary(self.name, {}, {}, {}, 2)
+
+    request = getattr(context, "REQUEST", None)
+    language = self.getCurrentLanguage(request)
+    return self.makeVocabulary(language)
