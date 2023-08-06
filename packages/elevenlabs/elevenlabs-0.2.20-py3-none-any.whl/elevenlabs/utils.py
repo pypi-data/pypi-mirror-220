f7ec62766a4c18f29f25b52ba71b89a1e615c37e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:26 +0000

    Add package elevenlabs/0.2.20/elevenlabs-0.2.20-py3-none-any.whl

---
+++ packages/elevenlabs/elevenlabs-0.2.20-py3-none-any.whl/elevenlabs/utils.py
@@ -0,0 +1,71 @@
+import shutil
+import subprocess
+from typing import Iterator
+
+
+def is_installed(lib_name: str) -> bool:
+    lib = shutil.which(lib_name)
+    if lib is None:
+        return False
+    return True
+
+
+def play(audio: bytes, notebook: bool = False) -> None:
+    if notebook:
+        from IPython.display import Audio, display
+
+        display(Audio(audio, rate=44100, autoplay=True))
+    else:
+        if not is_installed("ffplay"):
+            message = (
+                "ffplay from ffmpeg not found, necessary to play audio. "
+                "On mac you can install it with 'brew install ffmpeg'. "
+                "On linux and windows you can install it from https://ffmpeg.org/"
+            )
+            raise ValueError(message)
+        args = ["ffplay", "-autoexit", "-", "-nodisp"]
+        proc = subprocess.Popen(
+            args=args,
+            stdout=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        out, err = proc.communicate(input=audio)
+        proc.poll()
+
+
+def save(audio: bytes, filename: str) -> None:
+    with open(filename, "wb") as f:
+        f.write(audio)
+
+
+def stream(audio_stream: Iterator[bytes]) -> bytes:
+    if not is_installed("mpv"):
+        message = (
+            "mpv not found, necessary to stream audio. "
+            "On mac you can install it with 'brew install mpv'. "
+            "On linux and windows you can install it from https://mpv.io/"
+        )
+        raise ValueError(message)
+
+    mpv_command = ["mpv", "--no-cache", "--no-terminal", "--", "fd://0"]
+    mpv_process = subprocess.Popen(
+        mpv_command,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+
+    audio = b""
+
+    for chunk in audio_stream:
+        if chunk is not None:
+            mpv_process.stdin.write(chunk)  # type: ignore
+            mpv_process.stdin.flush()  # type: ignore
+            audio += chunk
+
+    if mpv_process.stdin:
+        mpv_process.stdin.close()
+    mpv_process.wait()
+
+    return audio
