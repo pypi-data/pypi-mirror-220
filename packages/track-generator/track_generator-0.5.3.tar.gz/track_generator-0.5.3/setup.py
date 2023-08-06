0bd868b186871c295a49e32b90216288ffd74f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:31 +0000

    Add package track-generator/0.5.3/track_generator-0.5.3.tar.gz

---
+++ packages/track-generator/track_generator-0.5.3.tar.gz/track_generator-0.5.3/setup.py
@@ -0,0 +1,37 @@
+# Copyright (C) 2022 twyleg
+import os
+import versioneer
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="track_generator",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Torsten Wylegala",
+    author_email="mail@twyleg.de",
+    description=("Track generator"),
+    license="GPL 3.0",
+    keywords="svg model vehicles track",
+    url="https://github.com/twyleg/track_generator",
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=read("README.md"),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "pytransform3d~=3.2.0",
+        "drawsvg~=2.2.0",
+        "watchdog~=3.0.0",
+        "pyside6==6.4.0.1",
+        "xmlschema~=2.3.1",
+    ],
+    entry_points={
+        "console_scripts": [
+            "track_generator = track_generator.starter:start",
+        ]
+    },
+)
