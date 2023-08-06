ccc7813ead3073ec0992b5c5193f12d4f73ca0c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:03 +0000

    Add package GAO/0.1.0/GAO-0.1.0.tar.gz

---
+++ packages/GAO/GAO-0.1.0.tar.gz/GAO-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GAO',
+    version='0.1.0',
+    description='Gis Application Objects',
+    author='Gao Zhiheng',
+    author_email='11279470@qq.com',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies required by your package here
+    ],
+)
+
