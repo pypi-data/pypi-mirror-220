1c65fb5bfaf2f16bbb9ce856712b21b510845031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:17 +0000

    Add package chatllm/2023.7.19.10.33.13/chatllm-2023.7.19.10.33.13.tar.gz

---
+++ packages/chatllm/chatllm-2023.7.19.10.33.13.tar.gz/chatllm-2023.7.19.10.33.13/chatllm/langchain/utils/common.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : common
+# @Time         : 2023/7/4 08:56
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+from meutils.pipe import *
+from langchain import LLMChain, OpenAI, PromptTemplate
+from langchain.document_loaders.base import Document
+
+from langchain.callbacks import AsyncIteratorCallbackHandler
+from langchain.chains.base import Chain
+
+# prompt_template = "Tell me a {adjective} joke"
+# prompt = PromptTemplate(
+#     input_variables=["adjective"], template=prompt_template
+# )
+
+template2prompt = PromptTemplate.from_template
+
+
+def docs2dataframe(docs: List[Document]) -> pd.DataFrame:
+    return pd.DataFrame(map(lambda doc: {**doc.metadata, **{'page_content': doc.page_content}}, docs))
+
+
+def dataframe2docs(df: pd.DataFrame) -> List[Document]:
+    df = df.copy()
+    docs = []
+    for page_content, metadata in zip(df.pop('page_content'), df.to_dict(orient='records')):
+        docs.append(Document(page_content=page_content, metadata=metadata))
+    return docs
