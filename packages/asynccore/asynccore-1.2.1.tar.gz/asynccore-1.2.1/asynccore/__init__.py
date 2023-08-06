3b07287635669d7c83bb774bd75eba01b254545a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:08 +0000

    Add package asynccore/1.2.1/asynccore-1.2.1.tar.gz

---
+++ packages/asynccore/asynccore-1.2.1.tar.gz/asynccore-1.2.1/asynccore/__init__.py
@@ -0,0 +1,27 @@
+from .client import Client
+from .enums import (
+    ChannelType,
+    Permissions,
+    ActivityType,
+    ActivityPlatform,
+    ActivityStatus,
+)
+from .typings import RGB_COLOR, ClientResponse
+from .permissionbuilder import PermissionBuilder
+from .activity import ActivityBuilder
+from .user import UserClient
+
+
+__all__: tuple[str, ...] = (
+    "Client",
+    "ChannelType",
+    "ClientResponse",
+    "RGB_COLOR",
+    "Permissions",
+    "PermissionBuilder",
+    "UserClient",
+    "ActivityType",
+    "ActivityStatus",
+    "ActivityPlatform",
+    "ActivityBuilder",
+)
---
+++ packages/asynccore/asynccore-1.2.1.tar.gz/asynccore-1.2.1/asynccore/gateway/__init__.py
@@ -0,0 +1,7 @@
+"""
+Module responsible for communication with the gateway
+"""
+
+from .gateway import GatewayConnection, GatewayResponse, Gateway
+
+__all__: tuple[str, str, str] = ("GatewayConnection", "GatewayResponse", "Gateway")
