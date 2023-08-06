d5c736debb33ba177283c71f61d67996f9403016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:20 +0000

    Add package phoenixtools/0.1.2/phoenixtools-0.1.2.tar.gz

---
+++ packages/phoenixtools/phoenixtools-0.1.2.tar.gz/phoenixtools-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION="0.1.2"
+DESCRIPTION = 'Easy hacking tools'
+LONG_DESCRIPTION = 'Easy hacking tools used for educational purposes. Update notes: Added writemodes update. Added minor bug fixes.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="phoenixtools", 
+        version=VERSION,
+        author="Jack B.",
+        author_email="<BurrJ22@outlook.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
