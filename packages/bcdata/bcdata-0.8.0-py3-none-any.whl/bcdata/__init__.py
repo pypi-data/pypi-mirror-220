b31e63de35e160f2acc0bb9e226e44fbbf1f7c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:20 +0000

    Add package bcdata/0.8.0/bcdata-0.8.0-py3-none-any.whl

---
+++ packages/bcdata/bcdata-0.8.0-py3-none-any.whl/bcdata/__init__.py
@@ -0,0 +1,14 @@
+from .bcdc import get_table_name
+from .bcdc import get_table_definition
+from .wfs import define_requests
+from .wfs import get_count
+from .wfs import get_data
+from .wfs import get_features
+from .wfs import get_spatial_types
+from .wfs import list_tables
+from .wfs import validate_name
+from .bc2pg import bc2pg
+from .wcs import get_dem
+
+
+__version__ = "0.8.0"
