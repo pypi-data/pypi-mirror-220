90d0e4196ee96231b1dbac1fa05e6ef565276248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:24 +0000

    Add package test-setup-test/1.0.25/test_setup_test-1.0.25.tar.gz

---
+++ packages/test-setup-test/test_setup_test-1.0.25.tar.gz/test_setup_test-1.0.25/setup.py
@@ -0,0 +1,31 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='test_setup_test',
+    version='1.0.25',
+    author='Divy TEST',
+    author_email='divy.jain@bombaysoftwares.com',
+    description='utils_bs_test',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    maintainers=['Maintainer 1', 'Maintainer 2'],
+
+    license='MIT',
+    project_urls={
+        "Bug Tracker": "https://github.com/utils-bs-test",
+        "Source Code": "https://github.com/abc",
+        "Documentation": "https://github.com/abc"
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development",
+        "Topic :: Scientific/Engineering",
+        "Operating System :: OS Independent",
+    ]
+)
