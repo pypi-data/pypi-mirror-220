54244e185b3d4d397474f16453f54560b37a1dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:59 +0000

    Add package argus-rico/0.0.6/argus_rico-0.0.6.tar.gz

---
+++ packages/argus-rico/argus_rico-0.0.6.tar.gz/argus_rico-0.0.6/src/argus_rico/cli.py
@@ -0,0 +1,79 @@
+import sys
+from typing import Sequence
+
+import click
+
+
+@click.group()
+def main() -> None:
+    """Console scripts for rico."""
+    pass
+
+
+class Config(object):
+    """Contains global options used for all pipeline actions."""
+
+    def __init__(self) -> None:
+        self.verbose = False
+        self.ncpus = 72
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
+
+
+@click.command("slack", short_help="Start Rico Slack server.")
+def slack() -> None:
+    """Starts the Rico Slack server."""
+    from .slack import slack_app_starter
+
+    slack_app_starter()
+
+
+@click.command("watch", short_help="Start Rico monitor for EFTE catalogs.")
+def watch() -> None:
+    """Starts the Rico Slack server."""
+    from .slack import slack_app_starter
+
+    slack_app_starter()
+
+
+@click.command("index_images", short_help="Index EVR images directly into MongoDB.")
+@click.argument("directories", nargs=-1, type=click.Path(exists=True))
+@pass_config
+def index_images(cli_config: Config, directories: Sequence[str]) -> None:
+    """Indexes EVR images from the specified directories.
+
+    Args:
+        cli_config: Config object containing global options.
+        directories: Iterable of directories containing EVR images.
+
+    """
+    import multiprocessing as mp
+
+    from . import EVRImageLoader
+
+    image_loader = EVRImageLoader(create_client=False)
+
+    pool = mp.Pool(cli_config.ncpus)
+
+    directories = sorted(directories)
+    n_directories = len(directories)
+    out = []
+    with click.progressbar(
+        pool.imap_unordered(image_loader.load_directory, directories),
+        label="Loading: ",
+        length=n_directories,
+    ) as pbar:
+        for _ in pbar:
+            out.append(_)
+
+    pool.close()
+    pool.join()
+
+
+main.add_command(slack)
+main.add_command(index_images)
