6dfb59d8600e410855e799f3174f4bf55d9ea1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:20 +0000

    Add package cornflakes/3.3.8/cornflakes-3.3.8-cp310-cp310-manylinux_2_35_x86_64.whl

---
+++ packages/cornflakes/cornflakes-3.3.8-cp310-cp310-manylinux_2_35_x86_64.whl/cornflakes/common/_patch_module.py
@@ -0,0 +1,79 @@
+from importlib import import_module
+from inspect import getmembers, ismodule
+import logging
+import os
+from pkgutil import iter_modules
+
+__pached_modules = []
+
+
+def isclassmethod(method):
+    """Check if a classmethod."""
+    bound_to = getattr(method, "__self__", None)
+    if not isinstance(bound_to, type):
+        # must be bound to a class
+        return False
+    name = method.__name__
+    for cls in bound_to.__mro__:
+        descriptor = vars(cls).get(name)
+        if descriptor is not None:
+            return isinstance(descriptor, classmethod)
+    return False
+
+
+patch_modules: list = []
+
+
+def _patch_module(
+    m,
+):
+    """Method to overwrite module variables in a generic way.
+
+    1. Overwrite names from submodules declared in __all__ to parent module.
+    2. Overwrite doc_string and adds auto summary with objects defined in __all__.
+    """
+    for obj in [getattr(m, x, None) for x in getattr(m, "__all__", [key for key, _ in getmembers(m)])]:
+        if not obj:
+            continue
+        if ismodule(obj):
+            _patch_module(obj)
+            for sub_m in iter_modules(getattr(obj, "__path__", [])):
+                if f"{obj.__name__}.{sub_m.name}" in patch_modules:
+                    continue
+                patch_modules.append(f"{obj.__name__}.{sub_m.name}")
+                _patch_module(import_module(f"{obj.__name__}.{sub_m.name}"))
+            return
+        if not isclassmethod(obj):
+            try:
+                obj.__module__ = getattr(m, "__name__", "")
+            except Exception as e:
+                logging.debug(e)
+
+    m.__doc__ = f"""{getattr(m, "__doc__", f"{getattr(m, '__name__', '')} module.")}
+
+.. currentmodule:: {getattr(m, '__name__', '')}
+
+.. autosummary::
+   :toctree: _generate
+
+    {'''
+    '''.join(getattr(m, "__all__", [key for key, _ in getmembers(m)]))}
+"""
+
+
+def patch_module(module: str):
+    """Method to overwrite module with all submodules in a generic way.
+
+    1. Overwrite names from submodules declared in __all__ to parent module.
+    2. Overwrite doc_string and adds auto summary with objects defined in __all__.
+    """
+    if os.environ.get("CORNFLAKES_GENERATING_CONFIG_MODULE", "") == "True" or module in __pached_modules:
+        return
+    __pached_modules.append(module)
+    m = import_module(module)
+    for sub_m in iter_modules(getattr(m, "__path__", [])):
+        if f"{m.__name__}.{sub_m.name}" in patch_modules:
+            continue
+        patch_modules.append(f"{m.__name__}.{sub_m.name}")
+        _patch_module(import_module(f"{m.__name__}.{sub_m.name}"))
+    _patch_module(m)
---
+++ packages/cornflakes/cornflakes-3.3.8-cp310-cp310-manylinux_2_35_x86_64.whl/cornflakes/decorator/_wrap_kwargs.py
@@ -0,0 +1,176 @@
+from dataclasses import dataclass, field
+from functools import wraps
+from inspect import Parameter, Signature, signature
+from typing import Any, Callable, Dict, List, Optional
+
+from cornflakes.types import HAS_DEFAULT_FACTORY, INSPECT_EMPTY, WITHOUT_DEFAULT
+
+
+def _not_excluded(default):
+    return default != HAS_DEFAULT_FACTORY
+
+
+def _not_empty(x):
+    return x not in [INSPECT_EMPTY, WITHOUT_DEFAULT]
+
+
+def _check_default(default):
+    return _not_empty(default) and _not_excluded(default)
+
+
+def _check_annotation(annotation):
+    return _not_empty(annotation)
+
+
+@dataclass
+class KwargsWrapper:
+    """KwargsWrapper Class."""
+
+    wrapped_sig: Optional[Signature] = field(default=None, init=False)
+    key_names_no_default: List[str] = field(default_factory=list, init=False)
+    key_names: List[str] = field(default_factory=list, init=False)
+    arg_names: List[str] = field(default_factory=list, init=False)
+    kwarg_names: List[str] = field(default_factory=list, init=False)
+    wrapped: Callable[..., object]
+    overwrites: Dict[str, Any] = field(default_factory=dict)
+    key_params: List[Parameter] = field(default_factory=list)
+    key_params_no_default: List[Parameter] = field(default_factory=list)
+    arg_params: List[Parameter] = field(default_factory=list)
+    kwarg_params: List[Parameter] = field(default_factory=list)
+
+    @property
+    def _names(self):
+        return [*self.key_names_no_default, *self.key_names, *self.arg_names, *self.kwarg_names]
+
+    @property
+    def _passed_names(self):
+        return ", ".join(
+            [
+                *[f"{key}" for key in self.key_names_no_default],
+                *[f"{key}={key}" for key in self.key_names],
+                *[f"*{arg}" for arg in self.arg_names],
+                *[f"**{arg}" for arg in self.kwarg_names],
+            ]
+        )
+
+    @property
+    def _params(self):
+        return [*self.key_params_no_default, *self.arg_params, *self.key_params, *self.kwarg_params]
+
+    @property
+    def _params_declaration(self):
+        return ", ".join(
+            [
+                (
+                    f'{str(param).split(":", 1)[0].split("=", 1)[0]}'
+                    f'{f": wrapped_type_{idx} " * _check_annotation(param.annotation)}'
+                    f'{f"=wrapped_default_value_{idx}" * _check_default(param.default)}'
+                )
+                for idx, param in enumerate(self._params)
+            ]
+        )
+
+    @property
+    def _defaults_dict(self):
+        return {
+            f"wrapped_default_value_{idx}": param.default
+            for idx, param in enumerate(self._params)
+            if _check_default(param.default)
+        }
+
+    @property
+    def _annotations_dict(self):
+        return {
+            f"wrapped_type_{idx}": param.annotation
+            for idx, param in enumerate(self._params)
+            if _check_annotation(param.annotation)
+        }
+
+    def _update_params(self, parameters):
+        for name, param in parameters.items():
+            if name not in self._names:
+                if _check_default(self.overwrites.get(param.name, INSPECT_EMPTY)):
+                    self.key_names.append(name)
+                    self.key_params.append(
+                        Parameter(
+                            param.name,
+                            kind=param.kind,
+                            default=self.overwrites.get(name),
+                            annotation=param.annotation,
+                        )
+                    )
+                    continue
+                if _check_default(param.default):
+                    self.key_names.append(name)
+                    self.key_params.append(param)
+                    continue
+                if param.kind == Parameter.VAR_POSITIONAL:
+                    self.arg_names.append(name)
+                    self.arg_params.append(param)
+                    continue
+                if param.kind == Parameter.VAR_KEYWORD:
+                    self.kwarg_names.append(name)
+                    self.kwarg_params.append(param)
+                    continue
+                if _not_excluded(param.default):
+                    self.key_names_no_default.append(name)
+                    self.key_params_no_default.append(param)
+                continue
+
+            is_variadic = Parameter.VAR_POSITIONAL or Parameter.VAR_KEYWORD
+            param_idx = self._names.index(name)
+            # print(f"overwrite {self.params[param_idx].name} at {param_idx} with {name} of {self.names}")
+            if is_variadic:
+                self._params[param_idx] = Parameter(
+                    param.name,
+                    kind=param.kind,
+                    annotation=param.annotation
+                    if _check_annotation(param.annotation)
+                    else self._params[param_idx].annotation,
+                )
+                continue
+            self._params[param_idx] = Parameter(
+                param.name,
+                kind=param.kind,
+                default=(
+                    param.default
+                    if _check_default(param.default)
+                    else
+                    # self.overwrites.get(param.name) if _check_default(self.overwrites.get(param.name, INSPECT_EMPTY)) else
+                    self._params[param_idx].default
+                ),
+                annotation=param.annotation
+                if _check_annotation(param.annotation)
+                else self._params[param_idx].annotation,
+            )
+
+    def __post_init__(self):
+        """Initialize the Class."""
+        self.wrapped_sig: Signature = signature(self.wrapped)
+        self._update_params(self.wrapped_sig.parameters)
+
+    def wrap(self, wrapper: Callable[..., Any]) -> Callable[..., Any]:
+        """Method to return the declaration string."""
+        wrapper_sig: Signature = signature(wrapper)
+        self._update_params(wrapper_sig.parameters)
+        ldict: Dict[str, Any] = {**self._defaults_dict, **self._annotations_dict}
+        wrapper_str = f"""
+def wrap_kwargs({self._params_declaration}):
+    return wrapper({self._passed_names})
+"""
+        exec(  # noqa: S102
+            wrapper_str,
+            locals(),
+            ldict,
+        )
+        return wraps(self.wrapped)(ldict["wrap_kwargs"])
+
+
+def wrap_kwargs(wrapped, **overwrites) -> Callable[..., Any]:
+    """Function Decorator that can update all passed arguments (that can be a variable) of function."""
+    kwargs_wrapper = KwargsWrapper(wrapped=wrapped, overwrites=overwrites)
+
+    def wrapper(func):
+        return kwargs_wrapper.wrap(func)
+
+    return wrapper
