5f0bd51ccab16f9edd9481b7b120f0e711f3bdac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:04 +0000

    Add package quivr/0.4.3/quivr-0.4.3.tar.gz

---
+++ packages/quivr/quivr-0.4.3.tar.gz/quivr-0.4.3/quivr/attributes.py
@@ -0,0 +1,140 @@
+import struct
+from typing import TYPE_CHECKING, Generic, Optional, TypeVar
+
+if TYPE_CHECKING:
+    from .tables import Table
+
+T = TypeVar("T")
+
+
+class Attribute(Generic[T]):
+    """
+    An Attribute is an accessor for scalar data in a table.
+
+    It works through the descriptor protocol.
+
+    All attributes must be serializable into bytes.
+
+    Attributes are stored as metadata on the underlying Arrow table's schema.
+    """
+
+    _type: type[T]
+
+    def __init__(
+        self,
+        default: Optional[T] = None,
+    ):
+        self.default = default
+        self.name = "__ERR_UNSET_NAME"
+
+    def __get__(self, instance: "Table", owner: type) -> T:
+        if instance is None:
+            return self
+        name = self.name.encode("utf8")
+        if instance.table.schema.metadata is None or name not in instance.table.schema.metadata:
+            if self.default is not None:
+                return self.default
+            raise AttributeError(f"Attribute {self.name} is not set and has no default")
+        raw = instance.table.schema.metadata[name]
+        return self.from_bytes(raw)
+
+    def __set__(self, instance: "Table", value: T) -> None:
+        metadata = instance.table.schema.metadata
+        if metadata is None:
+            metadata = {}
+        metadata[self.name.encode("utf8")] = self.to_bytes(value)
+        instance.table = instance.table.replace_schema_metadata(metadata)
+
+    def __set_name__(self, owner: type, name: str) -> None:
+        self.name = name
+
+    def to_bytes(self, value: T) -> bytes:
+        """
+        Convert the value to bytes.
+        """
+        raise NotImplementedError
+
+    def from_bytes(self, raw: bytes) -> T:
+        """
+        Convert the bytes to a value.
+        """
+        raise NotImplementedError
+
+    def to_string(self, value: T) -> str:
+        """
+        Convert the value to a string. Used for CSV writing.
+        """
+        raise NotImplementedError
+
+    def from_string(self, raw: str) -> T:
+        """
+        Convert a string to a value. Used for CSV reading.
+        """
+        raise NotImplementedError
+
+
+class StringAttribute(Attribute[str]):
+    _type = str
+
+    def __init__(self, default: Optional[str] = None):
+        super().__init__(default=default)
+
+    def to_bytes(self, value: str) -> bytes:
+        return value.encode("utf8")
+
+    def from_bytes(self, raw: bytes) -> str:
+        return raw.decode("utf8")
+
+    def to_string(self, value: str) -> str:
+        return value
+
+    def from_string(self, raw: str) -> str:
+        return raw
+
+
+class IntAttribute(Attribute[int]):
+    _type = int
+
+    def __init__(self, default: Optional[int] = None, nbytes: int = 8, signed: bool = True):
+        self.nbytes = nbytes
+        self.signed = signed
+        super().__init__(default=default)
+
+    def to_bytes(self, value: int) -> bytes:
+        return value.to_bytes(length=self.nbytes, byteorder="little", signed=self.signed)
+
+    def from_bytes(self, raw: bytes) -> int:
+        return int.from_bytes(raw, byteorder="little", signed=self.signed)
+
+    def to_string(self, value: int) -> str:
+        return str(value)
+
+    def from_string(self, raw: str) -> int:
+        return int(raw)
+
+
+class FloatAttribute(Attribute[float]):
+    _type = float
+
+    def __init__(self, default: Optional[float] = None, nbytes: int = 8):
+        if nbytes == 8:
+            self._struct_fmt = "<d"
+        elif nbytes == 4:
+            self._struct_fmt = "<f"
+        elif nbytes == 2:
+            self._struct_fmt = "<e"
+        else:
+            raise ValueError("nbytes must be 2, 4 or 8")
+        super().__init__(default=default)
+
+    def to_bytes(self, value: float) -> bytes:
+        return struct.pack(self._struct_fmt, value)
+
+    def from_bytes(self, raw: bytes) -> float:
+        return float(struct.unpack(self._struct_fmt, raw)[0])
+
+    def to_string(self, value: float) -> str:
+        return "{:.17g}".format(value)
+
+    def from_string(self, raw: str) -> float:
+        return float(raw)
---
+++ packages/quivr/quivr-0.4.3.tar.gz/quivr-0.4.3/quivr/concat.py
@@ -0,0 +1,35 @@
+from typing import Iterator, TypeVar
+
+import pyarrow as pa
+
+from .defragment import defragment
+from .tables import Table
+
+T = TypeVar("T", bound=Table)
+
+
+def concatenate(values: Iterator[T], defrag: bool = True) -> T:
+    """Concatenate a collection of Tables into a single Table.
+
+    All input Tables must have the same schema (typically, this will
+    be from being the same class).
+
+    By default, results are compacted to be contiguous in memory,
+    which involves a copy. In a tight loop, this can be very
+    inefficient, so you can set the 'defrag' parameter to False to
+    skip this compaction step, and instead call quiver.defragment on
+    the result after the loop is complete.
+
+    """
+    batches = []
+    first = True
+    for v in values:
+        batches += v.table.to_batches()
+        if first:
+            cls = v.__class__
+            first = False
+    table = pa.Table.from_batches(batches)
+    result = cls(table=table)
+    if defrag:
+        result = defragment(result)
+    return result
