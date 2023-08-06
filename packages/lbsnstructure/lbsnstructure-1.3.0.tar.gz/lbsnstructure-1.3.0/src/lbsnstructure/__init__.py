4f7b46db75110279cd572373b67479aba5e50001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:18 +0000

    Add package lbsnstructure/1.3.0/lbsnstructure-1.3.0.tar.gz

---
+++ packages/lbsnstructure/lbsnstructure-1.3.0.tar.gz/lbsnstructure-1.3.0/src/lbsnstructure/__init__.py
@@ -0,0 +1,24 @@
+"""lbsnstructure package [generated from protobuf spec]"""
+
+from lbsnstructure.version import __version__
+from lbsnstructure.topical_pb2 import Post, PostReaction
+from lbsnstructure.social_pb2 import Origin, User, UserGroup, CompositeKey, Language
+from lbsnstructure.temporal_pb2 import Event
+from lbsnstructure.spatial_pb2 import Place, City, Country
+from lbsnstructure.interlinkage_pb2 import Relationship, RelationshipKey
+
+__all__ = [
+    "Post",
+    "PostReaction",
+    "Origin",
+    "User",
+    "UserGroup",
+    "CompositeKey",
+    "Language",
+    "Event",
+    "Place",
+    "City",
+    "Country",
+    "Relationship",
+    "RelationshipKey",
+]
