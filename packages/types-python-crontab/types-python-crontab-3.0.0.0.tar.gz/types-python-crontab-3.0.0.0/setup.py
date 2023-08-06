44ebfc63b6d90b1f1ce4dc5993d9800feaa9bb03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:01 +0000

    Add package types-python-crontab/3.0.0.0/types-python-crontab-3.0.0.0.tar.gz

---
+++ packages/types-python-crontab/types-python-crontab-3.0.0.0.tar.gz/types-python-crontab-3.0.0.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+name = "types-python-crontab"
+description = "Typing stubs for python-crontab"
+long_description = '''
+## Typing stubs for python-crontab
+
+This is a PEP 561 type stub package for the `python-crontab` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`python-crontab`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/python-crontab. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `032f9195f9e3788a6e5c7ecb086f173a3ac92a95` and was tested
+with mypy 1.4.1, pyright 1.1.318, and
+pytype 2023.6.16.
+'''.lstrip()
+
+setup(name=name,
+      version="3.0.0.0",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/python-crontab.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['cronlog-stubs', 'crontab-stubs', 'crontabs-stubs'],
+      package_data={'cronlog-stubs': ['__init__.pyi', 'METADATA.toml'], 'crontab-stubs': ['__init__.pyi', 'METADATA.toml'], 'crontabs-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
