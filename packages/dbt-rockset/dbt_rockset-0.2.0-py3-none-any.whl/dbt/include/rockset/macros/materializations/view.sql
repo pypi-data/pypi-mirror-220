2e386f489eb58b1a70e3b677648abe4cf402ab9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:23 +0000

    Add package dbt-rockset/0.2.0/dbt_rockset-0.2.0-py3-none-any.whl

---
+++ packages/dbt-rockset/dbt_rockset-0.2.0-py3-none-any.whl/dbt/include/rockset/macros/materializations/view.sql
@@ -0,0 +1,15 @@
+{% materialization view, adapter='rockset' -%}  
+   {% set target_relation = this.incorporate(type='view') %}
+   {{ adapter.create_view(target_relation, sql) }}
+
+   {#-- Rockset does not support CREATE VIEW sql. All logic to create views happens in create_view --#}
+   {% call statement('main') -%}
+      {{ adapter.get_dummy_sql() }}
+   {%- endcall %}
+
+   {{ run_hooks(post_hooks) }}
+
+   {% do persist_docs(target_relation, model) %}
+
+   {{ return({'relations': [target_relation]}) }}
+{%- endmaterialization %}
