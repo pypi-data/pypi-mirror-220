22741d4c3aaf1801ac058148ff117dd1a65be8e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:58 +0000

    Add package pyDataFitting/0.0.4/pyDataFitting-0.0.4-py3-none-any.whl

---
+++ packages/pyDataFitting/pyDataFitting-0.0.4-py3-none-any.whl/pyDataFitting/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from . import linear_regression
+from .principle_component_regression import principal_component_regression
+from .partial_least_squares_regression import pls_regression
+from . import nonlinear_regression
+from . import polynomial_regression
+from .model_tools import model_tools
