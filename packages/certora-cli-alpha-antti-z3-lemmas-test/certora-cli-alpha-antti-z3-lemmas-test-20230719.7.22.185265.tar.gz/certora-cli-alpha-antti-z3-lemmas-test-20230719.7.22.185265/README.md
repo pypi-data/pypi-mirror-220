7d3af42a31408920e5651f40d83918eb1825556b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:46 +0000

    Add package certora-cli-alpha-antti-z3-lemmas-test/20230719.7.22.185265/certora-cli-alpha-antti-z3-lemmas-test-20230719.7.22.185265.tar.gz

---
+++ packages/certora-cli-alpha-antti-z3-lemmas-test/certora-cli-alpha-antti-z3-lemmas-test-20230719.7.22.185265.tar.gz/certora-cli-alpha-antti-z3-lemmas-test-20230719.7.22.185265/README.md
@@ -0,0 +1,1 @@
+Commit 341277c. Build and Run scripts for executing the Certora Prover on Solidity smart contracts.
---
+++ packages/certora-cli-alpha-antti-z3-lemmas-test/certora-cli-alpha-antti-z3-lemmas-test-20230719.7.22.185265.tar.gz/certora-cli-alpha-antti-z3-lemmas-test-20230719.7.22.185265/setup.py
@@ -0,0 +1,30 @@
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="certora-cli-alpha-antti-z3-lemmas-test",
+        version="20230719.7.22.185265",
+        author="Certora",
+        author_email="support@certora.com",
+        description="Runner for the Certora Prover",
+        long_description="Commit 341277c. Build and Run scripts for executing the Certora Prover on Solidity smart contracts.",
+        long_description_content_type="text/markdown",
+        url="https://pypi.org/project/certora-cli-alpha-antti-z3-lemmas-test",
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        install_requires=['tabulate', 'requests', 'pycryptodome', 'tqdm', 'click', 'sly', 'argcomplete', 'json5'],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        entry_points={
+            "console_scripts": [
+                "certoraRun = certora_cli.certoraRun:entry_point",
+                "certoraMutate = certora_cli.certoraMutate:ext_gambit_entry_point"
+            ]
+        },
+        python_requires='>=3.8.16',
+    )
+        
