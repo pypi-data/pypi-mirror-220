d27db3e60b193e3fe947bb6cf35e91e8bebf40b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:57 +0000

    Add package pytelibs/1.2.207/pytelibs-1.2.207.tar.gz

---
+++ packages/pytelibs/pytelibs-1.2.207.tar.gz/pytelibs-1.2.207/pytelibs/_globvals.py
@@ -0,0 +1,11 @@
+# pytel < https://t.me/kastaid >
+# Copyright (C) 2023-present kastaid
+#
+# This file is a part of < https://github.com/kastaid/pytel/ >
+# Please read the GNU Affero General Public License in
+# < https://github.com/kastaid/pytel/blob/main/LICENSE/ >.
+
+from typing import Set
+
+
+_GCAST_LOCKED: Set[int] = set()
