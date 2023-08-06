a01e272762c5d517a6b3d1fdc4aa034d0a1dbd47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:14 +0000

    Add package PharmaPy/0.0.1/pharmapy-0.0.1-py3-none-any.whl

---
+++ packages/PharmaPy/pharmapy-0.0.1-py3-none-any.whl/src/drug_list_ner/request.py
@@ -0,0 +1,28 @@
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+import os
+
+tokenizer = AutoTokenizer.from_pretrained("ronoys/PyRX", model_max_length=10000000)
+model = AutoModelForTokenClassification.from_pretrained("ronoys/PyRX")
+
+def get(sentence):
+    ner_model = pipeline(task="ner", model=model, tokenizer=tokenizer)
+    val = ner_model(sentence)
+    result = []
+    currentWord = ""
+
+    for x in val:
+        if x["entity"] == "LABEL_1":
+            currentWord = x["word"]
+        elif x["entity"] == "LABEL_2":
+            currentWord += x["word"][2:]
+        else:
+            result.append(currentWord)
+            currentWord = ""
+    
+    output = [*set(result)]
+    while("" in output):
+        output.remove("")
+    return (output)
+
+
+print (get("After surgery, David was given oxycodone for pain, prednisone to reduce inflammation, and benazepril for blood pressure control."))
