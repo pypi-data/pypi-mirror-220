ccb66db083ed71446067603b0fd1f3bebcf6b5b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:51 +0000

    Add package PandaPlyr/0.0.4/PandaPlyr-0.0.4.tar.gz

---
+++ packages/PandaPlyr/PandaPlyr-0.0.4.tar.gz/PandaPlyr-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PandaPlyr",
+    version="0.0.4",
+    author="Nick Olivier",
+    author_email="Olivier_N@lynchburg.edu",
+    description="The purpose of PandaPlyr is to make chained operations on pandas DataFrames easier and more readable.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/OlivierNDO/PandaPlyr/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+    python_requires='>=3.6',
+)
