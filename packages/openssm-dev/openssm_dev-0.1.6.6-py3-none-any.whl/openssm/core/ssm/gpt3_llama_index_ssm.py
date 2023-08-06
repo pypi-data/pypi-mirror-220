09cf7336be000b226ca4a0e57b14703368aaa046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:21 +0000

    Add package openssm-dev/0.1.6.6/openssm_dev-0.1.6.6-py3-none-any.whl

---
+++ packages/openssm-dev/openssm_dev-0.1.6.6-py3-none-any.whl/openssm/core/ssm/gpt3_llama_index_ssm.py
@@ -0,0 +1,23 @@
+"""SSM with GPT3-based SLM and LlamaIndex-based adapter."""
+
+
+from openssm.core.ssm.base_ssm import BaseSSM
+from openssm.core.slm.openai_slm import GPT3ChatCompletionSLM
+from openssm.core.adapter.llama_index_adapter import LlamaIndexAdapter
+from openssm.core.backend.abstract_backend import AbstractBackend
+
+
+class GPT3LlamaIndexSSM(BaseSSM):
+    """SSM with GPT3-based SLM and LlamaIndex-based adapter."""
+
+    def __init__(self, backends: list[AbstractBackend]):
+        adapter = LlamaIndexAdapter(backends)
+
+        slm = GPT3ChatCompletionSLM(adapter=adapter)
+        slm.pass_to_adapter: bool = True
+
+        super().__init__(slm, adapter, backends)
+
+    def __repr__(self) -> str:
+        """Return SSM instance's string representation."""
+        return f'LlamaIndexSSM[backends: {self.get_backends()}]'
