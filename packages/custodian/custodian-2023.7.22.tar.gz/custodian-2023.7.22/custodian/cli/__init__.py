ada31ff50eb5f772646bbc23a6f406820dcd75a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:14 +0000

    Add package custodian/2023.7.22/custodian-2023.7.22.tar.gz

---
+++ packages/custodian/custodian-2023.7.22.tar.gz/custodian-2023.7.22/custodian/cli/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) Materials Virtual Lab.
+# Distributed under the terms of the BSD License.
+
+"""
+Implement various command line utilities.
+"""
---
+++ packages/custodian/custodian-2023.7.22.tar.gz/custodian-2023.7.22/custodian/qchem/__init__.py
@@ -0,0 +1,7 @@
+"""
+This package implements various QChem Jobs and Error Handlers.
+"""
+
+
+__author__ = "Samuel Blau, Brandon Wood, Shyam Dwaraknath"
+__credits__ = "Xiaohui Qu"
