6c0bf037e0b5076878440beb0a11b3124265b9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:02 +0000

    Add package discohooker/0.0.0b9/discohooker-0.0.0b9.tar.gz

---
+++ packages/discohooker/discohooker-0.0.0b9.tar.gz/discohooker-0.0.0b9/discohooker/__init__.py
@@ -0,0 +1,3 @@
+from .errors import Errors
+from .embed import Embed, EmbedField
+from .webhook import Webhook
---
+++ packages/discohooker/discohooker-0.0.0b9.tar.gz/discohooker-0.0.0b9/discohooker/errors.py
@@ -0,0 +1,25 @@
+class Errors:
+
+    
+    class WebhookURLError(Exception):
+        f"""{Exception}"""
+
+
+    class WebhookNotFound(Exception):
+        f"""{Exception}"""
+
+
+    class URLError(Exception):
+        f"""{Exception}"""
+
+
+    class MessageNotFound(Exception):
+        f"""{Exception}"""
+
+    
+    class MessageError(Exception):
+        f"""{Exception}"""
+
+
+    class TimeZoneNotFound(Exception):
+        f"""{Exception}"""
