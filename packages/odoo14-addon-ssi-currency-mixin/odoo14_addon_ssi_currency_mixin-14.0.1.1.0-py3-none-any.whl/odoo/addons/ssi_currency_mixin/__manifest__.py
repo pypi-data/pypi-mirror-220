4b2a965f850f272d07cc5ab2bebf2a870cfe0a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:28 +0000

    Add package odoo14-addon-ssi-currency-mixin/14.0.1.1.0/odoo14_addon_ssi_currency_mixin-14.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-currency-mixin/odoo14_addon_ssi_currency_mixin-14.0.1.1.0-py3-none-any.whl/odoo/addons/ssi_currency_mixin/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Currency Mixin",
+    "version": "14.0.1.1.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_company_currency_mixin",
+        "currency_rate_inverted",
+    ],
+    "data": [],
+}
