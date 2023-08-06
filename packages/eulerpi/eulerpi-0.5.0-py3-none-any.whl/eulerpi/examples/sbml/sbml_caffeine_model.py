2a32de83f3c19aaae82a7601df4ac004f630258b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:52 +0000

    Add package eulerpi/0.5.0/eulerpi-0.5.0-py3-none-any.whl

---
+++ packages/eulerpi/eulerpi-0.5.0-py3-none-any.whl/eulerpi/examples/sbml/sbml_caffeine_model.py
@@ -0,0 +1,43 @@
+import importlib
+
+import numpy as np
+
+from eulerpi.core.model import ArtificialModelInterface, SBMLModel
+
+
+class CaffeineSBMLModel(SBMLModel, ArtificialModelInterface):
+
+    CENTRAL_PARAM = np.array([1.0, 1.0])
+    PARAM_LIMITS = np.array([[0.0, 2.0], [0.0, 2.0]])
+
+    def __init__(
+        self,
+        central_param: np.ndarray = CENTRAL_PARAM,
+        param_limits: np.ndarray = PARAM_LIMITS,
+        **kwargs,
+    ) -> None:
+        sbml_file = importlib.resources.path(
+            "eulerpi.examples.sbml", "Caffeine_2Wks_Exponential_decay.xml"
+        )
+        param_ids = ["A", "B"]
+        timepoints = np.array([0.5, 1.0])
+
+        super().__init__(
+            sbml_file,
+            central_param,
+            param_limits,
+            timepoints,
+            param_ids,
+            **kwargs,
+        )
+
+    def generate_artificial_params(self, num_samples: int) -> np.ndarray:
+        diff0 = 0.2
+        diff1 = 0.2
+        params = np.random.rand(num_samples, self.param_dim)
+        params[:, 0] *= diff0
+        params[:, 0] += 1.0 - diff0 / 2.0
+
+        params[:, 1] *= diff1
+        params[:, 1] += 1.0 - diff1 / 2.0
+        return params
