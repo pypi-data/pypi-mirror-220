c684841931478ff32ee085ae5f11535804b94b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:09 +0000

    Add package PyComps/0.3.2/PyComps-0.3.2.tar.gz

---
+++ packages/PyComps/PyComps-0.3.2.tar.gz/PyComps-0.3.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+    
+setup(
+    name='PyComps', 
+    version='0.3.2',
+    description='A sample Python package',
+    author='Edoardo Mancini',
+    author_email='edoardo.mancini0@gmail.com',
+    classifiers=["License :: OSI Approved :: MIT License"],
+    url='https://github.com/edo-147/PyComp',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=['PyComps'],
+    
+)
+
+
