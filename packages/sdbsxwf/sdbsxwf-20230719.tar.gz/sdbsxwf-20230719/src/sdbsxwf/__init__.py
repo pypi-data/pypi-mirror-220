aadccccf840092d2b7dbfc36b2ee070e99679e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:02 +0000

    Add package sdbsxwf/20230719/sdbsxwf-20230719.tar.gz

---
+++ packages/sdbsxwf/sdbsxwf-20230719.tar.gz/sdbsxwf-20230719/src/sdbsxwf/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+from . import xlsxzl
+from . import mkmain
+from . import mkgjx
+#import sys
+#import os
+ 
+# 把当前文件所在文件夹的父文件夹路径加入到PYTHONPATH
+#sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+#print("init:",os.path.dirname(os.path.abspath(__file__)))
+__version__ = '1.0.0'
+__license__ = ''
