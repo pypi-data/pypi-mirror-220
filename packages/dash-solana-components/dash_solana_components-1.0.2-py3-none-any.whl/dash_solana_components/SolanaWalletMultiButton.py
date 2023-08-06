c836d48b2c2eb806d1bb43a0e4d43eced441f51c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:07 +0000

    Add package dash-solana-components/1.0.2/dash_solana_components-1.0.2-py3-none-any.whl

---
+++ packages/dash-solana-components/dash_solana_components-1.0.2-py3-none-any.whl/dash_solana_components/SolanaWalletMultiButton.py
@@ -0,0 +1,31 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class SolanaWalletMultiButton(Component):
+    """A SolanaWalletMultiButton component.
+
+
+Keyword arguments:
+
+- id (string; optional):
+    Unique ID to identify this component in Dash callbacks.
+
+- network (a value equal to: 'devnet', 'mainnet', 'testnet'; default 'mainnet')"""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'dash_solana_components'
+    _type = 'SolanaWalletMultiButton'
+    @_explicitize_args
+    def __init__(self, network=Component.UNDEFINED, id=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'network']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'network']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        super(SolanaWalletMultiButton, self).__init__(**args)
