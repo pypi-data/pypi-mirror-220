9f94c1ef43a0ef01952aad6a3e1074a1704b234b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:37 +0000

    Add package argsdict/1.0.0/argsdict-1.0.0.tar.gz

---
+++ packages/argsdict/argsdict-1.0.0.tar.gz/argsdict-1.0.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="argsdict",
+    version="1.0.0",
+    author="Julio Cabria",
+    author_email="juliocabria@tutanota.com",
+    maintainer="Julio Cabria",
+    maintainer_email="juliocabria@tutanota.com",
+    url="https://github.com/Julynx/argsdict",
+    description=("Simple command-line argument parser."),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="GPLv2",
+    packages=["argsdict"])
