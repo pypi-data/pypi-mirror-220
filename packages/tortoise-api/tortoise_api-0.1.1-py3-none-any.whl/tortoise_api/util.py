45789e057dffd52c8c2c54d8fcb05141c51aee32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:34 +0000

    Add package tortoise-api/0.1.1/tortoise_api-0.1.1-py3-none-any.whl

---
+++ packages/tortoise-api/tortoise_api-0.1.1-py3-none-any.whl/tortoise_api/util.py
@@ -0,0 +1,25 @@
+from datetime import date
+from tortoise.fields import Field
+from tortoise.fields.relational import RelationalField, ReverseRelation
+from tortoise_api_model import Model
+
+def jsonify(obj: Model) -> dict:
+    def check(field: Field, key: str):
+        prop = getattr(obj, key)
+        if isinstance(prop, date):
+            return prop.__str__().split('.')[0].split('+')[0]
+        elif isinstance(field, RelationalField):
+            if isinstance(prop, Model):
+                return rel_pack(prop)
+            elif isinstance(prop, ReverseRelation) and isinstance(prop.related_objects, list):
+                return [rel_pack(d) for d in prop.related_objects]
+            elif prop is None:
+                return ''
+            return '[X]'
+        else:
+            return getattr(obj, key)
+
+    return {key: check(field, key) for key, field in obj._meta.fields_map.items() if not key.endswith('_id')}
+
+def rel_pack(mod: Model) -> dict:
+    return {'id': mod.id, 'type': mod.__class__.__name__, 'repr': mod.repr()}
