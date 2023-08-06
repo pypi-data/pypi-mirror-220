9017b5bc817e24d2b1bf7516ce9de7abba21fa40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:26 +0000

    Add package meilisearch-python-async/1.4.7/meilisearch_python_async-1.4.7-py3-none-any.whl

---
+++ packages/meilisearch-python-async/meilisearch_python_async-1.4.7-py3-none-any.whl/meilisearch_python_async/models/settings.py
@@ -0,0 +1,37 @@
+from typing import Any, Dict, List, Optional
+
+from camel_converter.pydantic_base import CamelBase
+
+
+class MinWordSizeForTypos(CamelBase):
+    one_typo: Optional[int] = None
+    two_typos: Optional[int] = None
+
+
+class TypoTolerance(CamelBase):
+    enabled: bool = True
+    disable_on_attributes: Optional[List[str]] = None
+    disable_on_words: Optional[List[str]] = None
+    min_word_size_for_typos: Optional[MinWordSizeForTypos] = None
+
+
+class Faceting(CamelBase):
+    max_values_per_facet: int
+
+
+class Pagination(CamelBase):
+    max_total_hits: int
+
+
+class MeilisearchSettings(CamelBase):
+    synonyms: Optional[Dict[str, Any]] = None
+    stop_words: Optional[List[str]] = None
+    ranking_rules: Optional[List[str]] = None
+    filterable_attributes: Optional[List[str]] = None
+    distinct_attribute: Optional[str] = None
+    searchable_attributes: Optional[List[str]] = None
+    displayed_attributes: Optional[List[str]] = None
+    sortable_attributes: Optional[List[str]] = None
+    typo_tolerance: Optional[TypoTolerance] = None
+    faceting: Optional[Faceting] = None
+    pagination: Optional[Pagination] = None
