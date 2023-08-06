5019e1c6f0098a429b381cf185326f338e5068e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:13 +0000

    Add package sn-test/1.0.1/sn_test-1.0.1.tar.gz

---
+++ packages/sn-test/sn_test-1.0.1.tar.gz/sn_test-1.0.1/sn_test/sn_test.py
@@ -0,0 +1,13 @@
+
+
+class ZerocapTest():
+    
+    def __init__(self, api_key: str) -> None:
+        self.api_key = api_key
+    
+    def test(self):
+        return self.api_key
+    
+    def test1(self):
+        print(123)
+        
