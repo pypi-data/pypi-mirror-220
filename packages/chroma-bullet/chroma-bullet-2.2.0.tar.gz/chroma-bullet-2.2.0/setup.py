2149df73794fc82d5600c3e3d862382388545020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:53 +0000

    Add package chroma-bullet/2.2.0/chroma-bullet-2.2.0.tar.gz

---
+++ packages/chroma-bullet/chroma-bullet-2.2.0.tar.gz/chroma-bullet-2.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='chroma-bullet',
+      version='2.2.0',
+      description='Chromas fork of bullet - Beautiful Python prompts made simple.',
+      long_description="Extensive support for Python list prompts \
+            formatting and colors",
+      url='https://github.com/chroma-core/bullet',
+      keywords = "cli list prompt customize colors",
+      author='THe chroma team and the original bullet author - Mckinsey666',
+      license='MIT',
+      packages=find_packages(),
+      python_requires=">=3.6"
+)
