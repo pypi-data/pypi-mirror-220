f5bb9a77076fa04116d0854c68117f16eeb3ffc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:20 +0000

    Add package imgopt/0.1/imgopt-0.1.tar.gz

---
+++ packages/imgopt/imgopt-0.1.tar.gz/imgopt-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name='imgopt',
+    version='0.1',
+    description='Compress & Optimize Images',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Vinay Chaudhary',
+    packages=['imgopt'],
+    install_requires=['requests']
+)
