2bffba86ccdc87a2464c9615a42b00737ffb4e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:38 +0000

    Add package weehok/1.0/weehok-1.0.tar.gz

---
+++ packages/weehok/weehok-1.0.tar.gz/weehok-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='weehok',
+    version='1.0',
+    description='It just makes ur life easy so why not use it?',
+    author='0xleft',
+    url='https://pageup.lt/',
+    packages=['weehok'],
+    long_description="Amazing",
+    license='Apache License 2.0',
+)
