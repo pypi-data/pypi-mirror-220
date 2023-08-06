e5513e5476f79d988e4f7451fae4c2450030ead0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:01 +0000

    Add package igKvj1o60B-test-project/0.0.1/igKvj1o60B-test-project-0.0.1.tar.gz

---
+++ packages/igKvj1o60B-test-project/igKvj1o60B-test-project-0.0.1.tar.gz/igKvj1o60B-test-project-0.0.1/README.md
@@ -0,0 +1,5 @@
+# work-time-log is now work
+
+This package has been renamed. Use `pipx install work` instead.
+
+New package: https://pypi.org/project/work/
---
+++ packages/igKvj1o60B-test-project/igKvj1o60B-test-project-0.0.1.tar.gz/igKvj1o60B-test-project-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import os
+
+
+setup(
+    name="igKvj1o60B-test-project",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    version="0.0.1",
+    install_requires=["work-time-log"],
+    classifiers=["Development Status :: 7 - Inactive"],
+)
