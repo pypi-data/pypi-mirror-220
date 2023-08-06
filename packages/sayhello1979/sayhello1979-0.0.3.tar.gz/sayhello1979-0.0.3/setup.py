44b1c838669a0287265d99c226c5eb4e81f30d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:26 +0000

    Add package sayhello1979/0.0.3/sayhello1979-0.0.3.tar.gz

---
+++ packages/sayhello1979/sayhello1979-0.0.3.tar.gz/sayhello1979-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="sayhello1979",
+      version="0.0.3",
+      author="Reza Mortazavi",
+      description="",
+      packages=["sayhello1979"],
+      zip_safe=False) 
+
+
