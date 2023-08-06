1d9eecd60959e2dbc8543b454bf31a571e8abd6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:00 +0000

    Add package grooveshop-django-api/0.8.8/grooveshop_django_api-0.8.8.tar.gz

---
+++ packages/grooveshop-django-api/grooveshop_django_api-0.8.8.tar.gz/grooveshop_django_api-0.8.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+__version__ = "0.8.7"
+
+setup(
+    name="grooveshop-django-api",
+    version=__version__,
+    description="GrooveShop Setup",
+    author="Vasilis Totskas",
+    author_email="vassilistotskas@msn.com",
+)
