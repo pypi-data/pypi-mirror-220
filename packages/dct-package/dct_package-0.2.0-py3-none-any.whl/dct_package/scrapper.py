d219ecdf5dc3561dae98f9592d5f1a58a3ff58d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:11 +0000

    Add package dct-package/0.2.0/dct_package-0.2.0-py3-none-any.whl

---
+++ packages/dct-package/dct_package-0.2.0-py3-none-any.whl/dct_package/scrapper.py
@@ -0,0 +1,10 @@
+def get_data():
+    return {
+        "message": "Data from DCT Poetry Package.",
+        "version": "0.2.0",
+        "kind": "best version ever"
+    }
+
+
+if __name__ == '__main__':
+    print(get_data())
