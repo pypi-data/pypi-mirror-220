514ae99b6359d36bb6238981d6f6cae42d2dc415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:48 +0000

    Add package secret-checker/1.0.4/secret_checker-1.0.4.tar.gz

---
+++ packages/secret-checker/secret_checker-1.0.4.tar.gz/secret_checker-1.0.4/README.md
@@ -0,0 +1,7 @@
+# secret_checker
+A Python module for checking if either of the secrets/environment variables include prod or dev or other env name.
+
+# Installation
+- Create a virtual environment
+- Install the dependencies pip install -r requirements.txt
+
---
+++ packages/secret-checker/secret_checker-1.0.4.tar.gz/secret_checker-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='secret_checker',
+    version='1.0.4',
+    description='A Python module for checking if either of the secrets/environment variables include prod or dev or other env name.',
+    author='Akash Deep',
+    author_email='dakash98@outlook.com',
+    packages=['secret_checker'],
+    install_requires=[],
+)
