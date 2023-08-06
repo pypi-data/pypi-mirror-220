273919c60a805765e00ad97384c15a03b810d41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:40 +0000

    Add package openctp-client/6.6.7.0/openctp_client-6.6.7.0-py3-none-any.whl

---
+++ packages/openctp-client/openctp_client-6.6.7.0-py3-none-any.whl/openctp_client/objects/config.py
@@ -0,0 +1,9 @@
+class CtpConfig(object):
+
+    def __init__(self, addr, broker_id, auth_code, app_id, user_id, password) -> None:
+        self.addr = addr
+        self.broker_id = broker_id
+        self.auth_code = auth_code
+        self.app_id = app_id
+        self.user_id = user_id
+        self.password = password
