16171f785ad49b101a752a50096ed3847c940385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:26 +0000

    Add package suinspy/0.1.2/suinspy-0.1.2.tar.gz

---
+++ packages/suinspy/suinspy-0.1.2.tar.gz/suinspy-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['suinspy', 'suinspy.types', 'suinspy.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pysui>=0.29.0,<0.30.0', 'requests>=2.31.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'suinspy',
+    'version': '0.1.2',
+    'description': 'Sui Name Service Python SDK Client',
+    'long_description': None,
+    'author': 'andreidev1',
+    'author_email': 'd.andrei01@yahoo.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
