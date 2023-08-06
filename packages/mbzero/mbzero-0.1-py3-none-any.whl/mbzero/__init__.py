ea3c3aef4af132ca5dbe1ec95f2eecc2b8dde5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:05 +0000

    Add package mbzero/0.1/mbzero-0.1-py3-none-any.whl

---
+++ packages/mbzero/mbzero-0.1-py3-none-any.whl/mbzero/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the mbzero library
+# Copyright (C) 2023 Louis Rannou
+# This file is distributed under a BSD-2-Clause type license.
+# See the LICENSE file for more information.
+
+"""
+Musicbrainz bindings
+"""
+
+from mbzero.mbzrequest import (
+    MbzRequestLookup,
+    MbzRequestBrowse,
+    MbzRequestSearch)
+
+from mbzero.mbzauth import MbzCredentials
