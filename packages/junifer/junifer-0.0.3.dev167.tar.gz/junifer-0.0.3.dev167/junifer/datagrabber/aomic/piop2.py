97d5ce44cabe369d25e08fdcaa97205e168ca661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:57 +0000

    Add package junifer/0.0.3.dev167/junifer-0.0.3.dev167.tar.gz

---
+++ packages/junifer/junifer-0.0.3.dev167.tar.gz/junifer-0.0.3.dev167/junifer/datagrabber/aomic/piop2.py
@@ -0,0 +1,165 @@
+"""Provide concrete implementation for AOMIC PIOP2 DataGrabber."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Vera Komeyer <v.komeyer@fz-juelich.de>
+#          Xuan Li <xu.li@fz-juelich.de>
+#          Leonard Sasse <l.sasse@fz-juelich.de>
+# License: AGPL
+
+from pathlib import Path
+from typing import Dict, List, Union
+
+from ...api.decorators import register_datagrabber
+from ...utils import raise_error
+from ..pattern_datalad import PatternDataladDataGrabber
+
+
+@register_datagrabber
+class DataladAOMICPIOP2(PatternDataladDataGrabber):
+    """Concrete implementation for pattern-based data fetching of AOMIC PIOP2.
+
+    Parameters
+    ----------
+    datadir : str or Path or None, optional
+        The directory where the datalad dataset will be cloned. If None,
+        the datalad dataset will be cloned into a temporary directory
+        (default None).
+    tasks : {"restingstate", "stopsignal", "emomatching", "workingmemory"} \
+            or list of the options, optional
+        AOMIC PIOP2 task sessions. If None, all available task sessions are
+        selected (default None).
+
+    """
+
+    def __init__(
+        self,
+        datadir: Union[str, Path, None] = None,
+        tasks: Union[str, List[str], None] = None,
+    ) -> None:
+        # The types of data
+        types = [
+            "BOLD",
+            "BOLD_confounds",
+            "BOLD_mask",
+            "T1w",
+            "T1w_mask",
+            "probseg_CSF",
+            "probseg_GM",
+            "probseg_WM",
+            "DWI",
+        ]
+
+        if isinstance(tasks, str):
+            tasks = [tasks]
+
+        all_tasks = [
+            "restingstate",
+            "emomatching",
+            "workingmemory",
+            "stopsignal",
+        ]
+
+        if tasks is None:
+            tasks = all_tasks
+        else:
+            for t in tasks:
+                if t not in all_tasks:
+                    raise_error(
+                        f"{t} is not a valid task in the AOMIC PIOP2"
+                        " dataset!"
+                    )
+
+        self.tasks = tasks
+
+        patterns = {
+            "BOLD": (
+                "derivatives/fmriprep/sub-{subject}/func/"
+                "sub-{subject}_task-{task}_acq-seq_"
+                "space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz"
+            ),
+            "BOLD_confounds": (
+                "derivatives/fmriprep/sub-{subject}/func/"
+                "sub-{subject}_task-{task}_acq-seq_"
+                "desc-confounds_regressors.tsv"
+            ),
+            "BOLD_mask": (
+                "derivatives/fmriprep/sub-{subject}/func/"
+                "sub-{subject}_task-{task}_acq-seq_space"
+                "-MNI152NLin2009cAsym_desc-brain_mask.nii.gz"
+            ),
+            "T1w": (
+                "derivatives/fmriprep/sub-{subject}/anat/"
+                "sub-{subject}_space-MNI152NLin2009cAsym_"
+                "desc-preproc_T1w.nii.gz"
+            ),
+            "T1w_mask": (
+                "derivatives/fmriprep/sub-{subject}/anat/"
+                "sub-{subject}_space-MNI152NLin2009cAsym_"
+                "desc-brain_mask.nii.gz"
+            ),
+            "probseg_CSF": (
+                "derivatives/fmriprep/sub-{subject}/anat/"
+                "sub-{subject}_space-MNI152NLin2009cAsym_label-"
+                "CSF_probseg.nii.gz"
+            ),
+            "probseg_GM": (
+                "derivatives/fmriprep/sub-{subject}/anat/"
+                "sub-{subject}_space-MNI152NLin2009cAsym_label-"
+                "GM_probseg.nii.gz"
+            ),
+            "probseg_WM": (
+                "derivatives/fmriprep/sub-{subject}/anat/"
+                "sub-{subject}_space-MNI152NLin2009cAsym_label-"
+                "WM_probseg.nii.gz"
+            ),
+            "DWI": (
+                "derivatives/dwipreproc/sub-{subject}/dwi/"
+                "sub-{subject}_desc-preproc_dwi.nii.gz"
+            ),
+        }
+        uri = "https://github.com/OpenNeuroDatasets/ds002790"
+        replacements = ["subject", "task"]
+        super().__init__(
+            types=types,
+            datadir=datadir,
+            uri=uri,
+            patterns=patterns,
+            replacements=replacements,
+            confounds_format="fmriprep",
+        )
+
+    def get_elements(self) -> List:
+        """Implement fetching list of elements in the dataset.
+
+        Returns
+        -------
+        list
+            The list of elements that can be grabbed in the dataset after
+            imposing constraints based on specified tasks.
+
+        """
+        all_elements = super().get_elements()
+        return [x for x in all_elements if x[1] in self.tasks]
+
+    def get_item(self, subject: str, task: str) -> Dict:
+        """Index one element in the dataset.
+
+        Parameters
+        ----------
+        subject : str
+            The subject ID.
+        task : str
+            The task to get. Possible values are:
+            {"restingstate", "stopsignal", "emomatching", "workingmemory"}
+
+        Returns
+        -------
+        out : dict
+            Dictionary of paths for each type of data required for the
+            specified element.
+
+        """
+        out = super().get_item(subject=subject, task=task)
+        out["BOLD"]["mask_item"] = "BOLD_mask"
+        out["T1w"]["mask_item"] = "T1w_mask"
+        return out
---
+++ packages/junifer/junifer-0.0.3.dev167.tar.gz/junifer-0.0.3.dev167/junifer/datagrabber/aomic/tests/test_piop1.py
@@ -0,0 +1,150 @@
+"""Provide tests for DataladAOMICPIOP1 DataGrabber."""
+
+# Authors: Federico Raimondo <f.raimondo@fz-juelich.de>
+#          Vera Komeyer <v.komeyer@fz-juelich.de>
+#          Xuan Li <xu.li@fz-juelich.de>
+#          Leonard Sasse <l.sasse@fz-juelich.de>
+# License: AGPL
+
+import pytest
+
+from junifer.datagrabber import DataladAOMICPIOP1
+from junifer.utils import configure_logging
+
+
+def test_DataladAOMICPIOP1() -> None:
+    """Test DataladAOMICPIOP1 DataGrabber."""
+    configure_logging(level="DEBUG")
+
+    uri_PIOP1 = "https://gin.g-node.org/juaml/datalad-example-aomicpiop1"
+    task_params = [None, "restingstate"]
+
+    for task_param in task_params:
+        dg = DataladAOMICPIOP1(tasks=task_param)
+
+        # change uri here to use fake data instead of real dataset
+        dg.uri = uri_PIOP1
+
+        with dg:
+            all_elements = dg.get_elements()
+            test_element = all_elements[0]
+            sub, task = test_element
+
+            out = dg[test_element]
+
+            # asserts type "BOLD"
+            assert "BOLD" in out
+
+            # depending on task 'acquisition is different'
+            task_acqs = {
+                "anticipation": "seq",
+                "emomatching": "seq",
+                "faces": "mb3",
+                "gstroop": "seq",
+                "restingstate": "mb3",
+                "workingmemory": "seq",
+            }
+            acq = task_acqs[task]
+            new_task = f"{task}_acq-{acq}"
+            assert (
+                out["BOLD"]["path"].name == f"sub-{sub}_task-{new_task}_"
+                "space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz"
+            )
+
+            assert out["BOLD"]["path"].exists()
+            assert out["BOLD"]["path"].is_file()
+
+            # asserts type "BOLD_confounds"
+            assert "BOLD_confounds" in out
+
+            assert (
+                out["BOLD_confounds"]["path"].name
+                == f"sub-{sub}_task-{new_task}_"
+                "desc-confounds_regressors.tsv"
+            )
+
+            assert out["BOLD_confounds"]["path"].exists()
+            assert out["BOLD_confounds"]["path"].is_file()
+
+            # assert BOLD_mask
+            assert out["BOLD_mask"]["path"].exists()
+
+            # asserts type "T1w"
+            assert "T1w" in out
+
+            assert (
+                out["T1w"]["path"].name
+                == f"sub-{sub}_space-MNI152NLin2009cAsym_"
+                "desc-preproc_T1w.nii.gz"
+            )
+
+            assert out["T1w"]["path"].exists()
+            assert out["T1w"]["path"].is_file()
+
+            # asserts T1w_mask
+            assert out["T1w_mask"]["path"].exists()
+
+            # asserts type "probseg_CSF"
+            assert "probseg_CSF" in out
+
+            assert (
+                out["probseg_CSF"]["path"].name
+                == f"sub-{sub}_space-MNI152NLin2009cAsym_label-"
+                "CSF_probseg.nii.gz"
+            )
+
+            assert out["probseg_CSF"]["path"].exists()
+            assert out["probseg_CSF"]["path"].is_file()
+
+            # asserts type "probseg_GM"
+            assert "probseg_GM" in out
+
+            assert (
+                out["probseg_GM"]["path"].name
+                == f"sub-{sub}_space-MNI152NLin2009cAsym_label-"
+                "GM_probseg.nii.gz"
+            )
+
+            assert out["probseg_GM"]["path"].exists()
+            assert out["probseg_GM"]["path"].is_file()
+
+            # asserts type "probseg_WM"
+            assert "probseg_WM" in out
+
+            assert (
+                out["probseg_WM"]["path"].name
+                == f"sub-{sub}_space-MNI152NLin2009cAsym_label-"
+                "WM_probseg.nii.gz"
+            )
+
+            assert out["probseg_WM"]["path"].exists()
+            assert out["probseg_WM"]["path"].is_file()
+
+            # asserts type "DWI"
+            assert "DWI" in out
+
+            assert (
+                out["DWI"]["path"].name == f"sub-{sub}_desc-preproc_dwi.nii.gz"
+            )
+
+            assert out["DWI"]["path"].exists()
+            assert out["DWI"]["path"].is_file()
+
+            # asserts meta
+            assert "meta" in out["BOLD"]
+            meta = out["BOLD"]["meta"]
+            assert "element" in meta
+            assert "subject" in meta["element"]
+            assert sub == meta["element"]["subject"]
+
+
+def test_DataladAOMICPIOP1_invalid_tasks():
+    """Test whether invalid task fails."""
+    with pytest.raises(
+        ValueError,
+        match=(
+            "thisisnotarealtask is not a valid task in "
+            "the AOMIC PIOP1 dataset!"
+        ),
+    ):
+        DataladAOMICPIOP1(tasks="thisisnotarealtask")
