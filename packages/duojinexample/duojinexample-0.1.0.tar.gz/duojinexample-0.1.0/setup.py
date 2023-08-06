139bb8c1da541591664ba728d71aeb51bf285e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:58 +0000

    Add package duojinexample/0.1.0/duojinexample-0.1.0.tar.gz

---
+++ packages/duojinexample/duojinexample-0.1.0.tar.gz/duojinexample-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='duojinexample',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Your Name',
+    description='A simple example package',
+    install_requires=[],
+)
