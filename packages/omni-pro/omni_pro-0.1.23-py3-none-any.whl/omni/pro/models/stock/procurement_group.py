3c3c4375810681a12c32805de208c37b1350d8a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:48 +0000

    Add package omni-pro/0.1.23/omni_pro-0.1.23-py3-none-any.whl

---
+++ packages/omni-pro/omni_pro-0.1.23-py3-none-any.whl/omni/pro/models/stock/procurement_group.py
@@ -0,0 +1,20 @@
+from omni.pro.models.base import BaseModel
+from omni.pro.protos.v1.stock.procurement_group_pb2 import ProcurementGroup as ProcurementGroupProto
+from peewee import CharField
+
+
+class ProcurementGroup(BaseModel):
+    name = CharField()
+    move_type = CharField()
+
+    class Meta:
+        table_name = "procurement_group"
+
+    def to_proto(self):
+        return ProcurementGroupProto(
+            id=self.id,
+            name=self.name,
+            move_type=self.move_type,
+            object_audit=self.get_audit_proto(),
+            active=self.active,
+        )
