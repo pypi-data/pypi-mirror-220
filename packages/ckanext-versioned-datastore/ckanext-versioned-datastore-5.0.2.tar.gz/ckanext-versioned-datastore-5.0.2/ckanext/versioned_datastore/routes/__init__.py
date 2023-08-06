5a8bc85c03e998072bf23ae8ce44ac779d8a8e50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:13 +0000

    Add package ckanext-versioned-datastore/5.0.2/ckanext-versioned-datastore-5.0.2.tar.gz

---
+++ packages/ckanext-versioned-datastore/ckanext-versioned-datastore-5.0.2.tar.gz/ckanext-versioned-datastore-5.0.2/ckanext/versioned_datastore/routes/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-versioned-datastore
+# Created by the Natural History Museum in London, UK
+from ckan.plugins import toolkit
+
+from . import datastore, search, downloads, status
+
+blueprints = [datastore.blueprint, search.blueprint, status.blueprint]
+
+if toolkit.config.get('DEBUG', False):
+    blueprints.append(downloads.blueprint)
