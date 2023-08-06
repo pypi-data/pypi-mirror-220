8576fbfa770bb7b5ff72fb57df5ac519adba0ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:24 +0000

    Add package sagemaker-ssh-helper/1.12.0/sagemaker-ssh-helper-1.12.0.tar.gz

---
+++ packages/sagemaker-ssh-helper/sagemaker-ssh-helper-1.12.0.tar.gz/sagemaker-ssh-helper-1.12.0/tests/test_local.py
@@ -0,0 +1,49 @@
+import logging
+import os
+import distutils.dir_util
+from pathlib import Path
+
+import mock
+
+import test_util
+
+os.environ["SAGEMAKER_BASE_DIR"] = os.path.join(os.path.dirname(__file__), "opt_ml")
+from sagemaker_training import environment as training_environment, params as training_parameters
+
+from sagemaker_training.cli.train import main as train_main
+
+
+def test_local_training():
+    logging.info("Starting training")
+
+    f"""
+    Compare with https://github.com/aws/amazon-sagemaker-examples/tree/main/advanced_functionality/scikit_bring_your_own/container/local_test .
+    """
+    assert training_environment.SAGEMAKER_BASE_PATH == "/opt/ml"
+    assert training_environment.base_dir.endswith("/opt_ml")
+
+    test_util._clean_training_opt_ml_dir()
+    distutils.dir_util.copy_tree("./source_dir/training_clean/", "./opt_ml/code/")
+
+    os.environ = {
+        training_parameters.USER_PROGRAM_ENV: Path('source_dir/training_clean/train_clean.py').name,
+    }
+
+    try:
+        # Note: it will start the subprocess, so we don't have the code coverage
+        # TODO: https://coverage.readthedocs.io/en/latest/subprocess.html
+        train_main()
+    except SystemExit as e:
+        assert e.code == 0
+
+    assert os.path.exists("./opt_ml/model/model.pth")
+    assert os.path.exists("./opt_ml/output/success")
+
+    logging.info("Finished training")
+
+
+@mock.patch.dict(os.environ, {"SM_MODEL_DIR": "./opt_ml/model"})
+def test_local_training_with_coverage():
+    # import will start training
+    from source_dir.training_clean import train_clean
+    assert train_clean
