d251b8546dfca86080875d19fd7b629b24c0e05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:26 +0000

    Add package forestadmin-datasource-toolkit/1.0.0b1/forestadmin_datasource_toolkit-1.0.0b1-py3-none-any.whl

---
+++ packages/forestadmin-datasource-toolkit/forestadmin_datasource_toolkit-1.0.0b1-py3-none-any.whl/forestadmin/datasource_toolkit/datasource_customizer/collection_customizer.py
@@ -0,0 +1,66 @@
+from typing import List
+
+from forestadmin.datasource_toolkit.decorators.action.types.actions import ActionAlias
+from forestadmin.datasource_toolkit.decorators.chart.types import CollectionChartDefinition
+from forestadmin.datasource_toolkit.decorators.computed.types import ComputedDefinition
+from forestadmin.datasource_toolkit.decorators.decorator_stack import DecoratorStack
+from forestadmin.datasource_toolkit.decorators.search.collections import SearchDefinition
+from forestadmin.datasource_toolkit.decorators.segments.collections import SegmentAlias
+from forestadmin.datasource_toolkit.decorators.write.write_replace.types import WriteDefinition
+
+
+class CollectionCustomizer:
+    def __init__(
+        self, datasource_customizer: "DatasourceCustomizer", stack: DecoratorStack, collection_name: str  # noqa: F821
+    ):
+        self.datasource_customizer = datasource_customizer
+        self.stack = stack
+        self.collection_name = collection_name
+
+    def add_action(self, name: str, action: ActionAlias):
+        self.stack.action.get_collection(self.collection_name).add_action(name, action)
+
+    def add_segment(self, name: str, segment: SegmentAlias):
+        self.stack.segment.get_collection(self.collection_name).add_segment(name, segment)
+
+    def rename_field(self, current_name: str, new_name: str):
+        self.stack.rename_field.get_collection(self.collection_name).rename_field(current_name, new_name)
+
+    def add_field(self, name: str, computed: ComputedDefinition):
+        self.stack.early_computed.get_collection(self.collection_name).register_computed(name, computed)
+        # TODO: implement the switch after relation decorator
+        # const collectionBeforeRelations = this.stack.earlyComputed.getCollection(this.name);
+        # const collectionAfterRelations = this.stack.lateComputed.getCollection(this.name);
+        # const canBeComputedBeforeRelations = definition.dependencies.every(field => {
+        # try {
+        #     return !!CollectionUtils.getFieldSchema(collectionBeforeRelations, field);
+        # } catch {
+        #     return false;
+        #     }
+        # });
+
+        # const collection = canBeComputedBeforeRelations
+        #     ? collectionBeforeRelations
+        #     : collectionAfterRelations;
+
+        # collection.registerComputed(name, definition as ComputedDefinition);
+
+    def add_validation(self, name: str, validation: List):
+        self.stack.validation.get_collection(self.collection_name).add_validation(name, validation)
+
+    def disable_count(self):
+        self.stack.schema.get_collection(self.collection_name).override_schema("countable", False)
+
+    def remove_field(self, *fields):
+        stack_collection = self.stack.publication.get_collection(self.collection_name)
+        for field in fields:
+            stack_collection.change_field_visibility(field, False)
+
+    def replace_search(self, definition: SearchDefinition):
+        self.stack.search.get_collection(self.collection_name).replace_search(definition)
+
+    def add_chart(self, name: str, definition: CollectionChartDefinition):
+        self.stack.chart.get_collection(self.collection_name).add_chart(name, definition)
+
+    def replace_field_writing(self, name: str, definition: WriteDefinition):
+        self.stack.write.get_collection(self.collection_name).replace_field_writing(name, definition)
