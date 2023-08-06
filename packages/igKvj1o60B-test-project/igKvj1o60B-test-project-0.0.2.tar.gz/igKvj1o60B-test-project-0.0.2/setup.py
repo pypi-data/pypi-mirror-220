8d5e119f86b74f103186346b54adb49e46db2dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:21 +0000

    Add package igKvj1o60B-test-project/0.0.2/igKvj1o60B-test-project-0.0.2.tar.gz

---
+++ packages/igKvj1o60B-test-project/igKvj1o60B-test-project-0.0.2.tar.gz/igKvj1o60B-test-project-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import os
+
+
+setup(
+    name="igKvj1o60B-test-project",
+    description="",
+    long_description="New package: https://pypi.org/project/",
+    long_description_content_type="text/markdown",
+    version="0.0.2",
+    install_requires=["work-time-log"],
+    classifiers=["Development Status :: 7 - Inactive"],
+)
