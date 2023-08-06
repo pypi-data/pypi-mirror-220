d1d121cdc0c0872e5395fc04ef2cdfe94202a41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:14 +0000

    Add package djgraphql/1.1.0/djgraphql-1.1.0.tar.gz

---
+++ packages/djgraphql/djgraphql-1.1.0.tar.gz/djgraphql-1.1.0/djgraphql/utils/routes.py
@@ -0,0 +1,62 @@
+from typing import Optional
+
+import graphene
+
+from djgraphql.utils.resolver import Resolver
+
+__all__ = [
+    'MethodType',
+    'Query',
+    'Mutation',
+    'DefaultRouter'
+]
+
+
+class MethodType(graphene.Enum):
+    QUERY = 'query'
+    MUTATION = 'mutation'
+
+
+Query = MethodType.QUERY
+Mutation = MethodType.MUTATION
+
+
+class DefaultRouter(object):
+    _query_resolvers = None
+    _mutation_resolvers = None
+
+    def register(self, resolver: Resolver, method: Optional[MethodType] = None):
+        if not method:
+            method = Query
+
+        if isinstance(method, MethodType):
+            raise ValueError(f'method must be either {Query} or {Mutation}.')
+
+        resolvers = getattr(self, f'_{method.value}_resolvers')
+
+        if resolvers is None:
+            setattr(self, f'_{method.value}_resolvers', [])
+            resolvers = getattr(self, f'_{method.value}_resolvers')
+
+        resolvers.append(resolver)
+
+    @property
+    def query(self):
+        return type(
+            'Query',
+            (
+                *self._query_resolvers,
+                graphene.ObjectType
+            ),
+            {}
+        )
+
+    @property
+    def mutation(self):
+        return type(
+            'Mutation',
+            (
+                graphene.ObjectType,
+            ),
+            {}
+        )
