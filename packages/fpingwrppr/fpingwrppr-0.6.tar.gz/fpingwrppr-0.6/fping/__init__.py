b7b607466aa206256e708a80ac42c72e345c5cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:23 +0000

    Add package fpingwrppr/0.6/fpingwrppr-0.6.tar.gz

---
+++ packages/fpingwrppr/fpingwrppr-0.6.tar.gz/fpingwrppr-0.6/fping/__init__.py
@@ -0,0 +1,31 @@
+from fping.backends import fping
+
+back = {"fping": fping.pinger_class}
+
+def get_backend(priority=None, **kwargs):
+    if not priority:
+        priority = ["fping"]
+
+    for name in priority:
+        b = back.get(name)
+        if b:
+            inst = b(**kwargs)
+            if inst.is_available():
+                return inst
+
+    raise RuntimeError("No pinger backends Available")
+
+def ping_one(host):
+    return get_backend().ping_one(host)
+
+def ping_many_updown(hosts):
+    return get_backend().ping_many_updown(hosts)
+
+def ping_many_updown_iter(hosts):
+    return get_backend().ping_many_updown_iter(hosts)
+
+def ping_many(hosts):
+    return get_backend().ping_many(hosts)
+
+def ping_many_iter(hosts):
+    return get_backend().ping_many_iter(hosts)
