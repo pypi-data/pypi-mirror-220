511b4cd08b6490cb77d3bf8bb599db55c06d2f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:35 +0000

    Add package kiri-walkgame/1.1.0/kiri_walkgame-1.1.0-py3-none-any.whl

---
+++ packages/kiri-walkgame/kiri_walkgame-1.1.0-py3-none-any.whl/kiri_walkgame/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 21 10:20:23 2023
+
+@author: anthony
+"""
+from kiri_walkgame import Game
+game = Game()
+screen = game.on_run()
