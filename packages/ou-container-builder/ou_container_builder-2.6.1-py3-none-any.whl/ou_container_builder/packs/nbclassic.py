d2b663abd1cebcfea3e2ef17dbe8d7a24ed5917e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:29 +0000

    Add package ou-container-builder/2.6.1/ou_container_builder-2.6.1-py3-none-any.whl

---
+++ packages/ou-container-builder/ou_container_builder-2.6.1-py3-none-any.whl/ou_container_builder/packs/nbclassic.py
@@ -0,0 +1,64 @@
+"""Pack to install the Nbclassic notebook interface."""
+from warnings import warn
+
+from jinja2 import Environment
+from pydantic import BaseModel
+from typing import Literal
+
+from ou_container_builder.utils import merge_settings
+
+
+class Options(BaseModel):
+    pass
+
+
+class Pack(BaseModel):
+    name: Literal["nbclassic"]
+    options: Options = Options()
+
+
+def process_settings(settings: dict, pack_settings: dict) -> dict:
+    """
+    Process the user-provided settings.
+
+    :param settings: The settings parsed from the configuration file
+    :type settings: dict
+    :param pack_settings: The pack-specific settings parsed from the configuration file
+    :type settings: dict
+    :return: The updated settings
+    :rtype: dict
+    """
+    warn(
+        "Automatically setting Nbclassic as the default is deprecated in 2.2.0 and will be removed in 3.0.0. Instead set server.default_path explicitly.",
+        FutureWarning,
+    )
+    settings = merge_settings(
+        settings,
+        {
+            "packages": {"pip": ["nbclassic>=0.4.8,<1.0.0"]},
+            "server": {"default_path": "/tree"},
+            "scripts": {
+                "build": [
+                    {"commands": ["pip uninstall -y notebook", "jupyter server extension enable --system nbclassic"]}
+                ]
+            },
+        },
+    )
+    return settings
+
+
+def generate_files(context: str, env: Environment, settings: dict, pack_settings: dict) -> dict:
+    """Generate the build files for the nbclassic pack.
+
+    This ensures that the the nbclassic package is installed
+
+    :param context: The context path within which the generation is running
+    :type context: str
+    :param env: The Jinja2 environment to use for loading and rendering templates
+    :type env: :class:`~jinja2.environment.Environment`
+    :param settings: The validated settings
+    :type settings: dict
+    :param pack_settings: The validated pack-specific settings
+    :type settings: dict
+    """
+    pass
