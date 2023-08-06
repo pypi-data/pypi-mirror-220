93731070d618bfe5ff05753771e840b7042b05fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:58 +0000

    Add package udn-songbook/1.1.8/udn_songbook-1.1.8.tar.gz

---
+++ packages/udn-songbook/udn_songbook-1.1.8.tar.gz/udn_songbook-1.1.8/udn_songbook/utils.py
@@ -0,0 +1,45 @@
+# utility functions that aren't really class-specific
+import logging
+import os
+import re
+from string import punctuation
+from typing import Union
+
+
+def make_dir(destdir: str, logger: Union[logging.Logger, None]):
+    """
+    Attempt to create a directory if it doesn't already exist
+    Raise an error if the creation fails
+    """
+    if os.path.exists(destdir):
+        return True
+    else:
+        try:
+            os.makedirs(destdir)
+            return True
+        except (IOError, OSError) as E:
+            logger.exception(f"Unable to create output dir {E.filename} - {E.strerror}")
+            raise
+
+def unpunctuate(name: str, replacement: str = "") -> str:
+    """
+    Simply removes punctuation from the given name, for ease of sorting.
+    Args:
+        name(str): the name you want to unpunctuate
+        replacement(str): what to replace punctuation with.
+    """
+    TRANS = {ord(char): replacement for char in punctuation}
+    return name.translate(TRANS)
+
+
+def safe_filename(filename: str) -> str:
+    """
+    Just simple string translation to remove UNIX-unfriendly characters from filenames
+    removes the following characters from filenames:
+
+    """
+    tt = {ord(char): "_" for char in punctuation if char not in ["#", "-", "_", "."]}
+    # this replaces '#' though, so escape that.
+    tt.update({ord("#"): "_sharp_"})
+
+    return re.sub(r"_+", r"_", filename.translate(tt))
