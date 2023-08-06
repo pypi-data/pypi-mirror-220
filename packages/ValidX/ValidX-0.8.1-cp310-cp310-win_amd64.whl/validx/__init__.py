cc7ee6d1924d1aee8cce1490c87e777beeae74dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:40 +0000

    Add package ValidX/0.8.1/ValidX-0.8.1-cp310-cp310-win_amd64.whl

---
+++ packages/ValidX/ValidX-0.8.1-cp310-cp310-win_amd64.whl/validx/__init__.py
@@ -0,0 +1,86 @@
+from . import exc
+
+try:
+    from .cy import (
+        __impl__,
+        Validator,
+        Int,
+        Float,
+        Decimal,
+        Str,
+        Bytes,
+        Date,
+        Time,
+        Datetime,
+        Bool,
+        List,
+        Set,
+        Tuple,
+        Dict,
+        AllOf,
+        OneOf,
+        LazyRef,
+        Type,
+        Const,
+        Any,
+        classes,
+        instances,
+    )
+except ImportError:  # pragma: no cover
+    from .py import (  # type: ignore
+        __impl__,
+        Validator,
+        Int,
+        Float,
+        Decimal,
+        Str,
+        Bytes,
+        Date,
+        Time,
+        Datetime,
+        Bool,
+        List,
+        Set,
+        Tuple,
+        Dict,
+        AllOf,
+        OneOf,
+        LazyRef,
+        Type,
+        Const,
+        Any,
+        classes,
+        instances,
+    )
+
+
+__all__ = [
+    "exc",
+    "Validator",
+    "Int",
+    "Float",
+    "Decimal",
+    "Str",
+    "Bytes",
+    "Date",
+    "Time",
+    "Datetime",
+    "Bool",
+    "List",
+    "Set",
+    "Tuple",
+    "Dict",
+    "AllOf",
+    "OneOf",
+    "LazyRef",
+    "Type",
+    "Const",
+    "Any",
+    "classes",
+    "instances",
+]
+
+__impl__ = __impl__
+__version__ = "0.8.1"
+__author__ = "Cottonwood Technology <info@cottonwood.tech>"
+__license__ = "BSD"
