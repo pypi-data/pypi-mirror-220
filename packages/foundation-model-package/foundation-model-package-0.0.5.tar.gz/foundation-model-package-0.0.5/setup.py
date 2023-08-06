067ea03bc544d54074ddc67f3d642f4e57cf34bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:52 +0000

    Add package foundation-model-package/0.0.5/foundation-model-package-0.0.5.tar.gz

---
+++ packages/foundation-model-package/foundation-model-package-0.0.5.tar.gz/foundation-model-package-0.0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="foundation-model-package",
+    version="0.0.5",
+    description="Package for the foundation model tool for prompt flow",
+    packages=find_packages(),
+    entry_points={
+        "package_tools": ["foundation_model = foundation_model_pkg.tools.utils:list_package_tools"],
+    },
+    include_package_data=True,   # This line tells setuptools to include files from MANIFEST.in
+)
