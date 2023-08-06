481ec3eeea99e43774894fc975360d6d4d106e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:57 +0000

    Add package hide-frames/1.0.1/hide_frames-1.0.1.tar.gz

---
+++ packages/hide-frames/hide_frames-1.0.1.tar.gz/hide_frames-1.0.1/hide_frames.py
@@ -0,0 +1,20 @@
+from functools import wraps
+
+def hide_nframes(n):
+    def decorator(f):
+        @wraps(f)
+        def _inner(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except Exception as e:
+                tb = e.__traceback__
+                tb = tb.tb_next # manually hide THIS frame
+                for _ in range(n):
+                    tb = tb.tb_next
+                e.__traceback__ = tb
+                raise
+        return _inner
+    return decorator
+
+def hide_frame(f):
+    return hide_nframes(1)(f)
