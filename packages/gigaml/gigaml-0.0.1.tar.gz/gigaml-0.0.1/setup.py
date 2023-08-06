5f34826de41421e3a47f55314a69fcbaad54fb99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:18 +0000

    Add package gigaml/0.0.1/gigaml-0.0.1.tar.gz

---
+++ packages/gigaml/gigaml-0.0.1.tar.gz/gigaml-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gigaml",
+    version="0.0.1",
+    author="gigaml",
+    author_email="founders@giga.do",
+    description="A placeholder package for gigaml",
+    packages=find_packages(),
+)
+
