9553e48d9c79bfa40b86f6b0ad2b1dd786782a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:53 +0000

    Add package fakefbench/5.0.0.dev0/fakefbench-5.0.0.dev0-py3-none-any.whl

---
+++ packages/fakefbench/fakefbench-5.0.0.dev0-py3-none-any.whl/bench/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding = utf-8 -*-
+# @Time:2023/7/22 18:07
+# @Author:BecauseFantasy
+# @File:__init__.py.PY
+# @Software:PyCharm
+VERSION = "5.0.0-dev"
+PROJECT_NAME = "fakefbench"
+FRAPPE_VERSION = None
+current_path = None
+updated_path = None
+LOG_BUFFER = []
+
+
+def set_frappe_version(bench_path="."):
+	from .utils.app import get_current_frappe_version
+
+	global FRAPPE_VERSION
+	if not FRAPPE_VERSION:
+		FRAPPE_VERSION = get_current_frappe_version(bench_path=bench_path)
