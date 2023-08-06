45f1ce58e204f84a86a5fa99e6635ea5b5b91aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:08 +0000

    Add package openxlab-test/0.0.7/openxlab_test-0.0.7-py3-none-any.whl

---
+++ packages/openxlab-test/openxlab_test-0.0.7-py3-none-any.whl/openxlab/model/handler/testdownload.py
@@ -0,0 +1,7 @@
+from openxlab.model.handler.download_file import download
+from openxlab.model import init
+
+
+# inference("mmocr/SVTR", ['./demo_text_ocr.jpg'])
+# download("dongxiaozhuang/digo_02", "model_1", overwrite=False)
+init()
