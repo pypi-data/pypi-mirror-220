a98564c0454c999e4bda7c5d1325ac26e7bd43de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:20 +0000

    Add package apkpatcher-cli/1.3.7/apkpatcher-cli-1.3.7.tar.gz

---
+++ packages/apkpatcher-cli/apkpatcher-cli-1.3.7.tar.gz/apkpatcher-cli-1.3.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+   name='apkpatcher-cli',
+   version='1.3.7',
+   description='Automate the task of patching an apk with frida-gadget. fork of badadaf/apkpatcher',
+   author='Marcel Alexandru Nitan',
+   author_email='nitan.marcel@gmail.com',
+   url='https://github.com/nitanmarcel/apkpatcher',
+   keywords=['FRIDA', 'APK'],
+   scripts=['apkpatcher'],
+   install_requires=[
+       'requests',
+       'appdirs',
+       'install-jdk'
+   ],
+   classifiers=[
+    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+
+   ]
+)
