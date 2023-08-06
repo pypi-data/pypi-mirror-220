c2d7d0476cc6493f162bc06bdd5aaf8410d0bab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:40 +0000

    Add package pulp-deb/2.20.3/pulp_deb-2.20.3-py3-none-any.whl

---
+++ packages/pulp-deb/pulp_deb-2.20.3-py3-none-any.whl/pulp_deb/tests/functional/api/test_crud_remotes.py
@@ -0,0 +1,212 @@
+"""Tests that CRUD deb remotes."""
+from random import choice
+import pytest
+
+from pulpcore.client.pulp_deb.exceptions import ApiException
+
+from pulp_smash import utils
+
+from pulp_deb.tests.functional.constants import DOWNLOAD_POLICIES, DEB_SIGNING_KEY
+from pulp_deb.tests.functional.utils import gen_local_deb_remote
+
+
+@pytest.mark.parallel
+def test_create_remote_repository(deb_remote_custom_data_factory, deb_get_fixture_server_url):
+    """Test creation of the remote."""
+    url = deb_get_fixture_server_url()
+    data = gen_verbose_remote_data(url)
+    remote = deb_remote_custom_data_factory(data)
+
+    for key, val in data.items():
+        if key != "username" and key != "password":
+            assert remote.to_dict()[key] == val
+
+
+@pytest.mark.parallel
+def test_create_remote_repository_with_same_name(
+    deb_remote_custom_data_factory, deb_get_fixture_server_url
+):
+    """Verify whether it is possible to create a remote with the same name."""
+    url = deb_get_fixture_server_url()
+    data = gen_verbose_remote_data(url)
+    deb_remote_custom_data_factory(data)
+
+    with pytest.raises(ApiException) as exc:
+        deb_remote_custom_data_factory(data)
+
+    assert exc.value.status == 400
+
+
+@pytest.mark.parallel
+def test_create_remote_repository_without_url(
+    deb_remote_custom_data_factory, deb_get_fixture_server_url
+):
+    """Verify whether it is possible to create a remote without an URL."""
+    url = deb_get_fixture_server_url()
+    data = gen_verbose_remote_data(url)
+    del data["url"]
+
+    with pytest.raises(ApiException) as exc:
+        deb_remote_custom_data_factory(data)
+
+    assert exc.value.status == 400
+
+
+@pytest.mark.parallel
+def test_read_remote_by_href(
+    deb_get_fixture_server_url,
+    deb_get_remote_by_href,
+    deb_remote_custom_data_factory,
+):
+    """Verify whether it is possible to read a remote repository by its href."""
+    url = deb_get_fixture_server_url()
+    data = gen_verbose_remote_data(url)
+    remote = deb_remote_custom_data_factory(data)
+    read_remote = deb_get_remote_by_href(remote.pulp_href)
+
+    for key, val in remote.to_dict().items():
+        assert read_remote.to_dict()[key] == val
+
+
+@pytest.mark.parallel
+def test_read_remote_by_name(
+    deb_get_fixture_server_url,
+    deb_get_remotes_by_name,
+    deb_remote_custom_data_factory,
+):
+    """Verify whether it is possible to read a remote repository by its name."""
+    url = deb_get_fixture_server_url()
+    data = gen_verbose_remote_data(url)
+    remote = deb_remote_custom_data_factory(data)
+    read_remote = deb_get_remotes_by_name(remote.name)
+
+    assert len(read_remote.results) == 1
+
+    for key, val in remote.to_dict().items():
+        assert read_remote.results[0].to_dict()[key] == val
+
+
+@pytest.mark.parallel
+def test_patch_remote(
+    deb_get_fixture_server_url,
+    deb_get_remote_by_href,
+    deb_patch_remote,
+    deb_remote_custom_data_factory,
+):
+    """Verify whether it is possible to update a remote with PATCH."""
+    url = deb_get_fixture_server_url()
+    data = gen_verbose_remote_data(url)
+    remote = deb_remote_custom_data_factory(data)
+    patch_data = gen_verbose_remote_data(url)
+    deb_patch_remote(remote, patch_data)
+    patch_remote = deb_get_remote_by_href(remote.pulp_href)
+
+    for key, val in patch_data.items():
+        if key != "username" and key != "password":
+            assert patch_remote.to_dict()[key] == val
+        if key == "distributions" or key == "components" or key == "architectures":
+            assert patch_remote.to_dict()[key] != remote.to_dict()[key]
+
+
+@pytest.mark.parallel
+def test_put_remote(
+    deb_get_fixture_server_url,
+    deb_get_remote_by_href,
+    deb_put_remote,
+    deb_remote_custom_data_factory,
+):
+    """Verify whether it is possible to update a remote with PUT."""
+    url = deb_get_fixture_server_url()
+    data = gen_verbose_remote_data(url)
+    remote = deb_remote_custom_data_factory(data)
+    put_data = gen_verbose_remote_data(url)
+    deb_put_remote(remote, put_data)
+    put_remote = deb_get_remote_by_href(remote.pulp_href)
+
+    for key, val in put_data.items():
+        if key != "username" and key != "password":
+            assert put_remote.to_dict()[key] == val
+        if key == "distributions" or key == "components" or key == "architectures":
+            assert put_remote.to_dict()[key] != remote.to_dict()[key]
+
+
+@pytest.mark.parallel
+def test_delete_remote(
+    deb_delete_remote,
+    deb_get_fixture_server_url,
+    deb_get_remote_by_href,
+    deb_remote_custom_data_factory,
+):
+    """Verify whether it is possible to delete a remote."""
+    url = deb_get_fixture_server_url()
+    data = gen_verbose_remote_data(url)
+    remote = deb_remote_custom_data_factory(data)
+    deb_delete_remote(remote)
+
+    with pytest.raises(ApiException) as exc:
+        deb_get_remote_by_href(remote.pulp_href)
+
+    assert exc.value.status == 404
+
+
+@pytest.mark.parallel
+def test_remote_download_policies(
+    deb_get_remote_by_href,
+    deb_get_fixture_server_url,
+    deb_patch_remote,
+    deb_remote_custom_data_factory,
+):
+    """Verify download policy behavior for valid and invalid values."""
+    # Create a remote without a download policy.
+    url = deb_get_fixture_server_url()
+    data = gen_verbose_remote_data(url)
+    del data["policy"]
+    remote = deb_remote_custom_data_factory(data)
+
+    # Verify the creation is successful and the "immediate" policy is applied.
+    assert remote.policy == "immediate"
+
+    # Pick a random policy that is not the default and apply it to the remote.
+    policies = DOWNLOAD_POLICIES
+    changed_policy = choice([item for item in policies if item != "immediate"])
+    deb_patch_remote(remote, {"policy": changed_policy})
+    remote = deb_get_remote_by_href(remote.pulp_href)
+
+    # Verify that the policy change is successful
+    assert remote.policy == changed_policy
+
+    # Create a snapshot of the remote for later reference
+    remote_snapshot = deb_get_remote_by_href(remote.pulp_href)
+
+    # Attempt to change the remote policy to an invalid string
+    with pytest.raises(ApiException) as exc:
+        deb_patch_remote(remote, {"policy": utils.uuid4()})
+    assert exc.value.status == 400
+
+    # Verify that the remote policy remains unchanged
+    remote = deb_get_remote_by_href(remote.pulp_href)
+    assert remote.policy == remote_snapshot.policy
+
+
+def gen_verbose_remote_data(url):
+    """Return a semi-random dict for use in defining a remote.
+
+    For most tests, it's desirable to create remotes with as few attributes
+    as possible, so that the tests can specifically target and attempt to break
+    specific features. This module specifically targets remotes, so it makes
+    sense to provide as many attributes as possible.
+    Note that 'username' and 'password' are write-only attributes.
+    """
+    data = gen_local_deb_remote(url)
+    data.update(
+        {
+            "password": utils.uuid4(),
+            "username": utils.uuid4(),
+            "policy": choice(DOWNLOAD_POLICIES),
+            "distributions": f"{utils.uuid4()} {utils.uuid4()}",
+            "components": f"{utils.uuid4()} {utils.uuid4()}",
+            "architectures": f"{utils.uuid4()} {utils.uuid4()}",
+            "gpgkey": DEB_SIGNING_KEY,
+        }
+    )
+    return data
---
+++ packages/pulp-deb/pulp_deb-2.20.3-py3-none-any.whl/pulp_deb/tests/functional/api/test_download_policies.py
@@ -0,0 +1,90 @@
+"""Tests for Pulp download policies."""
+import pytest
+
+from pulp_smash.pulp3.bindings import delete_orphans
+from pulp_smash.pulp3.utils import (
+    get_added_content_summary,
+    get_content_summary,
+)
+from pulp_deb.tests.functional.constants import DEB_FIXTURE_PACKAGE_COUNT, DEB_FIXTURE_SUMMARY
+
+
+@pytest.mark.parametrize("policy", ["on_demand", "streamed"])
+def test_download_policy(
+    apt_package_api,
+    deb_get_repository_by_href,
+    deb_publication_factory,
+    deb_remote_factory,
+    deb_repository_factory,
+    deb_sync_repository,
+    policy,
+):
+    """Test whether lazy synced content can be accessed with different download policies."""
+    # Create repository and remote and verify latest `repository_version` is 0
+    repo = deb_repository_factory()
+    remote = deb_remote_factory(policy=policy)
+    assert repo.latest_version_href.endswith("/0/")
+
+    # Sync and verify latest `repository_version` is 1
+    deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+    assert repo.latest_version_href.endswith("/1/")
+
+    # Verify the correct amount of content units are available
+    assert DEB_FIXTURE_SUMMARY == get_content_summary(repo.to_dict())
+    assert DEB_FIXTURE_SUMMARY == get_added_content_summary(repo.to_dict())
+
+    # Sync again and verify the same amount of content units are available
+    latest_version_href = repo.latest_version_href
+    deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+    assert repo.latest_version_href == latest_version_href
+    assert DEB_FIXTURE_SUMMARY == get_content_summary(repo.to_dict())
+
+    # Create a publication and verify the `repository_version` is not empty
+    publication = deb_publication_factory(repo, simple=True)
+    assert publication.repository_version is not None
+
+    # Verify the correct amount of packages are available
+    content = apt_package_api.list()
+    assert content.count == DEB_FIXTURE_PACKAGE_COUNT
+
+
+# TODO: Should think of a better approach to test this case.
+@pytest.mark.skip(reason="Currently breaking the CI")
+@pytest.mark.parametrize("policy", ["on_demand", "streamed"])
+def test_lazy_sync_immediate_download_test(
+    artifacts_api_client,
+    deb_get_remote_by_href,
+    deb_get_repository_by_href,
+    deb_patch_remote,
+    deb_remote_factory,
+    deb_repository_factory,
+    deb_sync_repository,
+    policy,
+):
+    """Test whether a immediate sync after a lazy one has all artifacts available."""
+    # Cleanup artifacts
+    NON_LAZY_ARTIFACT_COUNT = 17
+    delete_orphans()
+
+    # Create repository and remote and sync them
+    repo = deb_repository_factory()
+    remote = deb_remote_factory(policy=policy)
+    deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+
+    # Verify amount of artifacts are equal to NON_LAZY_ARTIFACT_COUNT
+    artifacts = artifacts_api_client.list()
+    assert artifacts.count == NON_LAZY_ARTIFACT_COUNT
+
+    # Update remote policy to immediate and verify it is set
+    deb_patch_remote(remote, {"policy": "immediate"})
+    remote = deb_get_remote_by_href(remote.pulp_href)
+    assert remote.policy == "immediate"
+
+    # Sync with updated remote and verify the correct amount artifacts
+    deb_sync_repository(remote, repo)
+    repo = deb_get_repository_by_href(repo.pulp_href)
+    artifacts = artifacts_api_client.list()
+    assert artifacts.count == NON_LAZY_ARTIFACT_COUNT + DEB_FIXTURE_PACKAGE_COUNT
