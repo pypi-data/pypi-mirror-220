f11d27c9edee949ead7017999e3faf836b907633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:21 +0000

    Add package id2xml/1.5.2/id2xml-1.5.2.tar.gz

---
+++ packages/id2xml/id2xml-1.5.2.tar.gz/id2xml-1.5.2/id2xml/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.5.2"
+
+__date__    = "$Date: 2023-07-22 19:34:44 +1200 (Sat, 22 July 2023) $"
+
+__rev__     = "$Rev:$"
+
+__id__      = "$Id: __init__.py 13345 2017-05-14 10:01:04Z henrik@levkowetz.com $"
