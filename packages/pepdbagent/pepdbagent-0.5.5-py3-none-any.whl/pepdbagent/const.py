14b84641e9a4b8d99c8da5e2f9a3aa1cd792e48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:57 +0000

    Add package pepdbagent/0.5.5/pepdbagent-0.5.5-py3-none-any.whl

---
+++ packages/pepdbagent/pepdbagent-0.5.5-py3-none-any.whl/pepdbagent/const.py
@@ -0,0 +1,17 @@
+PKG_NAME = "pepdbagent"
+
+DEFAULT_NAMESPACE = "_"
+DEFAULT_TAG = "default"
+
+DESCRIPTION_KEY = "description"
+NAME_KEY = "name"
+
+# from peppy.const import SAMPLE_RAW_DICT_KEY, SUBSAMPLE_RAW_LIST_KEY
+
+DEFAULT_OFFSET = 0
+DEFAULT_LIMIT = 100
+
+# db_dialects
+POSTGRES_DIALECT = "postgresql"
+
+DEFAULT_LIMIT_INFO = 5
