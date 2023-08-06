49f8d4aa0614eb4f590f8f3b6979aa7d077bf410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:29 +0000

    Add package faasmctl/0.6.0/faasmctl-0.6.0-py3-none-any.whl

---
+++ packages/faasmctl/faasmctl-0.6.0-py3-none-any.whl/faasmctl/util/config.py
@@ -0,0 +1,44 @@
+from configparser import ConfigParser
+from os import environ
+from os.path import exists
+
+BACKEND_INI_STRING = "backend"
+
+
+def get_faasm_ini_file():
+    """
+    Try to read faasm's ini file from the environment, fail otherwise
+    """
+    if "FAASM_INI_FILE" not in environ:
+        raise RuntimeError("No FAASM_INI_FILE env. variable detected!")
+
+    return environ["FAASM_INI_FILE"]
+
+
+def get_faasm_ini_value(ini_file, section, key):
+    if not exists(ini_file):
+        raise RuntimeError("Did not find faasm config at: {}".format(ini_file))
+
+    config = ConfigParser()
+    config.read(ini_file)
+    return config[section].get(key, "")
+
+
+def get_faasm_upload_host_port(ini_file, in_docker=False):
+    if in_docker:
+        host = get_faasm_ini_value(ini_file, "Faasm", "upload_host_in_docker")
+    else:
+        host = get_faasm_ini_value(ini_file, "Faasm", "upload_host")
+    port = get_faasm_ini_value(ini_file, "Faasm", "upload_port")
+
+    return host, port
+
+
+def get_faasm_planner_host_port(ini_file, in_docker=False):
+    if in_docker:
+        host = get_faasm_ini_value(ini_file, "Faasm", "planner_host_in_docker")
+    else:
+        host = get_faasm_ini_value(ini_file, "Faasm", "planner_host")
+    port = get_faasm_ini_value(ini_file, "Faasm", "planner_port")
+
+    return host, port
