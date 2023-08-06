e970961faa725a38d74aa52baf855f24362c54c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:57 +0000

    Add package lXtractor/0.1.3/lxtractor-0.1.3-py3-none-any.whl

---
+++ packages/lXtractor/lxtractor-0.1.3-py3-none-any.whl/lXtractor/core/base.py
@@ -0,0 +1,244 @@
+"""
+Base classes, commong types and functions for the `core` module.
+"""
+from __future__ import annotations
+
+import typing as t
+from abc import ABCMeta, abstractmethod
+from collections import abc, UserDict
+from io import TextIOBase
+from pathlib import Path
+from typing import runtime_checkable
+
+T = t.TypeVar("T")
+_T = t.TypeVar("_T", contravariant=True)
+
+_Fetcher = t.Callable[[t.Iterable[str]], T]
+_Getter = t.Callable[[T, t.Sequence[str]], t.Sequence[str]]
+
+_MapT = t.Dict[int, t.Optional[int]]
+
+
+class AminoAcidDict(UserDict):
+    """
+    Provides mapping between 3->1 and 1->3-letter amino acid residue codes.
+
+    >>> d = AminoAcidDict(any_unk='X')
+    >>> assert d['A'] == 'ALA'
+    >>> assert d['ALA'] == 'A'
+    >>> assert d['XXX'] == 'X'
+    >>> assert d['X'] == 'UNK'
+    >>> assert d['CA'] == 'X'
+
+    """
+
+    def __init__(
+        self, aa1_unk: str = "X", aa3_unk: str = "UNK", any_unk: str | None = None
+    ):
+        """
+        :param aa1_unk: Unknown character when mapping 3->1
+        :param aa3_unk: Unknown character when mapping 1->3
+        :param any_unk: Unknown character when a key doesn't
+            meet 1 or 3 length requirements.
+        """
+
+        self.any_unk = any_unk
+        self.aa1_unk = aa1_unk
+        self.aa3_unk = aa3_unk
+
+        from lXtractor.core.config import _AminoAcids
+
+        self.three21 = dict(_AminoAcids)
+        self.one23 = dict((x[1], x[0]) for x in _AminoAcids)
+
+        super().__init__(**self.three21, **self.one23)
+
+    def __getitem__(self, item: str) -> str:
+        if len(item) == 3:
+            return self.three21.get(item, self.aa1_unk)
+        if len(item) == 1:
+            return self.one23.get(item, self.aa3_unk)
+        if self.any_unk is not None:
+            return self.any_unk
+        raise KeyError(
+            f"Expected 3-sized or 1-sized item, " f"got {len(item)}-sized {item}"
+        )
+
+
+class AbstractResource(metaclass=ABCMeta):
+    """
+    Abstract base class defining basic interface any resource must provide.
+    """
+
+    def __init__(self, resource_path: str | Path, resource_name: str | None):
+        self.name = resource_name
+        self.path = resource_path
+
+    @abstractmethod
+    def read(self):
+        """
+        Read the resource using the :attr:`resource_path`
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def parse(self):
+        """
+        Parse the read resource, so it's ready for usage.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def dump(self, path: Path):
+        """
+        Save the resource under the given `path`.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def fetch(self, url: str):
+        """
+        Download the resource.
+        """
+        raise NotImplementedError
+
+
+@t.runtime_checkable
+class Ord(t.Protocol[_T]):
+    """
+    Any objects defining comparison operators.
+    """
+
+    def __le__(self, other: _T) -> bool:
+        ...
+
+    def __ge__(self, other: _T) -> bool:
+        ...
+
+    def __eq__(self, other: object) -> bool:
+        ...
+
+
+@runtime_checkable
+class SupportsWrite(t.Protocol):
+    """
+    Any object with the `write` method.
+    """
+
+    def write(self, data):
+        """
+        Write the supplied data.
+        """
+
+
+@runtime_checkable
+class AddMethod(t.Protocol):
+    """
+    A callable to add sequences to the aligned ones,
+    preserving the alignment length.
+    """
+
+    def __call__(
+        self,
+        msa: abc.Iterable[tuple[str, str]] | Path,
+        seqs: abc.Iterable[tuple[str, str]],
+        # **kwargs,
+    ) -> abc.Iterable[tuple[str, str]]:
+        ...
+
+
+@runtime_checkable
+class AlignMethod(t.Protocol):
+    """
+    A callable to align arbitrary sequences.
+    """
+
+    def __call__(
+        self, seqs: abc.Iterable[tuple[str, str]] | Path
+    ) -> abc.Iterable[tuple[str, str]]:
+        ...
+
+
+class SeqReader(t.Protocol):
+    """
+    A callable reading sequences into tuples of (header, _seq) pairs.
+    """
+
+    def __call__(
+        self, inp: Path | TextIOBase | abc.Iterable[str]
+    ) -> abc.Iterable[tuple[str, str]]:
+        ...
+
+
+class SeqWriter(t.Protocol):
+    """
+    A callable writing (header, _seq) pairs to disk.
+    """
+
+    def __call__(
+        self,
+        inp: abc.Iterable[tuple[str, str]],
+        out: Path | SupportsWrite,
+        # **kwargs
+    ) -> None:
+        ...
+
+
+class SeqMapper(t.Protocol):
+    """
+    A callable accepting and returning a pair (header, _seq).
+    """
+
+    def __call__(self, seq: tuple[str, str], **kwargs) -> tuple[str, str]:
+        ...
+
+
+class SeqFilter(t.Protocol):
+    """
+    A callable accepting a pair (header, _seq) and returning a boolean.
+    """
+
+    def __call__(self, seq: tuple[str, str], **kwargs) -> bool:
+        ...
+
+
+class UrlGetter(t.Protocol):
+    """
+    A callable accepting some string arguments and turning them into a
+    valid url.
+    """
+
+    def __call__(self, *args) -> str:
+        ...
+
+
+@t.runtime_checkable
+class ApplyTWithArgs(t.Protocol[T]):
+    def __call__(self, x: T, *args, **kwargs) -> T:
+        ...
+
+
+# ApplyT = abc.Callable[[T], T]
+@t.runtime_checkable
+class ApplyT(t.Protocol[T]):
+    def __call__(self, x: T) -> T:
+        ...
+
+
+@t.runtime_checkable
+class FilterT(t.Protocol[T]):
+    def __call__(self, x: T) -> bool:
+        ...
+
+
+@t.runtime_checkable
+class NamedTupleT(t.Protocol, abc.Iterable):
+    def __getattr__(self, item):
+        ...
+
+    def _asdict(self) -> dict[str, t.Any]:
+        ...
+
+
+if __name__ == "__main__":
+    raise RuntimeError
---
+++ packages/lXtractor/lxtractor-0.1.3-py3-none-any.whl/lXtractor/core/ligand.py
@@ -0,0 +1,258 @@
+from __future__ import annotations
+
+import typing as t
+from collections import abc
+
+import biotite.structure as bst
+import numpy as np
+import pandas as pd
+
+from lXtractor.core.config import MetaNames, LigandConfig
+from lXtractor.core.exceptions import FormatError
+from lXtractor.util.structure import (
+    filter_ligand,
+    iter_residue_masks,
+    find_contacts,
+    filter_solvent_extended,
+)
+
+if t.TYPE_CHECKING:
+    from lXtractor.core.structure import GenericStructure
+
+
+class Ligand:
+    """
+    Ligand object is a part of the structure falling under certain criteria.
+
+    Namely, a ligand is a non-polymer and non-solvent molecule or a single
+    monomer (thus, standalone amino acids are considered ligands, while peptides
+    of length >= 2 are not).
+
+    Attributes :attr:`mask` and :attr:`contact_mask` are
+    boolean masks allowing to obtain ligand and ligand-contacting atoms from
+    :attr:`parent`.
+
+    All array-type attributes, have the number of elements equal to the number
+    of atoms in :attr:`parent`.
+
+    .. seealso::
+        `find_ligands`
+
+    Methods ``__repr__`` and ``__str__`` output a string in the format:
+    ``{res_name}_{res_id}:{chain_id}<-({parent})``.
+    """
+
+    def __init__(
+        self,
+        parent: GenericStructure,
+        mask: np.ndarray,
+        contact_mask: np.ndarray,
+        parent_contacts: np.ndarray,
+        ligand_idx: np.ndarray,
+        dist: np.ndaray,
+        meta: dict[str, str] | None = None,
+    ):
+        if not len(parent) == len(mask) == len(contact_mask):
+            raise FormatError(
+                "The number of atoms in parent, the mask size and parent_contacts size "
+                f"must all have the same len. Got {len(parent)}, "
+                f"{len(mask)}, and {len(contact_mask)}, resp."
+            )
+        if not len(parent_contacts) == len(ligand_idx) == len(dist):
+            raise FormatError(
+                "The number of contact atoms, ligand indices and distances must match. "
+                f"Got {len(parent_contacts)}, {len(parent_contacts)}, and {len(dist)}, "
+                "resp."
+            )
+        if len(parent_contacts[parent_contacts != 0]) == 0:
+            raise FormatError(
+                "Ligand must have at least one contact atom in parent structure. Got 0."
+            )
+        ligand_atoms = parent.array[mask]
+        ligand_chains = set(ligand_atoms.chain_id)
+        ligand_resnames = set(ligand_atoms.res_name)
+        ligand_res_ids = set(ligand_atoms.res_id)
+        if len(ligand_chains) > 1:
+            raise FormatError(
+                f"Ligand atoms point to more than one chain: {ligand_chains}"
+            )
+        if len(ligand_resnames) > 1:
+            raise FormatError(
+                f"Ligand atoms point to more than one ligand res name {ligand_resnames}"
+            )
+        if len(ligand_res_ids) > 1:
+            raise FormatError(
+                f"Ligand atoms point to more than one ligand res id {ligand_res_ids}"
+            )
+
+        #: Parent structure.
+        self.parent: GenericStructure = parent
+
+        #: A boolean mask such that when applied to the parent, subsets the
+        #: latter to the ligand residues.
+        self.mask = mask
+
+        #: A boolean mask such that when applied to the parent, subsets the
+        #: latter to its ligand-contacting atoms.
+        self.contact_mask: np.ndarray = contact_mask
+
+        #: An integer array where numbers indicate contact types.
+        #: ``1`` signifies a non-covalent contact, ``2`` -- a covalent one.
+        self.parent_contacts = parent_contacts
+
+        #: An integer array with indices pointing to ligand atoms contacting
+        #: the parent structure.
+        self.ligand_idx = ligand_idx
+
+        #: An array of distances for each ligand-contacting parent's atom.
+        self.dist = dist
+
+        #: A dictionary of meta info.
+        self.meta = meta
+
+    def __str__(self) -> str:
+        return self.id
+
+    def __repr__(self) -> str:
+        return self.id
+
+    @property
+    def id(self) -> str:
+        return f"{self.res_name}_{self.res_id}:{self.chain_id}<-({self.parent})"
+
+    @property
+    def array(self) -> bst.AtomArray:
+        """
+        :return: An array of ligand atoms within :attr:`parent`.
+        """
+        return self.parent.array[self.mask]
+
+    @property
+    def parent_contact_atoms(self) -> bst.AtomArray:
+        """
+        :return: An array of ligand-contacting atoms within :attr:`parent`.
+        """
+        return self.parent.array[self.contact_mask]
+
+    @property
+    def parent_contact_chains(self) -> set[str]:
+        """
+        :return: A set of chain IDs involved in forming contacts with ligand.
+        """
+        return set(self.parent_contact_atoms.chain_id)
+
+    @property
+    def chain_id(self) -> str:
+        """
+        :return: Ligand chain ID.
+        """
+        return self.array.chain_id[0]
+
+    @property
+    def res_name(self) -> str:
+        """
+        :return: Ligand residue name.
+        """
+        return self.array.res_name[0]
+
+    @property
+    def res_id(self) -> int:
+        """
+        :return: Ligand residue number.
+        """
+        return self.array.res_id[0]
+
+    def is_locally_connected(self, mask: np.ndarray, cfg=LigandConfig()) -> bool:
+        """
+        Check whether this ligand is connected to a subset of parent atoms.
+
+        :param mask: A boolean mask to filter parent atoms.
+        :param cfg: Settings defining when a ligand is treated as "connected"
+            to a subset of atoms defined by `mask`.
+        :return: ``True`` if the ligand has at least `min_atom_connections` to
+            :attr:`parent` substructure imposed by the provided `mask`.
+        """
+        contact_atoms = self.parent.array[mask & self.contact_mask]
+        return (
+            len(contact_atoms) >= cfg.min_atom_connections
+            and bst.get_residue_count(contact_atoms) >= cfg.min_res_connections
+        )
+
+    def summary(self, meta: bool = True) -> pd.Series:
+        d = self.meta if meta else {}
+        d["ObjectID"] = self.id
+        d["ParentID"] = self.parent.id
+        return pd.Series(d.values(), index=d.keys())
+
+
+def find_ligands(
+    structure: GenericStructure,
+    cfg: LigandConfig = LigandConfig(),
+) -> abc.Generator[Ligand, None, None]:
+    """
+    Find ligands within the structure. It divides all `structure` into a ligand
+    part and non-ligand part. Namely, the ligand part comprises all non-solvent
+    residues, while residues of any macromolecular polymeric entity make up for
+    a non-ligand part. Then, for each residue within the "ligand part", it
+    calculates the distance to the atoms of the "non-ligand part."
+
+    Finally, a discovered ligand is retained if it has:
+
+    #. Enough atoms.
+    #. Enough connected structure atoms.
+    #. Enough connected structure residues.
+
+    What "enough" means is defined by the supplied `cfg`.
+
+    .. seealso::
+        :func:`lXtractor.util.structure.filter_ligand`
+        :func:`lXtractor.util.structure.filter_solvent_extended`
+        :func:`lXtractor.util.structure.filter_any_polymer`
+        :func:`lXtractor.util.structure.find_contacts`
+        :class:`lXtractor.core.config.LigandConfig`
+
+    :param structure: Arbitrary (generic) structure.
+    :param cfg: Ligand detection config.
+    :return: A generator of :class:`Ligand` objects.
+    """
+    a = structure.array
+    is_ligand = filter_ligand(a)
+    is_solvent = filter_solvent_extended(a)
+
+    if is_ligand.sum() == 0:
+        return
+
+    # Iter over all residues in a structure
+    for m_res in iter_residue_masks(a):
+        # A mask that is a single ligand residue
+        m_ligand = is_ligand & m_res
+
+        # Check whether ligand has enough atoms
+        if np.sum(m_ligand) < cfg.min_atoms:
+            continue
+
+        contacts, dist, ligand_idx = find_contacts(a, m_ligand, cfg.bond_thresholds)
+        contacts[is_ligand | is_solvent] = 0
+        m_contacts = contacts != 0
+
+        # The number of residues connected to a ligand
+        num_residues = bst.get_residue_count(a[m_contacts])
+
+        if (
+            np.sum(m_contacts) < cfg.min_atom_connections
+            or num_residues < cfg.min_res_connections
+        ):
+            continue
+
+        name = a[m_res].res_name[0]
+        meta = {
+            MetaNames.res_name: name,
+            MetaNames.res_id: str(a[m_res].res_id[0]),
+            MetaNames.structure_chain_id: a[m_res].chain_id[0],
+        }
+
+        yield Ligand(structure, m_ligand, m_contacts, contacts, ligand_idx, dist, meta)
+
+
+if __name__ == "__main__":
+    raise RuntimeError
