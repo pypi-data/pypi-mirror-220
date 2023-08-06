1563f7c82648a8437478e349a9776f670a780fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:32 +0000

    Add package getajob/0.3.5/getajob-0.3.5-py3-none-any.whl

---
+++ packages/getajob/getajob-0.3.5-py3-none-any.whl/getajob/vendor/clerk/companies/repository.py
@@ -0,0 +1,53 @@
+import typing as t
+
+from getajob.vendor.firestore.repository import FirestoreDB
+from getajob.vendor.kafka.repository import KafkaProducerRepository
+from getajob.vendor.kafka.models import KafkaEventConfig, KafkaTopic
+from getajob.abstractions.repository import ParentRepository, RepositoryDependencies
+from getajob.abstractions.models import Entity, EntityModels
+
+from .models import (
+    ClerkCompanyWebhookEvent,
+    ClerkCompany,
+    ClerkCompanyDeleted,
+    ClerkCompanyWebhookType,
+)
+
+entity_models = EntityModels(entity=ClerkCompany)
+
+
+class WebhookCompanyRepository(ParentRepository):
+    def __init__(
+        self, db: FirestoreDB, kafka: t.Optional[KafkaProducerRepository] = None
+    ):
+        kafka_event_config = KafkaEventConfig(
+            topic=KafkaTopic.companies,
+            create=True,
+            update=True,
+            delete=True,
+        )
+        super().__init__(
+            RepositoryDependencies(
+                db, Entity.COMPANIES.value, entity_models, kafka, kafka_event_config
+            )
+        )
+
+    def handle_webhook_event(self, event: ClerkCompanyWebhookEvent):
+        event_dict = {
+            ClerkCompanyWebhookType.organization_created: self.create_company,
+            ClerkCompanyWebhookType.organization_updated: self.update_company,
+            ClerkCompanyWebhookType.organization_deleted: self.delete_company,
+        }
+        return event_dict[event.type](event)
+
+    def create_company(self, event: ClerkCompanyWebhookEvent):
+        create_event = ClerkCompany(**event.data)
+        return self.create(data=create_event, provided_id=create_event.id)
+
+    def delete_company(self, event: ClerkCompanyWebhookEvent):
+        delete_event = ClerkCompanyDeleted(**event.data)
+        return self.delete(doc_id=delete_event.id)
+
+    def update_company(self, event: ClerkCompanyWebhookEvent):
+        update_event = ClerkCompany(**event.data)
+        return self.update(doc_id=update_event.id, data=update_event)
