e5de7d6dc231a311a145759f744e9887a45aa25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:54 +0000

    Add package rbc-project/0.12.0/rbc_project-0.12.0-py2.py3-none-any.whl

---
+++ packages/rbc-project/rbc_project-0.12.0-py2.py3-none-any.whl/rbc/stdlib/statistical_functions.py
@@ -0,0 +1,236 @@
+"""
+Array API specification for statistical functions.
+
+https://data-apis.org/array-api/latest/API_specification/statistical_functions.html
+"""
+from rbc.externals.stdio import printf
+from rbc import typesystem
+from rbc.heavydb import ArrayPointer
+from rbc.stdlib import Expose
+from numba.core import extending, types, errors
+from numba.np import numpy_support
+import numpy as np
+
+
+__all__ = [
+    'min', 'max', 'mean', 'prod', 'sum', 'std', 'var'
+]
+
+
+expose = Expose(globals(), 'statistical_functions')
+
+
+def _get_type_limits(eltype):
+    np_dtype = numpy_support.as_dtype(eltype)
+    if isinstance(eltype, types.Integer):
+        return np.iinfo(np_dtype)
+    elif isinstance(eltype, types.Float):
+        return np.finfo(np_dtype)
+    else:
+        msg = 'Type {} not supported'.format(eltype)
+        raise errors.TypingError(msg)
+
+
+@extending.overload(max)
+@expose.implements('max')
+@extending.overload_method(ArrayPointer, 'max')
+def _impl_array_max(x):
+    """
+    Calculates the maximum value of the input array x
+
+    Examples
+    --------
+    IGNORE:
+    >>> from rbc.heavydb import global_heavydb_singleton
+    >>> heavydb = next(global_heavydb_singleton)
+    >>> heavydb.unregister()
+
+    IGNORE
+
+    >>> from rbc.stdlib import array_api
+    >>> @heavydb('double(double[])')
+    ... def array_api_max(arr):
+    ...     return array_api.max(arr)
+    >>> array_api_max([1.0, 3.0, 2.0]).execute()
+    3.0
+
+    """
+    if isinstance(x, ArrayPointer):
+        # the array api standard says this is implementation specific
+        limits = _get_type_limits(x.eltype)
+        t = typesystem.Type.fromobject(x.eltype)
+        if t.is_float:
+            min_value = limits.min
+        elif t.is_int or t.is_uint:
+            min_value = 0 if t.is_uint else limits.min + 1
+        else:
+            raise TypeError(f'Unsupported type {t}')
+
+        def impl(x):
+            if len(x) <= 0:
+                printf("impl_array_max: cannot find max of zero-sized array")  # noqa: E501
+                return min_value
+            m = x[0]
+            for i in range(len(x)):
+                v = x[i]
+                if v > m:
+                    m = v
+            return m
+        return impl
+
+
+@extending.overload(min)
+@expose.implements('min')
+@extending.overload_method(ArrayPointer, 'min')
+def _impl_array_min(x):
+    """
+    Calculates the minimum value of the input array x.
+
+    Examples
+    --------
+    IGNORE:
+    >>> from rbc.heavydb import global_heavydb_singleton
+    >>> heavydb = next(global_heavydb_singleton)
+    >>> heavydb.unregister()
+
+    IGNORE
+
+    >>> from rbc.stdlib import array_api
+    >>> @heavydb('double(double[])')
+    ... def array_api_min(arr):
+    ...     return array_api.min(arr)
+    >>> array_api_min([1.0, 3.0, 2.0]).execute()
+    1.0
+
+    """
+    if isinstance(x, ArrayPointer):
+        max_value = _get_type_limits(x.eltype).max
+
+        def impl(x):
+            if len(x) <= 0:
+                printf("impl_array_min: cannot find min of zero-sized array")  # noqa: E501
+                return max_value
+            m = x[0]
+            for i in range(len(x)):
+                v = x[i]
+                if v < m:
+                    m = v
+            return m
+        return impl
+
+
+@extending.overload(sum)
+@expose.implements('sum')
+@extending.overload_method(ArrayPointer, 'sum')
+def _impl_np_sum(a):
+    """
+    Calculates the sum of the input array x.
+
+    Examples
+    --------
+    IGNORE:
+    >>> from rbc.heavydb import global_heavydb_singleton
+    >>> heavydb = next(global_heavydb_singleton)
+    >>> heavydb.unregister()
+
+    IGNORE
+
+    >>> from rbc.stdlib import array_api
+    >>> @heavydb('double(double[])')
+    ... def array_api_sum(arr):
+    ...     return array_api.sum(arr)
+    >>> array_api_sum([1.0, 3.0, 2.0]).execute()
+    6.0
+
+    """
+    if isinstance(a, ArrayPointer):
+        def impl(a):
+            s = 0
+            n = len(a)
+            for i in range(n):
+                s += a[i]
+            return s
+        return impl
+
+
+@expose.implements('prod')
+@extending.overload_method(ArrayPointer, 'prod')
+def _impl_np_prod(a):
+    """
+    Calculates the product of input array x elements.
+
+    Examples
+    --------
+    IGNORE:
+    >>> from rbc.heavydb import global_heavydb_singleton
+    >>> heavydb = next(global_heavydb_singleton)
+    >>> heavydb.unregister()
+
+    IGNORE
+
+    >>> from rbc.stdlib import array_api
+    >>> @heavydb('double(double[])')
+    ... def array_api_prod(arr):
+    ...     return array_api.prod(arr)
+    >>> array_api_prod([4.0, 3.0, 2.0]).execute()
+    24.0
+
+    """
+    if isinstance(a, ArrayPointer):
+        def impl(a):
+            s = 1
+            n = len(a)
+            for i in range(n):
+                s *= a[i]
+            return s
+        return impl
+
+
+@expose.implements('mean')
+@extending.overload_method(ArrayPointer, 'mean')
+def _impl_array_mean(x):
+    """
+    Calculates the arithmetic mean of the input array x.
+
+    Examples
+    --------
+    IGNORE:
+    >>> from rbc.heavydb import global_heavydb_singleton
+    >>> heavydb = next(global_heavydb_singleton)
+    >>> heavydb.unregister()
+
+    IGNORE
+
+    >>> from rbc.stdlib import array_api
+    >>> @heavydb('double(double[])')
+    ... def array_api_mean(arr):
+    ...     return array_api.mean(arr)
+    >>> array_api_mean([1.0, 3.0, 2.0]).execute()
+    2.0
+
+    """
+    zero_value = np.nan
+
+    if isinstance(x, ArrayPointer):
+        def impl(x):
+            if len(x) == 0:
+                printf("Mean of empty array\n")
+                return zero_value
+            return sum(x) / len(x)
+        return impl
+
+
+@expose.not_implemented('std')
+def _impl_array_std(x, axis=None, correction=0.0, keepdims=False):
+    """
+    Calculates the standard deviation of the input array x.
+    """
+    pass
+
+
+@expose.not_implemented('var')
+def _impl_array_var(x, axis=None, correction=0.0, keepdims=False):
+    """
+    Calculates the variance of the input array x.
+    """
+    pass
