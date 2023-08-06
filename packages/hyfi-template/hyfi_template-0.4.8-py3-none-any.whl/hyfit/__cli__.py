ce7809609c100ba0d5d6494e4baa4381486f96c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:10 +0000

    Add package hyfi-template/0.4.8/hyfi_template-0.4.8-py3-none-any.whl

---
+++ packages/hyfi-template/hyfi_template-0.4.8-py3-none-any.whl/hyfit/__cli__.py
@@ -0,0 +1,16 @@
+"""Command line interface for hyfit"""
+
+# Importing the libraries
+import sys
+
+from hyfi import about, hydra_main
+
+
+def main() -> None:
+    """Main function for the CLI"""
+    sys.argv.append(f"--config-path={about.config_path}")
+    hydra_main()
+
+
+if __name__ == "__main__":
+    main()
