7efbeded7271f0b4c6bad58a121bc668b6be0495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:14 +0000

    Add package testpocexecution001/0.1/testpocexecution001-0.1.tar.gz

---
+++ packages/testpocexecution001/testpocexecution001-0.1.tar.gz/testpocexecution001-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="testpocexecution001",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies required for your package here
+    ],
+)
