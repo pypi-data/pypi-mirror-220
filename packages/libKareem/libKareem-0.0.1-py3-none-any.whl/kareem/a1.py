a15938233fcf603cbbd4741084ff586010cdf6e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:50 +0000

    Add package libKareem/0.0.1/libKareem-0.0.1-py3-none-any.whl

---
+++ packages/libKareem/libKareem-0.0.1-py3-none-any.whl/kareem/a1.py
@@ -0,0 +1,7 @@
+def sum(n1,n2):
+    c=n1+n2
+    print("The result is:",c)
+
+def sub(n1,n2):
+    c=n1-n2
+    print("The result is:",c)
---
+++ packages/libKareem/libKareem-0.0.1-py3-none-any.whl/kareem/a2.py
@@ -0,0 +1,7 @@
+def mul(n1,n2):
+    c=n1*n2
+    print("The result is:",c)
+
+def div(n1,n2):
+    c=n1/n2
+    print("The result is:",c)
