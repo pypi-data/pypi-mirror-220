9d56cec077957d05b8e8e1186f76202377fd0f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:01 +0000

    Add package bojaxns/1.0.1/bojaxns-1.0.1.tar.gz

---
+++ packages/bojaxns/bojaxns-1.0.1.tar.gz/bojaxns-1.0.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup_requires = []
+# with open("requirements.txt", 'r') as fh:
+#     for line in fh:
+#         setup_requires.append(line)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='bojaxns',
+      version='1.0.1',
+      description='Bayesian Optimisation with JAXNS',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/joshuaalbert/bojax",
+      author='Joshua G. Albert',
+      author_email='albert@strw.leidenuniv.nl',
+      setup_requires=setup_requires,
+      tests_require=[
+          'pytest>=2.8',
+      ],
+      package_dir={'': './'},
+      packages=find_packages('./'),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: Apache Software License",
+          "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.9',
+      )
