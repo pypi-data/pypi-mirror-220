822046ae13658fbcfcf34a825c5597ac29744b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:59 +0000

    Add package st-selection-header/0.0.4/st_selection_header-0.0.4.tar.gz

---
+++ packages/st-selection-header/st_selection_header-0.0.4.tar.gz/st_selection_header-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="st_selection_header",
+    version="0.0.4",
+    author="Elliot Glas",
+    author_email="elliot.glas@viscando.com",
+    description="Panel for changing data",
+    long_description="Panel for changing data",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+)
