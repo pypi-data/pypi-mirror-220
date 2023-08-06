717a487bde9a4370b190c4e20f356a05c1a4a0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:02 +0000

    Add package lilacai/0.0.2/lilacai-0.0.2.tar.gz

---
+++ packages/lilacai/lilacai-0.0.2.tar.gz/lilacai-0.0.2/src/embeddings/sbert.py
@@ -0,0 +1,70 @@
+"""Sentence-BERT embeddings. Open-source models, designed to run on device."""
+import functools
+import os
+from typing import TYPE_CHECKING, Iterable, Optional, cast
+
+from typing_extensions import override
+
+from ..config import data_path
+from ..schema import Item, RichData
+from ..signals.signal import TextEmbeddingSignal
+from ..signals.splitters.chunk_splitter import split_text
+from ..utils import log
+from .embedding import compute_split_embeddings
+
+if TYPE_CHECKING:
+  from sentence_transformers import SentenceTransformer
+
+# The `all-mpnet-base-v2` model provides the best quality, while `all-MiniLM-L6-v2`` is 5 times
+# faster and still offers good quality. See https://www.sbert.net/docs/pretrained_models.html#sentence-embedding-models/
+MINI_LM_MODEL = 'all-MiniLM-L6-v2'
+
+SBERT_DEFAULT_BATCH_SIZE = 64
+# Maps a tuple of model name and device to the optimal batch size, found empirically.
+SBERT_OPTIMAL_BATCH_SIZE: dict[tuple[str, str], int] = {
+  (MINI_LM_MODEL, 'mps'): 256,
+}
+
+MODEL_NAME = MINI_LM_MODEL
+
+
+@functools.cache
+def _sbert() -> tuple[Optional[str], 'SentenceTransformer']:
+  try:
+    import torch
+    from sentence_transformers import SentenceTransformer
+  except ImportError:
+    raise ImportError('Could not import the "sentence_transformers" python package. '
+                      'Please install it with `pip install sentence-transformers`.')
+  preferred_device: Optional[str] = None
+  if torch.backends.mps.is_available():
+    preferred_device = 'mps'
+  elif not torch.backends.mps.is_built():
+    log('MPS not available because the current PyTorch install was not built with MPS enabled.')
+  return preferred_device, SentenceTransformer(
+    MODEL_NAME, device=preferred_device, cache_folder=os.path.join(data_path(), '.cache'))
+
+
+def _optimal_batch_size(preferred_device: Optional[str]) -> int:
+  model_device = (MODEL_NAME, str(preferred_device))
+  if model_device in SBERT_OPTIMAL_BATCH_SIZE:
+    return SBERT_OPTIMAL_BATCH_SIZE[model_device]
+  return SBERT_DEFAULT_BATCH_SIZE
+
+
+class SBERT(TextEmbeddingSignal):
+  """Computes embeddings using Sentence-BERT library."""
+
+  name = 'sbert'
+  display_name = 'SBERT Embeddings'
+
+  @override
+  def compute(self, docs: Iterable[RichData]) -> Iterable[Item]:
+    """Call the embedding function."""
+    preferred_device, model = _sbert()
+    batch_size = _optimal_batch_size(preferred_device)
+
+    embed_fn = model.encode
+    split_fn = split_text if self._split else None
+    docs = cast(Iterable[str], docs)
+    yield from compute_split_embeddings(docs, batch_size, embed_fn=embed_fn, split_fn=split_fn)
---
+++ packages/lilacai/lilacai-0.0.2.tar.gz/lilacai-0.0.2/src/signals/text_statistics.py
@@ -0,0 +1,79 @@
+"""Compute text statistics for a document."""
+from typing import TYPE_CHECKING, Iterable, Optional, cast
+
+from typing_extensions import override
+
+from ..schema import Field, Item, RichData, field
+from ..utils import chunks
+from .signal import TextSignal
+
+SPACY_LANG_MODEL = 'en_core_web_sm'
+SPACY_BATCH_SIZE = 128
+
+NUM_CHARS = 'num_characters'
+READABILITY = 'readability'
+TYPE_TOKEN_RATIO = 'type_token_ratio'
+
+if TYPE_CHECKING:
+  from spacy import Language
+  from spacy.tokens import Doc
+
+
+class TextStatisticsSignal(TextSignal):
+  """Compute text statistics for a document such as readability scores, type-token-ratio, etc.."""
+  name = 'text_statistics'
+  display_name = 'Text Statistics'
+
+  _lang: Optional['Language'] = None
+
+  @override
+  def fields(self) -> Field:
+    return field(fields={
+      NUM_CHARS: 'int32',
+      READABILITY: 'float32',
+      TYPE_TOKEN_RATIO: 'float32',
+    })
+
+  @override
+  def setup(self) -> None:
+    try:
+      import spacy
+    except ImportError:
+      raise ImportError('Could not import the "spacy" python package. '
+                        'Please install it with `pip install spacy`.')
+
+    if not spacy.util.is_package(SPACY_LANG_MODEL):
+      spacy.cli.download(SPACY_LANG_MODEL)
+    self._lang = spacy.load(
+      SPACY_LANG_MODEL,
+      disable=[
+        'parser', 'tagger', 'ner', 'lemmatizer', 'textcat', 'custom', 'tok2vec', 'attribute_ruler'
+      ])
+
+  @override
+  def compute(self, data: Iterable[RichData]) -> Iterable[Optional[Item]]:
+    try:
+      import textacy
+      from textacy import text_stats
+    except ImportError:
+      raise ImportError('Could not import the "textacy" python package. '
+                        'Please install it with `pip install textacy`.')
+    for batch in chunks(data, SPACY_BATCH_SIZE):
+      # Replace None with empty strings to avoid spacy errors.
+      batch = [x or '' for x in batch]
+      # See https://textacy.readthedocs.io/en/0.11.0/api_reference/text_stats.html for a list of
+      # available statistics.
+      corpus = textacy.Corpus(lang=self._lang, data=batch)
+      for doc in cast(Iterable['Doc'], corpus):
+        if not len(doc):
+          yield None
+          continue
+        readability = text_stats.readability.automated_readability_index(doc)
+        ttr = text_stats.diversity.ttr(doc)
+        num_chars = text_stats.basics.n_chars(doc)
+
+        yield {
+          NUM_CHARS: num_chars,
+          READABILITY: readability,
+          TYPE_TOKEN_RATIO: ttr,
+        }
