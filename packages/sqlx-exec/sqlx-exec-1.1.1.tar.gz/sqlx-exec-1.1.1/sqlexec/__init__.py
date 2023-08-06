3659975d9f7515686d235cffcdcbe958427737ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:37 +0000

    Add package sqlx-exec/1.1.1/sqlx-exec-1.1.1.tar.gz

---
+++ packages/sqlx-exec/sqlx-exec-1.1.1.tar.gz/sqlx-exec-1.1.1/sqlexec/__init__.py
@@ -0,0 +1,23 @@
+from .exec import (
+    init_engin,
+    connection,
+    transaction,
+    with_connection,
+    with_transaction,
+    execute,
+    insert,
+    save,
+    save_sql,
+    batch_insert,
+    batch_execute,
+    get,
+    select,
+    select_one,
+    select_page,
+    query,
+    query_one,
+    query_page,
+    get_connection
+)
+
+from .engin import Engin
