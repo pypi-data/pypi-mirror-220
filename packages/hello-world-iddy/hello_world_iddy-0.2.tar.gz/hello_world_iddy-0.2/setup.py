4f871960a2347aa178f5278f3a796828b35db558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:49 +0000

    Add package hello-world-iddy/0.2/hello_world_iddy-0.2.tar.gz

---
+++ packages/hello-world-iddy/hello_world_iddy-0.2.tar.gz/hello_world_iddy-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hello_world_iddy",
+    version="0.2",
+    packages=find_packages(),
+    author="Idriss Animashaun",
+    author_email="idriss.animashaun@intel.com",
+    description="Test",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/iddy-ani/hello_world_iddy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        # List your project dependencies here
+    ],
+    python_requires='>=3.10',
+)
