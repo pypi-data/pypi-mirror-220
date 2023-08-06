295da67d891ee2cce2eeee49b93dea4ba53ce755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:35 +0000

    Add package buscador/0.10/buscador-0.10.tar.gz

---
+++ packages/buscador/buscador-0.10.tar.gz/buscador-0.10/README.md
@@ -0,0 +1,16 @@
+# buscador
+
+Every time you want to update your package later on, upload a new version to github, create a new release as we just discussed, specify a new release tag and copy-paste the link to Source into the setup.py file (do not forget to also increment the version number).
+
+https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
+
+https://dillinger.io
+
+New Version*
+. Simply upload your new code to github, create a new release, then adapt the setup.py file (new download_url — according to your new release tag, new version), then run the setup.py and the twin command again (navigate to your folder first!)
+
+- python3 setup.py sdist
+- twine upload dist/*
+
+or if Vs already exist 
+- twine upload --skip-existing dist/*
---
+++ packages/buscador/buscador-0.10.tar.gz/buscador-0.10/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name="buscador",
+    packages=["buscador"],
+    version="0.10",
+    license="MIT",
+    description="Just a collection of helpful tools",
+    author="Samuel Cook",
+    author_email="samcook23@hotmail.com",
+    url="https://github.com/SamuelBCook",
+    download_url="https://github.com/SamuelBCook/buscador/archive/refs/tags/v0.10.tar.gz",
+    keywords=["tools", "json", "easy"],
+    install_requires=[
+        "pandas",
+        "boto3",
+        "botocore",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",  # either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
