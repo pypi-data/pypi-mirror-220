62de6f7a5d8ad63b074cba081b4991a4f95a7742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:04 +0000

    Add package shimpy/0.0.1/shimpy-0.0.1-py3-none-any.whl

---
+++ packages/shimpy/shimpy-0.0.1-py3-none-any.whl/shimpy/cli.py
@@ -0,0 +1,19 @@
+"""Shimpy"""
+
+import time
+import arrow
+import click
+
+from .node import commands as node
+from .account import commands as account
+from .address import commands as address
+from .nft import commands as nft
+
+@click.group()
+def shimpy():
+    pass
+
+shimpy.add_command(node.info)
+shimpy.add_command(account.account)
+shimpy.add_command(address.address)
+shimpy.add_command(nft.nft)
