14015e4445115368fa9eb59d7a033c14a6a5b8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:43 +0000

    Add package py-ptsl/101.0.0/py_ptsl-101.0.0-py3-none-any.whl

---
+++ packages/py-ptsl/py_ptsl-101.0.0-py3-none-any.whl/ptsl/ops/export_session_info_as_text.py
@@ -0,0 +1,12 @@
+from ptsl.ops import Operation
+
+
+# There is a documented bug with this, it won't return a string
+# if you select ESI_string as the output_type
+#
+# https://duc.avid.com/showthread.php?t=422971
+class ExportSessionInfoAsText(Operation):
+
+    @classmethod
+    def response_body(cls):
+        return None
