3bc57900f4fc17988e3cf86e12451b0e6ad70739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:09 +0000

    Add package IDsys/0.0.1/IDsys-0.0.1-py3-none-any.whl

---
+++ packages/IDsys/IDsys-0.0.1-py3-none-any.whl/mypackage/ID.py
@@ -0,0 +1,41 @@
+class IdSys:
+    def __init__(self):
+        self.idLength = 4
+        self.ids = 0
+        self.elements = []
+        self.myId = self.setId(self)
+        self.type = "IdSys"
+
+    def setId(self, obj):
+        myIdTmp = str(self.ids)
+        nbZero = self.idLength - len(myIdTmp)
+        myId = ""
+        for i in range(nbZero):
+            myId += "0"
+        myId += myIdTmp
+        self.ids += 1
+        self.elements.append(obj)
+        print(myId)
+
+        return myId
+
+    def getElement(self, myId):
+        return self.elements[int(myId)]
+
+    def getElements(self):
+        elements = []
+        for i in range(len(self.elements)):
+            elements.append(self.getElement(i).type)
+        return elements
+
+
+class Element:
+    def __init__(self, idSys, obj):
+        self.idSys = idSys
+        self.myId = self.idSys.setId(self)
+        self.type = obj.__class__.__name__
+
+
+class ObjectExemple(Element):
+    def __init__(self):
+        self.type = "ObjectExempleType"
