e652287685c7dfd9905a75224a531fef2f6f81b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:13 +0000

    Add package spekk/1.0.0/spekk-1.0.0.tar.gz

---
+++ packages/spekk/spekk-1.0.0.tar.gz/spekk-1.0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(
+    name="spekk",
+    version="1.0.0",
+    description="General speccing library",
+    author="Magnus Dalen Kvalevåg",
+    author_email="magnus.kvalevag@ntnu.no",
+    packages=find_packages(),
+    install_requires=["numpy"],
+)
