4258966cd022bbf3bacf9f7ad52e3d83b3b5bb12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:05 +0000

    Add package hotpot-zzy/0.3.1.0/hotpot-zzy-0.3.1.0.tar.gz

---
+++ packages/hotpot-zzy/hotpot-zzy-0.3.1.0.tar.gz/hotpot-zzy-0.3.1.0/hotpot/__init__.py
@@ -0,0 +1,20 @@
+"""
+python v3.7.9
+@Project: hotpot
+@File   : __init__.py.py
+@Author : Zhiyuan Zhang
+@Date   : 2023/3/14
+@Time   : 4:06
+"""
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+hp_root = os.path.abspath(os.path.dirname(__file__))
+data_root = os.path.abspath(os.path.join(hp_root, 'data'))
+
+from hotpot.cheminfo import Molecule
+from hotpot.bundle import MolBundle
+
+__version__ = '0.3.1.0'
