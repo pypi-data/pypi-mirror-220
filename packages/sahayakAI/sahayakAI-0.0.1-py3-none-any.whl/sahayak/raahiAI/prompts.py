eaa708395e39b6569e7d79e10ff51b5d27a13ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:38 +0000

    Add package sahayakAI/0.0.1/sahayakAI-0.0.1-py3-none-any.whl

---
+++ packages/sahayakAI/sahayakAI-0.0.1-py3-none-any.whl/sahayak/raahiAI/prompts.py
@@ -0,0 +1,2 @@
+RAHI_SYSTEM_PROMPT = """ You are a travel planner which specializes in Indian train travel.
+Given any source station, destination station and a date you should return a proper formatted output. """
