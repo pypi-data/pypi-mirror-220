1190c2155f677524d13095e11a6afdc4eaff6b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:05 +0000

    Add package pygwalker/0.2.0a4/pygwalker-0.2.0a4-py3-none-any.whl

---
+++ packages/pygwalker/pygwalker-0.2.0a4-py3-none-any.whl/pygwalker/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+from pygwalker.utils.randoms import rand_str as __rand_str
+from pygwalker_utils.config import get_config as __get_config
+
+__version__ = "0.2.0a4"
+__hash__ = __rand_str()
+
+from pygwalker.api.walker import walk
+from pygwalker.api.gwalker import GWalker
+from pygwalker.api.html import to_html
+from pygwalker.data_parsers.base import FieldSpec
+
+
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+
+if __get_config('privacy')[0] == 'offline':
+    logging.info("Running in offline mode. There might be newer releases available. Please check at https://github.com/Kanaries/pygwalker or https://pypi.org/project/pygwalker.")
+else:
+    from pygwalker.services.check_update import check_update
+    check_update()
