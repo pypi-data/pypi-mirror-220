1ee4033367cc8eb4e025b88c9a5d05fbf7f9da3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:33 +0000

    Add package pyinjector/1.3.0/pyinjector-1.3.0-cp311-cp311-macosx_10_9_universal2.whl

---
+++ packages/pyinjector/pyinjector-1.3.0-cp311-cp311-macosx_10_9_universal2.whl/pyinjector/__init__.py
@@ -0,0 +1,17 @@
+from .api import inject, attach, PyInjectorError, LibraryNotFoundException, InjectorError
+
+from types import ModuleType
+
+
+def legacy_pyinjector_import():
+    # A truly disgusting hack to cover for an import mistake in hypno<1.0.1
+    from sys import modules
+    pyinjector = ModuleType('pyinjector.pyinjector')
+    pyinjector.__package__ = "pyinjector"
+    pyinjector.InjectorError = InjectorError
+    modules[pyinjector.__name__] = pyinjector
+
+
+legacy_pyinjector_import()
+
+__all__ = ['inject', 'attach', 'PyInjectorError', 'LibraryNotFoundException', 'InjectorError']
