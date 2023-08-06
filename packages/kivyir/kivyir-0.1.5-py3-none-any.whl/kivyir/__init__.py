6bf7492dad84ebff865c44667f3337fe589276a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:28 +0000

    Add package kivyir/0.1.5/kivyir-0.1.5-py3-none-any.whl

---
+++ packages/kivyir/kivyir-0.1.5-py3-none-any.whl/kivyir/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "navid nasiri"
+__github__ = "https://github.com/goldaaa"
+__gmail__ = "goldaaa.program@gmail.com"
+
+__all__ = ('Config', 'ConfigBase', 'IrLabel', 'IrTextInput')
+
+from .ConfigBase import *
+from .IrLabel import *
+from .IrTextInput import *
