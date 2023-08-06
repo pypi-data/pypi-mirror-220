30602d6843feb82f9a9e26c0c5dcf412078cd2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:00 +0000

    Add package minecraft-script/0.1.31/minecraft_script-0.1.31.tar.gz

---
+++ packages/minecraft-script/minecraft_script-0.1.31.tar.gz/minecraft_script-0.1.31/minecraft_script/__init__.py
@@ -0,0 +1,19 @@
+from .interpreter import parse_line
+
+version = "0.1.31"
+
+
+def parse_text(text: str):
+    for line in text.split('\n'):
+        parse_line(line)
+
+
+def parse_file(filename: str):
+    filename = filename if '.' in filename else f'{filename}.mcs'  # add extension if missing
+    try:
+        with open(filename, 'rt') as file:
+            file_contents = file.read()
+    except FileNotFoundError:
+        print(f'Failed to start process: Couldn\'t find file {filename !r}.')
+
+    parse_text(file_contents)
---
+++ packages/minecraft-script/minecraft_script-0.1.31.tar.gz/minecraft_script-0.1.31/minecraft_script/shell_commands.py
@@ -0,0 +1,63 @@
+from . import parse_file
+import os
+
+shell_function_names = [
+    'sh_help',
+    'sh_run', 'sh_build',
+]
+
+
+def sh_build(mcs_file: str, parent_folder: str = None, *args) -> None:
+    parent_folder = parent_folder if parent_folder else os.getcwd()
+    mcs_file = f'{mcs_file}.mcs' if '.' not in mcs_file else mcs_file  # check for existing extension
+    try:
+        build_file = open(mcs_file, 'rt')
+    except FileNotFoundError:
+        print(f'File {mcs_file} not found.')
+        return
+    print(f'{build_file = }\n{parent_folder = }')  # placeholder; put build code here :)
+
+
+def sh_run_file_iteration(filename: str, *args) -> None:
+    parse_confirm = input(f'Do you wish to parse {filename}? [y/n]: ')
+    if parse_confirm.lower() in ['y', 'yes']:
+        parse_file(filename)
+
+    elif parse_confirm.lower() in ['n', 'no']:
+        pass
+    else:
+        sh_run_file_iteration(filename)
+
+
+def sh_run(*filenames) -> None:
+    if not filenames:
+        valid_files = filter(check_extension, os.listdir(os.getcwd()))
+        for file in valid_files:
+            sh_run_file_iteration(file)
+    else:
+        for file in filenames:
+            parse_file(file)
+
+
+def sh_help(*args) -> None:
+    help_message = """
+#-------------------------------HELP PAGE-------------------------------#
+    
+    
+- help: display this help page!
+    
+- run *[file(s): optional]: run file(s) and output result to terminal.
+If filename is omitted, a prompt will ask you to choose between mcs
+files in work directory (if there are any).
+    
+- build [file] [destination: optional]: build the mcs file
+into a functional datapack! 
+    
+    
+#-----------------------------------------------------------------------#
+    """
+    print(help_message)
+
+
+def check_extension(filename: str) -> bool:
+    return filename.split('.')[-1] == 'mcs'
