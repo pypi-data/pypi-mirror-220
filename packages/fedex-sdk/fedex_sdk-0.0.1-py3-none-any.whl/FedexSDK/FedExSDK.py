9a58933ed29cde8e2839661643491535789de62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:09 +0000

    Add package fedex-sdk/0.0.1/fedex_sdk-0.0.1-py3-none-any.whl

---
+++ packages/fedex-sdk/fedex_sdk-0.0.1-py3-none-any.whl/FedexSDK/FedExSDK.py
@@ -0,0 +1,94 @@
+import datetime
+import json
+import os
+from dataclasses import dataclass
+from requests_oauthlib import OAuth2Session
+import requests
+
+@dataclass
+class APIType:
+    Production: str = "Production"
+    Test: str = "Test"
+
+@dataclass
+class ImageType:
+    Letter_Head: str = "LETTER_HEAD"
+    Signature: str = "SIGNATURE"
+
+class FedExSDK:
+
+    def __init__(self, session: OAuth2Session) -> None:
+        if not isinstance(session, OAuth2Session):
+            raise Exception("Please Requests Session")
+        
+        self.session = session
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {session.access_token}"
+        }
+
+    def create_shipment(self, data: dict):
+        url = "https://apis.fedex.com/ship/v1/shipments"
+
+        res = self.session.post(url, json=data, headers=self.headers)
+        
+        now_date = datetime.datetime.now()
+        print(res.json())
+        with open(f"{now_date.strftime('%d_%m_%Y_tracking_numbers.txt')}", "a") as f:
+            f.write(
+                f"{res.json()['output']['transactionShipments'][0]['shipmentDocuments'][0]['trackingNumber']}:{data['requestedShipment']['recipients'][0]['contact']['personName']}\n"
+            )
+        
+        json.dump(res.json(), open("data.json", "w", encoding="utf-8"), ensure_ascii=False)
+
+    
+    def add_image(self, reference_id: str, image_name: str, image_index: str, image_type: ImageType, image_path: str):
+        name, ext = os.path.splitext(image_path)
+        data = {
+            "document": {
+                "referenceId": reference_id,
+                "name": image_name,
+                "contentType": f"image/{ext}",
+                "meta":{
+                    "imageType": image_type,
+                    "imageIndex": image_index
+                }
+            },
+            "rules":{
+                "workflowName":"LetterheadSignature"
+            }
+        }
+        files = {
+            "attachment": open(image_path, "rb").read()
+        }
+        url = "https://documentapi.prod.fedex.com/documents/v1/lhsimages/upload"
+        
+        headers = self.headers
+        headers["Content-Type"] = "multipart/form-data"
+        
+        res = self.session.post(url, data=data, headers=headers, files=files)
+        print(res.json())
+    
+    @classmethod
+    def authorize(cls, client_id: str, client_secret: str, api_type: APIType = APIType.Test) -> OAuth2Session:
+        if api_type == APIType.Production:
+            url = "https://apis.fedex.com/oauth/token"
+        elif api_type == APIType.Test:
+            url = "https://apis.sandbox.fedex.com/oauth/token"
+        else:
+            raise ValueError("Please Correct APIType!")
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        
+        data = {
+            "grant_type": "client_credentials",
+            "client_id": client_id,
+            "client_secret": client_secret
+        }
+
+        response = requests.request("POST", url, data=data, headers=headers)
+        print(response.json())
+        return OAuth2Session(client_id=client_id, token=response.json())
+
+
