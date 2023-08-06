7694e73ea051e2641e89beabd6e4fe1a89fac058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:52 +0000

    Add package email-phone-validator/1.0.0/email_phone_validator-1.0.0.tar.gz

---
+++ packages/email-phone-validator/email_phone_validator-1.0.0.tar.gz/email_phone_validator-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='email_phone_validator',
+    version='1.0.0',
+    description='A simple Python package for email and phone number validation',
+    author='Yonatan Dev', 
+    author_email='yonatan@yourhost.cloud',
+    packages=find_packages(),
+    install_requires=[],
+)
