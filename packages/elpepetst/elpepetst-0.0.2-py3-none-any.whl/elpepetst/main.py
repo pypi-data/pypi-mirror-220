4b475a1656b4f263c84aa942b076997c69a2144e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:34 +0000

    Add package elpepetst/0.0.2/elpepetst-0.0.2-py3-none-any.whl

---
+++ packages/elpepetst/elpepetst-0.0.2-py3-none-any.whl/elpepetst/main.py
@@ -0,0 +1,19 @@
+"""Main module for the package. This is the entry point for the package."""
+import json
+
+import requests
+
+
+def test_pkg_function():
+    """Makes a test api request to a placeholder api and prints result"""
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/1", timeout=5)
+    print(response.json())
+
+
+def test_json_function():
+    """Makes a test api request to a placeholder api and prints result"""
+    # read json file
+
+    with open("assets/static.json", encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        print(data)
