186bb958785988a511e73f7a30d2c380202ca49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:34 +0000

    Add package DBLite3/0.1.3.1/DBLite3-0.1.3.1.tar.gz

---
+++ packages/DBLite3/DBLite3-0.1.3.1.tar.gz/DBLite3-0.1.3.1/DBLite3/_alter.py
@@ -0,0 +1,41 @@
+import os
+
+from DBLite3._funcs import _open_db, _save_db
+
+
+def alter_object(db_name: str, collection: str, object_old: str, object_new: str) -> None:
+    """
+    The function makes changes to the name of the object
+    :param db_name: the name of the database to be modified
+    :param collection: the name of the collection to be modified
+    :param object_old: the old name of the object to be modified
+    :param object_new: the new name of the object to be modified
+    :return: None
+    """
+    DATABASE = _open_db(db_name=db_name)
+    DATABASE[collection][object_new] = DATABASE[collection][object_old]
+    del (DATABASE[collection][object_old])
+    _save_db(db_name=db_name, DB=DATABASE)
+
+
+def alter_collection(db_name: str, collection_new: str, collection_old: str) -> None:
+    """
+    The function makes changes to the name of the collection
+    :param db_name: the name of the database to be modified
+    :param collection_new: the new name of the collection to be modified
+    :param collection_old: the old name of the collection to be modified
+    :return: None
+    """
+    DATABASE = _open_db(db_name=db_name)
+    DATABASE[collection_new] = DATABASE.pop(collection_old)
+    _save_db(db_name=db_name, DB=DATABASE)
+
+
+def alter_db(db_name_old: str, db_name_new: str) -> None:
+    """
+    The function makes changes to the name of the database
+    :param db_name_old: the old name of the database to be modified
+    :param db_name_new: the new name of the database to be modified
+    :return: None
+    """
+    os.rename(src=f'{db_name_old}.json', dst=f'{db_name_new}.json')
---
+++ packages/DBLite3/DBLite3-0.1.3.1.tar.gz/DBLite3-0.1.3.1/DBLite3/_update.py
@@ -0,0 +1,34 @@
+from DBLite3._funcs import _open_db, _save_db, _get_value_index
+
+
+def update_value_by_id(db_name: str, collection: str, object: str, id: int, value) -> None:
+    """
+    The function updates the single value by the given identifier
+    :param db_name: the name of the database to be modified
+    :param collection: the name of the collection to be modified
+    :param object: the name of the object to be modified
+    :param id: identifier of the value to be modified
+    :param value: new value
+    :return: None
+    """
+    DATABASE = _open_db(db_name=db_name)
+    index = _get_value_index(db_name=db_name, collection=collection, object=object, id=id)
+    DATABASE[collection][object]['values'][index][1] = value
+    _save_db(db_name=db_name, DB=DATABASE)
+
+
+def update_values_by_id(db_name: str, collection: str, object: str, id: list, values: list) -> None:
+    """
+    The function updates the many values by the given identifiers
+    :param db_name: the name of the database to be modified
+    :param collection: the name of the collection to be modified
+    :param object: the name of the object to be modified
+    :param id: list of identifiers of the values to be modified
+    :param values: list of the new values
+    :return: None
+    """
+    DATABASE = _open_db(db_name=db_name)
+    for i, id in enumerate(id):
+        index = _get_value_index(db_name=db_name, collection=collection, object=object, id=id)
+        DATABASE[collection][object]['values'][index][1] = values[i]
+    _save_db(db_name=db_name, DB=DATABASE)
