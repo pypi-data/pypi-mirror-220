8ca37000ecc6affabbf9980bc8a0d8d4fee0a6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:00 +0000

    Add package ckanext-admin-panel/0.0.1/ckanext-admin-panel-0.0.1.tar.gz

---
+++ packages/ckanext-admin-panel/ckanext-admin-panel-0.0.1.tar.gz/ckanext-admin-panel-0.0.1/ckanext/admin_panel/plugin.py
@@ -0,0 +1,25 @@
+import ckan.plugins as p
+import ckan.plugins.toolkit as tk
+
+from ckanext.admin_panel.views import get_blueprints
+
+
+@tk.blanket.helpers
+@tk.blanket.auth_functions
+class AdminPanelPlugin(p.SingletonPlugin):
+    p.implements(p.IConfigurer)
+    p.implements(p.IBlueprint)
+    p.implements(p.IAuthFunctions)
+    p.implements(p.ITemplateHelpers)
+
+    # IConfigurer
+
+    def update_config(self, config_: tk.CKANConfig):
+        tk.add_template_directory(config_, "templates")
+        tk.add_public_directory(config_, "public")
+        tk.add_resource("assets", "admin_panel")
+
+    # IBlueprint
+
+    def get_blueprint(self):
+        return get_blueprints()
