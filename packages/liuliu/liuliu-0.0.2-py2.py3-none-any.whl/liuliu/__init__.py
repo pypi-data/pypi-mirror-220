3e83bebdc6b308fbe14e9466f12dc37d035a61dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:55 +0000

    Add package liuliu/0.0.2/liuliu-0.0.2-py2.py3-none-any.whl

---
+++ packages/liuliu/liuliu-0.0.2-py2.py3-none-any.whl/liuliu/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding = utf-8 -*-
+# @Time:2023/7/22 18:07
+# @Author:BecauseFantasy
+# @File:__init__.py.PY
+# @Software:PyCharm
---
+++ packages/liuliu/liuliu-0.0.2-py2.py3-none-any.whl/liuliu/core.py
@@ -0,0 +1,12 @@
+# -*- coding = utf-8 -*-
+# @Time:2023/7/22 18:08
+# @Author:BecauseFantasy
+# @File:core.py.PY
+# @Software:PyCharm
+# -*- coding: utf8 -*-
+
+# def hello(name: str = "world"):
+#     return f"hello {name} ."
+
+ikun = "哎呦！你干嘛"
+print(ikun)
