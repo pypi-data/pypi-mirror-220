ed6eecd2504ee965b8260b69ef4b9252e95ac6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:45 +0000

    Add package bec-lib/0.13.2/bec_lib-0.13.2.tar.gz

---
+++ packages/bec-lib/bec_lib-0.13.2.tar.gz/bec_lib-0.13.2/bec_lib/core/utils.py
@@ -0,0 +1,13 @@
+import functools
+
+
+def threadlocked(fcn):
+    """Ensure that the thread acquires and releases the lock."""
+
+    @functools.wraps(fcn)
+    def wrapper(self, *args, **kwargs):
+        # pylint: disable=protected-access
+        with self._lock:
+            return fcn(self, *args, **kwargs)
+
+    return wrapper
