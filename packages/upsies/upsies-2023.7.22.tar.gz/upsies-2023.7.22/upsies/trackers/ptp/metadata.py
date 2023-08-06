77879eeeb97058c1f9eff3a6ca3425ac32759ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:06 +0000

    Add package upsies/2023.7.22/upsies-2023.7.22.tar.gz

---
+++ packages/upsies/upsies-2023.7.22.tar.gz/upsies-2023.7.22/upsies/trackers/ptp/metadata.py
@@ -0,0 +1,92 @@
+"""
+Hardcoded PTP metadata from upload.php
+"""
+
+
+ptp_tags = (
+    'action',
+    'adventure',
+    'animation',
+    'arthouse',
+    'asian',
+    'biography',
+    'camp',
+    'comedy',
+    'crime',
+    'cult',
+    'documentary',
+    'drama',
+    'experimental',
+    'exploitation',
+    'family',
+    'fantasy',
+    'film.noir',
+    'history',
+    'horror',
+    'martial.arts',
+    'musical',
+    'mystery',
+    'performance',
+    'philosophy',
+    'politics',
+    'romance',
+    'sci.fi',
+    'short',
+    'silent',
+    'sport',
+    'thriller',
+    'video.art',
+    'war',
+    'western',
+)
+
+
+ptp_subtitles = {
+    # Special codes
+    'No Subtitles': '44',
+    'en (forced)': '50',
+
+    # TODO: No idea what intertitles are exactly and how to detect them.
+    # 'English Intertitles': '51',
+
+    # Regular languages
+    'ar': '22',     # Arabic
+    'bg': '29',     # Bulgarian
+    'zh': '14',     # Chinese
+    'hr': '23',     # Croatian
+    'cs': '30',     # Czech
+    'da': '10',     # Danish
+    'nl': '9',      # Dutch
+    'en': '3',      # English
+    'et': '38',     # Estonian
+    'fi': '15',     # Finnish
+    'fr': '5',      # French
+    'de': '6',      # German
+    'el': '26',     # Greek
+    'he': '40',     # Hebrew
+    'hi': '41',     # Hindi
+    'hu': '24',     # Hungarian
+    'is': '28',     # Icelandic
+    'id': '47',     # Indonesian
+    'it': '16',     # Italian
+    'ja': '8',      # Japanese
+    'ko': '19',     # Korean
+    'lv': '37',     # Latvian
+    'lt': '39',     # Lithuanian
+    'no': '12',     # Norwegian
+    'fa': '52',     # Persian
+    'pl': '17',     # Polish
+    'pt': '21',     # Portuguese
+    'pt-BR': '49',  # Brazilian Port.
+    'ro': '13',     # Romanian
+    'ru': '7',      # Russian
+    'sr': '31',     # Serbian
+    'sk': '42',     # Slovak
+    'sl': '43',     # Slovenian
+    'es': '4',      # Spanish
+    'sv': '11',     # Swedish
+    'th': '20',     # Thai
+    'tr': '18',     # Turkish
+    'uk': '34',     # Ukrainian
+    'vi': '25',     # Vietnamese
+}
