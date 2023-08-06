c21e5b7fe1fba262ed2ca259fa97909f665899c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:07 +0000

    Add package ctranslate2/3.17.1/ctranslate2-3.17.1-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/ctranslate2/ctranslate2-3.17.1-cp38-cp38-macosx_10_9_x86_64.whl/ctranslate2/specs/attention_spec.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+from ctranslate2.specs import common_spec, model_spec
+
+
+class MultiHeadAttentionSpec(model_spec.LayerSpec):
+    def __init__(
+        self,
+        self_attention=False,
+        relative_position=False,
+        relative_attention_bias=False,
+        rms_norm=False,
+        rotary_dim=None,
+        rotary_interleave=True,
+        num_heads_kv=None,
+    ):
+        self.queries_scale = model_spec.OPTIONAL
+
+        self.layer_norm = common_spec.LayerNormSpec(rms_norm=rms_norm)
+        self.linear = [
+            common_spec.LinearSpec() for _ in range(2 if self_attention else 3)
+        ]
+
+        if relative_position:
+            self.relative_position_keys = None
+            self.relative_position_values = None
+
+        if relative_attention_bias:
+            self.relative_attention_bias = None
+            self.relative_attention_max_distance = None
+
+        if rotary_dim is not None:
+            self.rotary_dim = np.dtype("int32").type(rotary_dim)
+            self.rotary_interleave = rotary_interleave
+
+        if num_heads_kv is not None:
+            self.num_heads_kv = np.dtype("int32").type(num_heads_kv)
