79235b4eca13abb327be2f34c057388828e291ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:33 +0000

    Add package helios-client/0.3.20230718.post1/helios-client-0.3.20230718.post1.tar.gz

---
+++ packages/helios-client/helios-client-0.3.20230718.post1.tar.gz/helios-client-0.3.20230718.post1/Source/helios/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#
+#   Helios, intelligent music.
+#   Copyright (C) 2015-2022 Cartesian Theatre. All rights reserved.
+#
+
+# Import everything in this module...
+from .client import client
+import helios.chunked_upload
+import helios.client
+import helios.exceptions
+import helios.requests
+import helios.responses
+import helios.__version__
