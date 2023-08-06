3a79bca4e7bdac80cee7e2669f459fe500032a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:45 +0000

    Add package oarepo-vocabularies/2.0.11/oarepo-vocabularies-2.0.11.tar.gz

---
+++ packages/oarepo-vocabularies/oarepo-vocabularies-2.0.11.tar.gz/oarepo-vocabularies-2.0.11/oarepo_vocabularies/ui/resources/resource.py
@@ -0,0 +1,58 @@
+from flask import g
+from flask_babelex import lazy_gettext as _
+
+from flask_resources import from_conf, request_parser
+from invenio_records_resources.resources.records.resource import (
+    request_read_args,
+    request_view_args,
+)
+from oarepo_ui.resources.resource import RecordsUIResource
+
+request_vocabulary_args = request_parser(
+    from_conf("request_vocabulary_type_args"), location="view_args"
+)
+
+
+class InvenioVocabulariesUIResource(RecordsUIResource):
+    @request_read_args
+    @request_view_args
+    @request_vocabulary_args
+    def detail(self):
+        return super().detail()
+
+    @request_read_args
+    @request_view_args
+    @request_vocabulary_args
+    def export(self):
+        return super().export()
+
+    @request_read_args
+    @request_view_args
+    @request_vocabulary_args
+    def search(self):
+        return super().search()
+
+    # TODO: !IMPORTANT!: needs to be enabled before production deployment
+    # @login_required
+    @request_read_args
+    @request_view_args
+    @request_vocabulary_args
+    def create(self):
+        return super().create()
+
+    # TODO: !IMPORTANT!: needs to be enabled before production deployment
+    # @login_required
+    @request_read_args
+    @request_view_args
+    @request_vocabulary_args
+    def edit(self):
+        return super().edit()
+
+    def _get_record(self, resource_requestctx):
+        return self._api_service.read(
+            g.identity,
+            (
+                resource_requestctx.view_args["vocabulary_type"],
+                resource_requestctx.view_args["pid_value"],
+            ),
+        )
