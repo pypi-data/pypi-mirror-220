cd17a0f547db4f6bf6a9f036760f8e73ea9a7020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:40 +0000

    Add package toRST/0.0.9/toRST-0.0.9.tar.gz

---
+++ packages/toRST/toRST-0.0.9.tar.gz/toRST-0.0.9/funcs/funcs.py
@@ -0,0 +1,28 @@
+from csv2rst.csv2rst import from_csv
+from json2rst.json2rst import from_json
+
+
+def get_extension(file: str) -> str:
+
+    return file.split(".")[-1].lower()
+
+def handle_file(file: str) -> list[list]:
+
+    extension = get_extension(file)
+    if extension == "csv":
+        return from_csv(file)
+    elif extension == "json":
+        return from_json(file)
+    else:
+        raise ValueError(f"{extension} files not currently supported")
+
+def handle_raw(input: list[list or dict]) -> list[list]:
+
+    if not isinstance(input, list):
+        raise ValueError("input must be of type list[list] or list[dict]")
+    else:
+        if isinstance(input[0], list):
+            return input
+        elif isinstance(input[0], dict):
+            return [[key for key in input[0].keys()]] + [list(row.values()) for row in input[1:]]
+        else: raise ValueError("input must be of type list[list] or list[dict]")
