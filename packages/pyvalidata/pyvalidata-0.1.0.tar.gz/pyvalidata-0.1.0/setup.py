bd92134d416aeb610339646b6a0c71102b669420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:17 +0000

    Add package pyvalidata/0.1.0/pyvalidata-0.1.0.tar.gz

---
+++ packages/pyvalidata/pyvalidata-0.1.0.tar.gz/pyvalidata-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='pyvalidata',
+    version='0.1.0',
+    description='A Python package for data validation',
+    author='Eklavya Tomar',
+    author_email='eklavyaprogramming@gmail.com',
+    url='https://github.com/EklavyaT/pyvalidata',
+    packages=['pyvalidata'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
