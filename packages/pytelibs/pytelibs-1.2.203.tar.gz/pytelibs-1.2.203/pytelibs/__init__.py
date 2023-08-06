e921040fe55190209e1dc5576cc58e0ed2d09ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:33 +0000

    Add package pytelibs/1.2.203/pytelibs-1.2.203.tar.gz

---
+++ packages/pytelibs/pytelibs-1.2.203.tar.gz/pytelibs-1.2.203/pytelibs/__init__.py
@@ -0,0 +1,22 @@
+# pytel < https://t.me/kastaid >
+# Copyright (C) 2023-present kastaid
+#
+# This file is a part of < https://github.com/kastaid/pytel/ >
+# Please read the GNU Affero General Public License in
+# < https://github.com/kastaid/pytel/blob/main/LICENSE/ >.
+
+from ._journal import (
+    developer,
+    _c,
+    _g,
+    _l,
+    _d,
+    gsc,
+    gse,
+    _i,
+    _kastaot,
+    _chpytel,
+    GCAST_BLACKLIST,
+)
+from .version import __version__
+
