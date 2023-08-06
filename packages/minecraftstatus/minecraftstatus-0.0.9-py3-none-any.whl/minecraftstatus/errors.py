dc5fe8a885bc7f510a6e10c32f32ab2c9b499e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:46 +0000

    Add package minecraftstatus/0.0.9/minecraftstatus-0.0.9-py3-none-any.whl

---
+++ packages/minecraftstatus/minecraftstatus-0.0.9-py3-none-any.whl/minecraftstatus/errors.py
@@ -0,0 +1,31 @@
+__all__ = (
+    "ServerNotFound",
+    "BadTextFormation",
+)
+
+
+class ServerNotFound(Exception):
+    """
+    Exception raised when a server is not found or is offline. Has the following attribute:
+
+    **Note:** the error is :class:`minecraftstatus.errors.ServerNotFound`. Due to an issue, it's incorrectly shown in the documentation above.
+
+    Attributes
+    ----------
+    address: (:class:`str`)the address of the server passed.
+    """
+
+    def __init__(self, address: str):
+        self.address = address
+        super().__init__(f"Server '{self.address}' not found or is offline.")
+
+
+class BadTextFormation(Exception):
+    """
+    Exception raised when the characters passed are not between 1 and 30 for some endpoints.
+
+    **Note:** the error is :class:`minecraftstatus.errors.BadTextFormation`. Due to an issue, it's incorrectly shown in the documentation above.
+    """
+
+    def __init__(self):
+        super().__init__("Character limit is between 1 and 30.")
