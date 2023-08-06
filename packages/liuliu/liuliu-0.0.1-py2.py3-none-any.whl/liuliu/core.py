ee4a209f5052e3ed19730e6f5eb3089f3f8c36ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:41 +0000

    Add package liuliu/0.0.1/liuliu-0.0.1-py2.py3-none-any.whl

---
+++ packages/liuliu/liuliu-0.0.1-py2.py3-none-any.whl/liuliu/core.py
@@ -0,0 +1,9 @@
+# -*- coding = utf-8 -*-
+# @Time:2023/7/22 18:08
+# @Author:BecauseFantasy
+# @File:core.py.PY
+# @Software:PyCharm
+# -*- coding: utf8 -*-
+
+def hello(name: str = "world"):
+    return f"hello {name} ."
