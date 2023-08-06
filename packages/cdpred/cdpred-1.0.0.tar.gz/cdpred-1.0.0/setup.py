8ecda7ef9bfe5c1939c18fb5773eca130b372bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:56 +0000

    Add package cdpred/1.0.0/cdpred-1.0.0.tar.gz

---
+++ packages/cdpred/cdpred-1.0.0.tar.gz/cdpred-1.0.0/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+	name = 'cdpred',
+	version= '1.0.0',
+	description='A python package', 
+	author='G.P.S Raghava',
+	packages=find_packages(),
+) 
