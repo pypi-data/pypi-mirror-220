d99a5bf760e6d3caba98be1527a73afcc7cc862e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:05 +0000

    Add package kulipdf/1.0/kulipdf-1.0.tar.gz

---
+++ packages/kulipdf/kulipdf-1.0.tar.gz/kulipdf-1.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="kulipdf",
+    version=1.0,
+    long_description="",
+    packages=setuptools.find_packages(["tests", "data"])
+)
