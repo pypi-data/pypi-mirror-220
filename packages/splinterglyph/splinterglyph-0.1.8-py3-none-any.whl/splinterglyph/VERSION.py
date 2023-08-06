62281bd1b85dc68aa907e418ea575212f4f5de3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:46 +0000

    Add package splinterglyph/0.1.8/splinterglyph-0.1.8-py3-none-any.whl

---
+++ packages/splinterglyph/splinterglyph-0.1.8-py3-none-any.whl/splinterglyph/VERSION.py
@@ -0,0 +1,5 @@
+# Software version.  (Note: there are two distinct versioning systems here:
+#   (*) the version associated with the software in Git (this version)
+#   (*) the file format version (listed in the encrypted file's header)
+
+version = "0.1.8"
