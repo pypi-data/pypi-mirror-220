0f72ec7758e73bbf768ecb1f02f2fa1ed7e0e8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:22 +0000

    Add package septem-circumstantiae/0.0.3/septem-circumstantiae-0.0.3.tar.gz

---
+++ packages/septem-circumstantiae/septem-circumstantiae-0.0.3.tar.gz/septem-circumstantiae-0.0.3/src/septem-circumstantiae/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+
+from .human_events import HumanEvent
+from .natural_events import NaturalEvent
+
+__all__ = [
+    'HumanEvent',
+    'NaturalEvent'
+]
