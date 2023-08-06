f0ed84e333baad179b90d7498e8c7d00aeceeafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:39 +0000

    Add package roblox-pyc/1.21.87/roblox-pyc-1.21.87.tar.gz

---
+++ packages/roblox-pyc/roblox-pyc-1.21.87.tar.gz/roblox-pyc-1.21.87/robloxpyc/colortext.py
@@ -0,0 +1,36 @@
+import sys
+
+def red(text, styles = []):
+    return color(text, 31, styles)
+def green(text, styles = []):
+    return color(text, 32, styles)
+def yellow(text, styles = []):
+    return color(text, 33, styles)
+def blue(text, styles = []):
+    return color(text, 34, styles)
+def magenta(text, styles = []):
+    return color(text, 35, styles)
+def cyan(text, styles = []):
+    return color(text, 36, styles)
+def white(text, styles = []):
+    return color(text, 37, styles)
+def rainbow_text(text):
+    colors = ['\033[31m', '\033[33m', '\033[32m', '\033[36m', '\033[34m', '\033[35m']
+    i = 0
+    for char in text:
+        sys.stdout.write(colors[i % len(colors)] + char)
+        i += 1
+    sys.stdout.write('\033[0m')
+    sys.stdout.flush()
+def color(text, color, styles = []):
+    style = ""
+    for s in styles:
+        if s == "bold":
+            style += "1;"
+        elif s == "underline":
+            style += "4;"
+        elif s == "reverse":
+            style += "7;"
+        elif s == "concealed":
+            style += "8;"
+    return "\033["+style+str(color)+"m"+text+"\033[0m"
