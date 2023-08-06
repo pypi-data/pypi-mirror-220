b97c9a482de03e6aff850f992d1f369cd4e39caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:49 +0000

    Add package pgsqlx/1.0.8/pgsqlx-1.0.8.tar.gz

---
+++ packages/pgsqlx/pgsqlx-1.0.8.tar.gz/pgsqlx-1.0.8/pgsqlx/dbx.py
@@ -0,0 +1,172 @@
+import re
+from . import db
+from . import sql_holder as holder
+from .support import get_batch_args, SqlAction
+from .log_support import logger, sql_id_log, page_sql_id_log
+
+
+def init_db(user='root', password='', database='test', host='127.0.0.1', port=3306, show_sql=False, mapper_path='mapper', **kwargs):
+    holder.load_mapper(mapper_path)
+    db.init_db(user, password, database, host, port, show_sql, **kwargs)
+
+
+def insert(table: str, **kwargs):
+    """
+    Insert data into table, return effect rowcount.
+    :param table: table name
+    :param kwargs: {'name': '张三', 'age': 20}
+    return: Effect rowcount
+    """
+    return db.insert(table, **kwargs)
+
+
+def save(table: str, **kwargs):
+    """
+    Insert data into table, return primary key.
+    :param table: table name
+    :param kwargs: {'name': '张三', 'age': 20}
+    :return: Primary key
+    """
+    return db.save(table, **kwargs)
+
+
+def mapper_save(sql_id: str, *args, **kwargs):
+    """
+    Execute insert SQL, return primary key.
+    :return: Primary key
+    """
+    sql_id_log('dbx.mapper_save', sql_id, *args, **kwargs)
+    sql_model = holder.get_sql_model(sql_id)
+    pk_seq = sql_model.pk_seq
+    if 'pk_seq' in kwargs:
+        pk_seq = kwargs.pop('pk_seq')
+    sql, args = holder.do_get_sql(sql_model, False, None, *args, **kwargs)
+    assert SqlAction.INSERT.value in sql.lower(), 'Only insert sql can return primary key.'
+    if not pk_seq:
+        table = re.search('(?<=into )\w+', sql.lower())
+        pk_seq = "{}_id_seq".format(table.group())
+        logger.warning("Exec func 'mysqlx.dbx.mapper_save': 'pk_seq' is None, will use default '{}'.".format(pk_seq))
+
+    return db.do_save(pk_seq, sql, *args)
+
+
+def execute(sql_id: str, *args, **kwargs):
+    """
+    Execute SQL.
+    sql: INSERT INTO person(name, age) VALUES(?, ?)  -->  args: ('张三', 20)
+         INSERT INTO person(name, age) VALUES(:name,:age)  -->  kwargs: {'name': '张三', 'age': 20}
+    """
+    sql_id_log('dbx.execute', sql_id, *args, **kwargs)
+    sql, args = holder.get_sql(sql_id, *args, **kwargs)
+    return db.do_execute(sql, *args)
+
+
+def batch_insert(table: str, *args):
+    """
+    Batch insert
+    :param table: table name
+    :param args: All number must have same key. [{'name': '张三', 'age': 20}, {'name': '李四', 'age': 28}]
+    :return: Effect row count
+    """
+    return db.batch_insert(table, *args)
+
+
+def batch_execute(sql_id: str, *args):
+    """
+    Batch execute
+    sql: INSERT INTO person(name, age) VALUES(?, ?)  -->  args: [('张三', 20), ('李四', 28)]
+         INSERT INTO person(name, age) VALUES(:name,:age)  -->  args: [{'name': '张三', 'age': 20}, {'name': '李四', 'age': 28}]
+    :return: Effect row count
+    """
+    logger.debug("Exec func 'mysqlx.dbx.%s' \n\t sql_id: '%s' \n\t args: %s" % ('batch_execute', sql_id, args))
+    args = get_batch_args(*args)
+    sql, _ = holder.do_get_sql(holder.get_sql_model(sql_id), True, None, *args)
+    return db.batch_execute(sql, *args)
+
+
+# ----------------------------------------------------------Query function------------------------------------------------------------------
+def get(sql_id: str, *args, **kwargs):
+    """
+    Execute select SQL and expected one int and only one int result. Automatically add 'limit ?' behind the sql statement if not.
+    MultiColumnsError: Expect only one column.
+    sql: SELECT count(1) FROM person WHERE name=? and age=? limit 1  -->  args: ('张三', 20)
+         SELECT count(1) FROM person WHERE name=:name and age=:age limit 1  -->  kwargs: ('张三', 20) --> kwargs: {'name': '张三', 'age': 20}
+    """
+    sql_id_log('get', sql_id, *args, **kwargs)
+    sql, args = holder.get_sql(sql_id, *args, **kwargs)
+    return db.do_get(sql, *args)
+
+
+def query(sql_id: str, *args, **kwargs):
+    """
+    Execute select SQL and return list or empty list if no result.
+    sql: SELECT * FROM person WHERE name=? and age=?  -->  args: ('张三', 20)
+         SELECT * FROM person WHERE name=:name and age=:age  -->  kwargs: ('张三', 20) --> kwargs: {'name': '张三', 'age': 20}
+    """
+    sql_id_log('query', sql_id, *args, **kwargs)
+    sql, args = holder.get_sql(sql_id, *args, **kwargs)
+    return db.do_query(sql, *args)
+
+
+def query_one(sql_id: str, *args, **kwargs):
+    """
+    Execute select SQL and expected one row result(dict). Automatically add 'limit ?' behind the sql statement if not.
+    If no result found, return None.
+    If multiple results found, the first one returned.
+    sql: SELECT * FROM person WHERE name=? and age=? limit 1 -->  args: ('张三', 20)
+         SELECT * FROM person WHERE name=:name and age=:age limit 1  -->  kwargs: ('张三', 20) --> kwargs: {'name': '张三', 'age': 20}
+    """
+    sql_id_log('query_one', sql_id, *args, **kwargs)
+    sql, args = holder.get_sql(sql_id, *args, **kwargs)
+    return db.do_query_one(sql, *args)
+
+
+def select(sql_id: str, *args, **kwargs):
+    """
+    Execute select SQL and return list(tuple) or empty list if no result.
+    sql: SELECT * FROM person WHERE name=? and age=?  -->  args: ('张三', 20)
+         SELECT * FROM person WHERE name=:name and age=:age   -->  kwargs: ('张三', 20) --> kwargs: {'name': '张三', 'age': 20}
+    """
+    sql_id_log('select', sql_id, *args, **kwargs)
+    sql, args = holder.get_sql(sql_id, *args, **kwargs)
+    return db.do_select(sql, *args)
+
+
+def select_one(sql_id: str, *args, **kwargs):
+    """
+    Execute select SQL and expected one row result(tuple). Automatically add 'limit ?' behind the sql statement if not.
+    If no result found, return None.
+    If multiple results found, the first one returned.
+    sql: SELECT * FROM person WHERE name=? and age=? limit 1  -->  args: ('张三', 20)
+         SELECT * FROM person WHERE name=:name and age=:age limit 1  -->  kwargs: ('张三', 20) --> kwargs: {'name': '张三', 'age': 20}
+    """
+    sql_id_log('select_one', sql_id, *args, **kwargs)
+    sql, args = holder.get_sql(sql_id, *args, **kwargs)
+    return db.do_select_one(sql, *args)
+
+
+def query_page(sql_id: str, page_num=1, page_size=10, *args, **kwargs):
+    """
+    Execute select SQL and return list or empty list if no result. Automatically add 'limit ?,?' after sql statement if not.
+    sql: SELECT * FROM person WHERE name=? and age=?  -->  args: ('张三', 20)
+         SELECT * FROM person WHERE name=:name and age=:age  -->  kwargs: ('张三', 20) --> kwargs: {'name': '张三', 'age': 20}
+    """
+    page_sql_id_log('query_page', sql_id, page_num, page_size, *args, **kwargs)
+    sql, args = holder.get_sql(sql_id, *args, **kwargs)
+    return db.do_query_page(sql, page_num, page_size, *args)
+
+
+def select_page(sql_id: str, page_num=1, page_size=10, *args, **kwargs):
+    """
+    Execute select SQL and return list or empty list if no result. Automatically add 'limit ?,?' after sql statement if not.
+    sql: SELECT * FROM person WHERE name=? and age=?  -->  args: ('张三', 20)
+         SELECT * FROM person WHERE name=:name and age=:age  -->  kwargs: ('张三', 20) --> kwargs: {'name': '张三', 'age': 20}
+    """
+    page_sql_id_log('select_page', sql_id, page_num, page_size, *args, **kwargs)
+    sql, args = holder.get_sql(sql_id, *args, **kwargs)
+    return db.do_select_page(sql, page_num, page_size, *args)
+
+
+def get_connection():
+    return db.get_connection()
+
---
+++ packages/pgsqlx/pgsqlx-1.0.8.tar.gz/pgsqlx-1.0.8/pgsqlx/sql_mapper.py
@@ -0,0 +1,119 @@
+import os
+import re
+import functools
+from .log_support import logger
+from .sql_holder import get_sql_model, do_get_sql, build_sql_id
+from .support import SqlAction, get_named_sql_args, simple_sql
+from .db import do_get, do_query, do_query_one, do_execute, do_batch_execute, do_select, do_select_one, batch_execute, do_save
+
+_UPDATE_ACTIONS = (SqlAction.INSERT.value, SqlAction.UPDATE.value, SqlAction.DELETE.value, SqlAction.CALL.value)
+
+
+def mapper(namespace: str = None, sql_id: str = None, batch=False, return_pk=False):
+    def _decorator(func):
+        @functools.wraps(func)
+        def _wrapper(*args, **kwargs):
+            param_names = func.__code__.co_varnames
+            full_sql_id, func_name = _before(func, namespace, sql_id, *args, **kwargs)
+            sql_model = get_sql_model(full_sql_id)
+            exec_func = _get_exec_func(func, sql_model.action, batch)
+            if return_pk:
+                return _save(sql_model, batch, param_names, *args, **kwargs)
+            if batch:
+                if kwargs:
+                    logger.warning("Batch exec sql better use like '{}(args)' or '{}(*args)' then '{}(args=args)'".format(func_name, func_name, func_name))
+                    args = list(kwargs.values())[0]
+                use_sql, _ = do_get_sql(sql_model, batch, param_names, *args)
+            else:
+                use_sql, args = do_get_sql(sql_model, batch, param_names, *args, **kwargs)
+            return exec_func(use_sql, *args)
+
+        return _wrapper
+    return _decorator
+
+
+def sql(value: str, batch=False, return_pk=False):
+    def _decorator(func):
+        @functools.wraps(func)
+        def _wrapper(*args, **kwargs):
+            use_sql = value
+            low_sql = value.lower()
+            if any([action in low_sql for action in _UPDATE_ACTIONS]):
+                if batch:
+                    if kwargs:
+                        args = list(kwargs.values())[0]
+                    return batch_execute(use_sql, *args)
+                if return_pk:
+                    pk_seq = None
+                    if 'pk_seq' in kwargs:
+                        pk_seq = kwargs.pop('pk_seq')
+                    if kwargs:
+                        use_sql, args = get_named_sql_args(use_sql, **kwargs)
+                    assert SqlAction.INSERT.value in low_sql, 'Only insert sql can return primary key.'
+                    if not pk_seq:
+                        table = re.search('(?<=into )\w+', low_sql)
+                        pk_seq = "{}_id_seq".format(table.group())
+                        logger.warning("Exec sql func: 'pk_seq' is None, will use default '{}'.".format(pk_seq))
+                    return do_save(pk_seq, use_sql, *args)
+
+                if kwargs:
+                    use_sql, args = get_named_sql_args(use_sql, **kwargs)
+                return do_execute(use_sql, *args)
+            elif SqlAction.SELECT.value in low_sql:
+                select_func = _get_select_func(func)
+                use_sql, args = simple_sql(use_sql, *args, **kwargs)
+                return select_func(use_sql, *args)
+            else:
+                return ValueError("Invalid sql: {}.".format(sql))
+
+        return _wrapper
+    return _decorator
+
+
+def _save(sql_model, batch, param_names, *args, **kwargs):
+    pk_seq = sql_model.pk_seq
+    if 'pk_seq' in kwargs:
+        pk_seq = kwargs.pop('pk_seq')
+    use_sql, args = do_get_sql(sql_model, batch, param_names, *args, **kwargs)
+    assert SqlAction.INSERT.value in use_sql.lower(), 'Only insert sql can return primary key.'
+    if not pk_seq:
+        table = re.search('(?<=into )\w+', use_sql.lower())
+        pk_seq = "{}_id_seq".format(table.group())
+        logger.warning("Exec mapper func: 'pk_seq' is None, will use default '{}'.".format(pk_seq))
+    return do_save(pk_seq, use_sql, *args)
+
+
+def _get_exec_func(func, action, batch):
+    if action == SqlAction.SELECT.value:
+        return _get_select_func(func)
+    elif batch:
+        return do_batch_execute
+    else:
+        return do_execute
+
+
+def _get_select_func(func):
+    names = func.__code__.co_names
+    is_list = 'list' in names or 'List' in names
+    if 'Mapping' in names and is_list:
+        return do_query
+    elif 'Mapping' in names:
+        return do_query_one
+    elif len(names) == 1 and names[0] in ('int', 'float', 'Decimal', 'str', 'AnyStr', 'date', 'time', 'datetime'):
+        return do_get
+    elif len(names) == 1 and names[0] in ('tuple', 'Tuple'):
+        return do_select_one
+    elif is_list:
+        return do_select
+    else:
+        return do_query
+
+
+def _before(func, namespace, _id, *args, **kwargs):
+    file_name = os.path.basename(func.__code__.co_filename)[:-3]
+    _namespace = namespace if namespace else file_name
+    _id = _id if _id else func.__name__
+    sql_id = build_sql_id(_namespace, _id)
+    func_name = file_name + '.' + func.__name__
+    logger.debug("Exec mapper func: '%s', sql_id: '%s', args: %s, kwargs: %s" % (func_name, sql_id, args, kwargs))
+    return sql_id, func_name
