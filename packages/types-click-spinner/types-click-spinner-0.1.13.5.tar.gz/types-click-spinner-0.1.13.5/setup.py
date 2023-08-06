b80bdfc5a7e19a76e382acab0a0fa37fe7c12843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:10 +0000

    Add package types-click-spinner/0.1.13.5/types-click-spinner-0.1.13.5.tar.gz

---
+++ packages/types-click-spinner/types-click-spinner-0.1.13.5.tar.gz/types-click-spinner-0.1.13.5/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+name = "types-click-spinner"
+description = "Typing stubs for click-spinner"
+long_description = '''
+## Typing stubs for click-spinner
+
+This is a PEP 561 type stub package for the `click-spinner` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`click-spinner`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/click-spinner. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `afe18e95a9592434e93b648de5194cfe54443f84` and was tested
+with mypy 1.4.1, pyright 1.1.318, and
+pytype 2023.6.16.
+'''.lstrip()
+
+setup(name=name,
+      version="0.1.13.5",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/click-spinner.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['click_spinner-stubs'],
+      package_data={'click_spinner-stubs': ['__init__.pyi', '_version.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
