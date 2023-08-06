b7efe0c1a973b38cd2f93e750626dc62a2e138db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:40 +0000

    Add package magicmath/1.0.0/magicmath-1.0.0.tar.gz

---
+++ packages/magicmath/magicmath-1.0.0.tar.gz/magicmath-1.0.0/magicmath/__init__.py
@@ -0,0 +1,1 @@
+from magicmath.magicmath import Magicmath
---
+++ packages/magicmath/magicmath-1.0.0.tar.gz/magicmath-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+    name='magicmath',
+    packages=['magicmath'],
+    version='1.0.0',
+    license='MIT',
+    description='A simple package to do basic math with lists',
+    author='Marcos Leandro Rosa',
+    author_email='marcosfac@gmail.com',
+    url='https://github.com/marcosrosa/magicmath',
+    keywords=['math', 'list', 'magicmath', 'math with lists', 'invert lists'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
