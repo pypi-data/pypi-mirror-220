2db52aebba2d39872dd35249367eff7bff1bf641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:17 +0000

    Add package Topsis-Royal-102016082/3.0/Topsis-Royal-102016082-3.0.tar.gz

---
+++ packages/Topsis-Royal-102016082/Topsis-Royal-102016082-3.0.tar.gz/Topsis-Royal-102016082-3.0/setup.py
@@ -0,0 +1,39 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="Topsis-Royal-102016082",
+    version="3.0",
+    description="Topsis Assignment",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RGarg2002/Topsis-Royal-102016082",
+    author="Royal Garg",
+    author_email="royalgarg36@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["topsis_assignment"],
+    include_package_data=True,
+    install_requires=['pandas',
+                      'math',
+                      'numpy',
+                      'sys',
+                      'logging'],
+    entry_points={
+        "console_scripts": [
+            "topsis=topsis_assignment.__main__:main",
+        ]
+    },
+)
