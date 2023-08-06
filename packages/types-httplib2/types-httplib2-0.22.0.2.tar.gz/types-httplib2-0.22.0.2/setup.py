6ec87a10585e2843611dbbe6927057cd25f9aa23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:09 +0000

    Add package types-httplib2/0.22.0.2/types-httplib2-0.22.0.2.tar.gz

---
+++ packages/types-httplib2/types-httplib2-0.22.0.2.tar.gz/types-httplib2-0.22.0.2/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+name = "types-httplib2"
+description = "Typing stubs for httplib2"
+long_description = '''
+## Typing stubs for httplib2
+
+This is a PEP 561 type stub package for the `httplib2` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`httplib2`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/httplib2. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `afe18e95a9592434e93b648de5194cfe54443f84` and was tested
+with mypy 1.4.1, pyright 1.1.318, and
+pytype 2023.6.16.
+'''.lstrip()
+
+setup(name=name,
+      version="0.22.0.2",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/httplib2.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['httplib2-stubs'],
+      package_data={'httplib2-stubs': ['__init__.pyi', 'auth.pyi', 'certs.pyi', 'error.pyi', 'iri2uri.pyi', 'socks.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
