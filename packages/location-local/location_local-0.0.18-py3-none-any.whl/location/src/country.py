b29b0fc96a19ae22dabf7e42cc6cafbe417d803c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:33 +0000

    Add package location-local/0.0.18/location_local-0.0.18-py3-none-any.whl

---
+++ packages/location-local/location_local-0.0.18-py3-none-any.whl/location/src/country.py
@@ -0,0 +1,49 @@
+import os
+from opencage.geocoder import OpenCageGeocode
+from logger_local_python_package.LoggerLocal import logger_local
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.getenv("OPENCAGE_KEY")
+component_id=113 
+class Country:
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def get_country_name(location):
+        # Create a geocoder instance
+        object1={
+            'component_id':component_id,
+            'location':location,
+        }
+        logger_local.start(object=object1)
+        
+
+        # Define the city or state
+        geocoder = OpenCageGeocode(api_key)
+
+        # Use geocoding to get the location details
+        results = geocoder.geocode(location)
+
+        if results and len(results) > 0:
+            first_result = results[0]
+            components = first_result['components']
+
+            # Extract the country from components
+            country_name = components.get('country', '')
+            if not country_name:
+                # If country is not found, check for country_code as an alternative
+                country_name = components.get('country_code', '')
+        else:
+            country_name = None
+            logger_local.error("country didnt  found for %s."%location)
+        object2={
+                'component_id':component_id ,
+                'country_name':country_name
+            }
+        logger_local.end(object=object2)
+        return country_name
+
+
+if __name__ == "__main__":
+    pass
