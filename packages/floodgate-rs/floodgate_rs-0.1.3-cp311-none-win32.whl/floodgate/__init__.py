b341f8e0f6ff68f5a21775850f534a7b306d0731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:18 +0000

    Add package floodgate-rs/0.1.3/floodgate_rs-0.1.3-cp311-none-win32.whl

---
+++ packages/floodgate-rs/floodgate_rs-0.1.3-cp311-none-win32.whl/floodgate/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from floodgate.floodgate import DynamicMapping, FixedMapping, JumpingWindow
+
+__all__: list[str] = ["FixedMapping", "DynamicMapping", "JumpingWindow"]
