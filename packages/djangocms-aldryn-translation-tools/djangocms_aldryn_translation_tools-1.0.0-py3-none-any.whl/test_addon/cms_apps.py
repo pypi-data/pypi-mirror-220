ac214b36469d461b7dfa9d9f44951ac93b704bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:52 +0000

    Add package djangocms-aldryn-translation-tools/1.0.0/djangocms_aldryn_translation_tools-1.0.0-py3-none-any.whl

---
+++ packages/djangocms-aldryn-translation-tools/djangocms_aldryn_translation_tools-1.0.0-py3-none-any.whl/test_addon/cms_apps.py
@@ -0,0 +1,15 @@
+from django.utils.translation import gettext_lazy as _
+
+from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
+
+
+class SimpleApp(CMSApp):
+    name = _('Simple')
+    app_name = 'simple'
+
+    def get_urls(self, page=None, language=None, **kwargs):
+        return ["test_addon.urls"]
+
+
+apphook_pool.register(SimpleApp)
