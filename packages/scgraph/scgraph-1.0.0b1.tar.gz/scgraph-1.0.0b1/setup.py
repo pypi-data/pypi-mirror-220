19a795c323ff227befc02263006ea94850ee7932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:36 +0000

    Add package scgraph/1.0.0b1/scgraph-1.0.0b1.tar.gz

---
+++ packages/scgraph/scgraph-1.0.0b1.tar.gz/scgraph-1.0.0b1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+  name = 'scgraph',
+  packages = find_packages(),
+  version = '1.0.0b1',
+  license='MIT',
+  description = 'Determine an approximate route between two points on earth',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author = 'Connor Makowski',
+  author_email = 'connor.m.makowski@gmail.com',
+  url = 'https://github.com/connor-makowski/scgraph',
+  download_url = 'https://github.com/connor-makowski/scgraph/dist/scgraph-1.0.0b1.tar.gz',
+  keywords = ['scgraph'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
