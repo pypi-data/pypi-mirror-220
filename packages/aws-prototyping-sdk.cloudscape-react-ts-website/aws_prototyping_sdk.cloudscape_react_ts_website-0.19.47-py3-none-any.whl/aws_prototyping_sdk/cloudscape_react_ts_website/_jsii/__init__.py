ec5d4d3625b33446f1f29035f6709ff671cc620c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:37 +0000

    Add package aws-prototyping-sdk.cloudscape-react-ts-website/0.19.47/aws_prototyping_sdk.cloudscape_react_ts_website-0.19.47-py3-none-any.whl

---
+++ packages/aws-prototyping-sdk.cloudscape-react-ts-website/aws_prototyping_sdk.cloudscape_react_ts_website-0.19.47-py3-none-any.whl/aws_prototyping_sdk/cloudscape_react_ts_website/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-prototyping-sdk/cloudscape-react-ts-website",
+    "0.19.47",
+    __name__[0:-6],
+    "cloudscape-react-ts-website@0.19.47.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
