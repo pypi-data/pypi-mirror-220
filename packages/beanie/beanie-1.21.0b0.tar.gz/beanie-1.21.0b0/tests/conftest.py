8bc6463f6b1e68aee3c40324a902949301223810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:18 +0000

    Add package beanie/1.21.0b0/beanie-1.21.0b0.tar.gz

---
+++ packages/beanie/beanie-1.21.0b0.tar.gz/beanie-1.21.0b0/tests/conftest.py
@@ -0,0 +1,23 @@
+import motor.motor_asyncio
+import pytest
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    mongodb_dsn: str = "mongodb://localhost:27017/beanie_db"
+    mongodb_db_name: str = "beanie_db"
+
+
+@pytest.fixture
+def settings():
+    return Settings()
+
+
+@pytest.fixture()
+def cli(settings):
+    return motor.motor_asyncio.AsyncIOMotorClient(settings.mongodb_dsn)
+
+
+@pytest.fixture()
+def db(cli, settings):
+    return cli[settings.mongodb_db_name]
