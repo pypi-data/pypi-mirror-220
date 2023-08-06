f138ea1c2c10a81bd57c24414255e707ba748c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:46 +0000

    Add package pydiff-planetarium-assignment/0.0.7/pydiff-planetarium-assignment-0.0.7.tar.gz

---
+++ packages/pydiff-planetarium-assignment/pydiff-planetarium-assignment-0.0.7.tar.gz/pydiff-planetarium-assignment-0.0.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pydiff-planetarium-assignment',
+    version='0.0.7',
+    author='channprj',
+    author_email='chann@chann.dev',
+    description='Simple implementation of unix diff',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/planetarium/take-home-2023-channprj',
+    project_urls={
+        'Bug Tracker':
+        'https://github.com/planetarium/take-home-2023-channprj/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    python_requires='>=3.8',
+)
