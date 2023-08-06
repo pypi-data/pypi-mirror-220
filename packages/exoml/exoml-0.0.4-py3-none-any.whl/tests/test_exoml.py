f9f178ac2e8dbcb26040bb82b586f10487f5c26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:10 +0000

    Add package exoml/0.0.4/exoml-0.0.4-py3-none-any.whl

---
+++ packages/exoml/exoml-0.0.4-py3-none-any.whl/tests/test_exoml.py
@@ -0,0 +1,12 @@
+import os
+import shutil
+import unittest
+
+
+class TestsExoml(unittest.TestCase):
+    def test_setup_files(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
