07e71003f6359e0845cd5bb4036cb1de3cdda740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:21 +0000

    Add package spectral-connectivity/1.1.2/spectral_connectivity-1.1.2.tar.gz

---
+++ packages/spectral-connectivity/spectral_connectivity-1.1.2.tar.gz/spectral_connectivity-1.1.2/spectral_connectivity/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+from spectral_connectivity.connectivity import Connectivity
+from spectral_connectivity.transforms import Multitaper
+from spectral_connectivity.wrapper import multitaper_connectivity
+
+__version__ = "1.1.2"
