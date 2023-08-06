f950fe4009fe3d1b53ed68eba11150aa356a1810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:22 +0000

    Add package QPUIQ/0.2.7/QPUIQ-0.2.7-py3-none-any.whl

---
+++ packages/QPUIQ/QPUIQ-0.2.7-py3-none-any.whl/PUI/PySide6/application.py
@@ -0,0 +1,23 @@
+from .. import *
+from .base import *
+
+class Application(QtPUIView):
+    def __init__(self):
+        super().__init__()
+        self.app = None
+
+    def update(self, prev=None):
+        if not self.app:
+            from PySide6 import QtWidgets
+            self.app = QtWidgets.QApplication([])
+
+        super().update(prev)
+
+    def addChild(self, idx, child):
+        child.ui.show()
+
+    def removeChild(self, idx, child):
+        child.ui.close()
+
+    def start(self):
+        self.app.exec_()
