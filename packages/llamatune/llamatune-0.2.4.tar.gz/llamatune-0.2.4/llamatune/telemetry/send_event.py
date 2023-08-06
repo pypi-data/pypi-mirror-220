299757e447f49d4b12e929e38b63195ab323acfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:21 +0000

    Add package llamatune/0.2.4/llamatune-0.2.4.tar.gz

---
+++ packages/llamatune/llamatune-0.2.4.tar.gz/llamatune-0.2.4/llamatune/telemetry/send_event.py
@@ -0,0 +1,12 @@
+from posthog import Posthog
+import os
+
+posthog = Posthog('phc_YpKoFD7smPe4SXRtVyMW766uP9AjUwnuRJ8hh2EJcVv',
+                  host='https://eu.posthog.com')
+
+
+def capture_event(event_name, event_properties):
+    print("inside function", os.environ.get('EVENT_CAPTURE'))
+    print(os.environ.get('EVENT_CAPTURE') == "disable")
+    if not os.environ.get('EVENT_CAPTURE') == "disable":
+        posthog.capture("not_distinct", event_name, event_properties)
