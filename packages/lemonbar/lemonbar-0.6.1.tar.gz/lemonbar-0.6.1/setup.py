1ecb67f1be76e55072be828c980eba8b803c2f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:57 +0000

    Add package lemonbar/0.6.1/lemonbar-0.6.1.tar.gz

---
+++ packages/lemonbar/lemonbar-0.6.1.tar.gz/lemonbar-0.6.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='lemonbar',
+    packages=['lemonbar'],
+    version='0.6.1',
+    license='MIT',
+    description='A Python API for interacting with Lemonbar',
+    author='Ori Harel',
+    author_email='oeharel@gmail.com',
+    url='https://github.com/Heknon/lemonbar-api',
+    download_url='https://github.com/Heknon/lemonbar-api/archive/refs/tags/v0.6.1.tar.gz',
+    keywords=['lemonbar', 'api', 'lemonbar-api', "arch", "linux"],
+    install_requires=[
+        'pydantic',
+        'screeninfo',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+    ],
+)
