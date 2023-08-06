1d36208246b8436e26d70a80ce3e7ff77fcdd138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:01 +0000

    Add package metamaterialanalysiscode/1.0/metamaterialanalysiscode-1.0-py3-none-any.whl

---
+++ packages/metamaterialanalysiscode/metamaterialanalysiscode-1.0-py3-none-any.whl/metamaterialanalysiscode/classes/MACNonLinear.py
@@ -0,0 +1,61 @@
+"""
+Module for the NonLinear Parameters for Large Displacements (NLaprmLD) class and NonLinear Output (NLout) class.
+"""
+
+
+class MACNLparmLD:
+    """
+    Class for the NonLinear Parameters for Large Displacements (NLparmLD).
+    """
+
+    def __init__(self, id: int, ninc: int, dt: float):
+        """
+        Constructor for MACNLparmLD class
+        """
+        self.ID = id
+        self.DT = dt
+        self.Ninc = ninc
+
+    def __str__(self):
+        """
+        Method to print the NonLinear Parameters for Large Displacements (NLaprmLD) class.
+        """
+        idspaces = " " * (8 - len(str(self.ID)))
+        dtspaces = " " * (8 - len(str(self.DT)))
+        nincspaces = " " * (8 - len(str(self.Ninc)))
+        return f"NLPARM  {idspaces}{self.ID}{nincspaces}{self.Ninc}{dtspaces}{self.DT}\n"
+
+
+class MACNLout:
+    """
+    Class for the NonLinear Output (NLout).
+    """
+
+    def __init__(self, id: int, nint: int):
+        """
+        Constructor for MACNLout class
+        """
+        self.ID = id
+        self.NINT = nint
+
+    def __str__(self):
+        """
+        Method to print the NonLinear Output (NLout) class.
+        """
+        idspaces = " " * (8 - len(str(self.ID)))
+        nintspaces = " " * (8 - len(str(self.NINT)))
+        return f"NLOUT   {idspaces}{self.ID}    NINT{nintspaces}{self.NINT}\n"
+
+
+def set_nlparmld(id: int, ninc: int, dt: float):
+    """
+    Function to create a NonLinear Parameters for Large Displacements (NLaprmLD) class.
+    """
+    return MACNLparmLD(id, ninc, dt)
+
+
+def set_nlout(id: int, nint: int):
+    """
+    Function to create a NonLinear Output (NLout) class.
+    """
+    return MACNLout(id, nint)
