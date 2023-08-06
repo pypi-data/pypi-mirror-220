18bda4d8043c7711f44e76a7f63843590b87e25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:14 +0000

    Add package allianceauth-discordbot/3.6.4/allianceauth_discordbot-3.6.4-py2.py3-none-any.whl

---
+++ packages/allianceauth-discordbot/allianceauth_discordbot-3.6.4-py2.py3-none-any.whl/aadiscordbot/bot_tasks.py
@@ -0,0 +1,119 @@
+import importlib
+import logging
+import warnings
+from datetime import timedelta
+
+from discord import Embed, Member
+from discord.ext import tasks
+from discord.ext.commands import Bot
+from discord.ui import View
+
+import django
+from django.conf import settings
+from django.utils import timezone
+
+logger = logging.getLogger(__name__)
+
+GUILD_ID = settings.DISCORD_GUILD_ID
+
+
+@tasks.loop()
+async def run_tasks(bot: Bot):
+    django.db.close_old_connections()
+
+    if len(bot.tasks) > 0:
+        task, args, kwargs = bot.tasks.pop(0)
+        requeue_task = False
+        if hasattr(bot, 'rate_limits'):
+            if not bot.rate_limits.check_rate_limit(task.__name__):
+                requeue_task = True
+        if requeue_task:
+            # timeout ( for now lets just add 1 second )
+            timeout = 1
+            eta = timezone.now() + timedelta(seconds=timeout)
+            bot.pending_tasks.append((eta, (task, args, kwargs)))
+            #logger.debug(f"Rate Limit hit! Re Queueing `{task}`")
+        else:
+            try:
+                await task(bot, *args, **kwargs)
+                bot.statistics.add_task(task.__name__)
+                bot.dispatch("authbot_task_completed", task.__name__)
+            except Exception as e:
+                bot.dispatch("authbot_task_failed",
+                             task.__name__, args, kwargs, e)
+                logger.error(
+                    f"Failed to run task {task} {args} {kwargs} {e}", exc_info=True)
+    else:
+        run_tasks.stop()
+    django.db.close_old_connections()
+
+
+async def send_channel_message_by_discord_id(bot, channel_id, message, embed=False, view_class=False, view_args=[], view_kwargs={}):
+    logger.debug(f"Sending Channel Message to Discord ID {channel_id}")
+    e = None
+    v = None
+    if embed:
+        e = Embed.from_dict(embed)
+
+    if view_class:
+        m = ".".join(view_class.split(".")[:-1])
+        c = view_class.split(".")[-1]
+        my_module = importlib.import_module(m)
+        Viewclass = getattr(my_module, c)
+        v = Viewclass(*view_args, **view_kwargs)
+
+    await bot.get_channel(channel_id).send(message, embed=e, view=v)
+
+
+async def send_channel_message(bot, channel_id, message, embed=False):
+    warnings.warn(
+        "send_channel_message is deprecated, use send_channel_message_by_discord_id instead",
+        DeprecationWarning
+    )
+    await send_channel_message_by_discord_id(bot, channel_id, message, embed=embed)
+
+
+async def send_direct_message_by_discord_id(bot, discord_user_id, message, embed=False):
+    logger.debug(f"Sending DM to Discord ID {discord_user_id}")
+
+    channel = await bot.get_user(discord_user_id).create_dm()
+    if embed:
+        e = Embed.from_dict(embed)
+        await channel.send(message, embed=e)
+    else:
+        await channel.send(message)
+
+
+async def send_direct_message(bot, discord_user_id, message, embed=False):
+    warnings.warn(
+        "send_direct_message is deprecated, use send_direct_message_by_discord_id instead",
+        DeprecationWarning
+    )
+    await send_direct_message_by_discord_id(bot, discord_user_id, message, embed=embed)
+
+
+async def send_direct_message_by_user_id(bot, user_pk, message, embed=False):
+    # App isn't loaded when this file is imported, so importing here
+    from django.contrib.auth.models import User
+    logger.debug(f"Sending DM to User ID {user_pk}")
+    user = User.objects.get(pk=user_pk)
+    if hasattr(user, "discord"):
+        discord_user_id = user.discord.uid
+        channel = await bot.get_user(discord_user_id).create_dm()
+        if embed:
+            e = Embed.from_dict(embed)
+            await channel.send(message, embed=e)
+        else:
+            await channel.send(message)
+    else:
+        logger.debug(f"No discord account on record for user_pk={user_pk}")
+
+
+async def pop_user_group_cache(bot, user_pk):
+    logger.debug(f"Refreshing user cache {user_pk}")
+    user = bot.get_guild(int(GUILD_ID)).get_member(user_pk)
+    r = user.roles[-1]
+    await user.remove_roles(r)
+    await user.add_roles(r)
+    logger.info(
+        f"Removed and added '{r}' to {user} to try and bust the invalid cache")
---
+++ packages/allianceauth-discordbot/allianceauth_discordbot-3.6.4-py2.py3-none-any.whl/aadiscordbot/cogs/tickets.py
@@ -0,0 +1,117 @@
+# Cog Stuff
+import logging
+from typing import Optional
+
+import discord
+from discord import ChannelType, Embed, command, ui
+from discord.ext import commands
+
+# AA Contexts
+from django.conf import settings
+from django.utils import timezone
+
+from .. import models
+
+logger = logging.getLogger(__name__)
+
+
+def get_groups():
+    groups = models.TicketGroups.get_solo().groups.all().order_by('name')
+    out = []
+    for g in groups:
+        out.append(
+            discord.SelectOption(
+                label=f"{g.name}"
+            )
+        )
+    return out
+
+
+class HelpView(ui.View):
+    """
+        View for picking a group to assign a help thread too
+    """
+
+    embed_text = None
+    message_text = ""
+    created = None
+
+    def __init__(self,
+                 *items: discord.ui.Item,
+                 timeout: Optional[float] = 60*60,  # 24h from last click
+                 embed: Optional[Embed] = None,
+                 message: Optional[str] = None,
+                 bot=None
+                 ):
+        if embed:
+            if isinstance(embed, dict):
+                self.embed_text = Embed.from_dict(embed)
+            elif isinstance(embed, Embed):
+                self.embed_text = embed
+        if message:
+            self.message_text = message
+        self.bot = bot
+        self.created = timezone.now()
+
+        super().__init__(*items, timeout=timeout)
+
+    # a custom_id must be set
+    @discord.ui.select(placeholder="Who do you need help from?", options=get_groups())
+    async def select_callback(self, select, interaction):
+        sup_channel = models.TicketGroups.get_solo().ticket_channel.channel
+        ch = interaction.guild.get_channel(sup_channel)
+        grp = discord.utils.get(interaction.guild.roles, name=select.values[0])
+        th = await ch.create_thread(name=f"{interaction.user.display_name} | {select.values[0]} | {timezone.now().strftime('%Y-%m-%d %H:%M')}",
+                                    # message=f"Ping in here if your request is urgent <@{interaction.user.id}>, Someone from <#{grp.id}> will be here soon!",
+                                    auto_archive_duration=10080,
+                                    type=discord.ChannelType.private_thread,
+                                    reason=None)
+        msg = f"<@{interaction.user.id}> needs help!, Someone from <@&{grp.id}> will get in touch soon!"
+        embd = Embed(title="Private Thread Guide",
+                     description="To add a person to this thread simply `@ping` them. This works with `@groups` as well to bulk add people to the channel. Use wisely, abuse will not be tolerated.\n\nThis is a beta feature if you experience issues please contact the admins. :heart:")
+        await th.send(msg, embed=embd)
+        await interaction.response.edit_message(content="Ping in the thread created for urgent help!", view=None)
+
+    async def on_timeout(self) -> None:
+        await super().on_timeout()
+
+
+class HelpCog(commands.Cog):
+    """
+        Compliance Related thingies
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @command(name='help', guild_ids=[int(settings.DISCORD_GUILD_ID)])
+    async def slash_halp(
+        self,
+        ctx,
+    ):
+        """
+            Help me authbot i dont know who to ping!
+        """
+        await ctx.defer(ephemeral=True)
+        return await ctx.respond(view=HelpView())
+
+    @command(name='close_ticket', guild_ids=[int(settings.DISCORD_GUILD_ID)])
+    async def slash_close(
+        self,
+        ctx,
+    ):
+        """
+            Mark help thread as completed and archive it!
+        """
+        ch = ctx.channel
+        if ch.type != ChannelType.private_thread:
+            return await ctx.respond("Not a private thread!", ephemeral=True)
+        await ctx.defer()
+        embd = Embed(title="Thread Marked Complete",
+                     description=f"{ctx.user.display_name} has marked this thread as completed. To reopen simply start chatting again.")
+        await ctx.respond(embed=embd)
+        return await ch.archive()
+
+
+def setup(bot):
+    bot.add_cog(HelpCog(bot))
