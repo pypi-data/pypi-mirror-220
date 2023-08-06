5d3a4d59924b238cfd79902268e41ce13170012d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:50 +0000

    Add package hiveviewer/0.1.0/hiveviewer-0.1.0.tar.gz

---
+++ packages/hiveviewer/hiveviewer-0.1.0.tar.gz/hiveviewer-0.1.0/src/hiveviewer/evaluation/__init__.py
@@ -0,0 +1,3 @@
+from .calculate_auc import Calculator
+
+__all__ = ["Calculator"]
---
+++ packages/hiveviewer/hiveviewer-0.1.0.tar.gz/hiveviewer-0.1.0/src/hiveviewer/sampling/base.py
@@ -0,0 +1,26 @@
+from abc import ABCMeta, abstractmethod
+from typing import List, Optional, Union
+
+import pandas as pd
+
+
+class BaseSampler(metaclass=ABCMeta):
+    """
+    This class provides methods to sample data.
+    """
+
+    def __init__(
+        self,
+        sample_size: int,
+        data: Union[pd.Series, List[float]],
+        slice_from: Optional[float] = None,
+        slice_to: Optional[float] = None,
+    ) -> None:
+        self.sample_size = sample_size
+        self.data = data
+        self.slice_from = slice_from
+        self.slice_to = slice_to
+
+    @abstractmethod
+    def sample(self) -> List[float]:
+        raise NotImplementedError("Should implement sample()!")
