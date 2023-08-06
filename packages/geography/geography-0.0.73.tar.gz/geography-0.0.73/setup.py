ba77187acaebd4583d9e23c937e47753e863c382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:10 +0000

    Add package geography/0.0.73/geography-0.0.73.tar.gz

---
+++ packages/geography/geography-0.0.73.tar.gz/geography-0.0.73/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+]
+
+setup(
+    name="geography",
+    version="0.0.73",
+    description="Get info about countries and states",
+    url="",
+    author="FrenchFries8854",
+    author_email="frenchfries8854@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="geography",
+    packages=find_packages(),
+    install_requires=[""]
+)
