39a3abe16db572769b903468f43560e0b9c898cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:13 +0000

    Add package functino/0.0.2/functino-0.0.2-py3-none-any.whl

---
+++ packages/functino/functino-0.0.2-py3-none-any.whl/functino/platform_path.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from PyQt6.QtCore import QStandardPaths
+
+
+def get_user_config_path() -> Path:
+    """
+    Get user configuration directory for this application.
+    """
+    user_config_path_str = QStandardPaths.writableLocation(
+        QStandardPaths.StandardLocation.AppConfigLocation
+    )
+    if user_config_path_str == "":
+        raise RuntimeError("could not determine user config path for this application")
+    return Path(user_config_path_str)
+
+
+def get_user_language_profiles_path() -> Path:
+    """
+    Get path of user language profiles directory.
+    """
+    return get_user_config_path() / "language_profiles"
