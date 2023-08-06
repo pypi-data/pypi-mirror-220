0458173015fb9ec717eecfa4a71ed4a4c394f0ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:32 +0000

    Add package pyEDFlib/0.1.34/pyEDFlib-0.1.34-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/pyEDFlib/pyEDFlib-0.1.34-cp39-cp39-macosx_10_9_x86_64.whl/pyedflib/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM pyedflib SETUP.PY
+short_version = '0.1.34'
+version = '0.1.34'
+full_version = '0.1.34'
+git_revision = '1af9ced84921b304fecdc994e7e8b9a4c1674245'
+release = True
+
+if not release:
+    version = full_version
