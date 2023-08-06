acba239cf9d5b752cbd02013c841b9896902a5b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:24 +0000

    Add package tooey/0.4.0/tooey-0.4.0-py3-none-any.whl

---
+++ packages/tooey/tooey-0.4.0-py3-none-any.whl/tooey/__version__.py
@@ -0,0 +1,20 @@
+__title__ = 'tooey'
+__version__ = '0.4.0'
+__description__ = 'Automatically turn script arguments into an interactive terminal interface'
+__author__ = 'Simon Robinson'
+__author_email__ = 'simon@robinson.ac'
+__url__ = 'https://github.com/simonrob/tooey'
+__copyright__ = 'Copyright (c) 2023 Simon Robinson'
+__license__ = 'Apache 2.0'
+__classifiers__ = [  # https://pypi.org/classifiers/
+    'Development Status :: 4 - Beta',
+    "Operating System :: OS Independent",
+    'Environment :: Console',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Intended Audience :: Developers',
+    'Topic :: Terminals',
+    'Topic :: Desktop Environment',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache Software License'
+]
