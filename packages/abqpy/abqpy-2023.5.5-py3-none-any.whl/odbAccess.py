49bb88394a0616d58e8471d0e9a4bee21a1781de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:52 +0000

    Add package abqpy/2023.5.5/abqpy-2023.5.5-py3-none-any.whl

---
+++ packages/abqpy/abqpy-2023.5.5-py3-none-any.whl/odbAccess.py
@@ -0,0 +1,11 @@
+from math import *
+
+from abqpy import run
+
+run(cae=False)
+
+from abaqus.Odb.OdbCommands import *
+from abaqus.UtilityAndView.BackwardCompatibility import BackwardCompatibility
+from abaqusConstants import *
+
+backwardCompatibility = BackwardCompatibility()
