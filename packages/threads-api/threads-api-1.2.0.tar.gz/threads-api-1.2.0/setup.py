93f89e0d8c20ad53ee0571287a5b731c2799f3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:26 +0000

    Add package threads-api/1.2.0/threads-api-1.2.0.tar.gz

---
+++ packages/threads-api/threads-api-1.2.0.tar.gz/threads-api-1.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='threads-api',
+    version='1.2.0',
+    description='Unofficial Python client for Meta Threads.net API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Daniel Saad',
+    author_email='danielsaad777@gmail.com',
+    url='https://github.com/danie1/threads-api',
+    packages=find_packages(),
+    install_requires=[
+        'aiohttp'
+    ],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
