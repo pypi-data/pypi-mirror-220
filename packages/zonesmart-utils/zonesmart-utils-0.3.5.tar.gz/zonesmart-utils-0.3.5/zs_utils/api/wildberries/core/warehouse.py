96a46c7ee68ef87a96ab77d9cf7b25a4c0716f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:13 +0000

    Add package zonesmart-utils/0.3.5/zonesmart-utils-0.3.5.tar.gz

---
+++ packages/zonesmart-utils/zonesmart-utils-0.3.5.tar.gz/zonesmart-utils-0.3.5/zs_utils/api/wildberries/core/warehouse.py
@@ -0,0 +1,19 @@
+from zs_utils.api.wildberries.base_api import WildberriesAPI
+
+
+class GetWildberriesWarehouseList(WildberriesAPI):
+    """
+    https://openapi.wildberries.ru/?ysclid=ljzow9j5e0958977292#tag/Marketplace-Sklady/paths/~1api~1v3~1warehouses/get
+    """
+
+    http_method = "GET"
+    resource_method = "api/v3/warehouses"
+
+
+class GetWildberriesWarehouseStock(WildberriesAPI):
+    """
+    https://openapi.wb.ru/#tag/Marketplace-Ostatki/paths/~1api~1v3~1stocks~1%7BwarehouseId%7D/post
+    """
+    http_method = "POST"
+    resource_method = "api/v3/stocks/{warehouseId}"
+    required_params = ["warehouseId", "skus"]
