64f1c977aa0ea3b45252c6d2d516655166d2a7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:48 +0000

    Add package finterstellar/0.2.20/finterstellar-0.2.20.tar.gz

---
+++ packages/finterstellar/finterstellar-0.2.20.tar.gz/finterstellar-0.2.20/finterstellar/__init__.py
@@ -0,0 +1,13 @@
+from .data_prep import *
+from .trading import *
+from .trend import *
+from .visualization import *
+from .financials import *
+from .krx import *
+
+import warnings
+warnings.filterwarnings(action='ignore')
+
+__doc__ = '''
+python library for quantitative analysis
+'''
