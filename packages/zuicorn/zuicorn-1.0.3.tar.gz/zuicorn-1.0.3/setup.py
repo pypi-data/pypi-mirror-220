f843fa8ed7a42401f6c9e66e62559827fa4115d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:09 +0000

    Add package zuicorn/1.0.3/zuicorn-1.0.3.tar.gz

---
+++ packages/zuicorn/zuicorn-1.0.3.tar.gz/zuicorn-1.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zuicorn',
+    version='1.0.3',
+    description='A WSGI server designed & developed for ZYLO Python web Framework.',
+    author='Pawan kumar',
+    author_email='control@vvfin.in',
+    url='https://github.com/E491K7/zuicorn',
+    packages=find_packages(),
+    install_requires=['gevent', 'colorama', 'python-daemon', 'zylo-admin', 'zylo'],  
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "zuicorn = zuicorn.wsgi:main"
+        ]
+    },
+)
