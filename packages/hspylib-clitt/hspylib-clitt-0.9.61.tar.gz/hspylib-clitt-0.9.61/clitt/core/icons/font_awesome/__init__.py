e41454fb0478125aa5caf7fa7997695ab76c1947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:50 +0000

    Add package hspylib-clitt/0.9.61/hspylib-clitt-0.9.61.tar.gz

---
+++ packages/hspylib-clitt/hspylib-clitt-0.9.61.tar.gz/hspylib-clitt-0.9.61/clitt/core/icons/font_awesome/__init__.py
@@ -0,0 +1,17 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-clitt v0.9.61
+#
+# Package: main.clitt.core.icons.font_awesome
+"""Package initialization."""
+
+__all__ = [
+    'app_icons', 
+    'awesome', 
+    'dashboard_icons', 
+    'form_icons', 
+    'nav_icons', 
+    'trickplay_icons', 
+    'widget_icons'
+]
+__version__ = '0.9.61'
