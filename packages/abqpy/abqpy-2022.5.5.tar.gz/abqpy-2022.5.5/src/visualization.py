f61caf1a7fa595bb065f4d33d3813ccb93af8fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:52 +0000

    Add package abqpy/2022.5.5/abqpy-2022.5.5.tar.gz

---
+++ packages/abqpy/abqpy-2022.5.5.tar.gz/abqpy-2022.5.5/src/visualization.py
@@ -0,0 +1,6 @@
+from abaqus import session
+from abaqus.Odb.OdbCommands import *
+from abaqus.Property.PlyStackPlot import OdbPlyStackPlot
+from abaqusConstants import *
+
+session.Viewport(name="Viewport: 1")
