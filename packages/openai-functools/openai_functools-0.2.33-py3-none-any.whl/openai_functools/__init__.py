7800af4f4ac9df9484adc762037e8267f1ab1d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:48 +0000

    Add package openai-functools/0.2.33/openai_functools-0.2.33-py3-none-any.whl

---
+++ packages/openai-functools/openai_functools-0.2.33-py3-none-any.whl/openai_functools/__init__.py
@@ -0,0 +1,7 @@
+from .metadata_generator import (extract_openai_function_metadata,
+                                 openai_function)
+
+__all__ = [
+    "openai_function",
+    "extract_openai_function_metadata",
+]
