341651c1c428e671f0a456f531a22f23255e94f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:23 +0000

    Add package binary2strings/0.1.12/binary2strings-0.1.12.tar.gz

---
+++ packages/binary2strings/binary2strings-0.1.12.tar.gz/binary2strings-0.1.12/setup.py
@@ -0,0 +1,47 @@
+#! /usr/bin/0env python3
+# Templated from https://github.com/pybind/python_example
+import sys
+import os
+from glob import glob
+
+from pybind11 import get_cmake_dir
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, Extension, find_packages
+
+__version__ = "0.1.12"
+
+ext_modules = [
+    Pybind11Extension("binary2strings",
+        sorted(glob("src/*.cpp")),  # Sort source files for reproducibility
+        headers = sorted(glob("src/*.hpp")), # Critical for Linux package building
+        define_macros = [('VERSION_INFO', __version__)],
+        include_dirs = ["src"],
+        ),
+]
+
+# Note to self to build and upload skip existing:
+#   Delete dist/ and build/
+#   python setup.py sdist bdist_wheel
+#   twine upload dist/* --skip-existing
+#     Get token from https://pypi.org/manage/account/token/
+#     Use username __token__
+#     Use password from token
+#     Delete token after use
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name='binary2strings',
+    version=__version__,
+    author='Geoff McDonald',
+    author_email='glmcdona@gmail.com',
+    url='https://github.com/glmcdona/binary2strings',
+    license='MIT',
+    description='Fast string extraction from binary buffers.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    test_suite='tests',
+    zip_safe=False,
+    python_requires=">=3.7",
+)
---
+++ packages/binary2strings/binary2strings-0.1.12.tar.gz/binary2strings-0.1.12/src/bindings.cpp
@@ -0,0 +1,44 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <stdint.h>
+#include <binary2strings.hpp>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(binary2strings, m)
+{
+    m.doc() = "Python module for extracting strings from binary data input.";
+
+    m.def("extract_all_strings", [](py::bytes &buffer, int min_chars, bool only_interesting) {
+        py::buffer_info info(py::buffer(buffer).request());
+
+        if(info.ndim != 1)
+            throw std::runtime_error("Only 1-dimensional arrays are supported");
+        
+        if(min_chars < 1)
+            throw std::runtime_error("min_chars must be 1 or larger");
+
+        const unsigned char *data = reinterpret_cast<const unsigned char *>(info.ptr);
+        
+        return extract_all_strings(data, info.shape[0], min_chars, only_interesting);
+    },"Extract all strings at least min_chars or longer from the input bytes string. Returns an array of tuples.",
+    py::arg("buffer"),
+    py::arg("min_chars")=4,
+    py::arg("only_interesting")=false);
+
+    m.def("extract_string", [](py::bytes &buffer, int min_chars, bool only_interesting) {
+        py::buffer_info info(py::buffer(buffer).request());
+
+        if(info.ndim != 1)
+            throw std::runtime_error("Only 1-dimensional arrays are supported");
+        
+        if(min_chars < 1)
+            throw std::runtime_error("min_chars must be 1 or larger");
+
+        const unsigned char *data = reinterpret_cast<const unsigned char *>(info.ptr);
+        return try_extract_string_tuple(data, info.shape[0], 0, min_chars, only_interesting);
+    },"Tries to extract a single string at least min_chars or longer from the start of the input bytes string. Returns a single of tuple. If it fails to parse a string from the start of the buffer it will return a tuple containing a zero-length string.",
+    py::arg("buffer"),
+    py::arg("min_chars")=4,
+    py::arg("only_interesting")=false);
+}
