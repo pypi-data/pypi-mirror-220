858bbaea9f1116f6b2e8d93feda322830a783911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:57 +0000

    Add package libtimed/0.2.0/libtimed-0.2.0-py3-none-any.whl

---
+++ packages/libtimed/libtimed-0.2.0-py3-none-any.whl/libtimed/__init__.py
@@ -0,0 +1,31 @@
+from inspect import isclass
+
+import requests
+
+from libtimed import models
+
+
+class TimedAPIClient:
+    def __init__(self, token, url, api_namespace):
+        self.token = token
+        self.url = f"{url}/{api_namespace}/"
+        self.session = requests.Session()
+        self.session.headers["Authorization"] = f"Bearer {token}"
+        self.session.headers["Content-Type"] = "application/vnd.api+json"
+
+        # Models
+        self.users = models.Users(self)
+        self.reports = models.Reports(self)
+        self.overtime = models.WorktimeBalances(self)
+        self.activities = models.Activities(self)
+        self.customers = models.Customers(self)
+        self.tasks = models.Tasks(self)
+        self.projects = models.Projects(self)
+        # self.employments = models.Employments(self)
+
+        self._type_map = {
+            getattr(models, model).resource_name: getattr(models, model)
+            for model in dir(models)
+            if isclass(getattr(models, model))
+            and issubclass(getattr(models, model), models.BaseModel)
+        }
