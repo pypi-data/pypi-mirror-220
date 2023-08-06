cb58f5a9fc95976d3950e6ef4f98e6871db795d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:15 +0000

    Add package idftools/0.1.15/idftools-0.1.15.tar.gz

---
+++ packages/idftools/idftools-0.1.15.tar.gz/idftools-0.1.15/setup.py
@@ -0,0 +1,39 @@
+# setup.py
+from setuptools import setup
+from pathlib import Path
+
+try:
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
+except:
+    requirements = ''
+
+print(f'requirements={requirements}')
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+print(f'long_description={long_description}')
+
+setup(
+    version="0.1.15",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    # install_requires=requirements,
+    name="idftools",
+    author_email="eniodefarias@gmail.com",
+    author="eniodefarias",
+    url="https://github.com/eniodefarias/pypi-idftools",
+    description="Um pacote com alguns utilitarios uteis",
+    # py_modules=["utilities", "driversfactory", "certificate"],
+    # package_dir={"": "idftools"}
+    # package_dir={"idftools": "modules_idftools"}
+    packages=["idftools"],
+    # data_files=[('fonts', ['idftools/Roboto-Regular.ttf']), ('', ['idftools/logging.conf'])]
+    data_files=[('etc', ['idftools/Roboto-Regular.ttf'])]
+    # include_package_data=True,
+    # package_data={'idftools': ['idftools/Roboto-Regular.ttf', 'idftools/logging.conf']}
+)
+
+# .venv/bin/python3 setup.py sdist bdist_wheel
+# twine upload --skip-existing dist/*
