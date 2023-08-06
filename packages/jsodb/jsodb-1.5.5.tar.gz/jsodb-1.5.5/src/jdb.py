2239a39297f086e265d1d6cc932583589411a38c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:22 +0000

    Add package jsodb/1.5.5/jsodb-1.5.5.tar.gz

---
+++ packages/jsodb/jsodb-1.5.5.tar.gz/jsodb-1.5.5/src/jdb.py
@@ -0,0 +1,36 @@
+import json
+
+def changekey(key, val):
+	data[f'{key}'] = val
+
+def getkeyval(key):
+	return data[f'{key}']
+
+def addkey(**kwargs):
+	for key, val in kwargs.items():
+		data[f'{key}'] = val
+
+def createjson(filepath):
+	with open(f"{filepath}.json", "w") as f:
+		file.write("{}")
+
+def addtokey(key, val):
+	try:
+		data[f'{key}'] += int(val)
+	except TypeError:
+		print("TypeError! Only use int.")
+
+def subfromkey(key, val):
+	try:
+		data[f'{key}'] -= int(val)
+	except TypeError:
+		print("TypeError! Only use int.")
+
+def json(func, file):
+	def wrapper(*args, **kwargs):
+		with open(f'{file}') as f:
+			data = json.load(f)
+		func(*args, **kwargs)
+		with open(f'{file}', "w") as f:
+			json.dump(data, f)
+	return wrapper
