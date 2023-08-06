d183c4bdf01925dea9db8f870ecb1eccf6a27cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:43 +0000

    Add package tybase/0.1.9/tybase-0.1.9.tar.gz

---
+++ packages/tybase/tybase-0.1.9.tar.gz/tybase-0.1.9/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tybase',
+    version='0.1.9',
+    include_package_data=True,
+    description='新增了阿里oss的操作,需要单独安装库',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',  # 版本描述
+    author='Tuya',
+    author_email='353335447@qq.com',
+    url='https://github.com/yourusername/your_package',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools',
+        "loguru",
+        'requests',
+        "python-dotenv",
+        "retrying",
+        "pymysql",
+        "mysql-connector-python",
+        "sqlalchemy",
+        "pandas",
+        "langchain",
+        "openai",
+        'python-dotenv',
+        "oss2"
+        ""
+
+        # List your package dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
