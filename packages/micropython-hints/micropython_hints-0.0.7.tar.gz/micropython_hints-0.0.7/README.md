fd26f6c6684d614767a59cdbc14471f27ee93226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:52 +0000

    Add package micropython-hints/0.0.7/micropython_hints-0.0.7.tar.gz

---
+++ packages/micropython-hints/micropython_hints-0.0.7.tar.gz/micropython_hints-0.0.7/README.md
@@ -0,0 +1,21 @@
+# MicroPython TypeHints
+
+**Please operate with caution when using command!!!**
+
+**s使用指令时保持清醒, 按照提示操作!!!**
+
+**否则会误删文件!!!**
+
+## Usage
+
+在当前路径创建模板文件:
+
+```sh
+build-hints
+```
+
+移除当前路径中的模板文件:
+
+```sh
+remove-hints
+```
---
+++ packages/micropython-hints/micropython_hints-0.0.7.tar.gz/micropython_hints-0.0.7/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import os
+import shutil
+from setuptools import setup, find_packages
+
+PROJ_NAME = "micropython_hints"
+    
+CUR_PATH = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(CUR_PATH, "build")
+if os.path.isdir(path):
+    print("INFO DEL DIR ", path)
+    shutil.rmtree(path)
+path = os.path.join(CUR_PATH, "dist")
+if os.path.isdir(path):
+    print("INFO DEL DIR ", path)
+    shutil.rmtree(path)
+
+with open(os.path.join(CUR_PATH, "README.md"), 'r+', encoding='utf8') as f:
+    long_description = f.read()
+
+URL = f"https://github.com/ITA-ZUFE/{PROJ_NAME}"
+
+setup(
+    name         = PROJ_NAME,
+    author       =  "miaobuao",
+    url          =  URL,
+    description  =  "MicroPython type hints",
+    version      =  "0.0.7",
+    license      =  "MIT License",
+    author_email =  "miaobuao@outlook.com",
+    long_description     = long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            f'build-hints = {PROJ_NAME}:run',
+            f"remove-hints = {PROJ_NAME}:remove"
+        ],
+    },
+    install_requires=[
+        'tqdm',
+        'meo>=0.1.13'
+    ],
+)
