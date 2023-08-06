16dfc2c42f54be9906833759a42aa55744a11ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:08 +0000

    Add package covert-ots/1.0.5/covert-ots-1.0.5.tar.gz

---
+++ packages/covert-ots/covert-ots-1.0.5.tar.gz/covert-ots-1.0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='covert-ots',
+   version='1.0.5',
+    author="Om Gupta",                     
+    description="A secure way to communicate your secrets",
+    long_description=long_description,      
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(), 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                         
+    python_requires='>=3.6',   
+    py_modules=["quicksample"],
+    package_dir={'':'.'},
+    install_requires=[
+      'typer',
+      'rich',
+      'inquirer',
+      'pycryptodome'
+   ],
+)
