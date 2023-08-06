190302d0e23a625876064dcae4434e05ba612810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:56 +0000

    Add package dvag-mon-shared/1.0.5/dvag_mon_shared-1.0.5.tar.gz

---
+++ packages/dvag-mon-shared/dvag_mon_shared-1.0.5.tar.gz/dvag_mon_shared-1.0.5/src/dvag_mon_shared/mapping_const.py
@@ -0,0 +1,42 @@
+environment_dict = {
+    "ent":      "Entwicklung",
+    "dev":      "Entwicklung",
+    "int":      "Integration",
+    "prd":      "Produktion",
+    "prod":     "Produktion",
+    "abn":      "Abnahme",
+    "preprod":  "Abnahme",
+    "id":       "Innendienst"
+}
+
+subscription_dict = {
+    "a07ed202-095f-4ff9-b87e-99c97feb258a":       "monitoring-prod-01",
+    "4b011a26-7412-4c17-94b0-4be1ef3262f6":       "default-dev-01",
+    "323af01f-6dd1-40bb-af83-bf1d09b03d15":       "default-preprod-01",
+    "1bba62b0-58fc-48e3-a27b-71610a2fcea5":       "default-prod-01",
+    "7d9b1440-9773-406f-a7d7-a7ed3061181d":       "dns-prod-01",
+    "fc81d3d1-d090-4151-b6ca-fa89a36b364f":       "kubernetes-dev-01",
+    "ecd229cc-f6d9-4e5d-9adc-fa249e68982a":       "kubernetes-preprod-01",
+    "687e92a3-b8c5-4add-9a3d-06045c185b4c":       "kubernetes-prod-01",
+    "eea48e9a-9c6e-418f-8c52-1581eee4de07":       "landingzone-prod-01",
+    "5ce64403-a71f-4feb-9076-1bdfe3612742":       "paw-prod-01",
+    "abe03732-5b76-46da-89c9-7e837221329b":       "publicip-prod-01",
+    "9966d0fb-f44f-41c9-8771-d15e3178db8b":       "slam-prod-01",
+    "ca14d2a8-0e05-4312-91bc-d7112ffa2e3d":       "spielwiese-dev-01",
+    "607da2f4-020a-4b0c-b0b7-5b9db8bbe6df":       "spielwiese-preprod-01",
+    "e453ac6a-bb68-4b9e-86d0-e778836b3d5d":       "virtualdesktop-prod-01",
+    "f297637f-5a4e-4187-b1a2-86e9d1b2448b":       "workai-prod-01"
+}
+
+def map_environment(env: str) -> str:
+    ENVIRONMENT_MAP = {
+        "ent":      "Entwicklung",
+        "dev":      "Entwicklung",
+        "int":      "Integration",
+        "prd":      "Produktion",
+        "prod":     "Produktion",
+        "abn":      "Abnahme",
+        "preprod":  "Abnahme",
+        "id":       "Innendienst"
+    }
+    return ENVIRONMENT_MAP.get(env, "Entwicklung")
