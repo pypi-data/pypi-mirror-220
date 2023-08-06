2d8e51a57adecb5d1f38b2b5b1d95e0f16031fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:05 +0000

    Add package query-everywhere/0.1.3/query_everywhere-0.1.3-py3-none-any.whl

---
+++ packages/query-everywhere/query_everywhere-0.1.3-py3-none-any.whl/query_everywhere/__init__.py
@@ -0,0 +1,126 @@
+import re
+from dataclasses import dataclass
+from typing import Any, Optional
+
+from sqlalchemy import MetaData, Table, create_engine, text
+from sqlalchemy.orm import Session
+
+OPERATOR_TO_SQL = {
+    "eq": "=",
+    "neq": "!=",
+    "lt": "<",
+    "gt": ">",
+    "lte": "<=",
+    "gte": ">=",
+}
+
+
+@dataclass
+class Filter:
+    field_name: str
+    placeholder: str
+    operator: str
+    value: Any
+
+
+class Queryer:
+    def __init__(self, dsn: str) -> None:
+        self.engine = create_engine(dsn, echo=True)
+
+    def _split_filter(self, filter: tuple[str, str]) -> tuple[str, str, Any]:
+        value = filter[1]
+        if "__" not in filter[0]:
+            field = filter[0]
+            operator = "eq"
+        else:
+            sep = filter[0].split("__")
+            if len(sep) != 2:
+                field = "".join(sep[:-1])
+                operator = sep[-1]
+            else:
+                field, operator = sep
+        return field, operator, value
+
+    def _parse_filters(self, filters: list[tuple[str, Any]]) -> list[Filter]:
+        results = []
+        for idx, filter in enumerate(filters):
+            field, operator, value = self._split_filter(filter)
+            placeholder = f"{field}__{idx}"
+            match operator:
+                case "startswith":
+                    operator = "LIKE"
+                    value = f"{value}%"
+                case "endswith":
+                    operator = "LIKE"
+                    value = f"%{value}"
+                case "contains":
+                    operator = "LIKE"
+                    value = f"%{value}%"
+                case "icontains":
+                    operator = "LIKE"
+                    value = f"%{value}%"
+                    field = f"LOWER({field})"
+                case _:
+                    operator = OPERATOR_TO_SQL.get(operator)
+            if operator is None:
+                raise ValueError("Invalid operator")
+            results.append(
+                Filter(
+                    field_name=field,
+                    placeholder=placeholder,
+                    operator=operator,
+                    value=value,
+                )
+            )
+        return results
+
+    def _build_where_conditions(self, conditions: list[Filter]) -> str:
+        if len(conditions) == 0:
+            return ""
+        return " WHERE " + " AND ".join(
+            [
+                f"{filter.field_name} {filter.operator} :{filter.placeholder}"
+                for filter in conditions
+            ]
+        )
+
+    def _check_table_name(self, table_name: str) -> None:
+        TABLE_NAME_REGEX = re.compile(r"^[a-zA-Z0-9_]+$")
+        if not TABLE_NAME_REGEX.match(table_name):
+            raise ValueError("Invalid table name")
+
+    def _check_if_fields_in_table(
+        self, table_name: str, parsed_filters: list[tuple[str, str, Any]]
+    ) -> None:
+        metadata = MetaData()
+        table = Table(table_name, metadata, autoload=True, autoload_with=self.engine)
+        select_stmt = table.select()
+        with Session(self.engine) as session:
+            session.execute(select_stmt)
+            column_names = set(table.columns.keys())
+            for field in parsed_filters:
+                if field[0] not in column_names:
+                    raise ValueError(f"Invalid field {field[0]}")
+
+    def query(
+        self,
+        resource_name: str,
+        filters: list[tuple[str, Any]],
+        order: Optional[str] = None,
+        limit: int = 10,
+        offset: int = 0,
+    ) -> list[dict[str, Any]]:
+        self._check_table_name(resource_name)
+        parsed_filters = self._parse_filters(filters)
+        where_conditions = self._build_where_conditions(parsed_filters)
+        query_sql = f'SELECT * FROM "{resource_name}"{where_conditions} ORDER BY :_order_by LIMIT :_limit OFFSET :_offset'  # noqa: E501
+        statement = text(query_sql)
+        params = (
+            {
+                **{filter.placeholder: filter.value for filter in parsed_filters},
+                **{"_order_by": order, "_limit": limit, "_offset": offset},
+            },
+        )
+        with Session(self.engine) as session:
+            result = session.execute(statement, params)
+            return [dict(row) for row in result]
