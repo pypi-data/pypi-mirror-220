44739584b851ade230d23aa6893a2482e82a7576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:34 +0000

    Add package ape-polygon/0.6.5/ape-polygon-0.6.5.tar.gz

---
+++ packages/ape-polygon/ape-polygon-0.6.5.tar.gz/ape-polygon-0.6.5/tests/test_ecosystem.py
@@ -0,0 +1,9 @@
+import pytest
+from ape_ethereum.transactions import TransactionType
+
+
+@pytest.mark.parametrize("type", (0, "0x0"))
+def test_create_transaction(polygon, type):
+    with polygon.local.use_provider("test"):
+        txn = polygon.create_transaction(type=type)
+        assert txn.type == TransactionType.STATIC.value
