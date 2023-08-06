7068335574275e8d661f22adca8f492a46103167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:42 +0000

    Add package rambaldi/2.2.0/rambaldi-2.2.0.tar.gz

---
+++ packages/rambaldi/rambaldi-2.2.0.tar.gz/rambaldi-2.2.0/readme.md
@@ -0,0 +1,3 @@
+# Rambaldi
+
+Python library for processing and standardizing carbonate clumped-isotope analyses, from low-level data out of a dual-inlet mass spectrometer to final, “absolute” Δ<sub>47</sub>, Δ<sub>48</sub> and Δ<sub>49</sub> values with fully propagated analytical error estimates.
---
+++ packages/rambaldi/rambaldi-2.2.0.tar.gz/rambaldi-2.2.0/src/rambaldi/__init__.py
@@ -0,0 +1,11 @@
+'''
+Standardization and analytical error propagation of Δ47, Δ48 and Δ49 clumped-isotope measurements
+'''
+
+__docformat__ = "restructuredtext"
+__author__    = 'Mathieu Daëron'
+__contact__   = 'daeron@lsce.ipsl.fr'
+__copyright__ = 'Copyright (c) 2023 Mathieu Daëron'
+__license__   = 'Modified BSD License - https://opensource.org/licenses/BSD-3-Clause'
+__date__      = '2023-07-20'
+__version__   = '2.2.0'
