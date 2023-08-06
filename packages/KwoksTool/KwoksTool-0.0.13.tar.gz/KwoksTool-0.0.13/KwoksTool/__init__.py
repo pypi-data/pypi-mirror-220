9e93459a768882f67f38816bb2367c3eeb5e45dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:15 +0000

    Add package KwoksTool/0.0.13/KwoksTool-0.0.13.tar.gz

---
+++ packages/KwoksTool/KwoksTool-0.0.13.tar.gz/KwoksTool-0.0.13/KwoksTool/__init__.py
@@ -0,0 +1,27 @@
+from KwoksTool.info import (welcome,how)
+from KwoksTool.function import (GetCityNumFromLiepin,
+                                GetCityNumFromBossZhiPing,
+                                GetCityNameFromLiepin,
+                                YesOrNot,
+                                CheckIp,
+                                IntoZip,
+                                ZipOut,
+                                SendEmail,
+                                GetEmail,
+                                ProgressBar,
+                                MergeTable,
+                                ChoiceColumn)
+from KwoksTool.source.IpPool import GetIpPool
+from KwoksTool.model import (GetProbMatrix,ToMat)
+from KwoksTool.spider import (Browser, PlateComponentStocks)
+from KwoksTool.Spider.stock import GetStocksData
+class spider:
+    from KwoksTool.Spider.function import (find_diffrent_key_from_str)
+dependencies=[
+    'selenium',
+    'tushare',
+    'pyexecjs',
+    'requests',
+    'pandas',
+    'numpy'
+]
