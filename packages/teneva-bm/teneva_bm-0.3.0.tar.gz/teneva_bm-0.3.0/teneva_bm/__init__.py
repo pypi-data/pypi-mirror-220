a9c844f97f3041e5981c3fc8b854781f9778a059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:27 +0000

    Add package teneva-bm/0.3.0/teneva_bm-0.3.0.tar.gz

---
+++ packages/teneva-bm/teneva_bm-0.3.0.tar.gz/teneva_bm-0.3.0/teneva_bm/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.3.0'
+
+
+from .bm import Bm
+from .teneva_bm_demo import teneva_bm_demo
+
+
+from .func import *
+from .hs import *
+from .oc import *
+from .qubo import *
+from .various import *
