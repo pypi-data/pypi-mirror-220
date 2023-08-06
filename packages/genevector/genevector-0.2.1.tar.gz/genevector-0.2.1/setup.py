5dfb2e28a553f37a9fb016ec55657bb8449b4012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:45 +0000

    Add package genevector/0.2.1/genevector-0.2.1.tar.gz

---
+++ packages/genevector/genevector-0.2.1.tar.gz/genevector-0.2.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='genevector',
+    version='0.2.1',
+    description='Single Cell GeneVector Library',
+    packages=find_packages(include=['genevector']),
+)
