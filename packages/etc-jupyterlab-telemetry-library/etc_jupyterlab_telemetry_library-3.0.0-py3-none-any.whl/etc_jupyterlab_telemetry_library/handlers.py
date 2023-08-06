f84f881329660a816466d2313b956f2bb503245b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:08 +0000

    Add package etc-jupyterlab-telemetry-library/3.0.0/etc_jupyterlab_telemetry_library-3.0.0-py3-none-any.whl

---
+++ packages/etc-jupyterlab-telemetry-library/etc_jupyterlab_telemetry_library-3.0.0-py3-none-any.whl/etc_jupyterlab_telemetry_library/handlers.py
@@ -0,0 +1,34 @@
+import os
+import tornado
+import json
+from jupyter_server.base.handlers import JupyterHandler
+from jupyter_server.extension.handler import ExtensionHandlerMixin
+import pprint
+from ._version import VERSION
+
+class RouteHandler(ExtensionHandlerMixin, JupyterHandler):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
+    # Jupyter server
+    @tornado.web.authenticated
+    def get(self, resource):
+
+        try:
+            self.set_header('Content-Type', 'application/json')
+            
+            if resource == 'version':
+                self.finish(json.dumps(VERSION))
+            elif resource == 'config':
+                self.finish(json.dumps(self.config))
+            elif resource == 'environ':
+                self.finish(json.dumps({k:v for k, v in os.environ.items()}))
+            else:
+                self.set_status(404)
+
+        except Exception as e:
+            self.set_status(500)
+            self.finish(json.dumps(str(e)))
