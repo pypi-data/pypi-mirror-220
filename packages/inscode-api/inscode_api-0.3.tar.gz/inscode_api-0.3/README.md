735f788e75d61b7ec69bbda33d532293ee335457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:18 +0000

    Add package inscode-api/0.3/inscode_api-0.3.tar.gz

---
+++ packages/inscode-api/inscode_api-0.3.tar.gz/inscode_api-0.3/README.md
@@ -0,0 +1,7 @@
+# INSCODE API Python Library
+
+This is a Python library for interacting with the INSCODE API.
+
+## Installation
+
+You can install the library using `pip`:
---
+++ packages/inscode-api/inscode_api-0.3.tar.gz/inscode_api-0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="inscode_api",
+    version="0.3",
+    author="yanhui",
+    author_email="yanhui@csdn.net",
+    description="A library for interacting with the INSCODE API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["requests"],
+)
