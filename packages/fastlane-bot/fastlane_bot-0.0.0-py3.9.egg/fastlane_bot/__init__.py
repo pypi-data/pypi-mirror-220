ba19ada4a945fa877e9478a5f9bc769b47d81e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:00 +0000

    Add package fastlane-bot/0.0.0/fastlane_bot-0.0.0-py3.9.egg

---
+++ packages/fastlane-bot/fastlane_bot-0.0.0-py3.9.egg/fastlane_bot/__init__.py
@@ -0,0 +1,8 @@
+from .bot import CarbonBot as Bot, __VERSION__, __DATE__
+from .config import Config, ConfigNetwork, ConfigDB, ConfigLogger, ConfigProvider
+
+__version__ = __VERSION__
+__date__ = __DATE__
+
+
+
