145d4dc276683b384648891c08ee6881a5e20ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:03 +0000

    Add package determined/0.23.3/determined-0.23.3-py3-none-any.whl

---
+++ packages/determined/determined-0.23.3-py3-none-any.whl/determined/cli/master.py
@@ -0,0 +1,64 @@
+from argparse import Namespace
+from typing import Any, List, Optional
+
+import determined.cli.render
+from determined import cli
+from determined.common import yaml
+from determined.common.api import authentication, bindings
+from determined.common.declarative_argparse import Arg, Cmd, Group
+
+
+@authentication.required
+def config(args: Namespace) -> None:
+    resp = bindings.get_GetMasterConfig(cli.setup_session(args)).config
+    if args.json:
+        determined.cli.render.print_json(resp)
+    else:
+        print(yaml.safe_dump(resp, default_flow_style=False))
+
+
+def get_master(args: Namespace) -> None:
+    resp = bindings.get_GetMaster(cli.setup_session(args))
+    if args.json:
+        determined.cli.render.print_json(resp.to_json())
+    else:
+        print(yaml.safe_dump(resp.to_json(), default_flow_style=False))
+
+
+def format_log_entry(log: bindings.v1LogEntry) -> str:
+    """Format v1LogEntry for printing."""
+    log_level = log.level if log.level else ""
+    return f"{log.timestamp} [{log_level}]: {log.message}"
+
+
+@authentication.required
+def logs(args: Namespace) -> None:
+    offset: Optional[int] = None
+    if args.tail:
+        offset = -args.tail
+    responses = bindings.get_MasterLogs(cli.setup_session(args), follow=args.follow, offset=offset)
+    for response in responses:
+        print(format_log_entry(response.logEntry))
+
+
+# fmt: off
+
+args_description = [
+    Cmd("master", None, "manage master", [
+        Cmd("config", config, "fetch master config", [
+            Group(cli.output_format_args["json"], cli.output_format_args["yaml"])
+        ]),
+        Cmd("info", get_master, "fetch master info", [
+            Group(cli.output_format_args["json"], cli.output_format_args["yaml"])
+        ]),
+        Cmd("logs", logs, "fetch master logs", [
+            Arg("-f", "--follow", action="store_true",
+                help="follow the logs of master, similar to tail -f"),
+            Arg("--tail", type=int,
+                help="number of lines to show, counting from the end "
+                "of the log (default is all)")
+        ]),
+    ])
+]  # type: List[Any]
+
+# fmt: on
