53f399bee77502e25ab5c62c1d48e8a96bf44325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:00 +0000

    Add package bagit-create/0.1.10.post1/bagit-create-0.1.10.post1.tar.gz

---
+++ packages/bagit-create/bagit-create-0.1.10.post1.tar.gz/bagit-create-0.1.10.post1/community.py
@@ -0,0 +1,45 @@
+import time
+from urllib.parse import urlparse
+
+from sickle import Sickle
+
+import bagit_create
+
+sickle = Sickle("https://zenodo.org/oai2d")
+
+""" Harvest the entire repository """
+records = sickle.ListRecords(metadataPrefix="oai_dc", set="user-tops")
+record = records.next()
+
+failed = []
+successful = []
+ids = []
+
+while record:
+    url = record.metadata["identifier"][0]
+    recid = urlparse(url).path.rpartition("/")[2]
+    ids.append(recid)
+    try:
+        record = records.next()
+    except:
+        record = None
+
+
+print(f"Final list of ids to process: {ids} \n")
+
+for recid in ids:
+    sip = bagit_create.main.process(
+        source="zenodo",
+        recid=recid,
+        loglevel=0,
+        target="zenodo_user-tops",
+        token="eMqeoPGrDQnbSRT5XWZDmbb4Uc19BVUtKZUGFk40ELydWDjq021X71IpVP4k",
+    )
+    if sip["status"] != 0:
+        failed.append(recid)
+    else:
+        successful.append(recid)
+
+
+print(f"Success: {successful} \n")
+print(f"Failed: {failed} \n")
