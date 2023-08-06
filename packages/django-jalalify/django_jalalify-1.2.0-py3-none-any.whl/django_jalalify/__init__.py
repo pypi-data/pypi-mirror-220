d60a478c177ee6ace13523b04e388fa753a19348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:43 +0000

    Add package django-jalalify/1.2.0/django_jalalify-1.2.0-py3-none-any.whl

---
+++ packages/django-jalalify/django_jalalify-1.2.0-py3-none-any.whl/django_jalalify/__init__.py
@@ -0,0 +1,10 @@
+from khayyam import JalaliDate, JalaliDatetime
+
+default_app_config = "django_jalalify.apps.DjangoJalalifyConfig"
+
+__author__ = "Mohammad Javad Nikbakht"
+__email__ = "javadnikbakht@mail.com"
+
+__version__ = "1.2.0"
+
+__all__ = ["JalaliDate", "JalaliDatetime"]
