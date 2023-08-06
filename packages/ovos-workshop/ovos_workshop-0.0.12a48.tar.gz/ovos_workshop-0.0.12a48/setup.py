9e30b2784256174f9529c7c371a85082f1155d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:08 +0000

    Add package ovos-workshop/0.0.12a48/ovos_workshop-0.0.12a48.tar.gz

---
+++ packages/ovos-workshop/ovos_workshop-0.0.12a48.tar.gz/ovos_workshop-0.0.12a48/setup.py
@@ -0,0 +1,74 @@
+import os
+from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_workshop', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
+setup(
+    name='ovos_workshop',
+    version=get_version(),
+    packages=['ovos_workshop',
+              'ovos_workshop.skills',
+              'ovos_workshop.skills.decorators',
+              'ovos_workshop.decorators'],
+    install_requires=required("requirements/requirements.txt"),
+    extras_require={
+        'ocp': required('requirements/ocp.txt')
+    },
+    package_data={'': package_files('ovos_workshop')},
+    url='https://github.com/OpenVoiceOS/OVOS-workshop',
+    license='apache-2.0',
+    author='jarbasAi',
+    author_email='jarbasai@mailfence.com',
+    include_package_data=True,
+    description='frameworks, templates and patches for the OpenVoiceOS universe',
+    entry_points={
+        'console_scripts': [
+            'ovos-skill-launcher=ovos_workshop.skill_launcher:_launch_script'
+        ]
+    }
+)
