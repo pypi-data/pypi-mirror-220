51c5e55f6a08a0686d512ce92285dc3b621a917f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:15 +0000

    Add package logger-extras/0.2.0/logger_extras-0.2.0-py3-none-any.whl

---
+++ packages/logger-extras/logger_extras-0.2.0-py3-none-any.whl/logger_extras/__init__.py
@@ -0,0 +1,13 @@
+"""A package for logging extras."""
+from ._version import __version__
+from .filters import DiffTimeFilter, RelativeTimeFilter
+from .formatters import TieredFormatter
+from .utils import log_function_call
+
+__all__ = [
+    '__version__',
+    'log_function_call',
+    'DiffTimeFilter',
+    'RelativeTimeFilter',
+    'TieredFormatter',
+]
