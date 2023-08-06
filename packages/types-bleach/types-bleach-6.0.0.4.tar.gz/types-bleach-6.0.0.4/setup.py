33b450aff9e6cdd5210999c8fba54aa06e652936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:52 +0000

    Add package types-bleach/6.0.0.4/types-bleach-6.0.0.4.tar.gz

---
+++ packages/types-bleach/types-bleach-6.0.0.4.tar.gz/types-bleach-6.0.0.4/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+name = "types-bleach"
+description = "Typing stubs for bleach"
+long_description = '''
+## Typing stubs for bleach
+
+This is a PEP 561 type stub package for the `bleach` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`bleach`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/bleach. All fixes for
+types and metadata should be contributed there.
+
+This stub package is marked as [partial](https://peps.python.org/pep-0561/#partial-stub-packages).
+If you find that annotations are missing, feel free to contribute and help complete them.
+
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `afe18e95a9592434e93b648de5194cfe54443f84` and was tested
+with mypy 1.4.1, pyright 1.1.318, and
+pytype 2023.6.16.
+'''.lstrip()
+
+setup(name=name,
+      version="6.0.0.4",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/bleach.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['bleach-stubs'],
+      package_data={'bleach-stubs': ['__init__.pyi', 'callbacks.pyi', 'css_sanitizer.pyi', 'html5lib_shim.pyi', 'linkifier.pyi', 'sanitizer.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
