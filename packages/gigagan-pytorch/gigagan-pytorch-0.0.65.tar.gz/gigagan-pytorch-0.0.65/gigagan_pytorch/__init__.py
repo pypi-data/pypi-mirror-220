f77d6930a6e428c959562be587bd8b1ea1e017c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:03 +0000

    Add package gigagan-pytorch/0.0.65/gigagan-pytorch-0.0.65.tar.gz

---
+++ packages/gigagan-pytorch/gigagan-pytorch-0.0.65.tar.gz/gigagan-pytorch-0.0.65/gigagan_pytorch/__init__.py
@@ -0,0 +1,29 @@
+from gigagan_pytorch.gigagan_pytorch import (
+    GigaGAN,
+    Generator,
+    Discriminator,
+    AdaptiveConv2DMod,
+    StyleNetwork,
+    TextEncoder
+)
+
+from gigagan_pytorch.unet_upsampler import UnetUpsampler
+
+from gigagan_pytorch.data import (
+    ImageDataset,
+    TextImageDataset,
+    MockTextImageDataset
+)
+
+__all__ = [
+    GigaGAN,
+    Generator,
+    Discriminator,
+    AdaptiveConv2DMod,
+    StyleNetwork,
+    UnetUpsampler,
+    TextEncoder,
+    ImageDataset,
+    TextImageDataset,
+    MockTextImageDataset
+]
