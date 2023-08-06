bb489e6352f8800c35437fbf5caa056aacb4b3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:38 +0000

    Add package catalystcoop.ferc-xbrl-extractor/0.8.4/catalystcoop.ferc_xbrl_extractor-0.8.4-py3-none-any.whl

---
+++ packages/catalystcoop.ferc-xbrl-extractor/catalystcoop.ferc_xbrl_extractor-0.8.4-py3-none-any.whl/ferc_xbrl_extractor/arelle_interface.py
@@ -0,0 +1,152 @@
+"""Abstract away interface to Arelle XBRL Library."""
+from typing import Literal
+
+import pydantic
+import stringcase
+from arelle import Cntlr, FileSource, ModelManager, ModelXbrl, XbrlConst
+from arelle.ModelDtsObject import ModelConcept
+from arelle.ViewFileRelationshipSet import ViewRelationshipSet
+from pydantic import BaseModel
+
+
+def load_xbrl(path: str | FileSource.FileSource):
+    """Load XBRL (either taxonomy or individual filing).
+
+    Args:
+        path: URL or local path pointing to an XBRL taxonomy or instance.
+    """
+    cntlr = Cntlr.Cntlr()
+    cntlr.startLogging(logFileName="logToPrint")
+    model_manager = ModelManager.initialize(cntlr)
+    return ModelXbrl.load(model_manager, path)
+
+
+def load_taxonomy(path: str | FileSource.FileSource):
+    """Load XBRL taxonomy, and parse relationships.
+
+    Args:
+        path: URL or local path pointing to an XBRL taxonomy.
+    """
+    taxonomy = load_xbrl(path)
+
+    # Interpret structure/relationships
+    view = ViewRelationshipSet(taxonomy, "taxonomy.json", "roles", None, None, None)
+    view.view(XbrlConst.parentChild, None, None, None)
+
+    return taxonomy, view
+
+
+def load_taxonomy_from_archive(filepath: str, archive_path: str):
+    """Load an XBRL taxonomy from a zipfile archive.
+
+    Args:
+        filepath: Path to zipfile on disc.
+        archive_path: Relative path to taxonomy entry point within archive.
+    """
+    # Create arelle FileSource object
+    f = FileSource.openFileSource(archive_path, sourceZipStream=filepath)
+
+    return load_taxonomy(f)
+
+
+class References(BaseModel):
+    """Pydantic model that defines XBRL references.
+
+    FERC uses XBRL references to link Concepts defined in its taxonomy to the physical
+    paper form. These are included in the output metadata and can be useful for linking
+    between XBRL and DBF data.
+
+    This model is not a generic representation of XBRL references, but specific to those
+    used by FERC.
+    """
+
+    account: str = pydantic.Field(None, alias="Account")
+    form_location: list[dict[str, str]] = pydantic.Field([], alias="Form Location")
+
+
+class Calculation(BaseModel):
+    """Pydantic model that defines XBRL calculations.
+
+    XBRL calculation relationships are also included in the metadata. Calculations are a
+    validation tool used to define relationships between facts using some mathematical
+    formula. For example, a calculation relationship might denote that one fact is equal
+    to the sum of 2 or more other facts, and this relationship can be used to validate a
+    filing.
+    """
+
+    name: str
+    weight: float
+
+
+class Metadata(BaseModel):
+    """Pydantic model that defines metadata extracted from XBRL taxonomies.
+
+    Taxonomies contain various metedata which are useful for interpreting XBRL filings.
+    The metadata fields being extracted here include references, calculations, and balances.
+    """
+
+    name: str
+    references: References
+    calculations: list[Calculation]
+    balance: Literal["credit", "debit"] | None = None
+
+    @classmethod
+    def from_concept(cls, concept: ModelConcept) -> "Metadata":
+        """Get metadata for a single XBRL Concept.
+
+        This function will create a Metadata object with metadata extracted for
+        a single Concept.
+
+        Args:
+            concept: Concept to extract metadata from.
+        """
+        # Get name and convert to snakecase to match output DB
+        name = stringcase.snakecase(concept.name)
+        concept_metadata = {"name": name}
+
+        references = concept.modelXbrl.relationshipSet(
+            XbrlConst.conceptReference
+        ).fromModelObject(concept)
+
+        # Loop through all references and add to metadata
+        reference_dict = {}
+        for reference in references:
+            reference = reference.toModelObject
+            reference_name = reference.modelXbrl.roleTypeDefinition(reference.role)
+            # Several values can make up a single reference. Create a dictionary with these
+            part_dict = {
+                part.localName: part.stringValue for part in reference.iterchildren()
+            }
+
+            # There can also be several references with the same name, so store in list
+            if reference_name in reference_dict:
+                reference_dict[reference_name].append(part_dict)
+            else:
+                reference_dict[reference_name] = [part_dict]
+
+            # Flatten out references where applicable
+            if len(reference_dict[reference_name]) == 1 and len(part_dict) == 1:
+                if reference_name in part_dict:
+                    reference_dict[reference_name] = part_dict[reference_name]
+
+        # Add references to metadata
+        concept_metadata["references"] = reference_dict
+
+        # Get calculations
+        calculations = concept.modelXbrl.relationshipSet(
+            XbrlConst.summationItem
+        ).fromModelObject(concept)
+
+        calculation_list = []
+        for calculation in calculations:
+            calculation_list.append(
+                {
+                    "name": stringcase.snakecase(calculation.toModelObject.name),
+                    "weight": calculation.weight,
+                }
+            )
+
+        concept_metadata["calculations"] = calculation_list
+        concept_metadata["balance"] = concept.balance
+
+        return cls(**concept_metadata)
---
+++ packages/catalystcoop.ferc-xbrl-extractor/catalystcoop.ferc_xbrl_extractor-0.8.4-py3-none-any.whl/ferc_xbrl_extractor/xbrl.py
@@ -0,0 +1,208 @@
+"""XBRL extractor."""
+import math
+from collections.abc import Iterable
+from concurrent.futures import ProcessPoolExecutor as Executor
+from functools import partial
+
+import numpy as np
+import pandas as pd
+import sqlalchemy as sa
+from frictionless import Package
+from lxml.etree import XMLSyntaxError  # nosec: B410
+
+from ferc_xbrl_extractor.datapackage import Datapackage, FactTable
+from ferc_xbrl_extractor.helpers import get_logger
+from ferc_xbrl_extractor.instance import InstanceBuilder
+from ferc_xbrl_extractor.taxonomy import Taxonomy
+
+
+def extract(
+    instances: list[InstanceBuilder],
+    engine: sa.engine.Engine,
+    taxonomy: str,
+    form_number: int,
+    archive_file_path: str | None = None,
+    requested_tables: set[str] | None = None,
+    batch_size: int | None = None,
+    workers: int | None = None,
+    datapackage_path: str | None = None,
+    metadata_path: str | None = None,
+) -> None:
+    """Extract data from all specified XBRL filings.
+
+    Args:
+        instances: A list of InstanceBuilder objects used for parsing XBRL filings.
+        engine: SQLite connection to output database.
+        form_number: FERC Form number (can be 1, 2, 6, 60, 714).
+        taxonomy: Specify taxonomy used to create structure of output DB.
+        archive_file_path: Path to taxonomy entry point within archive. If not None,
+                then `taxonomy` should be a path to zipfile, not a URL.
+        requested_tables: Optionally specify the set of tables to extract.
+                If None, all possible tables will be extracted.
+        batch_size: Number of filings to process before writing to DB.
+        workers: Number of threads to create for parsing filings.
+        datapackage_path: Create frictionless datapackage and write to specified path as JSON file.
+                          If path is None no datapackage descriptor will be saved.
+        metadata_path: Path to metadata json file to output taxonomy metadata.
+    """
+    logger = get_logger(__name__)
+
+    num_instances = len(instances)
+    if not batch_size:
+        batch_size = num_instances // workers if workers else num_instances
+
+    num_batches = math.ceil(num_instances / batch_size)
+
+    tables = get_fact_tables(
+        taxonomy,
+        form_number,
+        str(engine.url),
+        archive_file_path=archive_file_path,
+        tables=requested_tables,
+        datapackage_path=datapackage_path,
+        metadata_path=metadata_path,
+    )
+
+    with Executor(max_workers=workers) as executor:
+        # Bind arguments generic to all filings
+        process_batches = partial(
+            process_batch,
+            tables=tables,
+        )
+
+        batched_instances = np.array_split(
+            instances, math.ceil(num_instances / batch_size)
+        )
+
+        # Use thread pool to extract data from all filings in parallel
+        results = executor.map(process_batches, batched_instances)
+
+        # Write extracted data to database
+        for i, batch in enumerate(results):
+            logger.info(f"Finished batch {i + 1}/{num_batches}")
+
+            # Loop through tables and write to database
+            with engine.begin() as conn:
+                for key, df in batch.items():
+                    if not df.empty:
+                        df.to_sql(key, conn, if_exists="append")
+
+
+def process_batch(
+    instances: Iterable[InstanceBuilder],
+    tables: dict[str, FactTable],
+) -> dict[str, pd.DataFrame]:
+    """Extract data from one batch of instances.
+
+    Splitting instances into batches significantly improves multiprocessing
+    performance. This is done explicitly rather than using ProcessPoolExecutor's
+    chunk_size option so dataframes within the batch can be concatenated prior to
+    returning to the parent process.
+
+    Args:
+        instances: Iterator of instances.
+        tables: Dictionary mapping table names to FactTable objects describing table structure.
+    """
+    logger = get_logger(__name__)
+
+    dfs: dict[str, pd.DataFrame] = {}
+    for instance in instances:
+        # Parse XBRL instance. Log/skip if file is empty
+        try:
+            instance_dfs = process_instance(instance, tables)
+        except XMLSyntaxError:
+            logger.info(f"XBRL filing {instance.name} is empty. Skipping.")
+            continue
+
+        for key, df in instance_dfs.items():
+            if key not in dfs:
+                dfs[key] = []
+
+            dfs[key].append(df)
+
+    dfs = {key: pd.concat(df_list) for key, df_list in dfs.items()}
+
+    return dfs
+
+
+def process_instance(
+    instance_parser: InstanceBuilder,
+    tables: dict[str, FactTable],
+) -> dict[str, pd.DataFrame]:
+    """Extract data from a single XBRL filing.
+
+    This function will use the InstanceBuilder object to parse
+    a single XBRL filing. It then iterates through requested tables
+    and populates them with data extracted from the filing.
+
+    Args:
+        instance_parser: A single InstanceBuilder object used to parse an XBRL filing.
+        tables: Dictionary mapping table names to FactTable objects describing table structure.
+    """
+    logger = get_logger(__name__)
+    instance = instance_parser.parse()
+
+    logger.info(f"Extracting {instance.filing_name}")
+
+    dfs = {}
+    for key, table in tables.items():
+        dfs[key] = table.construct_dataframe(instance)
+
+    return dfs
+
+
+def get_fact_tables(
+    taxonomy_path: str,
+    form_number: int,
+    db_path: str,
+    archive_file_path: str | None = None,
+    tables: set[str] | None = None,
+    datapackage_path: str | None = None,
+    metadata_path: str | None = None,
+) -> dict[str, FactTable]:
+    """Parse taxonomy from URL.
+
+    XBRL defines 'fact tables' that groups related facts. These fact
+    tables are used directly to create the tables that will make up the
+    output SQLite database, and their structure. The output of this function
+    is a dictionary that maps table names to 'FactTable' objects that specify
+    their structure.
+
+    This function will also output a json file containing a Frictionless
+    Data-Package descriptor describing the output database if requested.
+
+    Args:
+        taxonomy_path: URL of taxonomy.
+        form_number: FERC Form number (can be 1, 2, 6, 60, 714).
+        db_path: Path to database used for constructing datapackage descriptor.
+        archive_file_path: Path to taxonomy entry point within archive. If not None,
+                then `taxonomy` should be a path to zipfile, not a URL.
+        tables: Optionally specify the set of tables to extract.
+                If None, all possible tables will be extracted.
+        datapackage_path: Create frictionless datapackage and write to specified path as JSON file.
+                          If path is None no datapackage descriptor will be saved.
+        metadata_path: Path to metadata json file to output taxonomy metadata.
+
+    Returns:
+        Dictionary mapping to table names to structure.
+    """
+    logger = get_logger(__name__)
+    logger.info(f"Parsing taxonomy from {taxonomy_path}")
+    taxonomy = Taxonomy.from_path(taxonomy_path, archive_file_path=archive_file_path)
+
+    # Save taxonomy metadata
+    taxonomy.save_metadata(metadata_path)
+
+    datapackage = Datapackage.from_taxonomy(taxonomy, db_path, form_number=form_number)
+
+    if datapackage_path:
+        # Verify that datapackage descriptor is valid before outputting
+        frictionless_package = Package(descriptor=datapackage.dict(by_alias=True))
+        if not frictionless_package.metadata_valid:
+            raise RuntimeError("Generated datapackage is invalid")
+
+        # Write to JSON file
+        with open(datapackage_path, "w") as f:
+            f.write(datapackage.json(by_alias=True))
+
+    return datapackage.get_fact_tables(tables=tables)
