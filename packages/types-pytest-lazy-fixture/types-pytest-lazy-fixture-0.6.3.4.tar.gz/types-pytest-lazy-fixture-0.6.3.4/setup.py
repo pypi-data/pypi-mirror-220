e3c4a6069f0d5d5400271c4b78ad6f5f0297dbd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:09 +0000

    Add package types-pytest-lazy-fixture/0.6.3.4/types-pytest-lazy-fixture-0.6.3.4.tar.gz

---
+++ packages/types-pytest-lazy-fixture/types-pytest-lazy-fixture-0.6.3.4.tar.gz/types-pytest-lazy-fixture-0.6.3.4/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+name = "types-pytest-lazy-fixture"
+description = "Typing stubs for pytest-lazy-fixture"
+long_description = '''
+## Typing stubs for pytest-lazy-fixture
+
+This is a PEP 561 type stub package for the `pytest-lazy-fixture` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`pytest-lazy-fixture`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/pytest-lazy-fixture. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `afe18e95a9592434e93b648de5194cfe54443f84` and was tested
+with mypy 1.4.1, pyright 1.1.318, and
+pytype 2023.6.16.
+'''.lstrip()
+
+setup(name=name,
+      version="0.6.3.4",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/pytest-lazy-fixture.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['pytest_lazyfixture-stubs'],
+      package_data={'pytest_lazyfixture-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
