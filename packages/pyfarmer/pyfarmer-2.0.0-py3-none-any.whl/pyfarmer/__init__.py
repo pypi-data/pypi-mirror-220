7f725aefce01dbe1c3e781c72d1dc8be77e671cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:17 +0000

    Add package pyfarmer/2.0.0/pyfarmer-2.0.0-py3-none-any.whl

---
+++ packages/pyfarmer/pyfarmer-2.0.0-py3-none-any.whl/pyfarmer/__init__.py
@@ -0,0 +1,78 @@
+"""
+# PyFarmer
+
+## Introduction
+
+> A farmer for your [Destructive Farm](https://github.com/DestructiveVoice/DestructiveFarm)
+>
+> <img src="https://i.kym-cdn.com/entries/icons/original/000/028/021/work.jpg" style="zoom: 50%;" />
+>
+> You can read the documentation at [rikyiso01.github.io/pyfarmer](https://rikyiso01.github.io/pyfarmer)
+
+## Code Samples
+
+> ```python
+> from httpx import post
+> from pyfarmer import farm, print_exception
+>
+>
+> def main(ip: str):
+>     for flag_id in ["a", "b", "c"]:
+>         try:
+>             r = post(
+>                 f"http://{ip}:5000/api/products/{flag_id}/download?a=/api/register"
+>             )
+>             flag = r.text
+>             yield flag
+>         except:
+>             print_exception()
+>
+>
+> if __name__ == "__main__":
+>     farm(main)
+> ```
+
+## Installation
+
+> Install locally with:
+>
+> ```bash
+> pip install pyfarmer
+> ```
+
+"""
+
+from pyfarmer._pyfarmer import (
+    farm,
+    async_farm,
+    SploitFunction,
+    Mode,
+)
+from pyfarmer._strategies import (
+    ProcessStrategy,
+    ThreadStrategy,
+    FarmingStrategy,
+    WriteCommunication,
+    Process,
+    Status,
+    SimpleFarmingStrategy,
+    ReadCommunication,
+)
+from pyfarmer._utils import random_string, print_exception
+
+__all__ = [
+    "farm",
+    "async_farm",
+    "SploitFunction",
+    "Mode",
+    "ProcessStrategy",
+    "ThreadStrategy",
+    "FarmingStrategy",
+    "WriteCommunication",
+    "random_string",
+    "print_exception",
+    "Process",
+    "Status",
+    "SimpleFarmingStrategy",
+    "ReadCommunication",
+]
