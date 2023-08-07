90521b40814c90c98b0955c1cacf0f64fd1adcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:53:32 +0000

    Add package mash-shell/0.2.14/mash-shell-0.2.14.tar.gz

---
+++ packages/mash-shell/mash-shell-0.2.14.tar.gz/mash-shell-0.2.14/src/mash/shell/__init__.py
@@ -0,0 +1,8 @@
+"""
+See `shell <../pages/SHELL.html>`_
+"""
+
+from mash.shell.errors import ShellError, ShellPipeError, ShellSyntaxError
+from mash.shell.shell import Shell
+from mash.shell.function import ShellFunction
+from mash.shell.with_filesystem import ShellWithFileSystem
